be342cac7611ced8c9ea05a5677740ab8fd2e6c5
rgarcia <voodoorai2000@gmail.com>
2015-07-16 19:22:20 +0200

    Basic CRU operations [#5]

---
+++ app/controllers/debates_controller.rb
@@ -0,0 +1,38 @@
+class DebatesController < ApplicationController
+  before_action :set_debate, only: [:show, :edit, :update]
+
+  def index
+    @debates = Debate.all
+  end
+
+  def show
+  end
+
+  def new
+    @debate = Debate.new
+  end
+
+  def edit
+  end
+
+  def create
+    @debate = Debate.create(debate_params)
+    respond_with @debate
+  end
+
+  def update
+    @debate.update(debate_params)
+    respond_with @debate
+  end
+
+
+  private
+    def set_debate
+      @debate = Debate.find(params[:id])
+    end
+
+    def debate_params
+      params.require(:debate).permit(:title, :description, :external_link, :terms_of_service)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+  resources :debates
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
