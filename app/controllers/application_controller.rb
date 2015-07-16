9b3d41552d4b3e28630470ab88fc4d28ae42b364
rgarcia <voodoorai2000@gmail.com>
2015-07-16 19:25:47 +0200

    configures responders gem [#5]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
+require "application_responder"
+
 class ApplicationController < ActionController::Base
+  self.responder = ApplicationResponder
+  respond_to :html
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
