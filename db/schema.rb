272c03f6a54c7a2f81a6e04cf009b9a08d5b05bf
rgarcia <voodoorai2000@gmail.com>
2015-07-17 19:46:10 +0200

    adds first name and last name to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150716174358) do
+ActiveRecord::Schema.define(version: 20150717164054) do
 
   create_table "debates", force: :cascade do |t|
     t.string   "title"
