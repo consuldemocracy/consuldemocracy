62d0a385aba7e4d4f7163d41019390d125dbface
rgarcia <voodoorai2000@gmail.com>
2015-07-17 19:46:10 +0200

    adds first name and last name to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
