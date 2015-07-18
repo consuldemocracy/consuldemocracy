45094a86bc430edeb8effd84c3f5d6143c43d40d
rgarcia <voodoorai2000@gmail.com>
2015-07-18 14:49:22 +0200

    adds author to debates [#11]

--- app/models/debate.rb
+++ app/models/debate.rb
@@ -1,6 +1,9 @@
 class Debate < ActiveRecord::Base
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
   validates :title, presence: true
   validates :description, presence: true
+  validates :author, presence: true
 
   validates :terms_of_service, acceptance: { allow_nil: false }, on: :create
 end
