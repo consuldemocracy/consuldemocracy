bf11dba0929d3579bf1875c59fc7ddd28ef4f37c
rgarcia <voodoorai2000@gmail.com>
2015-07-18 14:49:31 +0200

    adds convenience methods and association [#11]

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :debates
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
