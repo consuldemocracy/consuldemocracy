e783c2013a3219a5e40d56eadb3ffcaf8be5308a
rgarcia <voodoorai2000@gmail.com>
2015-07-18 17:50:40 +0200

    sanitises tag names [#8]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def tags(debate)
-    debate.tag_list.map { |tag| link_to tag, debates_path(tag: tag) }.join(', ').html_safe
+    debate.tag_list.map { |tag| link_to sanitize(tag), debates_path(tag: tag) }.join(', ').html_safe
   end
 
 end
