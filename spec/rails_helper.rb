be173ff581faec0d11da10ddc673c6e11cece27b
kikito <kikito@gmail.com>
2015-07-27 19:07:02 +0200

    removes use_transaction_fixtures from rails_helper
    It is already configured in spec_helper.rb (and makes other things fail)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,7 +14,6 @@ Warden.test_mode!
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
 end
 
