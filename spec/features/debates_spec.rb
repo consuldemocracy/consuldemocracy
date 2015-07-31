824ce3c8fe5f0a7f6b64451da12b04e564d7696d
kikito <kikito@gmail.com>
2015-07-31 16:10:20 +0200

    Add specs for description sanitisation

--- spec/features/debates_spec.rb
+++ spec/features/debates_spec.rb
@@ -45,6 +45,23 @@ feature 'Debates' do
     expect(page).to have_content I18n.l(Date.today)
   end
 
+  scenario 'JS injection is sanitized' do
+    author = create(:user)
+    login_as(author)
+
+    visit new_debate_path
+    fill_in 'debate_title', with: 'A test'
+    fill_in 'debate_description', with: 'This is <script>alert("an attack");</script>'
+    check 'debate_terms_of_service'
+
+    click_button 'Create Debate'
+
+    expect(page).to have_content 'Debate was successfully created.'
+    expect(page).to have_content 'A test'
+    expect(page).to have_content 'This is alert("an attack");'
+    expect(page.html).to_not include '<script>alert("an attack");</script>'
+  end
+
   scenario 'Update should not be posible if logged user is not the author' do
     debate = create(:debate)
     expect(debate).to be_editable
