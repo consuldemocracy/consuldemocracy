0ca3d2501f9717e3756d540a26231122462b68b9
Juanjo Bazán <jjbazan@gmail.com>
2015-07-31 14:54:49 +0200

    fixes build

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,6 +30,10 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = {
+    host: 'test'
+  }
+  config.action_mailer.asset_host = 'http://madrid.test'
 
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random
