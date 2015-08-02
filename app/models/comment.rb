7a50d2064cbf5e36f950a32f81974a7d8dfd6677
rgarcia <voodoorai2000@gmail.com>
2015-08-02 13:35:48 +0200

    adds voting methods to comment model [#25]

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   acts_as_nested_set scope: [:commentable_id, :commentable_type]
+  acts_as_votable
 
   validates :body, presence: true
   validates :user, presence: true
