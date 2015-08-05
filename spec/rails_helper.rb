c2091f570c2a4748b2dff0048667248565479d84
rgarcia <voodoorai2000@gmail.com>
2015-08-05 14:16:58 +0200

    integrates coveralls in test suite

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,5 @@
+require 'coveralls'
+Coveralls.wear!('rails')
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
