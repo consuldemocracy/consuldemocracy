72c33330cf2f99d729fc815df2d7ef29a4ea564b
kikito <kikito@gmail.com>
2015-08-06 16:53:18 +0200

    Adds nickname-related validations & name to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :first_name, presence: true, unless: :use_nickname?
+  validates :last_name, presence: true, unless: :use_nickname?
+  validates :nickname, presence: true, if: :use_nickname?
+
   def name
-    "#{first_name} #{last_name}"
+    use_nickname? ? nickname : "#{first_name} #{last_name}"
   end
 end
