42d6e499157e628c846f1ebcc4a45e42dd9e6a39
Eloy Gomez <eloy@indeos.es>
2015-08-08 12:23:28 +0200

    Save visitor ip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806163142) do
+ActiveRecord::Schema.define(version: 20150808100936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150806163142) do
     t.string   "name"
     t.jsonb    "properties"
     t.datetime "time"
+    t.string   "ip"
   end
 
   add_index "ahoy_events", ["time"], name: "index_ahoy_events_on_time", using: :btree
