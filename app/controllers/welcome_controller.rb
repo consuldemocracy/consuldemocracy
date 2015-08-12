ea5612c5bd9f97391af4f98c51fa88fa86569364
Alberto Garcia Cabeza <alberto@decabeza.es>
2015-08-12 15:27:28 +0200

    Modifies styles welcome and debates index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   skip_authorization_check
 
   def index
-    @featured_debates = Debate.order("created_at DESC").limit(3)
+    @featured_debates = Debate.order("created_at DESC").limit(9)
     set_voted_values @featured_debates.map(&:id)
   end
 
