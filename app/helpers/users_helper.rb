2f41341ca89fdefccd8e3512f57ce578ea11ba8a
David Gil <dgilperez@gmail.com>
2015-08-12 18:31:44 +0200

    cleanup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,9 +4,8 @@ module UsersHelper
     klass         = options.fetch(:class)         { '' }
     round_corners = options.fetch(:round_corners) { true }
 
-    data_attributes = { height: size, width: size, "font-size" => (size * 0.6) }
+    data_attributes = { name: user.name, height: size, width: size, "font-size" => (size * 0.6) }
     data_attributes.merge!(radius: (size * 0.13).round) if round_corners
-    data_attributes.merge!(name: user.name)
 
     content_tag :img, nil, class: "avatar #{klass}", data: data_attributes
   end
