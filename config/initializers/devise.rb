1f2f318e83b1dee529b2ab14331b0e7e7dfe3892
kikito <kikito@gmail.com>
2015-08-16 23:36:33 +0200

    Turns on devise scoped views
    That way we can have users/registrations & organizations/registrations

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -211,7 +211,7 @@ Devise.setup do |config|
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.scoped_views = false
+  config.scoped_views = true
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
