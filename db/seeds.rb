ba9b04a0d602c04250d61c93810b8aaf543b0801
Juanjo Bazán <jjbazan@gmail.com>
2015-08-17 18:14:03 +0200

    adds default settings to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Names for the moderation console, as a hint for moderators
+# to know better how to assign users with official positions
+Setting.create(key: 'official_level_0_name', value: 'No cargo público')
+Setting.create(key: 'official_level_1_name', value: 'Organización Municipal')
+Setting.create(key: 'official_level_2_name', value: 'Funcionariado')
+Setting.create(key: 'official_level_3_name', value: 'Directores generales')
+Setting.create(key: 'official_level_4_name', value: 'Concejales')
+Setting.create(key: 'official_level_5_name', value: 'Alcaldes')
