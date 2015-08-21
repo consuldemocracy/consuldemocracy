ff6285d114d0e4db379001a9a02724eb3c35b11c
Alberto Garcia Cabeza <alberto@decabeza.es>
2015-08-21 18:57:09 +0200

    Improves general styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 # Names for the moderation console, as a hint for moderators
 # to know better how to assign users with official positions
 Setting.create(key: 'official_level_0_name', value: 'No cargo público')
-Setting.create(key: 'official_level_1_name', value: 'Organización Municipal')
-Setting.create(key: 'official_level_2_name', value: 'Funcionariado')
+Setting.create(key: 'official_level_1_name', value: 'Funcionariado')
+Setting.create(key: 'official_level_2_name', value: 'Organización Municipal')
 Setting.create(key: 'official_level_3_name', value: 'Directores generales')
 Setting.create(key: 'official_level_4_name', value: 'Concejales')
-Setting.create(key: 'official_level_5_name', value: 'Alcaldes')
+Setting.create(key: 'official_level_5_name', value: 'Alcaldesa')
