5e31a9f3384e796878a6cffa8fee2c43953b64a7
kikito <kikito@gmail.com>
2015-08-24 14:34:58 +0200

    adds a new "organization sigup success" view. Related with #226

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
-  devise_for :organizations, class_name: 'User',
+  devise_for(:organizations, class_name: 'User',
              controllers: {
                registrations: 'organizations/registrations',
                sessions: 'devise/sessions'
-             }
+             })
+
+  devise_scope :organization do
+    get "organizations/sign_up/success", to: "organizations/registrations#success"
+  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
