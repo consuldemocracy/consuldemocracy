ad3b689a22b0ecd13707107497e67906177b1354
kikito <kikito@gmail.com>
2015-08-27 16:32:16 +0200

    remove duplicated migration

--- db/migrate/20150827080701_add_hide_reviewed_at_to_comments_and_debates.rb
+++
@@ -1,6 +0,0 @@
-class AddConfirmedHideAtToCommentsAndDebates < ActiveRecord::Migration
-  def change
-    add_column :debates, :confirmed_hide_at, :datetime
-    add_column :comments, :confirmed_hide_at, :datetime
-  end
-end
