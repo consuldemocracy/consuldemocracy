84feeba085329e6af71d6588ac3eca5158673bf9
Juanjo Bazán <jjbazan@gmail.com>
2015-08-29 19:32:08 +0200

    adds pages controller for static pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+
+  skip_authorization_check
+
+  def privacy
+  end
+
+  def legal
+  end
+
+  def terms
+  end
+
+  def transparency
+  end
+
+  def opendata
+  end
+end
