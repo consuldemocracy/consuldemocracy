8037248b73897005b20593e43b13c9cd84314c1f
Juanjo Bazán <jjbazan@gmail.com>
2015-09-01 12:56:15 +0200

    marks social network login specs as pending temporary, for the beta testing phase

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -44,7 +44,7 @@ feature 'Users' do
     end
   end
 
-  context 'OAuth authentication' do
+  xcontext 'OAuth authentication' do
     context 'Twitter' do
       background do
         #request.env["devise.mapping"] = Devise.mappings[:user]
