174bd73b8923b680c7e4ed4dd6a201f4d2efcc24
Juanjo Bazán <jjbazan@gmail.com>
2015-09-01 18:46:27 +0200

    removes official_level_0_name setting

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,5 @@
 # Names for the moderation console, as a hint for moderators
 # to know better how to assign users with official positions
-Setting.create(key: 'official_level_0_name', value: 'No cargo público')
 Setting.create(key: 'official_level_1_name', value: 'Empleados públicos')
 Setting.create(key: 'official_level_2_name', value: 'Organización Municipal')
 Setting.create(key: 'official_level_3_name', value: 'Directores generales')
