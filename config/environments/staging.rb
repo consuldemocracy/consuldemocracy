0fc51bee60071145eed8e5a197c0eff5bbdc75e8
rgarcia <voodoorai2000@gmail.com>
2015-09-01 17:31:28 +0200

    adds mailer asset host for staging and production envs

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: Rails.application.secrets.server_name }
+  config.action_mailer.asset_host = "https://#{Rails.application.secrets.server_name}"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
