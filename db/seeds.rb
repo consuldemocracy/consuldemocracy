650f95ea21ed4ea4d76bb0380ee3fe9f86afa4a0
Juanjo Bazán <jjbazan@gmail.com>
2015-09-01 11:47:53 +0200

    changes default setting value

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # Names for the moderation console, as a hint for moderators
 # to know better how to assign users with official positions
 Setting.create(key: 'official_level_0_name', value: 'No cargo público')
-Setting.create(key: 'official_level_1_name', value: 'Funcionariado')
+Setting.create(key: 'official_level_1_name', value: 'Empleados públicos')
 Setting.create(key: 'official_level_2_name', value: 'Organización Municipal')
 Setting.create(key: 'official_level_3_name', value: 'Directores generales')
 Setting.create(key: 'official_level_4_name', value: 'Concejales')
