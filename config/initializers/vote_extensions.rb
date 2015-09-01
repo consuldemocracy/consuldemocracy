c2faf0ef925594cdee42728efa194b2861ae4a30
kikito <kikito@gmail.com>
2015-09-01 13:08:32 +0200

    Merge Vote.in with Vote.for_debates

--- config/initializers/vote_extensions.rb
+++ config/initializers/vote_extensions.rb
@@ -1,10 +1,6 @@
 ActsAsVotable::Vote.class_eval do
-  def self.for_debates
-    where(votable_type: 'Debate')
-  end
-
-  def self.in(debates)
-    where(votable_id: debates)
+  def self.for_debates(debates)
+    where(votable_type: 'Debate', votable_id: debates)
   end
 
   def value
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,34 +3,31 @@ require 'rails_helper'
 describe 'Vote' do
 
   describe '#for_debates' do
-    it 'returns votes for debates' do
-      debate = create(:debate)
-      create(:vote, votable: debate)
-
-      expect(Vote.for_debates.count).to eq(1)
-    end
-
     it 'does not returns votes for other votables' do
+      debate = create(:debate)
       comment = create(:comment)
       create(:vote, votable: comment)
 
-      expect(Vote.for_debates.count).to eq(0)
-    end
-  end
-
-  describe '#in' do
-    it 'returns debates send in parameters' do
-      debate = create(:debate)
-      create(:vote, votable: debate)
-
-      expect(Vote.in(debate).count).to eq(1)
+      expect(Vote.for_debates(debate).count).to eq(0)
     end
 
-    it 'does not return debates not in parameters' do
-      debate = create(:debate)
-      create(:vote, votable: debate)
+    it 'returns votes only for debates in parameters' do
+      debate1 = create(:debate)
+      debate2 = create(:debate)
+      create(:vote, votable: debate1)
 
-      expect(Vote.in([]).count).to eq(0)
+      expect(Vote.for_debates(debate1).count).to eq(1)
+      expect(Vote.for_debates(debate2).count).to eq(0)
+    end
+
+    it 'accepts more than 1 debate' do
+      debate1 = create(:debate)
+      debate2 = create(:debate)
+      debate3 = create(:debate)
+      create(:vote, votable: debate1)
+      create(:vote, votable: debate3)
+
+      expect(Vote.for_debates([debate1, debate2]).count).to eq(1)
     end
   end
 
