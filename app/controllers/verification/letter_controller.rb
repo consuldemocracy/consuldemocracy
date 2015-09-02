99d779bcec8345bfb2a01333bfb6026ac3182231
rgarcia <voodoorai2000@gmail.com>
2015-09-02 21:56:39 +0200

    adds letter code verification

--- app/controllers/verification/letter_controller.rb
+++ app/controllers/verification/letter_controller.rb
@@ -11,17 +11,32 @@ class Verification::LetterController < ApplicationController
   def create
     @letter = Verification::Letter.new(user: current_user)
     if @letter.save
-      redirect_to account_path, notice: t('verification.letter.create.flash.success')
+      redirect_to edit_letter_path, notice: t('verification.letter.create.flash.success')
     else
       flash.now.alert = t('verification.letter.create.alert.failure')
       render :new
     end
   end
 
+  def edit
+    @letter = Verification::Letter.new(user: current_user)
+  end
+
+  def update
+    @letter = Verification::Letter.new(letter_params.merge(user: current_user))
+    if @letter.verify?
+      current_user.update(verified_at: Time.now)
+      redirect_to account_path, notice: t('verification.letter.update.flash.success')
+    else
+      @error = t('verification.letter.update.error')
+      render :edit
+    end
+  end
+
   private
 
     def letter_params
-      params.require(:letter).permit()
+      params.require(:letter).permit(:verification_code)
     end
 
     def verify_phone_or_email!
