eb689d7c7e39b4b3ceed5b2b83a160f409bbee0f
Juanjo Bazán <jjbazan@gmail.com>
2015-09-02 17:19:32 +0200

    refactors comment form

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,11 +1,11 @@
 module CommentsHelper
 
-  def comment_link_text(parent)
-    parent.is_a? Comment ? t("comments_helper.reply_link") : t("comments_helper.comment_link")
+  def comment_link_text(parent_id)
+    parent_id.present? ? t("comments_helper.reply_link") : t("comments_helper.comment_link")
   end
 
-  def comment_button_text(parent)
-    parent.is_a? Comment ? t("comments_helper.reply_button") : t("comments_helper.comment_button")
+  def comment_button_text(parent_id)
+    parent_id.present?  ? t("comments_helper.reply_button") : t("comments_helper.comment_button")
   end
 
 end
