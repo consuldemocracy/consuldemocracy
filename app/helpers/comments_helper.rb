df5ef2dc29e0bf8fe5c03b69a745dd9e06975c5b
Juanjo Bazán <jjbazan@gmail.com>
2015-09-02 17:06:11 +0200

    makes helper independent of commentable class

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,11 +1,11 @@
 module CommentsHelper
 
   def comment_link_text(parent)
-    parent.class == Debate ? t("comments_helper.comment_link") : t("comments_helper.reply_link")
+    parent.is_a? Comment ? t("comments_helper.reply_link") : t("comments_helper.comment_link")
   end
 
   def comment_button_text(parent)
-    parent.class == Debate ? t("comments_helper.comment_button") : t("comments_helper.reply_button")
+    parent.is_a? Comment ? t("comments_helper.reply_button") : t("comments_helper.comment_button")
   end
 
 end
