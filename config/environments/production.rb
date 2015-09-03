cb6949e918eae18266c20e0830004bfe9477e2cd
Juanjo Bazán <jjbazan@gmail.com>
2015-09-03 23:48:25 +0200

    disables english locale in production (temporary change until texts are ready)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.i18n.available_locales = [:es]
 end
