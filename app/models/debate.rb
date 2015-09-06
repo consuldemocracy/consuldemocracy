26a2a67a71d407a3477ee00f366fa63fd81f2f4f
Juanjo Bazán <jjbazan@gmail.com>
2015-09-06 22:51:34 +0200

    refactors debate's tags related utility methods tags are already loaded, using them instead of extracting them again for each debate saves 50 query per index (2 query per debate in the page)

--- app/models/debate.rb
+++ app/models/debate.rb
@@ -86,13 +86,15 @@ class Debate < ActiveRecord::Base
   end
 
   def tag_list_with_limit(limit = nil)
-    tags.most_used(limit).pluck :name
+    return tags if limit.blank?
+
+    tags.sort{|a,b| b.taggings_count <=> a.taggings_count}[0, limit]
   end
 
   def tags_count_out_of_limit(limit = nil)
     return 0 unless limit
 
-    count = tags.count - limit
+    count = tags.size - limit
     count < 0 ? 0 : count
   end
 
