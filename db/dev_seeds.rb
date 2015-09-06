e14afd3db66ccf226af591cc44c9f8795b06bc1f
rgarcia <voodoorai2000@gmail.com>
2015-09-07 01:01:21 +0200

    adds terms of services to seeds

--- db/dev_seeds.rb
+++ db/dev_seeds.rb
@@ -15,7 +15,7 @@ puts "Creating Users"
 def create_user(email, username = Faker::Name.name)
   pwd = '12345678'
   puts "    #{username}"
-  User.create!(username: username, email: email, password: pwd, password_confirmation: pwd, confirmed_at: Time.now)
+  User.create!(username: username, email: email, password: pwd, password_confirmation: pwd, confirmed_at: Time.now, terms_of_service: "1")
 end
 
 admin = create_user('admin@madrid.es', 'admin')
