dba3385a6c8aa6b56fdb5f12c6e364d6be0d9336
Alberto Garcia Cabeza <alberto@decabeza.es>
2015-09-09 20:32:38 +0200

    Adds rem and respond js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( ckeditor/* )
+Rails.application.config.assets.precompile += %w( rem.min.js, respond.min.js )
