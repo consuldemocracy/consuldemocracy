bc0e7b01dd2e869818e33505b2dbbf4626943ddb
David Gil <dgilperez@gmail.com>
2015-09-10 22:22:21 +0200

    moves voting code for vote_or_unvote from Debate to Vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,2 +1,15 @@
 class Vote < ActsAsVotable::Vote
+
+  def self.already_voted_like_this?(voter:, votable:, value:)
+    where(voter: voter, votable: votable, vote_flag: value).exists?
+  end
+
+  def self.vote_or_unvote_for(voter:, votable:, value:)
+    if already_voted_like_this?(voter: voter, votable: votable, value: value)
+      votable.unvote_by(voter)
+    else
+      votable.vote_by(voter: voter, vote: value)
+    end
+  end
+
 end
