480a9d6f4ed7442a10632484a37cf3498526f84b
kikito <kikito@gmail.com>
2015-09-10 19:04:57 +0200

    Truncates values from the db before shortening them

--- db/migrate/20150909131133_set_username_limit.rb
+++ db/migrate/20150909131133_set_username_limit.rb
@@ -1,5 +1,6 @@
 class SetUsernameLimit < ActiveRecord::Migration
   def change
+    execute "ALTER TABLE users ALTER COLUMN username TYPE VARCHAR(60) USING SUBSTR(username, 1, 60)"
     change_column :users, :username, :string, limit: 60
   end
 end
--- db/migrate/20150909142534_set_organization_name_limit.rb
+++ db/migrate/20150909142534_set_organization_name_limit.rb
@@ -1,5 +1,6 @@
 class SetOrganizationNameLimit < ActiveRecord::Migration
   def change
+    execute "ALTER TABLE organizations ALTER COLUMN name TYPE VARCHAR(60) USING SUBSTR(name, 1, 60)"
     change_column :organizations, :name, :string, limit: 60
   end
 end
