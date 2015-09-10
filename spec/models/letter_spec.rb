06a23eaccb6ded631a9d0e2c63a6c4f0c61daf7f
rgarcia <voodoorai2000@gmail.com>
2015-09-10 14:12:06 +0200

    adds specs for letter code verification [#395]

--- spec/models/letter_spec.rb
+++ spec/models/letter_spec.rb
@@ -53,7 +53,34 @@ describe 'Verification::Letter' do
         district:      "CENTRO"
       })
     end
+  end
 
+  describe "#verify?" do
+
+    let(:letter) { build(:verification_letter) }
+
+    it "letter not sent" do
+      letter.user.update(letter_sent_at: nil)
+
+      expect(letter.verify?).to eq(false)
+      expect(letter.errors[:verification_code].first).to eq("We have not sent you the letter with the code yet")
+    end
+
+    it "letter sent but incorrect code" do
+      letter.user.update(letter_sent_at: 1.day.ago, letter_verification_code: "123456")
+      letter.verification_code = nil
+
+      expect(letter.verify?).to eq(false)
+      expect(letter.errors[:verification_code].first).to eq("Incorrect confirmation code")
+    end
+
+    it "letter sent and correct code" do
+      letter.user.update(letter_sent_at: 1.day.ago, letter_verification_code: "123456")
+      letter.verification_code = "123456"
+
+      expect(letter.verify?).to eq(true)
+      expect(letter.errors).to be_empty
+    end
   end
 
 end
