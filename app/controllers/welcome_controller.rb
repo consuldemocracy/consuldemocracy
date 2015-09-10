2c04cd07d927e4b1754fbc34a32dc8610097fac1
kikito <kikito@gmail.com>
2015-09-10 17:40:54 +0200

    Adds independent welcome screen

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,14 @@
 class WelcomeController < ApplicationController
   skip_authorization_check
 
+  layout "devise", only: :welcome
+
   def index
     @featured_debates = Debate.sort_by_confidence_score.limit(3).for_render
     set_debate_votes(@featured_debates)
+  end
+
+  def welcome
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get '/welcome', to: 'welcome#welcome'
 
   resources :debates do
     member do
