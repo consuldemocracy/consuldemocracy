3958d2f4c20acae7ba6c7d0626ed736b42b17233
rgarcia <voodoorai2000@gmail.com>
2015-09-10 20:13:30 +0200

    fixes spec [#395]

--- spec/models/letter_spec.rb
+++ spec/models/letter_spec.rb
@@ -55,14 +55,14 @@ describe 'Verification::Letter' do
     end
   end
 
-  describe "#verify?" do
+  describe "#verified?" do
 
     let(:letter) { build(:verification_letter) }
 
     it "letter not sent" do
       letter.user.update(letter_sent_at: nil)
 
-      expect(letter.verify?).to eq(false)
+      expect(letter.verified?).to eq(false)
       expect(letter.errors[:verification_code].first).to eq("We have not sent you the letter with the code yet")
     end
 
@@ -70,7 +70,7 @@ describe 'Verification::Letter' do
       letter.user.update(letter_sent_at: 1.day.ago, letter_verification_code: "123456")
       letter.verification_code = nil
 
-      expect(letter.verify?).to eq(false)
+      expect(letter.verified?).to eq(false)
       expect(letter.errors[:verification_code].first).to eq("Incorrect confirmation code")
     end
 
@@ -78,7 +78,7 @@ describe 'Verification::Letter' do
       letter.user.update(letter_sent_at: 1.day.ago, letter_verification_code: "123456")
       letter.verification_code = "123456"
 
-      expect(letter.verify?).to eq(true)
+      expect(letter.verified?).to eq(true)
       expect(letter.errors).to be_empty
     end
   end
