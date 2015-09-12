800fa7c47f5f7ec39cc1e9e395ad435fda818ffd
kikito <kikito@gmail.com>
2015-09-12 14:02:47 +0200

    Adds some methods to to Verification module

--- lib/verification.rb
+++ lib/verification.rb
@@ -1,4 +1,12 @@
 module Verification
+  extend ActiveSupport::Concern
+
+  included do
+    scope :level_three_verified, -> { where.not(verified_at: nil) }
+    scope :level_two_verified, -> { where("users.confirmed_phone IS NOT NULL AND users.residence_verified_at IS NOT NULL") }
+    scope :level_two_or_three_verified, -> { where("users.verified_at IS NOT NULL OR (users.confirmed_phone IS NOT NULL AND users.residence_verified_at IS NOT NULL)") }
+    scope :unverified, -> { where("users.verified_at IS NULL AND (users.confirmed_phone IS NULL OR users.residence_verified_at IS NOT NULL)") }
+  end
 
   def verification_email_sent?
     email_verification_token.present?
@@ -26,6 +34,10 @@ module Verification
 
   def level_three_verified?
     verified_at.present?
+  end
+
+  def level_two_or_three_verified?
+    level_two_verified? || level_three_verified?
   end
 
   def unverified?
