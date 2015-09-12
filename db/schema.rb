40840f74c66cbd0fc1c7c0184e325541f1cd09ee
Juanjo Bazán <jjbazan@gmail.com>
2015-09-12 18:16:51 +0200

    updates dev_seeds

--- db/schema.rb
+++ db/schema.rb
@@ -188,24 +188,6 @@ ActiveRecord::Schema.define(version: 20150912145218) do
 
   add_index "organizations", ["user_id"], name: "index_organizations_on_user_id", using: :btree
 
-  create_table "proposals", force: :cascade do |t|
-    t.string   "title",             limit: 80
-    t.text     "description"
-    t.string   "question"
-    t.string   "external_url"
-    t.integer  "author_id"
-    t.datetime "hidden_at"
-    t.integer  "flags_count",                  default: 0
-    t.datetime "ignored_flag_at"
-    t.integer  "cached_votes_up",              default: 0
-    t.integer  "comments_count",               default: 0
-    t.datetime "confirmed_hide_at"
-    t.integer  "hot_score",         limit: 8,  default: 0
-    t.integer  "confidence_score",             default: 0
-    t.datetime "created_at",                               null: false
-    t.datetime "updated_at",                               null: false
-  end
-
   create_table "settings", force: :cascade do |t|
     t.string "key"
     t.string "value"
