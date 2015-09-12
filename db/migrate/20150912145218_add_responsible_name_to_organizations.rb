eb8d8f47663d4c5c2d074b34e309dd0fd1bf15f7
Juanjo Bazán <jjbazan@gmail.com>
2015-09-12 18:41:34 +0200

    changes migration to update invalid orgs

--- db/migrate/20150912145218_add_responsible_name_to_organizations.rb
+++ db/migrate/20150912145218_add_responsible_name_to_organizations.rb
@@ -1,5 +1,13 @@
 class AddResponsibleNameToOrganizations < ActiveRecord::Migration
-  def change
+  def up
     add_column :organizations, :responsible_name, :string, limit: 60
+
+    Organization.find_each do |org|
+      org.update(responsible_name: org.name) if org.responsible_name.blank?
+    end
+  end
+
+  def down
+    remove_column :organizations, :responsible_name
   end
 end
