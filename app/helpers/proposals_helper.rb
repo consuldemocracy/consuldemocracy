a472b4acc80a412afa27bf3306b270292d11cfb5
Juanjo Bazán <jjbazan@gmail.com>
2015-09-14 19:06:07 +0200

    implements percentage in proposal supports

--- app/helpers/proposals_helper.rb
+++ app/helpers/proposals_helper.rb
@@ -8,4 +8,14 @@ module ProposalsHelper
     end
   end
 
+  def supports_percentage(proposal)
+    percentage = (proposal.cached_votes_up.to_f * 100 / Proposal.votes_needed_for_success)
+    case percentage
+      when 0 then "0%"
+      when 0..(0.1) then "0.1%"
+      when (0.1)..100 then number_to_percentage(percentage, strip_insignificant_zeros: true, precision: 1)
+      else "100%"
+    end
+  end
+
 end
