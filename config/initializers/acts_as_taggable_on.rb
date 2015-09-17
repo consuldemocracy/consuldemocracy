d71a4046613598701ee651a3ed04287868ec6479
kikito <kikito@gmail.com>
2015-09-17 16:53:21 +0200

    Adds more methods to ActsAsTaggableOn

--- config/initializers/acts_as_taggable_on.rb
+++ config/initializers/acts_as_taggable_on.rb
@@ -1,24 +1,43 @@
-ActsAsTaggableOn::Tagging.class_eval do
-  after_create :increase_custom_counter
-  after_destroy :touch_taggable, :decrease_custom_counter
+module ActsAsTaggableOn
 
-  def touch_taggable
-    taggable.touch if taggable.present?
+  Tagging.class_eval do
+
+    after_create :increment_tag_custom_counter
+    after_destroy :touch_taggable, :decrement_tag_custom_counter
+
+    def touch_taggable
+      taggable.touch if taggable.present?
+    end
+
+    def increment_tag_custom_counter
+      tag.increment_custom_counter_for(taggable_type)
+    end
+
+    def decrement_tag_custom_counter
+      tag.decrement_custom_counter_for(taggable_type)
+    end
   end
 
-  def increase_custom_counter
-    ActsAsTaggableOn::Tag.increment_counter(custom_counter_field_name, tag_id)
+  Tag.class_eval do
+
+    def increment_custom_counter_for(taggable_type)
+      Tag.increment_counter(custom_counter_field_name_for(taggable_type), id)
+    end
+
+    def decrement_custom_counter_for(taggable_type)
+      Tag.decrement_counter(custom_counter_field_name_for(taggable_type), id)
+    end
+
+    def recalculate_custom_counter_for(taggable_type)
+      visible_taggables = taggable_type.constantize.includes(:taggings).where('taggings.taggable_type' => taggable_type, 'taggings.tag_id' => id)
+
+      update(custom_counter_field_name_for(taggable_type) => visible_taggables.count)
+    end
+
+    private
+      def custom_counter_field_name_for(taggable_type)
+        "#{taggable_type.underscore.pluralize}_count"
+      end
   end
-
-  def decrease_custom_counter
-    ActsAsTaggableOn::Tag.decrement_counter(custom_counter_field_name, tag_id)
-  end
-
-  private
-
-  def custom_counter_field_name
-    "#{self.taggable_type.underscore.pluralize}_count"
-  end
-
 
 end
