ba081f73c1f4bca954ebeb06341c95b6685e513e
Juanjo Bazán <jjbazan@gmail.com>
2015-09-24 13:58:01 +0200

    adds spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -124,6 +124,7 @@ FactoryGirl.define do
     description          'Proposal description'
     question             'Proposal question'
     external_url         'http://external_documention.es'
+    video_url            'http://video_link.com'
     responsible_name     'John Snow'
     terms_of_service     '1'
     association :author, factory: :user
