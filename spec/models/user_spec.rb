9ced80fe8c1a0e4f0658a4f1317f4e7af83c8861
rgarcia <voodoorai2000@gmail.com>
2015-09-24 14:35:03 +0200

    refactors verification specs [#500]

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -262,85 +262,8 @@ describe User do
     end
   end
 
-  describe "verification levels" do
-    it "residence_verified? is true only if residence_verified_at" do
-      user = create(:user, residence_verified_at: Time.now)
-      expect(user.residence_verified?).to eq(true)
-
-      user = create(:user, residence_verified_at: nil)
-      expect(user.residence_verified?).to eq(false)
-    end
-
-    it "sms_verified? is true only if confirmed_phone" do
-      user = create(:user, confirmed_phone: "123456789")
-      expect(user.sms_verified?).to eq(true)
-
-      user = create(:user, confirmed_phone: nil)
-      expect(user.sms_verified?).to eq(false)
-    end
-
-    it "level_two_verified? is true only if residence_verified_at and confirmed_phone" do
-      user = create(:user, confirmed_phone: "123456789", residence_verified_at: Time.now)
-      expect(user.level_two_verified?).to eq(true)
-
-      user = create(:user, confirmed_phone: nil, residence_verified_at: Time.now)
-      expect(user.level_two_verified?).to eq(false)
-
-      user = create(:user, confirmed_phone: "123456789", residence_verified_at: nil)
-      expect(user.level_two_verified?).to eq(false)
-    end
-
-    it "level_three_verified? is true only if verified_at" do
-      user = create(:user, verified_at: Time.now)
-      expect(user.level_three_verified?).to eq(true)
-
-      user = create(:user, verified_at: nil)
-      expect(user.level_three_verified?).to eq(false)
-    end
-
-    it "unverified? is true only if not level_three_verified and not level_two_verified" do
-      user = create(:user, verified_at: nil, confirmed_phone: nil)
-      expect(user.unverified?).to eq(true)
-
-      user = create(:user, verified_at: Time.now, confirmed_phone: "123456789", residence_verified_at: Time.now)
-      expect(user.unverified?).to eq(false)
-    end
-
-    it "verification_email_sent? is true only if user has email_verification_token" do
-      user = create(:user, email_verification_token: "xxxxxxx")
-      expect(user.verification_email_sent?).to eq(true)
-
-      user = create(:user, email_verification_token: nil)
-      expect(user.verification_email_sent?).to eq(false)
-    end
-
-    it "verification_sms_sent? is true only if user has unconfirmed_phone and sms_confirmation_code" do
-      user = create(:user, unconfirmed_phone: "666666666", sms_confirmation_code: "666")
-      expect(user.verification_sms_sent?).to eq(true)
-
-      user = create(:user, unconfirmed_phone: nil, sms_confirmation_code: "666")
-      expect(user.verification_sms_sent?).to eq(false)
-
-      user = create(:user, unconfirmed_phone: "666666666", sms_confirmation_code: nil)
-      expect(user.verification_sms_sent?).to eq(false)
-
-      user = create(:user, unconfirmed_phone: nil, sms_confirmation_code: nil)
-      expect(user.verification_sms_sent?).to eq(false)
-    end
-
-    it "verification_letter_sent? is true only if user has letter_requested_at and letter_verification_code" do
-      user = create(:user, letter_requested_at: Time.now, letter_verification_code: "666")
-      expect(user.verification_letter_sent?).to eq(true)
-
-      user = create(:user, letter_requested_at: nil, letter_verification_code: "666")
-      expect(user.verification_letter_sent?).to eq(false)
-
-      user = create(:user, letter_requested_at: Time.now, letter_verification_code: nil)
-      expect(user.verification_letter_sent?).to eq(false)
-
-      user = create(:user, letter_requested_at: nil, letter_verification_code: nil)
-      expect(user.verification_letter_sent?).to eq(false)
-    end
+  describe "verification" do
+    it_behaves_like "verifiable"
   end
 
   describe "cache" do
