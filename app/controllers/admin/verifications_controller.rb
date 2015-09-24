3bfbdcaf015247b2feea2985fd21b68cc53e77cf
rgarcia <voodoorai2000@gmail.com>
2015-09-24 10:25:06 +0200

    adds search of unverified users [#500]

--- app/controllers/admin/verifications_controller.rb
+++ app/controllers/admin/verifications_controller.rb
@@ -4,4 +4,9 @@ class Admin::VerificationsController < Admin::BaseController
     @users = User.unverified.page(params[:page])
   end
 
+  def search
+    @users = User.unverified.search(params[:name_or_email]).page(params[:page]).for_render
+    render :index
+  end
+
 end
