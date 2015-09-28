e76d2b9ad642d0b3a565e10214c96364c667d4da
kikito <kikito@gmail.com>
2015-09-28 17:19:53 +0200

    fixes travis

--- app/models/abilities/administrator.rb
+++ app/models/abilities/administrator.rb
@@ -31,7 +31,7 @@ module Abilities
 
       can :comment_as_administrator, [Debate, Comment, Proposal]
 
-      can [:search, :create, :index, :destroy], Moderator
+      can [:search, :create, :index, :destroy], ::Moderator
     end
   end
 end
