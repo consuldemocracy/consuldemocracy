967bfc5b311f5897c7b4d33393adcbf6646b911e
kikito <kikito@gmail.com>
2015-10-02 18:40:56 +0200

    Moves the humanize_document_type helper to a new helper file called user

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -12,17 +12,6 @@ module AdminHelper
     options
   end
 
-  def humanize_document_type(document_type)
-    case document_type
-    when "1"
-      t "verification.residence.new.document_type.spanish_id"
-    when "2"
-      t "verification.residence.new.document_type.passport"
-    when "3"
-      t "verification.residence.new.document_type.residence_card"
-    end
-  end
-
   private
 
     def namespace
