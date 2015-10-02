5069f86eb9f95f07e4290a7f5bac75c6f3114eeb
kikito <kikito@gmail.com>
2015-10-02 18:36:40 +0200

    Skips the welcome screen when the user is verifying via email

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,11 +1,18 @@
 class Users::SessionsController < Devise::SessionsController
 
   def after_sign_in_path_for(resource)
-    if resource.show_welcome_screen?
+    if stored_path_allows_welcome_screen? && resource.show_welcome_screen?
       welcome_path
     else
       root_path
     end
   end
+
+  private
+
+    def stored_path_allows_welcome_screen?
+      stored_path = session[stored_location_key_for(resource)]
+      stored_path[0..5] != "/email"
+    end
 
 end
