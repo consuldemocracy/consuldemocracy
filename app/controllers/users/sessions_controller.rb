6da78541528c0fc428c05e2ecbf51a04404a4125
kikito <kikito@gmail.com>
2015-10-09 13:30:19 +0200

    refactors sessions_controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Users::SessionsController < Devise::SessionsController
 
   def after_sign_in_path_for(resource)
-    if stored_path_allows_welcome_screen? && resource.show_welcome_screen?
+    if !verifying_via_email? && resource.show_welcome_screen?
       welcome_path
     else
       super
@@ -10,9 +10,9 @@ class Users::SessionsController < Devise::SessionsController
 
   private
 
-    def stored_path_allows_welcome_screen?
-      stored_path = session[stored_location_key_for(resource)]
-      stored_path.nil? || stored_path[0..5] != "/email"
+    def verifying_via_email?
+      stored_path = session[stored_location_key_for(resource)] || ""
+      stored_path[0..5] == "/email"
     end
 
 end
