786986053b3d6465bb3aa38f7cf8ee7f3255c249
rgarcia <voodoorai2000@gmail.com>
2015-10-20 14:50:26 +0200

    tests print button present

--- spec/features/management/proposals_spec.rb
+++ spec/features/management/proposals_spec.rb
@@ -109,12 +109,7 @@ feature 'Proposals' do
       click_link "Print proposals"
 
       expect(page).to have_css('.proposal', count: 5)
-      find("#print_link").click
-
-      ### CHANGE ME
-      # should probably test something else here
-      # maybe that we are loading a print.css stylesheet?
-      ###
+      expect(page).to have_css("a[href='javascript:window.print();']", text: 'Print')
     end
 
     scenario "Filtering proposals to be printed", :js do
