854499b13595ad948fd6a52d4e839a7fb74fc100
Juanjo Bazán <jjbazan@gmail.com>
2015-10-23 13:36:43 +0200

    adds localization to management

--- app/controllers/management/base_controller.rb
+++ app/controllers/management/base_controller.rb
@@ -2,6 +2,7 @@ class Management::BaseController < ActionController::Base
   layout 'management'
 
   before_action :verify_manager
+  before_action :set_locale
 
   helper_method :managed_user
 
@@ -17,6 +18,16 @@ class Management::BaseController < ActionController::Base
 
     def managed_user
       @managed_user ||= Verification::Management::ManagedUser.find(session[:document_type], session[:document_number])
+    end
+
+    def set_locale
+      if params[:locale] && I18n.available_locales.include?(params[:locale].to_sym)
+        session[:locale] = params[:locale]
+      end
+
+      session[:locale] ||= I18n.default_locale
+
+      I18n.locale = session[:locale]
     end
 
 end
