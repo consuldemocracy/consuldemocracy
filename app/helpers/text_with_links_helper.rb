3013d13b381ac219ebf6d50b879d4736345f2642
Sergio Arbeo <serabe@gmail.com>
2015-10-26 01:25:53 +0100

    Add paragraphs to comments
    Fix #600

--- app/helpers/text_with_links_helper.rb
+++ app/helpers/text_with_links_helper.rb
@@ -2,7 +2,7 @@ module TextWithLinksHelper
 
   def text_with_links(text)
     return unless text
-    sanitized = sanitize text, tags: [], attributes: []
+    sanitized = sanitize(text, tags: [], attributes: [])
     Rinku.auto_link(sanitized, :all, 'target="_blank" rel="nofollow"').html_safe
   end
 
