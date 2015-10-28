094ec095769e4f8f48f3370b997ad8a2f4d1aecf
kikito <kikito@gmail.com>
2015-10-28 16:00:34 +0100

    Adds migration for comment.confidence_score

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151021113348) do
+ActiveRecord::Schema.define(version: 20151028145921) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +73,7 @@ ActiveRecord::Schema.define(version: 20151021113348) do
     t.integer  "cached_votes_down",  default: 0
     t.datetime "confirmed_hide_at"
     t.string   "ancestry"
+    t.integer  "confidence_score"
   end
 
   add_index "comments", ["ancestry"], name: "index_comments_on_ancestry", using: :btree
