60b3c65b050a68e55a85b61b767f8471b3686f16
kikito <kikito@gmail.com>
2015-10-30 17:00:32 +0100

    Refactor comment tree calculation using a PORO

--- app/controllers/concerns/commentable_actions.rb
+++ app/controllers/concerns/commentable_actions.rb
@@ -16,11 +16,8 @@ module CommentableActions
   def show
     set_resource_votes(resource)
     @commentable = resource
-    @root_comments = resource.comments.roots.send("sort_by_#{@current_order}").page(params[:page]).per(10).for_render
-    @comments = @root_comments.inject([]){|all, root| all + Comment.descendants_of(root).for_render}
-    @all_visible_comments = @root_comments + @comments
-
-    set_comment_flags(@all_visible_comments)
+    @comment_tree = CommentTree.new(@commentable, params[:page], @current_order)
+    set_comment_flags(@comment_tree.comments)
     set_resource_instance
   end
 
---
+++ lib/comment_tree.rb
@@ -0,0 +1,28 @@
+class CommentTree
+
+  ROOT_COMMENTS_PER_PAGE = 10
+
+  attr_accessor :root_comments, :comments
+
+  def initialize(commentable, page, order = 'confidence_score')
+    @root_comments = commentable.comments.roots.send("sort_by_#{order}").page(page).per(ROOT_COMMENTS_PER_PAGE).for_render
+
+    root_descendants = @root_comments.each_with_object([]) do |root, col|
+      col += Comment.descendants_of(root).send("sort_by_#{order}").for_render.to_a
+    end
+
+    @comments = root_comments + root_descendants
+
+    @comments_by_parent_id = @comments.each_with_object({}) do |comment, col|
+      (col[comment.parent_id] ||= []) << comment
+    end
+  end
+
+  def children_of(parent)
+    @comments_by_parent_id[parent.id] || []
+  end
+
+  def comment_authors
+    comments.map(&:author)
+  end
+end
