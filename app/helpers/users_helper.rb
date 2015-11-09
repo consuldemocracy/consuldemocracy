ceb157b53b66300f42a7f9d7e7578da5349b3929
rgarcia <voodoorai2000@gmail.com>
2015-11-09 13:46:29 +0100

    removes unused code

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -13,9 +13,7 @@ module UsersHelper
 
   def comment_commentable_title(comment)
     commentable = comment.commentable
-    if commentable.nil?
-      deleted_commentable_text(comment)
-    elsif commentable.hidden?
+    if commentable.hidden?
       "<abbr title='#{deleted_commentable_text(comment)}'>".html_safe +
       commentable.title +
       "</abbr>".html_safe
@@ -30,8 +28,6 @@ module UsersHelper
       t("users.show.deleted_proposal")
     when "Debate"
       t("users.show.deleted_debate")
-    else
-      t("users.show.deleted")
     end
   end
 
