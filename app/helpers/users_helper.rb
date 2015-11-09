7259ac06e8068755e8a59eb57ae535e7a40f0553
rgarcia <voodoorai2000@gmail.com>
2015-11-09 13:28:02 +0100

    renames user_helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,16 @@
 module UsersHelper
 
+  def humanize_document_type(document_type)
+    case document_type
+    when "1"
+      t "verification.residence.new.document_type.spanish_id"
+    when "2"
+      t "verification.residence.new.document_type.passport"
+    when "3"
+      t "verification.residence.new.document_type.residence_card"
+    end
+  end
+
   def comment_commentable_title(comment)
     commentable = comment.commentable
     if commentable.nil?
