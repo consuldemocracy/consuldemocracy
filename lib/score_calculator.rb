c1c9ed6340070f680d56d85daef9eb81dfa37b38
rgarcia <voodoorai2000@gmail.com>
2015-11-17 09:57:47 +0100

    increases comment weight on hot scores

--- lib/score_calculator.rb
+++ lib/score_calculator.rb
@@ -1,7 +1,7 @@
 module ScoreCalculator
 
   EPOC           = Time.new(2015, 6, 15)
-  COMMENT_WEIGHT = 1.0/20 # 1 positive vote / x comments
+  COMMENT_WEIGHT = 1.0/5 # 1 positive vote / x comments
   TIME_UNIT      = 48.hours.to_f
 
   def self.hot_score(date, votes_total, votes_up, comments_count)
