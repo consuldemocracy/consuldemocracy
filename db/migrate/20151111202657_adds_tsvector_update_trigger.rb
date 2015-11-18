a34369be4335935854c49a163cfc1980a866bc73
rgarcia <voodoorai2000@gmail.com>
2015-11-18 18:03:32 +0100

    typo

--- db/migrate/20151111202657_adds_tsvector_update_trigger.rb
+++ db/migrate/20151111202657_adds_tsvector_update_trigger.rb
@@ -2,11 +2,11 @@ class AddsTsvectorUpdateTrigger < ActiveRecord::Migration
 
   def up
     add_column :proposals, :tsv, :tsvector
-    add_index :products, :tsv, using: "gin"
+    add_index :proposals, :tsv, using: "gin"
   end
 
   def down
-    remove_index :products, :tsv
+    remove_index :proposals, :tsv
     remove_column :proposals, :tsv
   end
 
