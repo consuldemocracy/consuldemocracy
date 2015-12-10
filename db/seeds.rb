e7e1c8a7d5c595b378ee84489f30225ae02b29e4
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2015-12-10 15:05:56 +0100

    Configurable dictionary for full text searches #730

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,3 +30,6 @@ Setting.create(key: 'votes_for_proposal_success', value: '53726')
 # Users with this email domain will automatically be marked as level 1 officials
 # Emails under the domain's subdomains will also be included
 Setting.create(key: 'email_domain_for_officials', value: '')
+
+# Dictionary for cache searches
+Setting.create(key: 'dictionary_for_searches', value: 'spanish')
