626cb24113c348caeedbff4cfee269306c0083c4
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2015-12-15 16:57:52 +0100

    Send email via sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,18 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: Rails.application.secrets.server_name }
   config.action_mailer.asset_host = "https://#{Rails.application.secrets.server_name}"
 
+  if ENV["SENDGRID_USERNAME"]
+    config.action_mailer.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+    }
+  end
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
