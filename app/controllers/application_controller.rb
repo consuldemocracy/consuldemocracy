7996f5f81612e8ff506fd6214f37c3911ec0918f
kikito <kikito@gmail.com>
2015-12-16 14:02:03 +0100

    makes cancan return a json error when asked for json and permissions fail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to main_app.root_url, alert: exception.message
+    respond_to do |format|
+      format.html { redirect_to main_app.root_url, alert: exception.message }
+      format.json { render json: {error: exception.message}, status: :forbidden }
+    end
   end
 
   layout :set_layout
