c98360fa87b689e1b5f3a4c870ee2a9e8fb85617
kikito <kikito@gmail.com>
2015-12-16 18:10:02 +0100

    Return a permissions hash with all the annotations

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -6,14 +6,14 @@ class AnnotationsController < ApplicationController
     @annotation = Annotation.new(annotation_params)
     @annotation.user = current_user
     if @annotation.save
-      render json: @annotation.to_json
+      render json: @annotation.to_json(methods: :permissions)
     end
   end
 
   def update
     @annotation = Annotation.find!(params[:id])
     if @annotation.update_attributes(annotation_params)
-      render json: @annotation.to_json
+      render json: @annotation.to_json(methods: :permissions)
     end
   end
 
@@ -26,7 +26,7 @@ class AnnotationsController < ApplicationController
   def search
     @annotations = Annotation.where(legislation_id: params[:legislation_id])
     annotations_hash = { total: @annotations.size, rows: @annotations }
-    render json: annotations_hash.to_json
+    render json: annotations_hash.to_json(methods: :permissions)
   end
 
   private
