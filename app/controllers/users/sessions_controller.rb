866e04f7768cb504972a4e8626ff1938603ba617
kikito <kikito@gmail.com>
2015-12-21 16:44:30 +0100

    makes devise remember the last visited page before login in

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,14 +1,14 @@
 class Users::SessionsController < Devise::SessionsController
 
-  def after_sign_in_path_for(resource)
-    if !verifying_via_email? && resource.show_welcome_screen?
-      welcome_path
-    else
-      super
-    end
-  end
-
   private
+
+    def after_sign_in_path_for(resource)
+      if !verifying_via_email? && resource.show_welcome_screen?
+        welcome_path
+      else
+        super
+      end
+    end
 
     def verifying_via_email?
       return false unless resource.present?
