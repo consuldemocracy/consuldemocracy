ca50983938a7e6b6efea7de88f21b9001a76d33b
Juanjo Bazán <jjbazan@gmail.com>
2016-01-04 21:36:50 +0100

    adds index for author_id

--- db/schema.rb
+++ db/schema.rb
@@ -282,6 +282,8 @@ ActiveRecord::Schema.define(version: 20160108133501) do
     t.datetime "updated_at",   null: false
   end
 
+  add_index "spending_proposals", ["author_id"], name: "index_spending_proposals_on_author_id", using: :btree
+
   create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
     t.integer  "taggable_id"
@@ -296,16 +298,18 @@ ActiveRecord::Schema.define(version: 20160108133501) do
   add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
 
   create_table "tags", force: :cascade do |t|
-    t.string  "name",            limit: 40
-    t.integer "taggings_count",             default: 0
-    t.boolean "featured",                   default: false
-    t.integer "debates_count",              default: 0
-    t.integer "proposals_count",            default: 0
+    t.string  "name",                     limit: 40
+    t.integer "taggings_count",                      default: 0
+    t.boolean "featured",                            default: false
+    t.integer "debates_count",                       default: 0
+    t.integer "proposals_count",                     default: 0
+    t.integer "spending_proposals_count",            default: 0
   end
 
   add_index "tags", ["debates_count"], name: "index_tags_on_debates_count", using: :btree
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
   add_index "tags", ["proposals_count"], name: "index_tags_on_proposals_count", using: :btree
+  add_index "tags", ["spending_proposals_count"], name: "index_tags_on_spending_proposals_count", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                                default: ""
