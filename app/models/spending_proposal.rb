54badef191d5369b92e84833e094a2fe7f38fb93
Juanjo Bazán <jjbazan@gmail.com>
2016-01-04 21:44:13 +0100

    users can create spending proposals

--- app/models/spending_proposal.rb
+++ app/models/spending_proposal.rb
@@ -3,14 +3,16 @@ class SpendingProposal < ActiveRecord::Base
   include Sanitizable
   include Taggable
 
+  apply_simple_captcha
+
   belongs_to :author, -> { with_hidden }, class_name: 'User', foreign_key: 'author_id'
 
   validates :title, presence: true
   validates :author, presence: true
   validates :description, presence: true
 
-  validates :title, length: { in: 4..Proposal.title_max_length }
-  validates :description, length: { maximum: Proposal.description_max_length }
+  validates :title, length: { in: 4..SpendingProposal.title_max_length }
+  validates :description, length: { maximum: SpendingProposal.description_max_length }
 
   validates :terms_of_service, acceptance: { allow_nil: false }, on: :create
 end
--- spec/features/spending_proposals_spec.rb
+++ spec/features/spending_proposals_spec.rb
@@ -8,4 +8,50 @@ feature 'Spending proposals' do
     expect(page).to have_link('Create spending proposal', href: new_spending_proposal_path)
   end
 
+  scenario 'Create' do
+    author = create(:user)
+    login_as(author)
+
+    visit new_spending_proposal_path
+    fill_in 'spending_proposal_title', with: 'Build a skyscraper'
+    fill_in 'spending_proposal_description', with: 'I want to live in a high tower over the clouds'
+    fill_in 'spending_proposal_external_url', with: 'http://http://skyscraperpage.com/'
+    fill_in 'spending_proposal_captcha', with: correct_captcha_text
+    check 'spending_proposal_terms_of_service'
+
+    click_button 'Create'
+
+    expect(page).to have_content 'Spending proposal created successfully'
+  end
+
+  scenario 'Captcha is required for proposal creation' do
+    login_as(create(:user))
+
+    visit new_spending_proposal_path
+    fill_in 'spending_proposal_title', with: 'Build a skyscraper'
+    fill_in 'spending_proposal_description', with: 'I want to live in a high tower over the clouds'
+    fill_in 'spending_proposal_external_url', with: 'http://http://skyscraperpage.com/'
+    fill_in 'spending_proposal_captcha', with: 'wrongText'
+    check 'spending_proposal_terms_of_service'
+
+    click_button 'Create'
+
+    expect(page).to_not have_content 'Spending proposal created successfully'
+    expect(page).to have_content '1 error'
+
+    fill_in 'spending_proposal_captcha', with: correct_captcha_text
+    click_button 'Create'
+
+    expect(page).to have_content 'Spending proposal created successfully'
+  end
+
+  scenario 'Errors on create' do
+    author = create(:user)
+    login_as(author)
+
+    visit new_spending_proposal_path
+    click_button 'Create'
+    expect(page).to have_content error_message
+  end
+
 end
