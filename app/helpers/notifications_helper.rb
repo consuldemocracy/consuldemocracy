40ffeeaeb01c41ec1c876621bbf8652836e5196a
Alberto Garcia Cabeza <alberto@decabeza.es>
2016-01-08 12:46:02 +0100

    Removes unused method

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -3,9 +3,4 @@ module NotificationsHelper
   def notification_action(notification)
     notification.notifiable.reply? ? "replied_to_your_comment" : "commented_on_your_debate"
   end
-
-  def notifications_class_for(user)
-    user.notifications.count > 0 ? "with_notifications" : "without_notifications"
-  end
-
 end
