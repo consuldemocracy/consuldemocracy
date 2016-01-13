32dc8b1d2d9b58f86681bcb52469153d9a530f8f
kikito <kikito@gmail.com>
2016-01-13 11:30:57 +0100

    Rename Identity method

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -4,7 +4,7 @@ class Identity < ActiveRecord::Base
   validates :provider, presence: true
   validates :uid, presence: true, uniqueness: { scope: :provider }
 
-  def self.find_for_oauth(auth)
+  def self.first_or_create_from_oauth(auth)
     where(uid: auth.uid, provider: auth.provider).first_or_create
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -52,6 +52,7 @@ class User < ActiveRecord::Base
   def self.find_for_oauth(auth, signed_in_resource = nil)
     # Get the identity and user if they exist
     identity = Identity.find_for_oauth(auth)
+    identity = Identity.first_or_create_from_oauth(auth)
 
     # If a signed_in_resource is provided it always overrides the existing user
     # to prevent the identity being locked with accidentally created accounts.
