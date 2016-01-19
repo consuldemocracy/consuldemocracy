9e441418a6ae75b471d36f7ab42529aca0d29807
kikito <kikito@gmail.com>
2016-01-19 19:47:07 +0100

    Uses registering_with_oauth persistent attribute to deactivate checks in certain occasions

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -28,13 +28,25 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       identity = Identity.first_or_create_from_oauth(auth)
       @user = current_user || identity.user || User.first_or_initialize_for_oauth(auth)
 
+      # If there are no problems with the email/username, then they were provided by oauth or they
+      # correspond to an existing user. Associate the identity and sign in
       if @user.save
         identity.update(user: @user)
         sign_in_and_redirect @user, event: :authentication
         set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
       else
-        session["devise.#{provider}_data"] = env["omniauth.auth"]
-        redirect_to new_user_registration_url
+        # If either the username or email have provoked a failure, we save the user anyway (but marked for revision)
+        # This mark will be detected by applicationcontroller and the user will be redirected to finish_signup
+        @user.registering_with_oauth = true
+        if @user.save
+          identity.update(user: @user)
+          sign_in_and_redirect @user, event: :authentication
+          set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+        else
+          # If the failure is because something else happens, just present the "new user" form
+          session["devise.#{provider}_data"] = auth
+          redirect_to new_user_registration_url
+        end
       end
     end
 
