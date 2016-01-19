b3c3d02a9822fcce96f91007dd657165ca655de7
kikito <kikito@gmail.com>
2016-01-19 19:43:54 +0100

    Adds 'registering_with_oauth' boolean to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160108133501) do
+ActiveRecord::Schema.define(version: 20160119164320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -340,6 +340,7 @@ ActiveRecord::Schema.define(version: 20160108133501) do
     t.boolean  "public_activity",                      default: true
     t.boolean  "newsletter",                           default: false
     t.integer  "notifications_count",                  default: 0
+    t.boolean  "registering_with_oauth",               default: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
