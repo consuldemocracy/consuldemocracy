1c130b1d919f3498b9a16c8f372852c540f6921f
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-01-21 18:45:38 +0100

    Improve meetings

--- app/assets/javascripts/components/meetings_map.es6.jsx
+++ app/assets/javascripts/components/meetings_map.es6.jsx
@@ -54,13 +54,13 @@ class MeetingsMap extends React.Component {
               lat: meeting.address_latitude,
               lng: meeting.address_longitude,
             },
-            animation: google.maps.Animation.DROP,
             icon: {
               path: "M25 0c-8.284 0-15 6.656-15 14.866 0 8.211 15 35.135 15 35.135s15-26.924 15-35.135c0-8.21-6.716-14.866-15-14.866zm-.049 19.312c-2.557 0-4.629-2.055-4.629-4.588 0-2.535 2.072-4.589 4.629-4.589 2.559 0 4.631 2.054 4.631 4.589 0 2.533-2.072 4.588-4.631 4.588z",
               fillOpacity: 1,
 		          strokeColor: '#fff',
               strokeWeight: 2,
-              size: {width: 25, height: 25},
+              size: {width: 27, height: 35},
+              anchor: new google.maps.Point(20, 40),
               scale: 0.6,
               fillColor: '#fa494a',
             },
@@ -87,7 +87,10 @@ class MeetingsMap extends React.Component {
     this.markers = markers;
     this.markerClusterer = new MarkerClusterer(this.map, this.markers, {
       gridSize: 30,
-      ignoreHidden: true
+      ignoreHidden: true,
+      enableRetinaIcons: true,
+      maxZoom: 13,
+      minimumClusterSize: 1
     });
   }
 
