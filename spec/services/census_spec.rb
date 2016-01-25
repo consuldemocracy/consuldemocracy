16b732a46ffcd21f4e0297084793efa22dd95f92
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-01-25 10:54:38 +0100

    Add postal code and date of birth validation on census

--- spec/services/census_spec.rb
+++ spec/services/census_spec.rb
@@ -1,23 +1,50 @@
 require 'rails_helper'
 
-describe Census do
+describe Census, vcr: true do
   let(:client) { Census.client }
-
-  it "returns valid", :vcr do
+  it "returns valid" do
     census = Census.new(
-      document_number: "123VALID",
+      date_of_birth: Date.civil(1987, 9, 17),
+      postal_code: "08001",
+      document_number: "12345678A",
       document_type: :nie
     )
 
     expect(census).to be_valid
   end
 
-  it "returns invalid", :vcr do
-    census = Census.new(
-      document_number: "12345678X",
-      document_type: :dni
-    )
+  context "when it's invalid" do
+    it "returns invalid on wrong date" do
+      census = Census.new(
+        date_of_birth: Date.civil(1987, 9, 18),
+        postal_code: "08001",
+        document_number: "12345678A",
+        document_type: :dni
+      )
 
-    expect(census).to_not be_valid
+      expect(census).to_not be_valid
+    end
+
+    it "returns invalid on wrong postal code" do
+      census = Census.new(
+        date_of_birth: Date.civil(1987, 9, 17),
+        postal_code: "08002",
+        document_number: "12345678A",
+        document_type: :dni
+      )
+
+      expect(census).to_not be_valid
+    end
+
+    it "returns invalid on wrong dni" do
+      census = Census.new(
+        date_of_birth: Date.civil(1987, 9, 17),
+        postal_code: "0800",
+        document_number: "12345678B",
+        document_type: :dni
+      )
+
+      expect(census).to_not be_valid
+    end
   end
 end
