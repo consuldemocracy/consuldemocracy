8e4fdf6c9f2dfe7671a835c3ae5f241fe17200b2
kikito <kikito@gmail.com>
2016-01-27 16:58:19 +0100

    makes user confirmation require email presence when authenticating with oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,7 +52,7 @@ class User < ActiveRecord::Base
   # Get the existing user by email if the provider gives us a verified email.
   def self.first_or_initialize_for_oauth(auth)
     oauth_email           = auth.info.email
-    oauth_email_confirmed = auth.info.verified || auth.info.verified_email
+    oauth_email_confirmed = oauth_email.present? && (auth.info.verified || auth.info.verified_email)
     oauth_user            = User.find_by(email: oauth_email) if oauth_email_confirmed
 
     oauth_user || User.new(
