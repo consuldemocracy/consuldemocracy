ffee71d5a3ed1970df26e66e0765b36d5f4bffe0
Perisso <perezljljose@gmail.com>
2016-01-28 12:14:20 +0100

    add tests

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -63,6 +63,7 @@ feature 'Users' do
 
       @user.debates.each do |debate|
         expect(page).to have_content(debate.title)
+        expect(page).to have_content(debate.total_votes)
       end
 
       @user.proposals.each do |proposal|
@@ -77,6 +78,7 @@ feature 'Users' do
 
       @user.comments.each do |comment|
         expect(page).to have_content(comment.body)
+        expect(page).to have_content(comment.total_votes)
       end
 
       @user.proposals.each do |proposal|
