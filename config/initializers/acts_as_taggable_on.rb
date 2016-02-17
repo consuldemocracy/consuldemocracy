9409190db1929b418622a7211fe5f2912f5a0210
rgarcia <voodoorai2000@gmail.com>
2016-02-17 20:50:37 +0100

    uses tag ids instead of tag names

--- config/initializers/acts_as_taggable_on.rb
+++ config/initializers/acts_as_taggable_on.rb
@@ -38,6 +38,10 @@ module ActsAsTaggableOn
       Tag.where("kind = 'category'").map {|tag| tag.name.downcase }
     end
 
+    def self.category_ids
+      Tag.where("kind = 'category'").map {|tag| tag.id }
+    end
+
     private
       def custom_counter_field_name_for(taggable_type)
         "#{taggable_type.underscore.pluralize}_count"
