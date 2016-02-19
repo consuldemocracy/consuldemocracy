de8b7cdb7036cd0a423fce537e92645dd2c654dc
rgarcia <voodoorai2000@gmail.com>
2016-02-19 18:45:45 +0100

    uses a more efficient way of getting category names

--- config/initializers/acts_as_taggable_on.rb
+++ config/initializers/acts_as_taggable_on.rb
@@ -35,7 +35,7 @@ module ActsAsTaggableOn
     end
 
     def self.category_names
-      Tag.where("kind = 'category'").map {|tag| tag.name }
+      Tag.where("kind = 'category'").pluck(:name)
     end
 
     private
