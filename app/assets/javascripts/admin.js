91cedd78ab645506492c313fd15490913eea96bb
Juanjo Bazán <jjbazan@gmail.com>
2016-02-24 18:53:08 +0100

    adds new js manifest for admin

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,15 @@
+// This is a manifest file that'll be compiled into admin.js
+// It will be included in the admin layout
+// and will require all the files listed below.
+//
+//= require admin_valuators_forms
+
+var initialize_admin_modules = function() {
+
+};
+
+$(function(){
+  $(document).ready(initialize_admin_modules);
+  $(document).on('page:load', initialize_admin_modules);
+  $(document).on('ajax:complete', initialize_admin_modules);
+});
