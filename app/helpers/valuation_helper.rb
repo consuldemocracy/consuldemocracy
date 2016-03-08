1cbd1f9324489c7cf2016ec7a6a37ca7dbf943dd
Juanjo Bazán <jjbazan@gmail.com>
2016-03-08 13:33:03 +0100

    moves assigned valuators info to helper

--- app/helpers/valuation_helper.rb
+++ app/helpers/valuation_helper.rb
@@ -8,4 +8,19 @@ module ValuationHelper
     end
   end
 
+  def assigned_valuators_info(valuators)
+    case valuators.size
+    when 0
+      t("valuation.spending_proposals.index.no_valuators_assigned")
+    when 1
+      "<span title=\"#{t('valuation.spending_proposals.index.valuators_assigned', count: 1)}\">".html_safe +
+        valuators.first.name +
+      "</span>".html_safe
+    else
+      "<span title=\"".html_safe + valuators.map(&:name).join(', ') + "\">".html_safe +
+        t('valuation.spending_proposals.index.valuators_assigned', count: valuators.size) +
+      "</span>".html_safe
+    end
+  end
+
 end
