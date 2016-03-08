35b296cf0e417f528c037f7e51256d549ab2d0f4
kikito <kikito@gmail.com>
2016-03-08 15:16:05 +0100

    Refactor Valuator#name_and_email

--- app/models/valuator.rb
+++ app/models/valuator.rb
@@ -1,13 +1,9 @@
 class Valuator < ActiveRecord::Base
   belongs_to :user, touch: true
-  delegate :name, :email, to: :user
+  delegate :name, :email, :name_and_email, to: :user
 
   has_many :valuation_assignments, dependent: :destroy
   has_many :spending_proposals, through: :valuation_assignments
 
   validates :user_id, presence: true, uniqueness: true
-
-  def name_and_email
-    "#{name} (#{email})"
-  end
 end
