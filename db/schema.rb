57fba7c0df4f41e27d265be7dafbff58f7a87d44
Juanjo Bazán <jjbazan@gmail.com>
2016-03-15 15:30:19 +0100

    adds description to valuators

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308184050) do
+ActiveRecord::Schema.define(version: 20160315092854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -434,6 +434,7 @@ ActiveRecord::Schema.define(version: 20160308184050) do
 
   create_table "valuators", force: :cascade do |t|
     t.integer "user_id"
+    t.string  "description"
   end
 
   add_index "valuators", ["user_id"], name: "index_valuators_on_user_id", using: :btree
