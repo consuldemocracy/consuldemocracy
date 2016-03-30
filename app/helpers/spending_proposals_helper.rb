2d2d4ebc175cf02307816c3dff2739814c640369
kikito <kikito@gmail.com>
2016-03-30 19:59:31 +0200

    Implements first version of sidebar counter (city done, district pending)

--- app/helpers/spending_proposals_helper.rb
+++ app/helpers/spending_proposals_helper.rb
@@ -3,4 +3,8 @@ module SpendingProposalsHelper
   def spending_proposal_tags_select_options
     ActsAsTaggableOn::Tag.spending_proposal_tags.pluck(:name)
   end
+
+  def can_support_spending_proposals_on_current_district?
+    false
+  end
 end
