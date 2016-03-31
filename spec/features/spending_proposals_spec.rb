832602541ec3adafa1d41c80f77cd38cef3bfe87
Alberto Garcia Cabeza <alberto@decabeza.es>
2016-03-31 23:59:14 +0200

    Removes create spending proposal buttons

--- spec/features/spending_proposals_spec.rb
+++ spec/features/spending_proposals_spec.rb
@@ -18,58 +18,6 @@ feature 'Spending proposals' do
     expect(page).to_not have_content 'Only verified users can create spending proposals, verify your account'
   end
 
-  scenario 'Create' do
-    login_as(author)
-
-    visit spending_proposals_path
-    click_link 'Create spending proposal'
-
-    expect(current_path).to eq(new_spending_proposal_path)
-
-    fill_in 'spending_proposal_title', with: 'Build a skyscraper'
-    fill_in 'spending_proposal_description', with: 'I want to live in a high tower over the clouds'
-    fill_in 'spending_proposal_external_url', with: 'http://http://skyscraperpage.com/'
-    fill_in 'spending_proposal_association_name', with: 'People of the neighbourhood'
-    fill_in 'spending_proposal_captcha', with: correct_captcha_text
-    select  'All city', from: 'spending_proposal_geozone_id'
-    check 'spending_proposal_terms_of_service'
-
-    click_button 'Create'
-
-    expect(page).to have_content 'Investment project created successfully'
-    expect(page).to have_content('Build a skyscraper')
-    expect(page).to have_content('I want to live in a high tower over the clouds')
-    expect(page).to have_content('Isabel')
-    expect(page).to have_content('People of the neighbourhood')
-    expect(page).to have_content('All city')
-  end
-
-  scenario 'Create notice' do
-    login_as(author)
-
-    visit new_spending_proposal_path
-    fill_in 'spending_proposal_title', with: 'Build a skyscraper'
-    fill_in 'spending_proposal_description', with: 'I want to live in a high tower over the clouds'
-    fill_in 'spending_proposal_external_url', with: 'http://http://skyscraperpage.com/'
-    fill_in 'spending_proposal_association_name', with: 'People of the neighbourhood'
-    fill_in 'spending_proposal_captcha', with: correct_captcha_text
-    select  'All city', from: 'spending_proposal_geozone_id'
-    check 'spending_proposal_terms_of_service'
-
-    click_button 'Create'
-
-    expect(page).to have_content 'Investment project created successfully'
-    expect(page).to have_content 'You can access it from My activity'
-
-    within "#notice" do
-      click_link 'My activity'
-    end
-
-    expect(current_url).to eq(user_url(author, filter: :spending_proposals))
-    expect(page).to have_content "1 Spending proposal"
-    expect(page).to have_content "Build a skyscraper"
-  end
-
   scenario 'Captcha is required for proposal creation' do
     login_as(author)
 
