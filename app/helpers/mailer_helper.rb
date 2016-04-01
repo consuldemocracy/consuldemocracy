03b5db6cfe59472a84b4a641c7215d266d72a74f
Juanjo Bazán <jjbazan@gmail.com>
2016-04-01 13:38:10 +0200

    adds mailer url for spending proposals

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -3,6 +3,7 @@ module MailerHelper
   def commentable_url(commentable)
     return debate_url(commentable) if commentable.is_a?(Debate)
     return proposal_url(commentable) if commentable.is_a?(Proposal)
+    return spending_proposal_url(commentable) if commentable.is_a?(SpendingProposal)
   end
 
 end
