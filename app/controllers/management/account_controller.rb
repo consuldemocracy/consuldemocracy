991b189610b2087c6bf5b6c2605cbecba8dac58a
Juanjo Bazán <jjbazan@gmail.com>
2016-04-08 12:16:09 +0200

    refactors triplicated code

--- app/controllers/management/account_controller.rb
+++ app/controllers/management/account_controller.rb
@@ -1,19 +1,13 @@
 class Management::AccountController < Management::BaseController
 
-  before_action :check_verified_user
+  before_action :only_verified_users
 
   def show
   end
 
   private
-    def check_verified_user
-      unless current_user.level_two_or_three_verified?
-        redirect_to management_document_verifications_path, alert: t("management.account.alert.unverified_user")
-      end
-    end
-
-    def current_user
-      managed_user
+    def only_verified_users
+      check_verified_user t("management.account.alert.unverified_user")
     end
 
 end
