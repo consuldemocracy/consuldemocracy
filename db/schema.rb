3b754fd998741b230a9ac5e686178dcf86aaf219
kikito <kikito@gmail.com>
2016-04-11 18:28:28 +0200

    Adds two new fields to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160406163649) do
+ActiveRecord::Schema.define(version: 20160411161531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -422,6 +422,8 @@ ActiveRecord::Schema.define(version: 20160406163649) do
     t.string   "oauth_email"
     t.integer  "geozone_id"
     t.string   "redeemable_code"
+    t.string   "genre",                     limit: 10
+    t.datetime "date_of_birth"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
