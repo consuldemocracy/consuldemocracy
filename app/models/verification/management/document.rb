3a5928e83f6fa23dbb30432dba32be76c695476b
kikito <kikito@gmail.com>
2016-04-13 12:06:43 +0200

    aligns document verification with Madrid

--- app/models/verification/management/document.rb
+++ app/models/verification/management/document.rb
@@ -32,7 +32,7 @@ class Verification::Management::Document
   end
 
   def under_sixteen?(response)
-    16.years.ago < response.date_of_birth
+    16.years.ago.beginning_of_day < response.date_of_birth.beginning_of_day
   end
 
   def verified?
