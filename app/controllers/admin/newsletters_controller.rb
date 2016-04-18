80be0c6b65585a9b14cb2ebda90b614190a9c376
rgarcia <voodoorai2000@gmail.com>
2016-04-18 05:15:16 +0200

    refactors zip file creation

--- app/controllers/admin/newsletters_controller.rb
+++ app/controllers/admin/newsletters_controller.rb
@@ -1,19 +1,12 @@
-require 'zip'
 class Admin::NewslettersController < Admin::BaseController
 
   def index
   end
 
   def users
-    folder = Rails.root + "/tmp/"
-    zipfile_name = folder + "emails.zip"
-
-    Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
-      zipfile.get_output_stream("emails.txt") do |os|
-        os.write User.newsletter.pluck(:email).join("\n")
-      end
-    end
-    send_file(File.join(folder + "emails.zip"), :type => 'application/zip')
+    zip = NewsletterZip.new('emails')
+    zip.create
+    send_file(File.join(zip.path), type: 'application/zip')
   end
 
 end
