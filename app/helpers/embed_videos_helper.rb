42502cecb73906ee04de22ede3f6ec7aecffe834
Alberto Garcia Cabeza <alberto@decabeza.es>
2016-04-19 00:08:12 +0200

    Fixes youtu.be embedded videos

--- app/helpers/embed_videos_helper.rb
+++ app/helpers/embed_videos_helper.rb
@@ -1,30 +1,30 @@
 module EmbedVideosHelper
 
   def embedded_video_code
-    link = @proposal.video_url 
+    link = @proposal.video_url
     if link.match(/vimeo.*/)
       server = "Vimeo"
     elsif link.match(/youtu*.*/)
       server = "YouTube"
-    end  
-    
+    end
+
     if server == "Vimeo"
       regExp = /vimeo.*(staffpicks\/|channels\/|videos\/|video\/|\/)([^#\&\?]*).*/
       src =  "https://player.vimeo.com/video/"
     elsif server == "YouTube"
-      regExp = /youtu.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/
-      src = "https://www.youtube.com/embed/"    
+      regExp = /youtu.*(be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/
+      src = "https://www.youtube.com/embed/"
     end
-    
-    if regExp 
+
+    if regExp
       match  = link.match(regExp)
     end
 
     if match and match[2]
       '<iframe src="' + src + match[2] + '" frameborder="0" allowfullscreen></iframe>'
-    else 
+    else
       ''
     end
-  end 
+  end
 
 end
