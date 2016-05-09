db467fa34d02ee7c190964c3404fb8cac49ac5ee
Juanjo Bazán <jjbazan@gmail.com>
2016-05-09 13:17:04 +0200

    moves adding to ballot model and adds checks

--- app/models/ballot.rb
+++ app/models/ballot.rb
@@ -6,6 +6,12 @@ class Ballot < ActiveRecord::Base
   has_many :city_wide_spending_proposals, -> { city_wide }, through: :ballot_lines, source: :spending_proposal
   has_many :district_wide_spending_proposals, -> { district_wide }, through: :ballot_lines, source: :spending_proposal
 
+  def add(spending_proposal)
+    if valid_spending_proposal?(spending_proposal)
+      ballot_lines.create(spending_proposal: spending_proposal)
+      set_geozone(spending_proposal.geozone_id)
+    end
+  end
 
   def amount_spent
     spending_proposals.sum(:price).to_i
@@ -29,6 +35,21 @@ class Ballot < ActiveRecord::Base
 
   def district_wide_amount_available
     24000000 - district_wide_amount_spent
+  end
+
+  def valid_spending_proposal?(spending_proposal)
+    if spending_proposal.geozone_id.present?
+      return false if geozone_id.present? && spending_proposal.geozone_id != geozone_id
+      return false if district_wide_amount_available < spending_proposal.price.to_i
+    else
+      return false if city_wide_amount_available < spending_proposal.price.to_i
+    end
+    true
+  end
+
+  def set_geozone(new_geozone_id)
+    return if new_geozone_id.blank?
+    self.update(geozone_id: new_geozone_id) unless new_geozone_id == geozone_id
   end
 
 end
