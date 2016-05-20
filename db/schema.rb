ac7e2dfab8f502f4206251a6a23ef26b0d4b0a51
kikito <kikito@gmail.com>
2016-05-20 17:55:51 +0200

    Adds missing budget_id to investments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160520114820) do
+ActiveRecord::Schema.define(version: 20160520151954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,11 +114,13 @@ ActiveRecord::Schema.define(version: 20160520114820) do
     t.datetime "created_at",                                                   null: false
     t.datetime "updated_at",                                                   null: false
     t.integer  "heading_id"
+    t.integer  "budget_id"
   end
 
   add_index "budget_investments", ["administrator_id"], name: "index_budget_investments_on_administrator_id", using: :btree
   add_index "budget_investments", ["author_id"], name: "index_budget_investments_on_author_id", using: :btree
   add_index "budget_investments", ["heading_id"], name: "index_budget_investments_on_heading_id", using: :btree
+  add_index "budget_investments", ["budget_id"], name: "index_budget_investments_on_budget_id", using: :btree
   add_index "budget_investments", ["tsv"], name: "index_budget_investments_on_tsv", using: :gin
 
   create_table "budgets", force: :cascade do |t|
