c48059594b478b16611ccf8c528e41d0c216fdbb
Juanjo Bazán <jjbazan@gmail.com>
2016-05-23 16:35:05 +0200

    removes belongs_to :heading relation from ballot

--- app/models/budget/ballot.rb
+++ app/models/budget/ballot.rb
@@ -2,7 +2,6 @@ class Budget
   class Ballot < ActiveRecord::Base
     belongs_to :user
     belongs_to :budget
-    belongs_to :heading
 
     has_many :lines, dependent: :destroy
     has_many :spending_proposals, through: :lines
