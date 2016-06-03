be6d7a1d39b8fcd8948263dd104325bd1bb97523
kikito <kikito@gmail.com>
2016-06-03 18:50:49 +0200

    Adds views and helpers for investments

--- app/helpers/budget_helper.rb
+++ app/helpers/budget_helper.rb
@@ -5,4 +5,19 @@ module BudgetHelper
                        locale: I18n.default_locale,
                        unit: budget.currency_symbol)
   end
+
+  def heading_name(heading)
+    heading.present? ? heading.name : t("budget.headings.none")
+  end
+
+  def namespaced_budget_investment_path(investment, options={})
+    @namespaced_budget_investment_path ||= namespace
+    options[:budget_id] ||= investment.budget.id
+    case @namespace_budget_investment_path
+    when "management"
+      management_budget_investment_path(investment, options)
+    else
+      budget_investment_path(investment, options)
+    end
+  end
 end
