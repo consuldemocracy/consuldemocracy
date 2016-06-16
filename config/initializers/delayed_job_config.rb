cd868048adac5563e18c9b5600058b1ed08b062a
rgarcia <voodoorai2000@gmail.com>
2016-06-16 17:01:31 +0200

    reduces delayed job sleep delay

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -4,7 +4,7 @@ else
   Delayed::Worker.delay_jobs = true
 end
 Delayed::Worker.destroy_failed_jobs = false
-Delayed::Worker.sleep_delay = 30
+Delayed::Worker.sleep_delay = 10
 Delayed::Worker.max_attempts = 3
 Delayed::Worker.max_run_time = 60.seconds
 Delayed::Worker.read_ahead = 10
