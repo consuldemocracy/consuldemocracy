58fd397ce96746804b4b0ab65701b439307ec135
rgarcia <voodoorai2000@gmail.com>
2016-06-17 12:41:53 +0200

    displays message when forum has not balloted in either city or district

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -2,15 +2,28 @@ class Forum < ActiveRecord::Base
   belongs_to :user
   has_many :represented_users, class_name: User, foreign_key: :representative_id
 
-  def votes_for(geozone_scope)
-    user.votes.for_spending_proposals(SpendingProposal.send("#{geozone_scope}_wide"))
-  end
-
   def has_voted?
     user.votes.for_spending_proposals(SpendingProposal.all).any?
   end
 
   def has_balloted?
-    Ballot.where(user: user).first_or_create.spending_proposals.any?
+    ballot.spending_proposals.any?
   end
+
+  def votes_for(geozone_scope)
+    user.votes.for_spending_proposals(SpendingProposal.send("#{geozone_scope}_wide"))
+  end
+
+  def ballot
+    Ballot.where(user: user).first_or_create
+  end
+
+  def balloted_city_proposals
+    ballot.spending_proposals.city_wide
+  end
+
+  def balloted_district_proposals
+    ballot.spending_proposals.district_wide
+  end
+
 end
