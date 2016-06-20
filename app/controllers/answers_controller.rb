1a989e33157fb5ce0a063a265bfbafe7183a5722
Alberto Garcia Cabeza <alberto@decabeza.es>
2016-06-20 15:31:37 +0200

    Improves styles for human rights pages

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,6 @@
 class AnswersController < ApplicationController
 
-  before_action :authenticate_user!, except: :new
+  before_action :authenticate_user!
   load_and_authorize_resource
 
   def index
