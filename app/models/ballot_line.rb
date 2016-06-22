ff1898cc820ed28c62110e38cbc1f85ab84a96e3
kikito <kikito@gmail.com>
2016-06-22 13:36:57 +0200

    typo

--- app/models/ballot_line.rb
+++ app/models/ballot_line.rb
@@ -5,7 +5,7 @@ class BallotLine < ActiveRecord::Base
   validate :insufficient_funds
   validate :different_geozone, :if => :district_proposal?
   validate :unfeasible
-  validate :spending_proposal_id, uniqueness: { scope: :ballot_id }
+  validates :spending_proposal_id, uniqueness: { scope: :ballot_id }
 
   def insufficient_funds
     errors.add(:money, "") if ballot.amount_available(spending_proposal.geozone) < spending_proposal.price.to_i
