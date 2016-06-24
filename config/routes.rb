1e4e779a23fc7bc821adfc2b3c68f9069a2a0898
Alberto Garcia Cabeza <alberto@decabeza.es>
2016-06-24 13:55:13 +0200

    Adds town planning page

--- config/routes.rb
+++ config/routes.rb
@@ -305,6 +305,7 @@ Rails.application.routes.draw do
   get 'processes/human_rights_question_1', to: 'pages#show', id: 'processes/human_rights_question_1'
   get 'processes/human_rights_question_2', to: 'pages#show', id: 'processes/human_rights_question_2'
   get 'processes/human_rights_question_3', to: 'pages#show', id: 'processes/human_rights_question_3'
+  get 'processes/town_planning', to: 'pages#show', id: 'processes/town_planning'
   get 'noticias', to: 'pages#show', id: 'news'
   get 'participatory_budget/in_two_minutes', to: 'pages#show', id: 'participatory_budget/in_two_minutes'
   get 'presupuestos-participativos-resultados', to: 'spending_proposals#results', as: 'participatory_budget_results'
