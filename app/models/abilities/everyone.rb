c9e0fc156e68a891480efff3f05cbcd09cd0b4e6
Juanjo Bazán <jjbazan@gmail.com>
2016-07-12 16:15:57 +0200

    changes abilities for sps' stats & results

--- app/models/abilities/everyone.rb
+++ app/models/abilities/everyone.rb
@@ -6,7 +6,10 @@ module Abilities
       can [:read, :map], Debate
       can [:read, :map, :summary], Proposal
       can :read, Comment
-      can [:read, :welcome, :select_district, :stats, :results], SpendingProposal
+      can [:read, :welcome, :select_district], SpendingProposal
+      if Setting["feature.spending_proposal_features.open_results_page"].present?
+        can [:stats, :results], SpendingProposal
+      end
       can :read, Legislation
       can :read, User
       can [:search, :read], Annotation
