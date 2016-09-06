65428126479b86926191c1c02a630fcc383da2c4
Juanjo Bazán <jjbazan@gmail.com>
2016-09-06 17:04:31 +0200

    adds simple investment show view to admin valuators info is missing

--- app/controllers/admin/budget_investments_controller.rb
+++ app/controllers/admin/budget_investments_controller.rb
@@ -1,15 +1,21 @@
 class Admin::BudgetInvestmentsController < Admin::BaseController
 
+  before_action :load_budget, only: [:index, :show]
+
   has_filters %w{valuation_open without_admin managed valuating valuation_finished all}, only: :index
 
   def index
-    @budget = Budget.includes(:groups).find params[:budget_id]
-    @investments = @budget.investments.scoped_filter(params, @current_filter).order(cached_votes_up: :desc, created_at: :desc).page(params[:page])
+    @investments = Budget::Investment.scoped_filter(params, @current_filter).order(cached_votes_up: :desc, created_at: :desc).page(params[:page])
   end
 
   def show
-    @budget = Budget.includes(:groups).find params[:budget_id]
-    @investment = @budget.investments.find params[:id]
+    @investment = Budget::Investment.where(budget_id: @budget.id).find params[:id]
   end
+
+  private
+
+    def load_budget
+      @budget = Budget.includes(:groups).find params[:budget_id]
+    end
 
 end
