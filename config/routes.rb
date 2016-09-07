a527de4f9224dc67a6da675a9d980f2dcc5de7b2
Alberto Garcia Cabeza <alberto@decabeza.es>
2016-09-07 12:43:12 +0200

    Adds proposal ballots controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -61,6 +61,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :proposal_ballots, only: [:index]
+
   resources :comments, only: [:create, :show], shallow: true do
     member do
       post :vote
