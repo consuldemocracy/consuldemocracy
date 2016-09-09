15509da4b40a524045ed6cc96af1f419cee6e361
kikito <kikito@gmail.com>
2016-09-09 12:39:15 +0200

    removes deprecated spending_proposal test

--- spec/features/spending_proposals_spec.rb
+++ spec/features/spending_proposals_spec.rb
@@ -186,26 +186,4 @@ feature 'Spending proposals' do
     end
   end
 
-  context "Destroy" do
-
-    scenario "Admin can destroy owned spending proposals" do
-      admin = create(:administrator)
-      user = create(:user, :level_two)
-      spending_proposal = create(:spending_proposal, author: user)
-
-      login_as(admin.user)
-
-      visit user_path(user)
-      within("#spending_proposal_#{spending_proposal.id}") do
-        click_link "Delete"
-      end
-
-      expect(page).to have_content("Spending proposal deleted succesfully.")
-
-      visit user_path(user)
-      expect(page).not_to have_css("spending_proposal_list")
-    end
-
-  end
-
 end
