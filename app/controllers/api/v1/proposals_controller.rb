0fb7d39d88c5240c154756fedbbbe95d5968fd95
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-09-12 13:26:20 +0200

    Simplified proposals controller

--- app/controllers/api/v1/proposals_controller.rb
+++ app/controllers/api/v1/proposals_controller.rb
@@ -1,13 +1,12 @@
 module Api
   module V1
     class ProposalsController < ApplicationController
-
       before_action :authenticate_user!, except: [:index, :show]
-      before_filter :load_by_pagination, :only => :index
 
       load_and_authorize_resource
 
       def index
+        @proposals = @proposals.page(params[:page])
         render :json => @proposals
       end
 
@@ -15,11 +14,6 @@ module Api
         render :json => @proposal
       end
 
-      def load_by_pagination
-        @proposals = Proposal.accessible_by(current_ability).page(params[:page])
-      end
-
     end
-
   end
 end
