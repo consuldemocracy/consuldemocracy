bb94357784de2ef352645da6e037530f96f75cf7
kikito <kikito@gmail.com>
2016-09-14 12:24:20 +0200

    changes test description

--- spec/models/enquiry_spec.rb
+++ spec/models/enquiry_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Enquiry, type: :model do
   describe "#open?" do
-    it "works when it is too early" do
+    it "returns false when it is too early" do
       expect(create(:enquiry, open_at: Date.today + 1.days, closed_at: Date.today + 1.month)).to_not be_open
     end
 
-    it "works with it is too late" do
+    it "return false when it is too late" do
       expect(create(:enquiry, open_at: 1.year.ago, closed_at: 1.month.ago)).to_not be_open
     end
 
-    it "works in the middle" do
+    it "returns true when it is neither too early nor too late" do
       expect(create(:enquiry, open_at: 1.year.ago, closed_at: Date.today + 1.month)).to be_open
     end
   end
