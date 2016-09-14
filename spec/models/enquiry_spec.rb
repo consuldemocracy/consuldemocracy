2316f96b610587618b4125b1b9eeb6c7e9dcfb08
kikito <kikito@gmail.com>
2016-09-14 12:34:03 +0200

    refactors enquiry_spec

--- spec/models/enquiry_spec.rb
+++ spec/models/enquiry_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Enquiry, type: :model do
   describe "#open?" do
     it "returns false when it is too early" do
-      expect(create(:enquiry, open_at: Date.today + 1.days, closed_at: Date.today + 1.month)).to_not be_open
+      expect(create(:enquiry, open_at: 1.day.from_now, closed_at: 1.month.from_now)).to_not be_open
     end
 
     it "return false when it is too late" do
@@ -11,7 +11,7 @@ RSpec.describe Enquiry, type: :model do
     end
 
     it "returns true when it is neither too early nor too late" do
-      expect(create(:enquiry, open_at: 1.year.ago, closed_at: Date.today + 1.month)).to be_open
+      expect(create(:enquiry, open_at: 1.year.ago, closed_at: 1.month.from_now)).to be_open
     end
   end
 end
