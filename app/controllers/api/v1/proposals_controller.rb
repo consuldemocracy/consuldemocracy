f886d6ec548c0e7e6dccdb281cf0af13bb0a8251
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-09-16 10:13:31 +0200

    Rescue errors in controller and return 404

--- app/controllers/api/v1/proposals_controller.rb
+++ app/controllers/api/v1/proposals_controller.rb
@@ -1,15 +1,24 @@
 class Api::V1::ProposalsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
-  load_and_authorize_resource
+  authorize_resource
 
   def index
-    @proposals = @proposals.page(params[:page])
-    render json: @proposals, each_serializer: Api::V1::Proposals::IndexSerializer
+    begin
+      proposals = Proposal.all.page(params[:page])
+      render json: proposals, each_serializer: Api::V1::Proposals::IndexSerializer
+    rescue
+      render json: {errors: [{status: 404, title: "not-found"}]}.to_json, status: 404
+    end
   end
 
   def show
-    # https://github.com/rails-api/active_model_serializers/pull/1156#issuecomment-142320010
-    render json: @proposal, serializer: Api::V1::Proposals::ShowSerializer
+    begin
+      # https://github.com/rails-api/active_model_serializers/pull/1156#issuecomment-142320010
+      proposal = Proposal.find(params[:id])
+      render json: proposal, serializer: Api::V1::Proposals::ShowSerializer
+    rescue
+      render json: {errors: [{status: 404, title: "not-found"}]}.to_json, status: 404
+    end
   end
 end
