8dc7285681495646c2a08c141a42fac3a31afca2
rgarcia <voodoorai2000@gmail.com>
2016-09-19 18:22:35 +0200

    validates user has not already voted

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,2 +1,4 @@
 class Poll < ActiveRecord::Base
+  has_many :booths
+  has_many :voters, through: :booths, class_name: "Poll::Voter"
 end
--- app/models/poll/voter.rb
+++ app/models/poll/voter.rb
@@ -1,15 +1,26 @@
 class Poll
   class Voter < ActiveRecord::Base
     belongs_to :booth
+    delegate :poll, to: :booth
 
     validate :in_census
+    validate :has_not_voted
 
     def in_census
       errors.add(:document_number, :not_in_census) unless census_api_response.valid?
     end
 
+    def has_not_voted
+      errors.add(:document_number, :has_voted) if has_voted?
+    end
+
     def census_api_response
       CensusApi.new.call(document_type, document_number)
     end
+
+    def has_voted?
+      poll.voters.where(document_number: document_number, document_type: document_type).present?
+    end
+
   end
 end
