6c1846e66dfd6b8e0a015eb1dfd606d28405cbb3
kikito <kikito@gmail.com>
2016-09-21 17:53:18 +0200

    Adds filters to enquiries controller

--- app/controllers/enquiries_controller.rb
+++ app/controllers/enquiries_controller.rb
@@ -1,8 +1,10 @@
 class EnquiriesController < ApplicationController
   load_and_authorize_resource
 
+  has_filters %w{opened expired incoming}
+
   def index
-    @enquiries = @enquiries.sort_for_list.for_render
+    @enquiries = @enquiries.send(@current_filter).sort_for_list.for_render
   end
 
   def show
--- spec/features/enquiries_spec.rb
+++ spec/features/enquiries_spec.rb
@@ -12,5 +12,43 @@ feature 'Enquiries' do
 
       expect(proposal_enquiry.title).to appear_before(normal_enquiry.title)
     end
+
+    scenario 'Filtering enquiries' do
+      create(:enquiry, title: "Open Enquiry")
+      create(:enquiry, :incoming, title: "Incoming Enquiry")
+      create(:enquiry, :expired, title: "Expired Enquiry")
+
+      visit enquiries_path
+      expect(page).to have_content('Open Enquiry')
+      expect(page).to_not have_content('Incoming Enquiry')
+      expect(page).to_not have_content('Expired Enquiry')
+
+      visit enquiries_path(filter: 'incoming')
+      expect(page).to_not have_content('Open Enquiry')
+      expect(page).to have_content('Incoming Enquiry')
+      expect(page).to_not have_content('Expired Enquiry')
+
+      visit enquiries_path(filter: 'expired')
+      expect(page).to_not have_content('Open Enquiry')
+      expect(page).to_not have_content('Incoming Enquiry')
+      expect(page).to have_content('Expired Enquiry')
+    end
+
+    scenario "Current filter is properly highlighted" do
+      visit enquiries_path
+      expect(page).to_not have_link('Open')
+      expect(page).to have_link('Incoming')
+      expect(page).to have_link('Expired')
+
+      visit enquiries_path(filter: 'incoming')
+      expect(page).to have_link('Open')
+      expect(page).to_not have_link('Incoming')
+      expect(page).to have_link('Expired')
+
+      visit enquiries_path(filter: 'expired')
+      expect(page).to have_link('Open')
+      expect(page).to have_link('Incoming')
+      expect(page).to_not have_link('Expired')
+    end
   end
 end
