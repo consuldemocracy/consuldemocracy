3a9aa3a64bbe61348d762e872983456c67248058
Juanjo Bazán <jjbazan@gmail.com>
2016-09-21 15:25:27 +0200

    adds presence/uniqueness validation of codename

--- app/models/custom/probe.rb
+++ app/models/custom/probe.rb
@@ -1,6 +1,8 @@
 class Probe < ActiveRecord::Base
   has_many :probe_options, dependent: :destroy
 
+  validates :codename, presence: true, uniqueness: true
+
   def option_voted_by(user)
     options_ids = probe_option_ids
     return nil if options_ids.blank?
