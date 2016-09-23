a648ef50ef1f1c69d87b9c2c8a7c1fd2eb741aff
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-09-23 11:10:43 +0200

    Updated rendered attributes for proposal

--- app/serializers/api/v1/proposal_serializer.rb
+++ app/serializers/api/v1/proposal_serializer.rb
@@ -1,4 +1,6 @@
 class Api::V1::ProposalSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :question, :external_url, :flags_count,
-      :comments_count, :responsible_name, :summary, :video_url, :geozone_id
+      :cached_votes_up, :comments_count, :created_at, :updated_at, :summary,
+      :video_url, :physical_votes, :retired_at, :retired_reason, :retired_explanation,
+      :geozone_id
 end
