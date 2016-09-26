da7d02ec7d00c7cc0153e72a9444c247d3913e01
kikito <kikito@gmail.com>
2016-09-26 12:48:23 +0200

    first version of enquiry::answer model

---
+++ app/models/enquiry/answer.rb
@@ -0,0 +1,10 @@
+class Enquiry::Answer < ActiveRecord::Base
+
+  belongs_to :enquiry, -> { with_hidden }
+  belongs_to :author, -> { with_hidden }, class_name: 'User', foreign_key: 'author_id'
+
+  validates :enquiry, presence: true
+  validates :author, presence: true
+  validates :answer, presence: true
+
+end
