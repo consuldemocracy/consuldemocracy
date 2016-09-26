525e6d1b2577f67f6247b741c915f722461bb511
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-09-26 11:57:02 +0200

    Added id attribute to CommentSerializer

--- app/serializers/api/v1/comment_serializer.rb
+++ app/serializers/api/v1/comment_serializer.rb
@@ -1,5 +1,3 @@
 class Api::V1::CommentSerializer < ActiveModel::Serializer
-  attributes :commentable_id, :commentable_type, :body, :subject, :user_id,
-      :created_at, :updated_at, :flags_count, :cached_votes_total, :cached_votes_up,
-      :cached_votes_down
+  attributes :id, :commentable_id, :commentable_type, :body, :subject, :created_at, :updated_at, :flags_count, :cached_votes_total, :cached_votes_up, :cached_votes_down
 end
