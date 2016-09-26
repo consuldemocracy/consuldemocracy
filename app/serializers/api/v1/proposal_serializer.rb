f28a4232fea84899557759afd7ba1ba10f9492df
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-09-26 11:10:38 +0200

    Deleted geozone_id from ProposalSerializer rendered attributes list

--- app/serializers/api/v1/proposal_serializer.rb
+++ app/serializers/api/v1/proposal_serializer.rb
@@ -1,6 +1,3 @@
 class Api::V1::ProposalSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :question, :external_url, :flags_count,
-      :cached_votes_up, :comments_count, :created_at, :updated_at, :summary,
-      :video_url, :physical_votes, :retired_at, :retired_reason, :retired_explanation,
-      :geozone_id
+  attributes :id, :title, :description, :question, :external_url, :flags_count, :cached_votes_up, :comments_count, :created_at, :updated_at, :summary, :video_url, :physical_votes, :retired_at, :retired_reason, :retired_explanation
 end
