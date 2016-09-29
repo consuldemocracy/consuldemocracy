5c7f03bdf10b9b9ea9ec085e1120a09f79af9190
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-09-29 11:22:18 +0200

    Added more fields to graphql proposal_type

--- app/graph/types/proposal_type.rb
+++ app/graph/types/proposal_type.rb
@@ -5,4 +5,11 @@ ProposalType = GraphQL::ObjectType.define do
   field :id, types.ID, "The id of this proposal"
   field :title, types.String, "The title of this proposal"
   field :description, types.String, "The description of this proposal"
+  field :question, types.String, "The question of this proposal"
+  field :external_url, types.String, "External url related to this proposal"
+  field :flags_count, types.Int, "Number of flags of this proposal"
+  field :cached_votes_up, types.Int, "Number of upvotes of this proposal"
+  field :comments_count, types.Int, "Number of comments on this proposal"
+  field :summary, types.String, "The summary of this proposal"
+  field :video_url, types.String, "External video url related to this proposal"
 end
