eae18d9e376ad71d3e2f5f6ac6cf2a8d6d94b2db
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-09-30 11:34:12 +0200

    Deleted useless debugging comment

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -2,8 +2,6 @@ class GraphqlController < ApplicationController
   authorize_resource :proposal
 
   def query
-    #puts "I'm the GraphqlController inside the #query action!!"
-
     render json: ConsulSchema.execute(
       params[:query],
       variables: params[:variables] || {}
