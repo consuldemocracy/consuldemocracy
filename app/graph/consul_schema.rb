5f407d505955a5d0f9be189cc1f99ca8c5c18308
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-09-30 12:04:03 +0200

    Added schema interface resolver

--- app/graph/consul_schema.rb
+++ app/graph/consul_schema.rb
@@ -1,3 +1,9 @@
 ConsulSchema = GraphQL::Schema.define do
   query QueryRoot
+
+  resolve_type -> (object, ctx) {
+    # look up types by class name
+    type_name = object.class.name
+    ConsulSchema.types[type_name]
+  }
 end
