1fc663894b2e727cb9d34c39a682c4d868286807
Juanjo Bazán <jjbazan@gmail.com>
2016-10-03 12:42:30 +0200

    adds selecting plaza specs

--- spec/features/custom/probes_spec.rb
+++ spec/features/custom/probes_spec.rb
@@ -54,4 +54,60 @@ feature 'Probes' do
     end
   end
 
+  context 'Plaza' do
+    background do
+      @probe = Probe.create(codename: 'plaza')
+      @probe_option_1 = @probe.probe_options.create(code: 'PL1' , name: 'Plaza Option 1')
+      @probe_option_2 = @probe.probe_options.create(code: 'PL2' , name: 'Plaza Option II')
+      @user = create(:user, :level_two)
+      logout
+      login_as(@user)
+    end
+
+    context 'Selecting is allowed' do
+      scenario 'User needs permission to select' do
+        logout
+
+        visit probe_path(id: @probe.codename)
+
+        expect(page).to have_content 'You must Sign in or Sign up to continue'
+        expect(page).to have_content('Plaza Option 1')
+        expect(page).to have_content('Plaza Option II')
+        expect(page).to_not have_css("#probe_option_#{@probe_option_1.id}_form")
+        expect(page).to_not have_css("#probe_option_#{@probe_option_2.id}_form")
+        expect(page).to_not have_content 'Votar'
+
+        login_as(create(:user))
+
+        visit probe_path(id: @probe.codename)
+
+        expect(page).to have_content 'To participate in this process you need to verify your account'
+        expect(page).to have_content('Plaza Option 1')
+        expect(page).to have_content('Plaza Option II')
+        expect(page).to_not have_css("#probe_option_#{@probe_option_1.id}_form")
+        expect(page).to_not have_css("#probe_option_#{@probe_option_2.id}_form")
+        expect(page).to_not have_content 'Votar'
+      end
+
+      scenario 'User selects an option' do
+        visit probe_path(id: @probe.codename)
+
+        within("#probe_option_#{@probe_option_2.id}_form") do
+          click_button "Votar"
+        end
+        expect(page).to have_content "Tu voto ha sido recibido"
+        expect(page).to have_content "Has votado el proyecto: #{@probe_option_2.name}"
+
+        visit probe_path(id: @probe.codename)
+
+        within("#probe_option_#{@probe_option_1.id}_form") do
+          click_button "Votar"
+        end
+
+        expect(page).to have_content "Tu voto ha sido recibido"
+        expect(page).to have_content "Has votado el proyecto: #{@probe_option_1.name}"
+      end
+    end
+
+  end
 end
