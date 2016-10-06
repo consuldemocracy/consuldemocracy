4c121cb9f5d8480bb94fab298e577a82fcc7903f
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-10-06 11:05:47 +0200

    Reject deeply-nested queries

--- app/graph/consul_schema.rb
+++ app/graph/consul_schema.rb
@@ -1,6 +1,9 @@
 ConsulSchema = GraphQL::Schema.define do
   query QueryRoot
 
+  # Reject deeply-nested queries
+  max_depth 7
+
   resolve_type -> (object, ctx) {
     # look up types by class name
     type_name = object.class.name
