3d6742a9fb0a44fa03a70e925d4c940e8dabfc86
rgarcia <voodoorai2000@gmail.com>
2016-10-18 16:22:46 +0200

    adds spec placeholders

---
+++ spec/features/human_rights_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+feature 'Human Rights' do
+
+  context 'Navigation' do
+
+    scenario 'Home' do
+    end
+
+    scenario 'Proposals' do
+    end
+
+    scenario 'Legislataion' do
+    end
+
+  end
+
+  context 'Proposals' do
+
+    scenario 'Index' do
+    end
+
+    scenario 'Vote' do
+    end
+
+    scenario 'Filter' do
+    end
+
+    scenario 'Highlight' do
+    end
+
+  end
+
+  context 'Legislation' do
+
+    scenario 'View' do
+    end
+
+    scenario 'Comment' do
+    end
+
+  end
+
+  context 'Closed' do
+
+    scenario 'Home' do
+    end
+
+    scenario 'proposals' do
+    end
+
+    scenario 'Legislation' do
+    end
+
+  end
+
+end
