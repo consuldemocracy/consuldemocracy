4c336b71b649e493b388a73b7ee123eb6411c22d
rgarcia <voodoorai2000@gmail.com>
2016-10-19 18:37:37 +0200

    Adds filtering by sub_proceeding

--- app/controllers/human_rights_controller.rb
+++ app/controllers/human_rights_controller.rb
@@ -3,14 +3,23 @@ class HumanRightsController < ApplicationController
   include CommentableActions
 
   def index
-    @proposals = Proposal.where(proceeding: "Derechos Humanos").page(params[:page])
+    @proposals = human_right_proposals
+    @proposals = @proposals.where("sub_proceeding = ?", params[:sub_proceeding]) if params[:sub_proceeding].present?
+
     set_resource_votes(@proposals)
+
     @tag_cloud = tag_cloud
-    @categories = @proposals.distinct.pluck(:sub_proceeding)
+    @categories = human_right_proposals.distinct.pluck(:sub_proceeding)
+
+    @proposals = @proposals.page(params[:page])
     render "proposals/index"
   end
 
   private
+
+  def human_right_proposals
+    @human_right_proposals ||= Proposal.where(proceeding: "Derechos Humanos")
+  end
 
   def resource_name
     "proposal"
