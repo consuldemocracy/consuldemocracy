44691a0bd73e30a9657bfbd6b1ca38492d60c3c6
Juanjo Bazán <jjbazan@gmail.com>
2016-10-20 16:14:03 +0200

    adds dates to polls

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928113143) do
+ActiveRecord::Schema.define(version: 20161020112156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -294,7 +294,9 @@ ActiveRecord::Schema.define(version: 20160928113143) do
   end
 
   create_table "polls", force: :cascade do |t|
-    t.string "name"
+    t.string   "name"
+    t.datetime "starts_at"
+    t.datetime "ends_at"
   end
 
   create_table "proposal_notifications", force: :cascade do |t|
