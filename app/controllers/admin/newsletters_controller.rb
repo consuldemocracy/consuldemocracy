a3d28064b7d5392a970707a97b2139d0d07e08c2
rgarcia <voodoorai2000@gmail.com>
2016-10-25 13:52:50 +0200

    uses X-sendfile headers in preproduction

--- app/controllers/admin/newsletters_controller.rb
+++ app/controllers/admin/newsletters_controller.rb
@@ -6,6 +6,7 @@ class Admin::NewslettersController < Admin::BaseController
   def users
     zip = NewsletterZip.new('emails')
     zip.create
+    response.headers["X-Sendfile"] = nil
     send_file(File.join(zip.path), type: 'application/zip')
   end
 
