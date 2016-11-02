cdec6ec1fca484d5faba751dc8d56200b9d01f34
rgarcia <voodoorai2000@gmail.com>
2016-11-02 19:07:50 +0100

    Adds placeholders tracking specs

--- spec/features/tracks_spec.rb
+++ spec/features/tracks_spec.rb
@@ -2,130 +2,35 @@ require 'rails_helper'
 
 feature 'Tracking' do
 
-  context 'Custom variable' do
+  context 'User data' do
 
-     scenario 'Usertype anonymous' do
-      visit proposals_path
-
-      expect(page.html).to include I18n.t("tracking.user_data.user_type.anonymous")
+    context 'Verification level' do
+      scenario 'Level 1'
+      scenario 'Level 2'
+      scenario 'Level 3'
     end
 
-    scenario 'Usertype level_1_user' do
-      user = create(:user)
-      login_as(user)
-
-      visit proposals_path
-
-      expect(page.html).to include I18n.t("tracking.user_data.user_type.level_one")
+    context 'Demographics' do
+      scenario 'Age'
+      scenario 'Gender'
+      scenario 'District'
     end
 
-    scenario 'Usertype level_2_user', :js do
-      create(:geozone)
-      user = create(:user)
-      login_as(user)
-
-      visit account_path
-      click_link 'Verify my account'
-
-      verify_residence
-
-      fill_in 'sms_phone', with: "611111111"
-      click_button 'Send'
-
-      user = user.reload
-      fill_in 'sms_confirmation_code', with: user.sms_confirmation_code
-      click_button 'Send'
-      expect(page.html).to include I18n.t("tracking.user_data.user_type.level_two")
-    end
   end
 
-  context 'Tracking events' do
-    scenario 'Verification: start residence verification', :js do
-      create(:geozone)
-      user = create(:user)
-      login_as(user)
-
-      visit account_path
-
-      click_link 'Verify my account'
-
-      expect(page).to have_css('meta[data-track-event-category="Verification"]', visible: false)
-      expect(page.html).to include 'data-track-event-action="Start residence verification"'
-    end
-
-    scenario 'Verification: success residence verification' do
-      create(:geozone)
-      user = create(:user)
-      login_as(user)
-
-      visit account_path
-      click_link 'Verify my account'
-
-      verify_residence
-
-        expect(page.html).to include 'data-track-event-category="Verification"'
-        expect(page.html).to include 'data-track-event-action="Residence verified"'
-    end
-
-    scenario 'Verification: start phone verification' do
-      create(:geozone)
-      user = create(:user)
-      login_as(user)
-
-      visit account_path
-      click_link 'Verify my account'
-
-      verify_residence
-
-      fill_in 'sms_phone', with: "611111111"
-      click_button 'Send'
-
-      expect(page.html).to include 'data-track-event-category="Verification"'
-      expect(page.html).to include 'data-track-event-action="Start phone verification"'
-    end
-
-    scenario 'Verification: success phone verification' do
-      create(:geozone)
-      user = create(:user)
-      login_as(user)
-
-      visit account_path
-      click_link 'Verify my account'
-
-      verify_residence
-
-      fill_in 'sms_phone', with: "611111111"
-      click_button 'Send'
-
-      user = user.reload
-      fill_in 'sms_confirmation_code', with: user.sms_confirmation_code
-      click_button 'Send'
-
-      expect(page.html).to include 'data-track-event-category="Verification"'
-      expect(page.html).to include 'data-track-event-action="Phone verified"'
-    end
-
-    scenario 'Verification: letter code' do
-      create(:geozone)
-      user = create(:user)
-      login_as(user)
-
-      visit account_path
-      click_link 'Verify my account'
-
-      verify_residence
-
-      fill_in 'sms_phone', with: "611111111"
-      click_button 'Send'
-
-      user = user.reload
-      fill_in 'sms_confirmation_code', with: user.sms_confirmation_code
-      click_button 'Send'
-
-      click_link "Send me a letter with the code"
-
-      expect(page.html).to include 'data-track-event-category="Verification"'
-      expect(page.html).to include 'data-track-event-action="Requested sending letter"'
-    end
+  context 'Events' do
+    scenario 'Login'
+    scenario 'Registration'
+    scenario 'Up vote a debate'
+    scenario 'Down vote a debate'
+    scenario 'Support a proposal'
+    scenario 'Create a proposal'
+    scenario 'Comment a proposal'
+    scenario 'Verify census'
+    scenario 'Verify sms'
+    scenario 'Ask for postal letter'
+    scenario 'Verify postal letter'
+    scenario 'Delete account'
   end
+
 end
