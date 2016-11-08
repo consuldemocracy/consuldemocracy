00da2f32aa58bdcbab6d754c4f89c828fa1075dc
Alberto Garcia Cabeza <alberto@decabeza.es>
2016-11-08 19:46:01 +0100

    adds results table on probe plaza results page

--- config/routes.rb
+++ config/routes.rb
@@ -329,8 +329,9 @@ Rails.application.routes.draw do
   get 'proceso/plaza-espana-gracias', to: 'probes#thanks', id: 'plaza', as: 'plaza_thanks'
   get 'proceso/plaza-espana/proyectos/:id', to: 'probe_options#show', probe_id: 'plaza', as: 'plaza_probe_option'
 
-  get 'proceso/remodelacion-plaza-espana', to: 'pages#show', id: 'processes_plaza_espana',            as: 'remodeling_plaza'
-  get 'proceso/plaza-espana-encuesta',     to: 'pages#show', id: 'encuesta-plaza-espana-resultados',  as: 'survey_plaza'
+  get 'proceso/plaza-espana-preseleccion', to: 'pages#show',   id: 'processes_plaza_espana_stats',      as: 'preselection_plaza'
+  get 'proceso/remodelacion-plaza-espana', to: 'pages#show',   id: 'processes_plaza_espana',            as: 'remodeling_plaza'
+  get 'proceso/plaza-espana-encuesta',     to: 'pages#show',   id: 'encuesta-plaza-espana-resultados',  as: 'survey_plaza'
 
   #Human Rights
   get 'derechos-humanos',                  to: 'pages#show', id: 'processes/human_rights',      as: 'human_rights_page'
