50d01d084c81362ea8b03d98bc046a753c6d1e46
rgarcia <voodoorai2000@gmail.com>
2016-11-11 19:20:37 +0100

    sends ranking value instead of placeholder string

--- app/controllers/concerns/analytics.rb
+++ app/controllers/concerns/analytics.rb
@@ -4,7 +4,6 @@ module Analytics
   def log_event(category, action, name=nil, custom_value=nil, dimension=nil, dimension_value=nil)
     category        = I18n.t("tracking.events.category.#{category}")
     action          = I18n.t("tracking.events.action.#{action}")
-    dimension_value = I18n.t("tracking.events.dimension_value.#{dimension_value}") if dimension_value.present?
 
     session[:event] = {category: category, action: action, name: name.to_s, custom_value: custom_value.to_s, dimension: dimension.to_s, dimension_value: dimension_value.to_s}
   end
