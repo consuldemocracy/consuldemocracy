0c9e6db04d382a14f3598c1264839d797f985e17
kikito <kikito@gmail.com>
2016-11-11 19:10:11 +0100

    Implements answers in a partial

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -9,10 +9,14 @@ class PollsController < ApplicationController
   end
 
   def show
-    questions = @poll.questions.sort_for_list.for_render
+    @answerable_questions     = @poll.questions.answerable_by(current_user).for_render.sort_for_list
+    @non_answerable_questions = @poll.questions.where.not(id: @answerable_questions.map(&:id)).for_render.sort_for_list
 
-    @answerable_questions = questions.answerable_by(current_user)
-    @non_answerable_questions = questions.where.not(id: @answerable_questions.pluck(:id))
+    @answers_by_question_id = {}
+    poll_partial_results = Poll::PartialResult.by_question(@poll.question_ids).by_author(current_user.try(:id))
+    poll_partial_results.each do |result|
+      @answers_by_question_id[result.question_id] = result.answer
+    end
   end
 
 end
