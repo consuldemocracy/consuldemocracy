34071a0f9f4c63df66b26bdd06644fed2b5bfabf
kikito <kikito@gmail.com>
2016-12-06 18:03:03 +0100

    Unifies budget_headings_helper

--- app/helpers/budget_headings_helper.rb
+++ app/helpers/budget_headings_helper.rb
@@ -1,15 +1,9 @@
 module BudgetHeadingsHelper
 
   def budget_heading_select_options(budget)
-    budget.headings.map {|heading| [heading.name, heading.id]}
-  end
-
-  def multiple_budgets_heading_select_options(headings)
-    headings.map { |heading| ["#{heading.budget.name} - #{heading.group.name} - #{heading.name}", heading.id] }
-  end
-
-  def budget_scoped_heading_select_options(budget)
-    budget.headings.includes(:group).order("group_id ASC, budget_headings.name ASC").map {|heading| [heading.group.name + ':  ' + heading.name, heading.id]}
+    budget.headings.includes(:group).order('budget_groups.name', 'budget_headings.name').map do |heading|
+      ["#{heading.group.name}: #{heading.name}", heading.id]
+    end
   end
 
 end
