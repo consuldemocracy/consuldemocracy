05643929646b11ffa6b3394df745668040b43dd4
kikito <kikito@gmail.com>
2016-12-06 18:02:25 +0100

    Introduces management budgets

--- app/controllers/management/budgets_controller.rb
+++ app/controllers/management/budgets_controller.rb
@@ -3,15 +3,16 @@ class Management::BudgetsController < Management::BaseController
   include HasFilters
   feature_flag :budgets
 
-  has_filters %w{open finished}, only: :index
-
-  load_and_authorize_resource
-
-  def index
-    @budgets = @budgets.send(@current_filter).order(created_at: :desc).page(params[:page])
+  def create_investments
+    @budgets = Budget.accepting.order(created_at: :desc).page(params[:page])
   end
 
-  def show
-    @budget = Budget.includes(groups: :headings).find(params[:id])
+  def support_investments
+    @budgets = Budget.accepting.order(created_at: :desc).page(params[:page])
   end
+
+  def print_investments
+    @budgets = Budget.current.order(created_at: :desc).page(params[:page])
+  end
+
 end
