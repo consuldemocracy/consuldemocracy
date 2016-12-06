e6cbc4acf6aeea3bca3bfdb0374d69a01ad54527
kikito <kikito@gmail.com>
2016-12-06 18:13:29 +0100

    Fixes spec

--- app/controllers/management/budgets_controller.rb
+++ app/controllers/management/budgets_controller.rb
@@ -3,6 +3,8 @@ class Management::BudgetsController < Management::BaseController
   include HasFilters
   feature_flag :budgets
 
+  before_action :only_verified_users, except: :print_investments
+
   def create_investments
     @budgets = Budget.accepting.order(created_at: :desc).page(params[:page])
   end
@@ -14,5 +16,11 @@ class Management::BudgetsController < Management::BaseController
   def print_investments
     @budgets = Budget.current.order(created_at: :desc).page(params[:page])
   end
+
+  private
+
+    def only_verified_users
+      check_verified_user t("management.budget_investments.alert.unverified_user")
+    end
 
 end
