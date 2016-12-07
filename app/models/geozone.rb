4b31360d2ec2514ce522cc5707f0209aa6a77fb6
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-12-07 22:04:39 +0100

    Updated geozone model associations

--- app/models/geozone.rb
+++ app/models/geozone.rb
@@ -1,5 +1,8 @@
 class Geozone < ActiveRecord::Base
+  has_many :proposals
   has_many :spending_proposals
+  has_many :debates
+  has_many :users
   validates :name, presence: true
 
   def self.names
