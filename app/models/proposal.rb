2311cd4b1d3c8719cbafbba3d28b9f39e0b2edcc
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-12-26 18:42:12 +0100

    Renamed User::public_activity scope

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -14,7 +14,7 @@ class Proposal < ActiveRecord::Base
   RETIRE_OPTIONS = %w(duplicated started unfeasible done other)
 
   belongs_to :author, -> { with_hidden }, class_name: 'User', foreign_key: 'author_id'
-  belongs_to :public_author, -> { public_activity }, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :public_author, -> { with_public_activity }, class_name: 'User', foreign_key: 'author_id'
   belongs_to :geozone
   has_many :comments, as: :commentable
   has_many :proposal_notifications
