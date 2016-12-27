15b072a983678e55857dd3b90f0ea7c14d6ad7f7
Juanjo Bazán <jjbazan@gmail.com>
2016-12-27 17:43:44 +0100

    adds date to poll_officer_assignments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207181001) do
+ActiveRecord::Schema.define(version: 20161227140109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -295,6 +295,7 @@ ActiveRecord::Schema.define(version: 20161207181001) do
     t.integer  "officer_id"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.datetime "date"
   end
 
   create_table "poll_officers", force: :cascade do |t|
