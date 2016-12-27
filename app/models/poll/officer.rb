a0118cb163108eb2de525561af8ffd2f54623c3a
Juanjo Bazán <jjbazan@gmail.com>
2016-12-27 17:45:01 +0100

    associates officer_assignments to officer

--- app/models/poll/officer.rb
+++ app/models/poll/officer.rb
@@ -1,8 +1,10 @@
 class Poll
   class Officer < ActiveRecord::Base
     belongs_to :user
-    delegate :name, :email, to: :user
+    has_many :officer_assignments, class_name: "Poll::OfficerAssignment"
 
     validates :user_id, presence: true, uniqueness: true
+
+    delegate :name, :email, to: :user
   end
 end
