ca2370eb7c10cda90754760b63bd7e5ef54a565e
Fernando Blat <ferblape@gmail.com>
2016-12-29 10:20:52 +0100

    Manage offices url as a setting

--- db/dev_seeds.rb
+++ db/dev_seeds.rb
@@ -35,7 +35,7 @@ Setting.create(key: 'per_page_code', value: "")
 Setting.create(key: 'comments_body_max_length', value: '1000')
 Setting.create(key: 'mailer_from_name', value: 'Consul')
 Setting.create(key: 'mailer_from_address', value: 'noreply@consul.dev')
-
+Setting.create(key: 'verification_offices_url', value: 'http://oficinas-atencion-ciudadano.url/')
 
 puts "Creating Geozones"
 ('A'..'Z').each { |i| Geozone.create(name: "District #{i}", external_code: i.ord, census_code: i.ord) }
