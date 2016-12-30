181749b4f65a7891eed8a5bb828bf4a0a7dee2c5
kikito <kikito@gmail.com>
2016-12-30 18:41:35 +0100

    Removes valuating boolean from budgets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161229153505) do
+ActiveRecord::Schema.define(version: 20161230172816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -164,7 +164,6 @@ ActiveRecord::Schema.define(version: 20161229153505) do
     t.text     "description"
     t.string   "currency_symbol", limit: 10
     t.string   "phase",           limit: 15, default: "on_hold"
-    t.boolean  "valuating",                  default: false
     t.datetime "created_at",                                     null: false
     t.datetime "updated_at",                                     null: false
   end
