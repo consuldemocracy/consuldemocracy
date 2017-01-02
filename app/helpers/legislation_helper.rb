d29a9fe761979eee4ee45c9bc47892762707740d
Amaia Castro <amaia@amaiac.net>
2017-01-02 13:22:33 +0100

    Match js calendar date format in admin process form text fields

--- app/helpers/legislation_helper.rb
+++ app/helpers/legislation_helper.rb
@@ -3,6 +3,10 @@ module LegislationHelper
     l(date, format: "%d %b %Y") if date
   end
 
+  def format_date_for_calendar_form(date)
+    l(date, format: "%d/%m/%Y") if date
+  end
+
   def legislation_question_path(question)
     legislation_process_question_path(question.process, question)
   end
