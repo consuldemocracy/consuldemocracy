9cfb3eb52e246f0b62cb147ab89d2d78cea400b5
Alberto Miedes Garcés <albertomg994@gmail.com>
2017-01-03 18:30:53 +0100

    Write specs for GraphQL::AssociationResolver

--- lib/graph_ql/association_resolver.rb
+++ lib/graph_ql/association_resolver.rb
@@ -13,23 +13,24 @@ module GraphQL
       filter_forbidden_elements(requested_elements)
     end
 
-    def target_public_elements
-      target_model.respond_to?(:public_for_api) ? target_model.public_for_api : target_model.all
-    end
-
-    def filter_forbidden_elements(requested_elements)
-      if AssociationResolver.matching_exceptions.include?(field_name)
-        requested_elements
-      elsif requested_elements.respond_to?(:each)
-        requested_elements.all & allowed_elements.all
-      else
-        allowed_elements.include?(requested_elements) ? requested_elements : nil
-      end
-    end
-
     def self.matching_exceptions
       [:public_voter]
     end
 
+    private
+
+      def target_public_elements
+        target_model.respond_to?(:public_for_api) ? target_model.public_for_api : target_model.all
+      end
+
+      def filter_forbidden_elements(requested_elements)
+        if AssociationResolver.matching_exceptions.include?(field_name)
+          requested_elements
+        elsif requested_elements.respond_to?(:each)
+          requested_elements.all & allowed_elements.all
+        else
+          allowed_elements.include?(requested_elements) ? requested_elements : nil
+        end
+      end
   end
 end
