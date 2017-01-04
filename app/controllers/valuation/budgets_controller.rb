9c66fa0dc8f17ff269d644855d8fda8e8d7d447a
kikito <kikito@gmail.com>
2017-01-04 16:31:16 +0100

    removes filter from valuation index page

--- app/controllers/valuation/budgets_controller.rb
+++ app/controllers/valuation/budgets_controller.rb
@@ -2,12 +2,10 @@ class Valuation::BudgetsController < Valuation::BaseController
   include FeatureFlags
   feature_flag :budgets
 
-  has_filters %w{current finished}, only: :index
-
   load_and_authorize_resource
 
   def index
-    @budgets = Budget.send(@current_filter).order(created_at: :desc).page(params[:page])
+    @budgets = @budgets.current.order(created_at: :desc).page(params[:page])
   end
 
 end
