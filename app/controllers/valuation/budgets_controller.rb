635de83f866eda6d97604764fef4c004c9e7d19c
kikito <kikito@gmail.com>
2017-01-04 18:48:01 +0100

    prints the right value in budgets table

--- app/controllers/valuation/budgets_controller.rb
+++ app/controllers/valuation/budgets_controller.rb
@@ -6,6 +6,12 @@ class Valuation::BudgetsController < Valuation::BaseController
 
   def index
     @budgets = @budgets.current.order(created_at: :desc).page(params[:page])
+    @investments_with_valuation_open = {}
+    @budgets.each do |b|
+      @investments_with_valuation_open[b.id] = b.investments
+                                                .by_valuator(current_user.valuator.try(:id))
+                                                .valuation_open
+                                                .count
+    end
   end
-
 end
