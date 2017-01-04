f5dcb51d37bcbf0df40424f947b0b7185f58ec5a
kikito <kikito@gmail.com>
2017-01-04 13:56:42 +0100

    Adds valid_heading validation to budget line. Renames validation methods.

--- spec/models/budget/ballot_spec.rb
+++ spec/models/budget/ballot_spec.rb
@@ -23,29 +23,35 @@ describe Budget::Ballot do
     end
 
     it "returns the amount spent on all investments assigned to a specific heading" do
-      heading = create(:budget_heading)
-      budget = heading.group.budget
-      inv1 = create(:budget_investment, :selected, price: 10000, heading: heading)
-      inv2 = create(:budget_investment, :selected, price: 20000, heading: create(:budget_heading, group: heading.group))
-      inv3 = create(:budget_investment, :selected, price: 40000, heading: heading)
+      budget = create(:budget)
+      group1 = create(:budget_group, budget: budget)
+      group2 = create(:budget_group, budget: budget)
+      heading1 = create(:budget_heading, group: group1, price: 100000)
+      heading2 = create(:budget_heading, group: group2, price: 200000)
+      inv1 = create(:budget_investment, :selected, price: 10000, heading: heading1)
+      inv2 = create(:budget_investment, :selected, price: 20000, heading: heading2)
+      inv3 = create(:budget_investment, :selected, price: 40000, heading: heading1)
 
       ballot = create(:budget_ballot, budget: budget)
       ballot.investments << inv1 << inv2
 
-      expect(ballot.amount_spent(heading)).to eq 10000
+      expect(ballot.amount_spent(heading1)).to eq 10000
+      expect(ballot.amount_spent(heading2)).to eq 20000
 
       ballot.investments << inv3
 
-      expect(ballot.amount_spent(heading)).to eq 50000
+      expect(ballot.amount_spent(heading1)).to eq 50000
+      expect(ballot.amount_spent(heading2)).to eq 20000
     end
   end
 
   describe "#amount_available" do
     it "returns how much is left after taking some investments" do
       budget = create(:budget)
-      group = create(:budget_group, budget: budget)
-      heading1 = create(:budget_heading, group: group, price: 1000)
-      heading2 = create(:budget_heading, group: group, price: 300)
+      group1 = create(:budget_group, budget: budget)
+      group2 = create(:budget_group, budget: budget)
+      heading1 = create(:budget_heading, group: group1, price: 1000)
+      heading2 = create(:budget_heading, group: group2, price: 300)
       inv1 = create(:budget_investment, :selected, price: 100, heading: heading1)
       inv2 = create(:budget_investment, :selected, price: 200, heading: heading2)
       inv3 = create(:budget_investment, :selected, price: 400, heading: heading1)
