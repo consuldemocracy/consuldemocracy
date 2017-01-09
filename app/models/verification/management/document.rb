e2118f701b250d695451c7403d50a4161510393e
Juanjo Bazán <jjbazan@gmail.com>
2017-01-09 16:05:27 +0100

    removes hardcoded age, favours setting

--- app/models/verification/management/document.rb
+++ app/models/verification/management/document.rb
@@ -23,7 +23,7 @@ class Verification::Management::Document
   end
 
   def valid_age?(response)
-    if under_sixteen?(response)
+    if under_age?(response)
       errors.add(:age, true)
       return false
     else
@@ -31,8 +31,8 @@ class Verification::Management::Document
     end
   end
 
-  def under_sixteen?(response)
-    16.years.ago.beginning_of_day < response.date_of_birth.beginning_of_day
+  def under_age?(response)
+    User.minimum_required_age.years.ago.beginning_of_day < response.date_of_birth.beginning_of_day
   end
 
   def verified?
