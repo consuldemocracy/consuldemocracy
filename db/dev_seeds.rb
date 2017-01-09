be0cbb9ea1dd01c616d5812a6386687610bacb8e
Juanjo Bazán <jjbazan@gmail.com>
2017-01-09 16:05:33 +0100

    adds setting to dev_seeds values

--- db/dev_seeds.rb
+++ db/dev_seeds.rb
@@ -39,6 +39,7 @@ Setting.create(key: 'mailer_from_address', value: 'noreply@consul.dev')
 Setting.create(key: 'meta_description', value: 'Citizen Participation and Open Government Application')
 Setting.create(key: 'meta_keywords', value: 'citizen participation, open government')
 Setting.create(key: 'verification_offices_url', value: 'http://oficinas-atencion-ciudadano.url/')
+Setting.create(key: 'min_age_to_participate', value: '16')
 
 puts "Creating Geozones"
 ('A'..'Z').each { |i| Geozone.create(name: "District #{i}", external_code: i.ord, census_code: i.ord) }
