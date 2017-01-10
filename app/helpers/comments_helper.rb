3466d628806fbd56b3277c0c62bd5b3aedc7e582
Juanjo Bazán <jjbazan@gmail.com>
2017-01-10 22:27:44 +0100

    removes Commentable concern

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -24,6 +24,8 @@ module CommentsHelper
   def commentable_path(comment)
     if comment.commentable_type == "Budget::Investment"
       budget_investment_path(comment.commentable.budget_id, comment.commentable)
+    elsif comment.commentable_type == "ProbeOption"
+      probe_probe_option_path(probe_id: comment.commentable.probe.codename, id: comment.commentable.id)
     else
       comment.commentable
     end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -17,10 +17,10 @@ module UsersHelper
       deleted_commentable_text(comment)
     elsif commentable.hidden?
       "<abbr title='#{deleted_commentable_text(comment)}'>".html_safe +
-      comment.commentable_title +
+      commentable.title +
       "</abbr>".html_safe
     else
-      link_to(comment.commentable_title, comment)
+      link_to(commentable.title, comment)
     end
   end
 
