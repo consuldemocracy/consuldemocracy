4a5276f0898fbb2ec449b1ccd00f6ac77ec0fb3a
Amaia Castro <amaia@amaiac.net>
2017-01-11 15:34:41 +0100

    Revert change in admin namespace helper and redefine it in the controller

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -5,7 +5,7 @@ module AdminHelper
   end
 
   def official_level_options
-    options = [["",0]]
+    options = [["", 0]]
     (1..5).each do |i|
       options << [[t("admin.officials.level_#{i}"), setting["official_level_#{i}_name"]].compact.join(': '), i]
     end
@@ -23,7 +23,7 @@ module AdminHelper
   private
 
     def namespace
-      controller.class.parent.name.downcase.gsub("::", "/")
+      controller.class.parent.name.downcase
     end
 
 end
