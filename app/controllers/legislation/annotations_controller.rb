f1be212eeb052b5431c6abab53d4d4b9a1a3ead9
Fernando Blat <ferblape@gmail.com>
2017-01-14 16:56:55 +0100

    Fix and complete controller specs

--- app/controllers/legislation/annotations_controller.rb
+++ app/controllers/legislation/annotations_controller.rb
@@ -66,7 +66,7 @@ class Legislation::AnnotationsController < ApplicationController
     end
 
     def convert_ranges_parameters
-      if params[:legislation_annotation] && params[:legislation_annotation][:ranges]
+      if params[:legislation_annotation] && params[:legislation_annotation][:ranges] && params[:legislation_annotation][:ranges].is_a?(String)
         params[:legislation_annotation][:ranges] = JSON.parse(params[:legislation_annotation][:ranges])
       end
     rescue JSON::ParserError
