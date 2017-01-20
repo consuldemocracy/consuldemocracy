d53c867ba43b16f2bec09a86665d758b6c95a267
kikito <kikito@gmail.com>
2017-01-20 17:45:11 +0100

    Adds geozone_restricted to polls

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170120153244) do
+ActiveRecord::Schema.define(version: 20170120161058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -459,7 +459,8 @@ ActiveRecord::Schema.define(version: 20170120153244) do
     t.string   "name"
     t.datetime "starts_at"
     t.datetime "ends_at"
-    t.boolean  "published", default: false
+    t.boolean  "published",          default: false
+    t.boolean  "geozone_restricted", default: false
   end
 
   create_table "proposal_notifications", force: :cascade do |t|
