0f71c195dc33cfd094109d431ad0c90621bcd7f8
rgarcia <voodoorai2000@gmail.com>
2017-01-24 14:20:26 +0100

    fixes specs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   skip_authorization_check
 
   def show
-    @proposal_successfull_exists = Proposal.successfull.exists?
+    @proposal_successfull_exists = Proposal.successful.exists?
     render action: params[:id]
   rescue ActionView::MissingTemplate
     head 404
