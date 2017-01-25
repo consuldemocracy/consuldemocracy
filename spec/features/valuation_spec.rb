7d0f71486174b85802943f3cc7f2811f211fae0c
rgarcia <voodoorai2000@gmail.com>
2017-01-25 18:37:47 +0100

    fixes specs

--- spec/features/valuation_spec.rb
+++ spec/features/valuation_spec.rb
@@ -55,7 +55,7 @@ feature 'Valuation' do
     visit valuation_root_path
 
     expect(current_path).not_to eq(valuation_root_path)
-    expect(current_path).to eq(proposals_path)
+    expect(current_path).to eq(root_path_for_logged_in_users)
     expect(page).to have_content "You do not have permission to access this page"
   end
 
