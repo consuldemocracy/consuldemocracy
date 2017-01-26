fb07ca1df73db222362ba41a2388810d1dc63f1a
Hugo BARTHELEMY <barthelemyhugo@gmail.com>
2017-01-26 19:01:55 +0100

    change default language

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,8 @@ module Consul
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :es
-    config.i18n.available_locales = [:en, :es, :fr, 'pt-BR']
+    config.i18n.default_locale = :fr
+    config.i18n.available_locales = [:fr, :en, :es, 'pt-BR']
     config.i18n.fallbacks = {'fr' => 'es', 'pt-br' => 'es'}
 
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
