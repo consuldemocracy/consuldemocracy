77ec2856f5fd09feb8bada0177c002c9d48a8778
rgarcia <voodoorai2000@gmail.com>
2017-01-27 09:58:26 +0100

    uses nvote instead of vote

--- app/controllers/nvote_controller.rb
+++ app/controllers/nvote_controller.rb
@@ -8,9 +8,9 @@ class NvoteController < ApplicationController
 
   def token
     poll = Poll.find(params[:poll_id])
-    vote = current_user.get_or_create_nvote(poll)
-    message = vote.generate_message
-    render content_type: 'text/plain', status: :ok, text: "#{vote.generate_hash message}/#{message}"
+    nvote = current_user.get_or_create_nvote(poll)
+    message = nvote.generate_message
+    render content_type: 'text/plain', status: :ok, text: "#{nvote.generate_hash message}/#{message}"
   end
 
   #Agora Callback
