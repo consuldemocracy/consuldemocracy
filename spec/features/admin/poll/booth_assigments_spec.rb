e634e801139e2df7e9a010d1f8e4c884713e4d0f
Juanjo Bazán <jjbazan@gmail.com>
2017-01-27 15:19:03 +0100

    adds system recounting to admin booth_assignments

--- spec/features/admin/poll/booth_assigments_spec.rb
+++ spec/features/admin/poll/booth_assigments_spec.rb
@@ -92,10 +92,22 @@ feature 'Admin booths assignments' do
       poll = create(:poll)
       booth = create(:poll_booth)
       booth_assignment = create(:poll_booth_assignment, poll: poll, booth: booth)
-      recount = create(:poll_recount, booth_assignment: booth_assignment, count: 33)
+      officer_assignment_1 = create(:poll_officer_assignment, booth_assignment: booth_assignment, date: poll.starts_at)
+      officer_assignment_2 = create(:poll_officer_assignment, booth_assignment: booth_assignment, date: poll.ends_at)
+
+      recount_1 = create(:poll_recount,
+                         booth_assignment: booth_assignment,
+                         officer_assignment: officer_assignment_1,
+                         date: officer_assignment_1.date,
+                         count: 33)
+      recount_2 = create(:poll_recount,
+                         booth_assignment: booth_assignment,
+                         officer_assignment: officer_assignment_2,
+                         date: officer_assignment_2.date,
+                         count: 1)
 
       booth_assignment_2 = create(:poll_booth_assignment, poll: poll)
-      recount_2 = create(:poll_recount, booth_assignment: booth_assignment_2, count: 100)
+      other_recount = create(:poll_recount, booth_assignment: booth_assignment_2, count: 100)
 
       visit admin_poll_path(poll)
       click_link 'Booths (2)'
@@ -104,9 +116,56 @@ feature 'Admin booths assignments' do
 
       click_link 'Recounts'
       within('#recounts_list') do
-        expect(page).to have_content recount.count
-        expect(page).to_not have_content recount_2.count
+        expect(page).to_not have_content other_recount.count
+
+        within("#recount_#{recount_1.id}") do
+          expect(page).to have_content recount_1.count
+        end
+
+        within("#recount_#{recount_2.id}") do
+          expect(page).to have_content recount_2.count
+        end
+
       end
     end
+
+    scenario 'Marks recount values with count-errors' do
+      poll = create(:poll)
+      booth = create(:poll_booth)
+      booth_assignment = create(:poll_booth_assignment, poll: poll, booth: booth)
+      today = Time.current.to_date
+      officer_assignment = create(:poll_officer_assignment, booth_assignment: booth_assignment, date: today)
+
+      recount = create(:poll_recount,
+                         booth_assignment: booth_assignment,
+                         officer_assignment: officer_assignment,
+                         date: officer_assignment.date,
+                         count: 1)
+
+      visit admin_poll_booth_assignment_path(poll, booth_assignment)
+      click_link 'Recounts'
+
+      within('#recounts_list') do
+        expect(page).to have_css("#recount_#{recount.id}.count-error")
+        within("#recount_#{recount.id}") do
+          expect(page).to have_content recount.count
+          expect(page).to have_content 0
+        end
+      end
+
+      create(:poll_voter, :valid_document, poll: poll, booth_assignment: booth_assignment)
+
+      visit admin_poll_booth_assignment_path(poll, booth_assignment)
+      click_link 'Recounts'
+
+      within('#recounts_list') do
+        expect(page).to_not have_css('.count-error')
+        within("#recount_#{recount.id}") do
+          expect(page).to have_content(recount.count)
+        end
+      end
+    end
+
+
   end
 end
