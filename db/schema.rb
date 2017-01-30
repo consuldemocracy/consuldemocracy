4c54d9da554fb7961231f3d7e2cdb8b632be5a07
kikito <kikito@gmail.com>
2017-01-30 18:30:53 +0100

    Removes summary from poll_questions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170130163030) do
+ActiveRecord::Schema.define(version: 20170130171322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -443,7 +443,6 @@ ActiveRecord::Schema.define(version: 20170130163030) do
     t.integer  "author_id"
     t.string   "author_visible_name"
     t.string   "title"
-    t.string   "summary"
     t.string   "valid_answers"
     t.text     "description"
     t.integer  "comments_count"
