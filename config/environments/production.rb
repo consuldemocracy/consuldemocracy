e05951742c647fa25af67b33117e0f2b6017172f
Fernando Faubell Valls <ffaubell@fadrell.com>
2017-02-01 12:02:23 +0100

    Correcciones en proceso de registro.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: Rails.application.secrets.server_name }
+  config.action_mailer.default_url_options = { host: Rails.application.secrets.server_name, protocol: 'https' }
   config.action_mailer.asset_host = "https://#{Rails.application.secrets.server_name}"
 
   # config.action_mailer.delivery_method = :smtp
