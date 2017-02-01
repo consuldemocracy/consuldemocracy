cf41a9540987a5eb777755ce40696a470680d0d7
Fernando Faubell Valls <ffaubell@fadrell.com>
2017-02-01 15:11:15 +0100

    Corrección errores en la verificación de residencia.

--- app/models/custom/verification/residence.rb
+++ app/models/custom/verification/residence.rb
@@ -3,22 +3,22 @@ require_dependency Rails.root.join('app', 'models', 'verification', 'residence')
 
 class Verification::Residence
 
-  validate :postal_code_in_castellon
-  validate :residence_in_castellon
+  # validate :postal_code_in_castellon
+  # validate :residence_in_castellon
 
-  def postal_code_in_castellon
-    errors.add(:postal_code, I18n.t('verification.residence.new.error_not_allowed_postal_code')) unless valid_postal_code?
-  end
+  # def postal_code_in_castellon
+  #   errors.add(:postal_code, I18n.t('verification.residence.new.error_not_allowed_postal_code')) unless valid_postal_code?
+  # end
 
-  def residence_in_castellon
-    return if errors.any?
-
-    unless residency_valid?
-      errors.add(:residence_in_castellon, false)
-      store_failed_attempt
-      Lock.increase_tries(user)
-    end
-  end
+  # def residence_in_castellon
+  #   return if errors.any?
+  #
+  #   unless residency_valid?
+  #     errors.add(:residence_in_castellon, false)
+  #     store_failed_attempt
+  #     Lock.increase_tries(user)
+  #   end
+  # end
 
   def save
     return false unless valid?
