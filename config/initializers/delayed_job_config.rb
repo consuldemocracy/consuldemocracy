9b8e680d5394636d46a9ac458d4ec39bf35fa9c7
Fernando Faubell Valls <ffaubell@fadrell.com>
2017-02-02 09:54:49 +0100

    Reducir la periodicidad de delayed job, reducimos la información de los logs.

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -4,7 +4,7 @@ else
   Delayed::Worker.delay_jobs = true
 end
 Delayed::Worker.destroy_failed_jobs = false
-Delayed::Worker.sleep_delay = 2
+Delayed::Worker.sleep_delay = 10
 Delayed::Worker.max_attempts = 3
 Delayed::Worker.max_run_time = 30.minutes
 Delayed::Worker.read_ahead = 10
