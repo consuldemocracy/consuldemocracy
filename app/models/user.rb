35078848c39569390be0b3e23915863133984256
Juanjo Bazán <jjbazan@gmail.com>
2017-02-09 20:09:06 +0100

    adds methods to take votes from other user (generic & erased)

--- app/models/user.rb
+++ app/models/user.rb
@@ -194,6 +194,22 @@ class User < ActiveRecord::Base
     erased_at.present?
   end
 
+  def take_votes_if_erased_document(document_number, document_type)
+    erased_user = User.erased.where(document_number: document_number).where(document_type: document_type).first
+    if erased_user.present?
+      self.take_votes_from(erased_user)
+      erased_user.update(document_number: nil, document_type: nil)
+    end
+  end
+
+  def take_votes_from(other_user)
+    return if other_user.blank?
+    Poll::Voter.where(user_id: other_user.id).update_all(user_id: self.id)
+    Budget::Ballot.where(user_id: other_user.id).update_all(user_id: self.id)
+    Vote.where("voter_id = ? AND voter_type = ?", other_user.id, "User").update_all(voter_id: self.id)
+    self.update(former_users_data_log: "#{self.former_users_data_log} | id: #{other_user.id} - #{Time.current.strftime('%Y-%m-%d %H:%M:%S')}")
+  end
+
   def locked?
     Lock.find_or_create_by(user: self).locked?
   end
