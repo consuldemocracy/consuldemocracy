d327dceb08135afbedea261fb9c8918ec737088a
kikito <kikito@gmail.com>
2017-02-09 20:05:28 +0100

    Makes nvote model update traceability fields in voter

--- app/models/poll/nvote.rb
+++ app/models/poll/nvote.rb
@@ -5,6 +5,8 @@ class Poll
 
   belongs_to :user
   belongs_to :poll
+  belongs_to :officer_assignment
+  belongs_to :booth_assignment
 
   validates :user_id, :poll_id, :voter_hash, presence: true
   validates :voter_hash, uniqueness: {scope: :user_id}
@@ -41,7 +43,10 @@ class Poll
     nvote, poll = parse_authorization(message)
 
     if nvote && poll
-      Poll::Voter.create!(user: nvote.user, poll: poll)
+      Poll::Voter.create!(user: nvote.user,
+                          poll: poll,
+                          officer_assignment: nvote.officer_assignment,
+                          booth_assignment: nvote.booth_assignment)
     end
   end
 
