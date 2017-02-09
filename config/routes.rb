48563585a0ed89286e6e161bc01b15f922f08cd4
Hugo BARTHELEMY <barthelemyhugo@gmail.com>
2017-02-10 00:15:04 +0100

    better translating

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
   end
 
   # root 'welcome#index'
-  root 'budgets#index'
+  root 'budgets#index/1'
   get '/welcome', to: 'welcome#welcome'
   get '/cuentasegura', to: 'welcome#verification', as: :cuentasegura
 
