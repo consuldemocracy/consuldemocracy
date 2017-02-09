ba051242def2eeb0c6df7fa2c4489136f6fc87a0
Fernando Faubell Valls <ffaubell@fadrell.com>
2017-02-09 16:48:49 +0100

    Revisión de verificaciones incompletas.

--- app/controllers/budgets/investments_controller.rb
+++ app/controllers/budgets/investments_controller.rb
@@ -25,7 +25,9 @@ module Budgets
     respond_to :html, :js
 
     def index
-      @investments = @investments.apply_filters_and_search(@budget, params).send("sort_by_#{@current_order}").page(params[:page]).per(10).for_render
+      @investments = @investments
+                     .includes([{ author: :organization }, :tags])
+                     .apply_filters_and_search(@budget, params).send("sort_by_#{@current_order}").page(params[:page]).per(10).for_render
       @investment_ids = @investments.pluck(:id)
       load_investment_votes(@investments)
       @tag_cloud = tag_cloud
