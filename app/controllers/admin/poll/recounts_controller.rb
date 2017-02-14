d68be1f6f007a39ddb3866b28906274c0b85111a
Juanjo Bazán <jjbazan@gmail.com>
2017-02-14 19:44:19 +0100

    adds automatic recount to admin poll and paginates recounts

--- app/controllers/admin/poll/recounts_controller.rb
+++ app/controllers/admin/poll/recounts_controller.rb
@@ -2,7 +2,10 @@ class Admin::Poll::RecountsController < Admin::BaseController
   before_action :load_poll
 
   def index
-    @booth_assignments = @poll.booth_assignments.includes(:recounts, :final_recounts)
+    @booth_assignments = @poll.booth_assignments.
+                              includes(:booth, :recounts, :final_recounts, :voters).
+                              order(created_at: :desc).
+                              page(params[:page]).per(50)
   end
 
   private
