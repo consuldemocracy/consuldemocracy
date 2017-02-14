2b68f02b955f4dfded18eabeef02f343526ec436
Juanjo Bazán <jjbazan@gmail.com>
2017-02-14 19:09:31 +0100

    paginates booths

--- app/controllers/admin/poll/booth_assignments_controller.rb
+++ app/controllers/admin/poll/booth_assignments_controller.rb
@@ -3,7 +3,7 @@ class Admin::Poll::BoothAssignmentsController < Admin::BaseController
   before_action :load_poll, except: [:create, :destroy]
 
   def index
-    @booth_assignments = @poll.booth_assignments.includes(:booth)
+    @booth_assignments = @poll.booth_assignments.includes(:booth).order('poll_booths.name').page(params[:page]).per(50)
   end
 
   def search_booths
