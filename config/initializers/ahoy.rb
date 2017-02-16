8d7b81308e90a75b9e4ab42ac9c7bd025e648586
Fernando Faubell Valls <ffaubell@fadrell.com>
2017-02-16 09:18:31 +0100

    Pruebas de registro de visitas.

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -5,6 +5,7 @@ class Ahoy::Store < Ahoy::Stores::ActiveRecordStore
   # Track user IP
   def track_event(name, properties, options)
     super do |event|
+      event.user_id = nil
       event.ip = request.ip
     end
   end
