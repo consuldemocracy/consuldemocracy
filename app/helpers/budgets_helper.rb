8d93f36729bbf78679f1d4e3e1ec3c8f4fcc5e8e
Alberto Garcia Cabeza <alberto@decabeza.es>
2017-02-20 16:36:57 +0100

    removes js and css of countdown

--- app/helpers/budgets_helper.rb
+++ app/helpers/budgets_helper.rb
@@ -26,10 +26,6 @@ module BudgetsHelper
     end
   end
 
-  def display_budget_countdown?(budget)
-    budget.balloting?
-  end
-
   def css_for_ballot_heading(heading)
     return '' unless current_ballot.present?
     current_ballot.has_lines_in_heading?(heading) ? 'active' : ''
