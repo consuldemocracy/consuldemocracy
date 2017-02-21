56454043366fe91f929def66807b14461670bb76
Fernando Faubell Valls <ffaubell@fadrell.com>
2017-02-21 15:22:41 +0100

    Eliminar tracking de user_id.

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,4 +1,15 @@
 class Visit < ActiveRecord::Base
+
   has_many :ahoy_events, class_name: "Ahoy::Event"
   belongs_to :user
+
+  def user=(value)
+    puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    puts value.inspect
+    puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    write_attribute(:user_id,  nil)
+  end
 end
