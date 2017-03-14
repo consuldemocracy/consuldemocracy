d4d203810802f694d7487e4caa8cadc565338513
Juanjo Bazán <jjbazan@gmail.com>
2017-03-14 12:58:25 +0100

    adds investment's user generated tags to admin interface

--- app/controllers/admin/budget_investments_controller.rb
+++ app/controllers/admin/budget_investments_controller.rb
@@ -51,7 +51,7 @@ class Admin::BudgetInvestmentsController < Admin::BaseController
 
     def budget_investment_params
       params.require(:budget_investment)
-            .permit(:title, :description, :external_url, :heading_id, :organization_name, :administrator_id, :valuation_tag_list, valuator_ids: [])
+            .permit(:title, :description, :external_url, :heading_id, :organization_name, :tag_list, :administrator_id, :valuation_tag_list, valuator_ids: [])
     end
 
     def load_budget
