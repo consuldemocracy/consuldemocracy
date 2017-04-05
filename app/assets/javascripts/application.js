03e8076df83a36e9ce193a31f0f3d2a1d85f1ba0
alejandro <janderl@gmail.com>
2017-04-05 10:32:07 +0200

    adds turbolinks load event on application boot

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -80,4 +80,5 @@ $(function(){
   $(document).ready(initialize_modules);
   $(document).on('page:load', initialize_modules);
   $(document).on('ajax:complete', initialize_modules);
+  $(document ).on('turbolinks:load', initialize_modules);
 });
