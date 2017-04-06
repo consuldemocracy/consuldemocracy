1fd6cd3188bdec50142ac1419f882d5207c86130
alejandro <janderl@gmail.com>
2017-04-06 11:54:59 +0200

    sanitizes params for views

--- app/models/budget/investment.rb
+++ app/models/budget/investment.rb
@@ -62,7 +62,7 @@ class Budget
     before_validation :set_denormalized_ids
 
     def self.filter_params(params)
-      params.select{|x,_| %w{heading_id group_id administrator_id tag_name valuator_id}.include? x.to_s }
+      params.permit(%i(heading_id group_id administrator_id tag_name valuator_id))
     end
 
     def self.scoped_filter(params, current_filter)
