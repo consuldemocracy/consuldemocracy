e1323fe189b41cc5093583d25b457aff89f633a2
Alberto Garcia Cabeza <alberto@decabeza.es>
2017-04-12 18:32:06 +0200

    removes duplicated line on pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,6 @@ class PagesController < ApplicationController
 
   def show
     @proposal_successfull_exists = Proposal.successful.exists?
-    render action: params[:id]
 
     if @custom_page = SiteCustomization::Page.published.find_by(slug: params[:id])
       render action: :custom_page
