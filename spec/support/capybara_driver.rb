922f9b7e44a22fc34eb992b7d084fd2c39eef4cb
alejandro <janderl@gmail.com>
2017-04-21 15:34:54 +0200

    ignores js errors using poltergesit

--- spec/support/capybara_driver.rb
+++ spec/support/capybara_driver.rb
@@ -12,11 +12,12 @@ else
   Capybara.javascript_driver = :poltergeist
   Capybara.register_driver :poltergeist do |app|
     Capybara::Poltergeist::Driver.new(app,
-        timeout: 1.minute,
-        inspector: true, # allows remote debugging by executing page.driver.debug
-        phantomjs_logger: File.open(File::NULL, "w"), # don't print console.log calls in console
+        js_errors:         false,
+        timeout:           1.minute,
+        inspector:         true, # allows remote debugging by executing page.driver.debug
+        phantomjs_logger:  File.open(File::NULL, "w"), # don't print console.log calls in console
         phantomjs_options: ['--load-images=no', '--disk-cache=false'],
-        extensions: [File.expand_path("../phantomjs_ext/disable_js_fx.js", __FILE__)] # disable js effects
+        extensions:        [File.expand_path("../phantomjs_ext/disable_js_fx.js", __FILE__)] # disable js effects
       )
   end
 end
