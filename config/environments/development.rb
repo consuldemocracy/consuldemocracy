0e9e6053cf393e6d26fe8e1c88b64ccfef7ad2cb
jbci <jbari@ciudadanoineligente.org>
2017-05-15 18:05:18 -0400

    send email from development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.action_mailer.asset_host = "http://localhost:3000"
 
   # Deliver emails to a development mailbox at /letter_opener
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smpt
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
