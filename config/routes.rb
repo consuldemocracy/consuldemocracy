4ac464149dbc68239d7d164330e217286f1a37a4
decabeza <alberto@decabeza.es>
2017-05-17 13:14:08 +0200

    adds old routes to prevent break old spending proposals tests

--- config/routes.rb
+++ config/routes.rb
@@ -71,6 +71,8 @@ Rails.application.routes.draw do
     end
   end
 
+  get 'participatory_budget',                to: 'pages#show', id: 'budgets/welcome',            as: 'participatory_budget'
+  get 'participatory_budget/in_two_minutes', to: 'pages#show', id: 'more_info/budgets/in_two_minutes'
   get 'presupuestos', to: 'pages#show', id: 'more_info/budgets/welcome',  as: 'budgets_welcome'
   resources :budgets, only: [:show, :index], path: 'presupuestos' do
     resources :groups, controller: "budgets/groups", only: [:show], path: 'grupo'
