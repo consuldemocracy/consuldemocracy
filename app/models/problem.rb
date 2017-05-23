a8a75440f4828ba2f31a1e1452d8623280730c22
daquiroz <daquiroz@uc.cl>
2017-05-23 13:07:53 -0400

    prorblem has user

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,10 +1,11 @@
 class Problem < ActiveRecord::Base
 
-  validates :title, presence: true
-  validates :summary, presence: true
-  has_and_belongs_to_many :geozones
+  # validates :title, presence: true
+  # validates :summary, presence: true
+  # validates :user_id, presence: true
 
-  belongs_to :author, -> { with_hidden }, class_name: 'User', foreign_key: 'author_id'
+  has_and_belongs_to_many :geozones
+  belongs_to :user
   has_many :proposals
 
 end
