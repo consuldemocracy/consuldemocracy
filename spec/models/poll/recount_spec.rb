3dde935051a0fefd1b5aaf9fad6407de7913fcbd
Bertocq <bertocq@gmail.com>
2017-05-25 11:58:15 +0200

    Avoid creating objects, on poll recount spec, with fixed id numbers
    This was generating a flaky test that sometimes generated a unique key value constraint violation

--- spec/models/poll/recount_spec.rb
+++ spec/models/poll/recount_spec.rb
@@ -23,18 +23,21 @@ describe :recount do
     expect(recount.count_log).to eq("")
 
     recount.count = 33
-    recount.officer_assignment = create(:poll_officer_assignment, id: 11)
+    poll_officer_assignment_1 = create(:poll_officer_assignment)
+    recount.officer_assignment = poll_officer_assignment_1
     recount.save
 
     recount.count = 32
-    recount.officer_assignment = create(:poll_officer_assignment, id: 12)
+    poll_officer_assignment_2 = create(:poll_officer_assignment)
+    recount.officer_assignment = poll_officer_assignment_2
     recount.save
 
     recount.count = 34
-    recount.officer_assignment = create(:poll_officer_assignment, id: 13)
+    poll_officer_assignment_3 = create(:poll_officer_assignment)
+    recount.officer_assignment = poll_officer_assignment_3
     recount.save
 
-    expect(recount.officer_assignment_id_log).to eq(":11:12")
+    expect(recount.officer_assignment_id_log).to eq(":#{poll_officer_assignment_1.id}:#{poll_officer_assignment_2.id}")
   end
 
 end
