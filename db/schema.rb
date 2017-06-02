ae27eba2b5d422c49494807ad91e9e0362f049ed
Bertocq <bertocq@gmail.com>
2017-06-02 18:34:56 +0200

    Add summary column to Legislative Processes table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170519084239) do
+ActiveRecord::Schema.define(version: 20170602162155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -418,6 +418,7 @@ ActiveRecord::Schema.define(version: 20170519084239) do
     t.datetime "hidden_at"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.text     "summary"
   end
 
   add_index "legislation_processes", ["allegations_end_date"], name: "index_legislation_processes_on_allegations_end_date", using: :btree
