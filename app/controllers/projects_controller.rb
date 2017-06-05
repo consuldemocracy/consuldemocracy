6dfd3460ace5356cdd5ac6198a6959900eff89b5
daquiroz <daquiroz@uc.cl>
2017-06-05 13:59:15 -0400

    controller project added

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
   skip_authorization_check
+  load_and_authorize_resource
+  before_action :load_geozones, only: [:new, :create, :edit, :update]
 
   def index
     @projects = Project.all
@@ -9,12 +11,36 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
 
-
-
-  private
-
-    def project_params
-      params.require(:project).permit(:name)
+  def create
+    if @project.save
+      redirect_to admin_projects_url, notice: t("flash.actions.create.problem")
+    else
+      render :new
     end
+  end
+
+  def edit
+    @project.design_events.build
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to admin_projects_url, notice: t("flash.actions.update.project")
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
+private
+  def load_geozones
+    @geozones = Geozone.all.order(:name)
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :description, :starts_at, :ends_at, :proposal_id, :geozone_restricted, geozone_ids: [], design_events_attributes: [:starts_at, :name, :place, :pax] )
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -856,7 +856,7 @@ ActiveRecord::Schema.define(version: 20170531153458) do
     t.boolean  "email_digest",                              default: true
     t.boolean  "email_on_direct_message",                   default: true
     t.boolean  "official_position_badge",                   default: false
-    t.datetime "password_changed_at",                       default: '2017-05-31 16:56:18', null: false
+    t.datetime "password_changed_at",                       default: '2017-06-05 17:44:14', null: false
     t.boolean  "created_from_signature",                    default: false
     t.integer  "failed_email_digests_count",                default: 0
     t.text     "former_users_data_log",                     default: ""
