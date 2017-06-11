b79df113cdc92851e60f3001506a8bbad1828a66
Bertocq <bertocq@gmail.com>
2017-06-11 10:40:41 +0200

    Switch all Time.curren.to_date for Date.current, same result but better syntax

--- lib/age.rb
+++ lib/age.rb
@@ -1,5 +1,5 @@
 module Age
-  def self.in_years(dob, now = Time.current.to_date)
+  def self.in_years(dob, now = Date.current)
     return nil if dob.blank?
     # reference: http://stackoverflow.com/questions/819263/get-persons-age-in-ruby#comment21200772_819263
     now.year - dob.year - (now.month > dob.month || (now.month == dob.month && now.day >= dob.day) ? 0 : 1)
