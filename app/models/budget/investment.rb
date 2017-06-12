c5bb6dca65e988e60d5f3f7053479d4572ed9ffa
taitus <sebastia.roig@gmail.com>
2017-06-12 19:33:26 +0200

    Add image to budget investments.

--- app/models/budget/investment.rb
+++ app/models/budget/investment.rb
@@ -30,6 +30,9 @@ class Budget
     has_many :comments, as: :commentable
     has_many :milestones
 
+    has_attached_file :image, styles: { large: "600x600>"  ,medium: "300x300>", thumb: "100x100>" }
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
     validates :title, presence: true
     validates :author, presence: true
     validates :description, presence: true
