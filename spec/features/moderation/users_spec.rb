91fb48d966fda26ade4548bf21ffc13102764f2e
Juanjo Bazán <jjbazan@gmail.com>
2017-06-16 11:27:52 +0200

    fixes specs with current devise response implementation

--- spec/features/moderation/users_spec.rb
+++ spec/features/moderation/users_spec.rb
@@ -46,7 +46,7 @@ feature 'Moderate users' do
     fill_in 'user_password', with: citizen.password
     click_button 'Enter'
 
-    expect(page).to have_content 'Invalid login or password'
+    expect(page).to have_content 'Invalid Email or username or password'
     expect(current_path).to eq(new_user_session_path)
   end
 
