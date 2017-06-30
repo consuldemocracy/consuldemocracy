98504f400a239698d6d596e77ecf3ddca798e43d
Bertocq <bertocq@gmail.com>
2017-06-30 12:08:44 +0200

    Complete Budget Stats view with translations and charts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,6 +14,8 @@ Rails.application.config.assets.precompile += %w( ie_lt9.js )
 Rails.application.config.assets.precompile += %w( stat_graphs.js )
 Rails.application.config.assets.precompile += %w( print.css )
 Rails.application.config.assets.precompile += %w( ie.css )
+Rails.application.config.assets.precompile += %w( chart.js )
+Rails.application.config.assets.precompile += %w( budgets-stats-charts.js )
 
 # Loads app/assets/images/custom before app/assets/images
 images_path = Rails.application.config.assets.paths
