ed7b7f0361ce6eaa900edf8f5e08241a19ba7313
María Checa <mariajecheca@gmail.com>
2017-07-02 14:54:11 +0200

    Added scope to pages model
    New scope to use when listing pages to show only the ones with current locale.

--- app/models/site_customization/page.rb
+++ app/models/site_customization/page.rb
@@ -9,6 +9,7 @@ class SiteCustomization::Page < ActiveRecord::Base
 
   scope :published, -> { where(status: 'published').order('id DESC') }
   scope :with_more_info_flag, -> { where(status: 'published', more_info_flag: true).order('id ASC') }
+  scope :with_same_locale, -> { where(locale: I18n.locale).order('id ASC') }
 
   def url
     "/#{slug}"
