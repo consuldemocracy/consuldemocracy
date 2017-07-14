77b8d93893601f5e5863738ec6ba2698fbd36d98
María Checa <MariaCheca@users.noreply.github.com>
2017-07-14 15:34:31 +0200

    Removed unnecessary validation in `LocalCensusRecord`

--- app/models/local_census_record.rb
+++ app/models/local_census_record.rb
@@ -1,7 +1,3 @@
 class LocalCensusRecord < ActiveRecord::Base
 
-  validates :document_number, presence: true
-  validates :document_type, presence: true
-  validates :date_of_birth, presence: true
-  validates :postal_code, presence: true
 end
