b71f55af23431ec2c0d49a6b2f825bf7c565830e
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2017-07-20 18:33:35 +0200

    Refactor Legislation::Process model using HasPhase concern
    Some scopes provided by the concern has been aliased and a method
    delegated to not have to rename calls in other parts of the
    application.

--- app/models/legislation/process.rb
+++ app/models/legislation/process.rb
@@ -1,8 +1,7 @@
 class Legislation::Process < ActiveRecord::Base
   acts_as_paranoid column: :hidden_at
   include ActsAsParanoidAliases
-
-  PHASES_AND_PUBLICATIONS = %i(debate_phase allegations_phase draft_publication result_publication).freeze
+  include HasPhases
 
   has_many :draft_versions, -> { order(:id) }, class_name: 'Legislation::DraftVersion',
                                                foreign_key: 'legislation_process_id', dependent: :destroy
@@ -11,64 +10,28 @@ class Legislation::Process < ActiveRecord::Base
   has_many :questions, -> { order(:id) }, class_name: 'Legislation::Question', foreign_key: 'legislation_process_id', dependent: :destroy
 
   validates :title, presence: true
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-  validates :debate_start_date, presence: true, if: :debate_end_date?
-  validates :debate_end_date, presence: true, if: :debate_start_date?
-  validates :allegations_start_date, presence: true, if: :allegations_end_date?
-  validates :allegations_end_date, presence: true, if: :allegations_start_date?
-  validate :valid_date_ranges
 
-  scope :open, -> { where("start_date <= ? and end_date >= ?", Date.current, Date.current).order('id DESC') }
-  scope :next, -> { where("start_date > ?", Date.current).order('id DESC') }
-  scope :past, -> { where("end_date < ?", Date.current).order('id DESC') }
+  has_phase :debate, :allegations
+  has_phase :global, start_date: :start_date,
+                     end_date: :end_date,
+                     phase_enabled: :published
+  has_phase :draft, start_date: :draft_publication_date,
+                    phase_enabled: :draft_publication_enabled,
+                    publication: true,
+                    phase_keyword: :publication
+  has_phase :result, start_date: :result_publication_date,
+                     phase_enabled: :result_publication_enabled,
+                     publication: true,
+                     phase_keyword: :publication
 
-  scope :published, -> { where(published: true) }
-
-  def debate_phase
-    Legislation::Process::Phase.new(debate_start_date, debate_end_date, debate_phase_enabled)
-  end
-
-  def allegations_phase
-    Legislation::Process::Phase.new(allegations_start_date, allegations_end_date, allegations_phase_enabled)
-  end
-
-  def draft_publication
-    Legislation::Process::Publication.new(draft_publication_date, draft_publication_enabled)
-  end
-
-  def result_publication
-    Legislation::Process::Publication.new(result_publication_date, result_publication_enabled)
-  end
-
-  def enabled_phases_and_publications_count
-    PHASES_AND_PUBLICATIONS.count { |process| send(process).enabled? }
-  end
+  delegate :status, to: :global_phase
+  scope :open, -> { global_open }
+  scope :next, -> { global_next }
+  scope :past, -> { global_past }
+  scope :published, -> { global_published }
 
   def total_comments
     questions.sum(:comments_count) + draft_versions.map(&:total_comments).sum
   end
-
-  def status
-    today = Date.current
-
-    if today < start_date
-      :planned
-    elsif end_date < today
-      :closed
-    else
-      :open
-    end
-  end
-
-  private
-
-    def valid_date_ranges
-      errors.add(:end_date, :invalid_date_range) if end_date && start_date && end_date < start_date
-      errors.add(:debate_end_date, :invalid_date_range) if debate_end_date && debate_start_date && debate_end_date < debate_start_date
-      if allegations_end_date && allegations_start_date && allegations_end_date < allegations_start_date
-        errors.add(:allegations_end_date, :invalid_date_range)
-      end
-    end
 
 end
