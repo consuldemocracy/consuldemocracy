522c4e2cfcea45714ac0018d8ad42244d98504ba
taitus <sebastia.roig@gmail.com>
2017-07-22 12:44:03 +0200

    Regroup all carousels on 1 partial. Rename partial

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -7,5 +7,22 @@ module WelcomeHelper
   def slide_display(index)
     "display: none;" if index > 0
   end
-  
+
+  def recommended_path(recommended)
+    case recommended.class
+    when Debate
+      debates_path(recommended)
+    when Proposal
+      proposals_path(recommended)
+    when Budget::Investment
+      budget_investments_path(recommended)
+    else
+      '#'
+    end
+  end
+
+  def title_key(key)
+    key.gsub("-", "_")
+  end
+
 end
