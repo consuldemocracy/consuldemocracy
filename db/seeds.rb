e5d7a242fc56867a67a9d35e79dd5e8d01920de6
Angel Perez <iangel.p93@gmail.com>
2017-09-21 10:11:00 -0400

    'Proposals' feature can be enabled/disabled
    Fixes #1886

--- db/seeds.rb
+++ db/seeds.rb
@@ -70,6 +70,7 @@ Setting["meta_keywords"] = nil
 
 # Feature flags
 Setting['feature.debates'] = true
+Setting['feature.proposals'] = true
 Setting['feature.spending_proposals'] = nil
 Setting['feature.polls'] = true
 Setting['feature.twitter_login'] = true
