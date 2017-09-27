f40a0d09ed2d2537efb7d7ec40a9216569cfcba0
Bertocq <bertocq@gmail.com>
2017-09-27 18:08:03 +0200

    Add Shift vote and recount date ranges, filter selected one on controller

--- app/controllers/admin/poll/shifts_controller.rb
+++ app/controllers/admin/poll/shifts_controller.rb
@@ -54,7 +54,7 @@ class Admin::Poll::ShiftsController < Admin::Poll::BaseController
     end
 
     def shift_params
-      params.require(:shift).permit(:booth_id, :officer_id, :date, :task)
+      shift_params = params.require(:shift).permit(:booth_id, :officer_id, :task, date:[:vote_collection_date, :recount_scrutiny_date])
+      shift_params.merge(date: shift_params[:date]["#{shift_params[:task]}_date".to_sym])
     end
-
 end
--- app/helpers/shifts_helper.rb
+++ app/helpers/shifts_helper.rb
@@ -1,11 +1,15 @@
 module ShiftsHelper
 
-  def shift_dates_select_options(polls)
-    options = []
-    (start_date(polls)..end_date(polls)).each do |date|
-      options << [l(date, format: :long), l(date)]
-    end
-    options_for_select(options, params[:date])
+  def shift_vote_collection_dates(polls)
+    date_options((start_date(polls)..end_date(polls)))
+  end
+
+  def shift_recount_scrutiny_dates(polls)
+    date_options(polls.map(&:ends_at).map(&:to_date).inject([]) { |total, date| total << (date..date + 1.week).to_a }.flatten.uniq)
+  end
+
+  def date_options(dates)
+    dates.map { |date| [l(date, format: :long), l(date)] }
   end
 
   def start_date(polls)
