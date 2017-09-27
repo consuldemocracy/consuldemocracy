e2b8043cf09c3650fce89b07a877d03e40ce921c
pedroleon <pedro.leon@wearestrings.com>
2017-09-27 20:56:35 +0200

    Added line logo to mailers to fit ToledoParticipa corporative email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,4 +2,10 @@ class ApplicationMailer < ActionMailer::Base
   helper :settings
   default from: "#{Setting['mailer_from_name']} <#{Setting['mailer_from_address']}>"
   layout 'mailer'
+
+  before_action :add_inline_attachment!
+
+  def add_inline_attachment!
+    attachments.inline["logo_email.png"] = File.read(Rails.root.join('app/assets/images/custom/logo_email.png'))
+  end
 end
