bf61dffd4b354e3b0d8fe016265e652151e2ef8b
María Checa <MariaCheca@users.noreply.github.com>
2017-10-03 10:47:48 +0200

    Removed files that are not used

--- app/controllers/admin/poll/polls_controller.rb
+++ app/controllers/admin/poll/polls_controller.rb
@@ -1,7 +1,7 @@
 class Admin::Poll::PollsController < Admin::Poll::BaseController
   load_and_authorize_resource
 
-  before_action :load_search, only: [:search_booths, :search_questions, :search_officers]
+  before_action :load_search, only: [:search_booths, :search_officers]
   before_action :load_geozones, only: [:new, :create, :edit, :update]
 
   def index
@@ -45,13 +45,6 @@ class Admin::Poll::PollsController < Admin::Poll::BaseController
       notice = t("admin.polls.flash.error_on_question_added")
     end
     redirect_to admin_poll_path(@poll), notice: notice
-  end
-
-  def search_questions
-    @questions = ::Poll::Question.where("poll_id IS ? OR poll_id != ?", nil, @poll.id).search(search: @search).order(title: :asc)
-    respond_to do |format|
-      format.js
-    end
   end
 
   private
