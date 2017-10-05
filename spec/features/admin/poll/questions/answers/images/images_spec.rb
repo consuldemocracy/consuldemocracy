1922bc55a0b474d093056214b0711ce9c23f78dc
rgarcia <voodoorai2000@gmail.com>
2017-10-05 19:27:04 +0200

    adds pending specs

--- spec/features/admin/poll/questions/answers/images/images_spec.rb
+++ spec/features/admin/poll/questions/answers/images/images_spec.rb
@@ -2,30 +2,13 @@ require 'rails_helper'
 
 feature 'Images' do
 
-	background do
-		admin = create(:administrator)
-		login_as(admin.user)
-	end
+  background do
+    admin = create(:administrator)
+    login_as(admin.user)
+  end
 
-	scenario "Index" do
-	end
-
-	scenario "Create", :js do
-		question = create(:poll_question)
-		answer = create(:poll_question_answer, question: question)
-
-		visit admin_question_path(question)
-
-		within("#poll_question_answer_#{answer.id}") do
-			click_link "Lista de imágenes"
-		end
-
-		click_link "Add image"
-
-		imageable_attach_new_file(:poll_question_answer, "spec/fixtures/files/clippy.jpg")
-    click_button "Save image"
-
-    expect(page).to have_content "Image uploaded successfully"
-	end
+  pending "Index"
+  pending "Create"
+  pending "Destroy"
 
 end
