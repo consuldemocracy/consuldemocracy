c5c1ea7251b3cd7dacd78b3fc0aa1a17fedf029b
María Checa <MariaCheca@users.noreply.github.com>
2017-10-06 12:02:24 +0200

    Fixed answer documents form

--- app/controllers/admin/poll/questions/answers_controller.rb
+++ app/controllers/admin/poll/questions/answers_controller.rb
@@ -20,7 +20,7 @@ class Admin::Poll::Questions::AnswersController < Admin::Poll::BaseController
 
   def update
     if @answer.update(answer_params)
-      redirect_to admin_question_path(@question), notice: t("flash.actions.save_changes.notice")
+      redirect_to admin_question_path(@answer.question), notice: t("flash.actions.save_changes.notice")
     else
       redirect_to :back
     end
