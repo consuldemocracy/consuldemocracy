843048a86176cbfe4b4f023a0b0debcd740add63
rgarcia <voodoorai2000@gmail.com>
2017-10-10 12:14:04 +0200

    cleans up

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,6 +1,4 @@
 class PollsController < ApplicationController
-  include CommentableActions
-
   include PollsHelper
 
   load_and_authorize_resource
@@ -17,13 +15,13 @@ class PollsController < ApplicationController
   def show
     @questions = @poll.questions.for_render.sort_for_list
     @token = poll_voter_token(@poll, current_user)
-    
+
     @answers_by_question_id = {}
     poll_answers = ::Poll::Answer.by_question(@poll.question_ids).by_author(current_user.try(:id))
     poll_answers.each do |answer|
       @answers_by_question_id[answer.question_id] = answer.answer
     end
-    
+
     @commentable = @poll
     @comment_tree = CommentTree.new(@commentable, params[:page], @current_order)
   end
