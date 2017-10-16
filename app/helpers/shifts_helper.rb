45f73aa1baff8e995ade7f58c7988f95f1e6b00e
María Checa <MariaCheca@users.noreply.github.com>
2017-10-16 12:22:54 +0200

    Excluded officer shift dates from each task date range

--- app/helpers/shifts_helper.rb
+++ app/helpers/shifts_helper.rb
@@ -1,15 +1,15 @@
 module ShiftsHelper
 
   def shift_vote_collection_dates(polls)
-    date_options((start_date(polls)..end_date(polls)))
+    date_options((start_date(polls)..end_date(polls)), 0)
   end
 
   def shift_recount_scrutiny_dates(polls)
-    date_options(polls.map(&:ends_at).map(&:to_date).sort.inject([]) { |total, date| total << (date..date + 1.week).to_a }.flatten.uniq)
+    date_options(polls.map(&:ends_at).map(&:to_date).sort.inject([]) { |total, date| total << (date..date + 1.week).to_a }.flatten.uniq, 1)
   end
 
-  def date_options(dates)
-    dates.map { |date| [l(date, format: :long), l(date)] }
+  def date_options(dates, task_id)
+    dates.reject { |date| officer_shifts(task_id).include?(date) }.map { |date| [l(date, format: :long), l(date)] }
   end
 
   def start_date(polls)
@@ -24,4 +24,9 @@ module ShiftsHelper
     officers.collect { |officer| [officer.name, officer.id] }
   end
 
+  private
+
+  def officer_shifts(task_id)
+    @officer.shifts.where(task: task_id).map(&:date)
+  end
 end
