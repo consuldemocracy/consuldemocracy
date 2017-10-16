a6d533a76750c86aa2a7ab623bcf01a5a9b8891c
María Checa <MariaCheca@users.noreply.github.com>
2017-10-16 13:47:10 +0200

    Helper improvements

--- app/helpers/shifts_helper.rb
+++ app/helpers/shifts_helper.rb
@@ -1,15 +1,19 @@
 module ShiftsHelper
 
   def shift_vote_collection_dates(polls)
-    date_options((start_date(polls)..end_date(polls)), 0)
+    date_options((start_date(polls)..end_date(polls)), Poll::Shift.tasks[:vote_collection])
   end
 
   def shift_recount_scrutiny_dates(polls)
-    date_options(polls.map(&:ends_at).map(&:to_date).sort.inject([]) { |total, date| total << (date..date + 1.week).to_a }.flatten.uniq, 1)
+    date_options(polls.map(&:ends_at).map(&:to_date).sort.inject([]) { |total, date| total << (date..date + 1.week).to_a }.flatten.uniq, Poll::Shift.tasks[:recount_scrutiny])
   end
 
   def date_options(dates, task_id)
-    dates.reject { |date| officer_shifts(task_id).include?(date) }.map { |date| [l(date, format: :long), l(date)] }
+    valid_dates(dates, task_id).map { |date| [l(date, format: :long), l(date)] }
+  end
+
+  def valid_dates(dates, task_id)
+    dates.reject { |date| officer_shifts(task_id).include?(date) }
   end
 
   def start_date(polls)
