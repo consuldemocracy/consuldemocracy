9e22a1812f3d9ffa8f8a44ce3ca09b0988c428c8
Bertocq <bertocq@gmail.com>
2017-10-18 01:22:15 +0200

    Remove no longer needed valid_answers attribute from poll_question factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -503,7 +503,6 @@ FactoryGirl.define do
     poll
     association :author, factory: :user
     sequence(:title) { |n| "Question title #{n}" }
-    valid_answers { Faker::Lorem.words(3).join(', ') }
   end
 
   factory :poll_question_answer, class: 'Poll::Question::Answer' do
