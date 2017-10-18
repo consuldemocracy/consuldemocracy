d15f150e988af033df5c57aa3cb8b7f857faeb1e
Bertocq <bertocq@gmail.com>
2017-10-18 20:49:30 +0200

    Trim officer shifts by booth as well

--- app/helpers/shifts_helper.rb
+++ app/helpers/shifts_helper.rb
@@ -1,23 +1,23 @@
 module ShiftsHelper
 
-  def shift_vote_collection_dates(polls)
-    date_options((start_date(polls)..end_date(polls)), Poll::Shift.tasks[:vote_collection])
+  def shift_vote_collection_dates(booth, polls)
+    date_options((start_date(polls)..end_date(polls)), Poll::Shift.tasks[:vote_collection], booth)
   end
 
-  def shift_recount_scrutiny_dates(polls)
+  def shift_recount_scrutiny_dates(booth, polls)
     dates = polls.map(&:ends_at).map(&:to_date).sort.inject([]) do |total, date|
       initial_date = date < Date.current ? Date.current : date
       total << (initial_date..date + Poll::RECOUNT_DURATION).to_a
     end
-    date_options(dates.flatten.uniq, Poll::Shift.tasks[:recount_scrutiny])
+    date_options(dates.flatten.uniq, Poll::Shift.tasks[:recount_scrutiny], booth)
   end
 
-  def date_options(dates, task_id)
-    valid_dates(dates, task_id).map { |date| [l(date, format: :long), l(date)] }
+  def date_options(dates, task_id, booth)
+    valid_dates(dates, task_id, booth).map { |date| [l(date, format: :long), l(date)] }
   end
 
-  def valid_dates(dates, task_id)
-    dates.reject { |date| officer_shifts(task_id).include?(date) }
+  def valid_dates(dates, task_id, booth)
+    dates.reject { |date| officer_shifts(task_id, booth).include?(date) }
   end
 
   def start_date(polls)
@@ -35,7 +35,7 @@ module ShiftsHelper
 
   private
 
-  def officer_shifts(task_id)
-    @officer.shifts.where(task: task_id).map(&:date)
+  def officer_shifts(task_id, booth)
+    @officer.shifts.where(task: task_id, booth: booth).map(&:date)
   end
 end
