2a5d20868db85a5cba0c1d01c8ac985f9317308f
iagirre <itziar.agirre@m2c.es>
2017-10-19 10:35:38 +0200

    Sluggable modified to change the slug when it's not modified by hand

--- app/models/concerns/sluggable.rb
+++ app/models/concerns/sluggable.rb
@@ -2,10 +2,14 @@ module Sluggable
   extend ActiveSupport::Concern
 
   included do
-    before_validation :generate_slug
+    before_validation :generate_slug, if: :update_slug?
   end
 
   def generate_slug
     self.slug = name.to_s.parameterize
+  end
+
+  def update_slug?
+    new_record? || (name_changed? && (name_was.to_s.parameterize == slug))
   end
 end
