714c61519f710a918af2d5cfdd8591025635ee65
Bertocq <bertocq@gmail.com>
2017-10-21 00:23:33 +0200

    Fix store officer and booth info spec

--- spec/features/officing/voters_spec.rb
+++ spec/features/officing/voters_spec.rb
@@ -149,20 +149,20 @@ feature 'Voters' do
      expect(page).to_not have_content "You already have participated in this poll."
    end
 
-   xscenario "Store officer and booth information", :js do
+   scenario "Store officer and booth information", :js do
      user  = create(:user, :in_census, id: rand(9999999))
      poll1 = create(:poll, nvotes_poll_id: 128, name: "¿Quieres que XYZ sea aprobado?")
      poll2 = create(:poll, nvotes_poll_id: 136, name: "Pregunta de votación de prueba")
 
-     booth = create(:poll_booth)
+     second_booth = create(:poll_booth)
 
-     ba1 = create(:poll_booth_assignment, poll: poll1, booth: booth )
-     ba2 = create(:poll_booth_assignment, poll: poll2, booth: booth )
-     oa1 = create(:poll_officer_assignment, officer: officer, booth_assignment: ba1, date: Date.current)
-     oa2 = create(:poll_officer_assignment, officer: officer, booth_assignment: ba2, date: Date.current)
+     ba1 = create(:poll_booth_assignment, poll: poll1, booth: second_booth )
+     ba2 = create(:poll_booth_assignment, poll: poll2, booth: second_booth )
+     create(:poll_shift, officer: officer, booth: second_booth, date: Date.current, task: :vote_collection)
 
      validate_officer
      visit new_officing_residence_path
+     set_officing_booth(second_booth)
      officing_verify_residence
 
      within("#poll_#{poll1.id}") do
@@ -182,11 +182,11 @@ feature 'Voters' do
      voter1 = Poll::Voter.first
 
      expect(voter1.booth_assignment).to eq(ba1)
-     expect(voter1.officer_assignment).to eq(oa1)
+     expect(voter1.officer_assignment).not_to be_nil
 
      voter2 = Poll::Voter.last
      expect(voter2.booth_assignment).to eq(ba2)
-     expect(voter2.officer_assignment).to eq(oa2)
+     expect(voter2.officer_assignment).not_to be_nil
    end
 
    context "Booth type" do
