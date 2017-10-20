01bd89e4d0414b6c5842bc08b29145826f1fe8b2
María Checa <MariaCheca@users.noreply.github.com>
2017-10-21 00:18:17 +0200

    Questions controller improvements

--- app/controllers/polls/questions_controller.rb
+++ app/controllers/polls/questions_controller.rb
@@ -12,11 +12,12 @@ class Polls::QuestionsController < ApplicationController
 
     voter = Poll::Voter.find_or_initialize_by(user: answer.author, poll: answer.poll, origin: "web", token: params[:token])
 
-    if params[:token].present? && answer.save! && voter.save!
+    if params[:token].present? && answer.valid? && voter.valid?
+      answer.save!
+      voter.save!
+
       @answers_by_question_id = { @question.id => params[:answer] }
       log_event("poll", 'vote')
-
-      render :answer
     else
       flash.now[:error] = t("poll_questions.show.vote_error")
       render :error
