59c97b4db7c655b942a930e2e7b2d44f651856c1
Angel Perez <iangel.p93@gmail.com>
2017-10-23 15:18:23 -0400

    Enable search by name/email for Valuators

--- spec/features/admin/valuators_spec.rb
+++ spec/features/admin/valuators_spec.rb
@@ -15,8 +15,8 @@ feature 'Admin valuators' do
     expect(page).to_not have_content @user.name
   end
 
-  scenario 'Create Valuator', :js do
-    fill_in 'email', with: @user.email
+  pending 'Create Valuator', :js do
+    fill_in 'name_or_email', with: @user.email
     click_button 'Search'
 
     expect(page).to have_content @user.name
@@ -29,5 +29,52 @@ feature 'Admin valuators' do
     end
   end
 
-end
+  context 'Search' do
 
+    background do
+      user  = create(:user, username: 'David Foster Wallace', email: 'david@wallace.com')
+      user2 = create(:user, username: 'Steven Erikson', email: 'steven@erikson.com')
+      @valuator1 = create(:valuator, user: user)
+      @valuator2 = create(:valuator, user: user2)
+      visit admin_valuators_path
+    end
+
+    scenario 'returns no results if search term is empty' do
+      expect(page).to have_content(@valuator1.name)
+      expect(page).to have_content(@valuator2.name)
+
+      fill_in 'name_or_email', with: ' '
+      click_button 'Search'
+
+      expect(page).to have_content('Valuators: User search')
+      expect(page).to have_content('users cannot be found')
+      expect(page).to_not have_content(@valuator1.name)
+      expect(page).to_not have_content(@valuator2.name)
+    end
+
+    scenario 'search by name' do
+      expect(page).to have_content(@valuator1.name)
+      expect(page).to have_content(@valuator2.name)
+
+      fill_in 'name_or_email', with: 'Foster'
+      click_button 'Search'
+
+      expect(page).to have_content('Valuators: User search')
+      expect(page).to have_content(@valuator1.name)
+      expect(page).to_not have_content(@valuator2.name)
+    end
+
+    scenario 'search by email' do
+      expect(page).to have_content(@valuator1.email)
+      expect(page).to have_content(@valuator2.email)
+
+      fill_in 'name_or_email', with: @valuator2.email
+      click_button 'Search'
+
+      expect(page).to have_content('Valuators: User search')
+      expect(page).to have_content(@valuator2.email)
+      expect(page).to_not have_content(@valuator1.email)
+    end
+  end
+
+end
