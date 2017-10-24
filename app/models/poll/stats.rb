5982260a066cfe0aa9498511ebc9460edca4b0ee
Bertocq <bertocq@gmail.com>
2017-10-24 05:27:13 +0200

    Fix again, total and valid booth numnbers, this is not funny anymore pls stap

--- app/models/poll/stats.rb
+++ app/models/poll/stats.rb
@@ -31,7 +31,7 @@ class Poll
       end
 
       def total_participants_booth
-        stats_cache('total_participants_booth') { recounts.sum(:total_amount) + total_booth_white + total_booth_null }
+        stats_cache('total_participants_booth') { total_booth_valid + total_booth_white + total_booth_null }
       end
 
       def total_participants_booth_percentage
@@ -76,7 +76,7 @@ class Poll
       end
 
       def total_booth_valid
-        stats_cache('total_booth_valid') { total_participants_booth }
+        stats_cache('total_booth_valid') { recounts.sum(:total_amount) }
       end
 
       def valid_percentage_booth
@@ -132,7 +132,7 @@ class Poll
       end
 
       def stats_cache(key, &block)
-        Rails.cache.fetch("polls_stats/#{@poll.id}/#{key}/v7", &block)
+        Rails.cache.fetch("polls_stats/#{@poll.id}/#{key}/v8", &block)
       end
 
   end
