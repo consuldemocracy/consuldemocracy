cd8cf4229413cccc926eac4fe1af0d1c0eb0fd79
Angel Perez <iangel.p93@gmail.com>
2017-10-25 10:13:07 -0400

    'Add to valuators' option added to Valuators search view

--- spec/features/admin/valuators_spec.rb
+++ spec/features/admin/valuators_spec.rb
@@ -2,30 +2,30 @@ require 'rails_helper'
 
 feature 'Admin valuators' do
   background do
-    @admin = create(:administrator)
-    @user  = create(:user, username: 'Jose Luis Balbin')
+    @admin    = create(:administrator)
+    @user     = create(:user, username: 'Jose Luis Balbin')
     @valuator = create(:valuator)
     login_as(@admin.user)
     visit admin_valuators_path
   end
 
   scenario 'Index' do
-    expect(page).to have_content @valuator.name
-    expect(page).to have_content @valuator.email
-    expect(page).to_not have_content @user.name
+    expect(page).to have_content(@valuator.name)
+    expect(page).to have_content(@valuator.email)
+    expect(page).to_not have_content(@user.name)
   end
 
-  pending 'Create Valuator', :js do
+  scenario 'Create Valuator', :js do
     fill_in 'name_or_email', with: @user.email
     click_button 'Search'
 
-    expect(page).to have_content @user.name
+    expect(page).to have_content(@user.name)
     fill_in 'valuator_description', with: 'environmental expert'
     click_button 'Add to valuators'
 
-    within("#valuators") do
-      expect(page).to have_content @user.name
-      expect(page).to have_content 'environmental expert'
+    within('#valuators') do
+      expect(page).to have_content(@user.name)
+      expect(page).to have_content('environmental expert')
     end
   end
 
