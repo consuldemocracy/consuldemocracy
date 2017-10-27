c70030c51c3beedbfcf1d94fc6acba527e0759da
pedroleon <pedro.leon@wearestrings.com>
2017-10-27 11:43:33 +0200

    Toledos TOL-1 and TOL-2 requirements met
    - Added custom SMS API service interface
    - Added custom Census API interface
    - Refactoriced code using Consul's custom structure to avoid conflicts during merges
    - Added extra spec to check Toledo Census Service
    - Added custom translations

---
+++ lib/toledo_census_api.rb
@@ -0,0 +1,104 @@
+include DocumentParser
+class ToledoCensusApi < CensusApi
+  def call(document_type, document_number)
+    response = nil
+    get_document_number_variants(document_type, document_number).each do |variant|
+      response = Response.new(get_response_body(document_type, variant))
+      return response if response.valid?
+    end
+    response
+  end
+
+  class Response
+    def initialize(body)
+      @census_response = body
+    end
+
+    def valid?
+      @census_response[:error].nil?
+    end
+
+    def gender
+      @census_response['Gender']
+    end
+
+    def name
+      "#{@census_response['Nombre']} #{@census_response['PrimerApellido']} #{@census_response['SegundoApellido']}"
+    end
+
+    def date_of_birth
+      str = @census_response['FechaNacimiento']
+      day, month, year = str.match(/(\d\d\d?\d?)\D(\d\d?)\D(\d\d?)/)[1..3]
+      return nil unless day.present? && month.present? && year.present?
+      Date.new(day.to_i, month.to_i, year.to_i)
+    end
+
+    def postal_code
+      @census_response['CP']
+    end
+
+    def district_code
+      @census_response['Barrio_id']
+    end
+
+  end
+
+  def get_response_body(document_type, document_number)
+    if end_point_available?
+      begin
+        @parsed_to_json = JSON.parse(rest_client_response(document_number))
+        @census_response = @parsed_to_json.kind_of?(Array)? @parsed_to_json[0] : @parsed_to_json
+      rescue Exception => ex
+
+        @census_response = { error: { code: 500, message: ex.message } }
+      end
+      @census_response
+    else
+      stubbed_response(document_type, document_number)
+    end
+  end
+
+  def rest_client_response(document_number)
+
+    RestClient.get "#{Rails.application.secrets.census_api_endpoint}#{document_number}",
+                   Rails.application.secrets.census_x_key => Rails.application.secrets.census_api_key
+
+  end
+
+  def end_point_available?
+    Rails.env.staging? || Rails.env.preproduction? || Rails.env.production?
+  end
+
+  def stubbed_response(document_type, document_number)
+    if document_number == "01234567" && document_type == "1"
+      stubbed_valid_response
+    else
+      stubbed_invalid_response
+    end
+  end
+
+  def stubbed_valid_response
+    {
+      'IDHAB' => '111111',
+      'DNI' => '01234567',
+      'Nombre' => 'Doménikos',
+      'PrimerApellido' => 'Theotokópoulo',
+      'SegundoApellido' => 'ElGreco',
+      'FechaNacimiento' => '1992-11-01 00:00:00',
+      'Telefono' => '666666666',
+      'Barrio' => 'CENTRO',
+      'Barrio_ID'=> '08',
+      'CP' => '45003'
+    }
+  end
+
+  def stubbed_invalid_response
+    {
+      error:
+        {
+          code: 500,
+          message: '404 Not Found'
+        }
+    }
+  end
+end
---
+++ lib/toledo_sms_api.rb
@@ -0,0 +1,69 @@
+require 'open-uri'
+require_dependency Rails.root.join('lib', 'sms_api').to_s
+
+# Custom Ayto.Toledo SMS service implementation.
+# SOAP
+class SMSApi
+  attr_accessor :client
+
+  def initialize
+    @client = Savon.client(wsdl: url, ssl_verify_mode: Rails.application.secrets.sms_ssl_verification, convert_request_keys_to: :camelcase)
+  end
+
+  def url
+    return '' unless end_point_available?
+    open(Rails.application.secrets.sms_end_point).base_uri.to_s
+  end
+
+  def authorization
+    Base64.encode64("#{Rails.application.secrets.sms_username}:#{Rails.application.secrets.sms_password}")
+  end
+
+  def sms_deliver(phone, code)
+
+    raise "*******"
+    return stubbed_response unless end_point_available?
+
+    response = client.call(:sms_text_submit, message: request(phone, code))
+    success?(response)
+  end
+
+  def request(phone, code)
+    {
+      version: Rails.application.secrets.version,
+      authorization:  authorization,
+      sender: Rails.application.secrets.sender,
+      recipients: [{ to: phone }],
+      SMSText: I18n.t('.sms_text', code: code)
+    }
+  end
+
+  def success?(response)
+    response.body[:submit_res][:status][:status_text] == 'Success'
+  end
+
+  def end_point_available?
+    Rails.env.staging? || Rails.env.preproduction? || Rails.env.production?
+  end
+
+  def stubbed_response
+    { submit_res: { version: '1.0',
+                    message_id: 'vasadpt1-is@3090@20171025162450455@02012397',
+                    status: { status_code: '1000',
+                             status_text: 'Success',
+                             details: 'Request processed succesfully'
+                            }
+                  }
+    }
+  end
+
+  def stubbed_invalid_response
+    { submit_res: { version: '1.0',
+                    status: { status_code: '4000',
+                              status_text: 'Error',
+                              details: 'Error'
+                            }
+                  }
+    }
+  end
+end
