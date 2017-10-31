bfcbb6c0ad6a646fb44ece0512557535e14c13ac
iagirre <itziar.agirre@m2c.es>
2017-10-31 13:04:30 +0100

    Validation in model and in form added. When users makes the on focus on population field an explanation appears. Some specs added to test the model validations

--- spec/models/budget/heading_spec.rb
+++ spec/models/budget/heading_spec.rb
@@ -25,4 +25,18 @@ describe Budget::Heading do
     end
   end
 
+  describe "Save population" do
+    it "Allows nil for population" do
+      expect(create(:budget_heading, group: group, name: 'Population is nil')).to be_valid
+    end
+
+    it "Doesn't allow 0 for population" do
+      expect(create(:budget_heading, group: group, name: 'Population is 0', population: 0)).not_to be_valid
+    end
+
+    it "Allows value > 0 for population" do
+      expect(create(:budget_heading, group: group, name: 'Population is 10', population: 10)).to be_valid
+    end    
+  end
+
 end
