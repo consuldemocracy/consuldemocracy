834bbf4be3ddd2463b8ec57f7bff770158f85634
rgarcia <voodoorai2000@gmail.com>
2017-11-03 13:10:08 +0100

    removes unnecessary binding

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -106,6 +106,3 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 end
-
-# Parallel build helper configuration for travis
-KnapsackPro::Adapters::RSpecAdapter.bind
