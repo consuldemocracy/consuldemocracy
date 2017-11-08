04145bf6adeb4a8cd3a3f192c93eb61d279fe43d
rgarcia <voodoorai2000@gmail.com>
2017-11-08 12:05:02 +0100

    uses correct order for permitted params

--- app/controllers/admin/legislation/processes_controller.rb
+++ app/controllers/admin/legislation/processes_controller.rb
@@ -58,9 +58,9 @@ class Admin::Legislation::ProcessesController < Admin::Legislation::BaseControll
         :draft_publication_enabled,
         :result_publication_enabled,
         :published,
-        documents_attributes: [:id, :title, :attachment, :cached_attachment, :user_id, :_destroy],
         :proposals_description,
-        :custom_list
+        :custom_list,
+        documents_attributes: [:id, :title, :attachment, :cached_attachment, :user_id, :_destroy]
       )
     end
 
