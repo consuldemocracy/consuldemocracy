2624d7579207d855abbf28a82da6fde82859b34c
Bertocq <bertocq@gmail.com>
2017-11-13 02:27:38 +0100

    Replace == 0 comparisions for .zero?

--- app/models/poll/question/answer.rb
+++ app/models/poll/question/answer.rb
@@ -41,7 +41,7 @@ class Poll::Question::Answer < ActiveRecord::Base
   end
 
   def total_votes_percentage
-    question.answers_total_votes == 0 ? 0 : (total_votes * 100) / question.answers_total_votes
+    question.answers_total_votes.zero? ? 0 : (total_votes * 100) / question.answers_total_votes
   end
 
   def set_most_voted
