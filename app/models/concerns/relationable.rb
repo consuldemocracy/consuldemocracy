09560c3c5f49aa16697f5ec15790f38287138cfe
Bertocq <bertocq@gmail.com>
2017-11-28 01:46:00 +0100

    Add report_related_content helper funcion on Relationable concern with spec

--- app/models/concerns/relationable.rb
+++ app/models/concerns/relationable.rb
@@ -4,4 +4,12 @@ module Relationable
   included do
     has_many :related_contents, as: :parent_relationable, dependent: :destroy
   end
+
+  def report_related_content(relationable)
+    related_content = related_contents.find_by(child_relationable: relationable)
+    if related_content.present?
+      related_content.increment!(:times_reported)
+      related_content.opposite_related_content.increment!(:times_reported)
+    end
+  end
 end
