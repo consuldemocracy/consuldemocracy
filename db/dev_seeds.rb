22c399ebec750d64b5e09f7bfaf412491fe366c2
decabeza <alberto@decabeza.es>
2017-12-06 20:44:43 +0100

    adds setting to allow images for proposals and investment projects

--- db/dev_seeds.rb
+++ db/dev_seeds.rb
@@ -50,6 +50,7 @@ section "Creating Settings" do
   Setting.create(key: 'feature.user.recommendations', value: "true")
   Setting.create(key: 'feature.community', value: "true")
   Setting.create(key: 'feature.map', value: "true")
+  Setting.create(key: 'feature.allow_images', value: "true")
   Setting.create(key: 'feature.public_stats', value: "true")
   Setting.create(key: 'per_page_code_head', value: "")
   Setting.create(key: 'per_page_code_body', value: "")
