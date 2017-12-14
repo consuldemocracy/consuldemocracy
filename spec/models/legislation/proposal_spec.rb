398251973b8d2409fda5751f82f8debe6725a002
decabeza <alberto@decabeza.es>
2017-12-14 17:00:16 +0100

    fixes specs for legislation proposals title length

--- spec/models/legislation/proposal_spec.rb
+++ spec/models/legislation/proposal_spec.rb
@@ -29,18 +29,18 @@ describe Legislation::Proposal do
 
   describe "title length" do
     it "a title length smaller than TITLE_MIN_LENGTH is invalid" do
-      proposal = build(:proposal, title: Faker::Lorem.characters(Legislation::Proposal::TITLE_MIN_LENGTH - 1))
+      proposal = build(:legislation_proposal, title: Faker::Lorem.characters(Legislation::Proposal::TITLE_MIN_LENGTH - 1))
       expect(proposal).not_to be_valid
     end
 
     it "a title length between TITLE_MIN_LENGTH and TITLE_MAX_LENGTH is valid" do
       title_length = rand(Legislation::Proposal::TITLE_MIN_LENGTH..Legislation::Proposal::TITLE_MAX_LENGTH)
-      proposal = build(:proposal, title: Faker::Lorem.characters(title_length))
+      proposal = build(:legislation_proposal, title: Faker::Lorem.characters(title_length))
       expect(proposal).to be_valid
     end
 
     it "a title length greater than TITLE_MXN_LENGTH is invalid" do
-      proposal = build(:proposal, title: Faker::Lorem.characters(Legislation::Proposal::TITLE_MAX_LENGTH + 1))
+      proposal = build(:legislation_proposal, title: Faker::Lorem.characters(Legislation::Proposal::TITLE_MAX_LENGTH + 1))
       expect(proposal).not_to be_valid
     end
   end
