097b3c3e900446cec0cc7cc6324df029d7b3c436
rgarcia <voodoorai2000@gmail.com>
2017-12-16 10:59:36 +0100

    allows querying for a proposal's votes

--- config/initializers/vote_extensions.rb
+++ config/initializers/vote_extensions.rb
@@ -1,6 +1,33 @@
 ActsAsVotable::Vote.class_eval do
+  include Graphqlable
+
   belongs_to :signature
   belongs_to :budget_investment, foreign_key: 'votable_id', class_name: 'Budget::Investment'
+
+  scope :public_for_api, -> do
+    where(%{(votes.votable_type = 'Debate' and votes.votable_id in (?)) or
+            (votes.votable_type = 'Proposal' and votes.votable_id in (?)) or
+            (votes.votable_type = 'Comment' and votes.votable_id in (?))},
+          Debate.public_for_api.pluck(:id),
+          Proposal.public_for_api.pluck(:id),
+          Comment.public_for_api.pluck(:id))
+  end
+
+  def self.public_columns_for_api
+    ["votable_id",
+     "votable_type",
+     "vote_flag",
+     "created_at"]
+  end
+
+  def public_for_api?
+    return false unless ["Proposal", "Debate", "Comment"].include? votable_type
+    return false unless votable.present?
+    return false if votable.hidden?
+    return false if votable_type == "Comment" && (votable.commentable.blank? || votable.commentable.hidden?)
+    return false unless votable.public_for_api?
+    return true
+  end
 
   def self.for_debates(debates)
     where(votable_type: 'Debate', votable_id: debates)
