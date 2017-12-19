a8ca3ec5f7600d85d6b837a07aafac3630b6a7ec
María Checa <MariaCheca@users.noreply.github.com>
2017-12-19 20:15:29 +0100

    Added related content one positive score by default on create

--- db/migrate/20171219111046_add_related_contents_positive_and_negative_score.rb
+++ db/migrate/20171219111046_add_related_contents_positive_and_negative_score.rb
@@ -2,7 +2,7 @@ class AddRelatedContentsPositiveAndNegativeScore < ActiveRecord::Migration
   def change
     remove_column :related_contents, :flags_count
 
-    add_column :related_contents, :positive_score, :integer, default: 0
+    add_column :related_contents, :positive_score, :integer, default: 1
     add_column :related_contents, :negative_score, :integer, default: 0
   end
 end
