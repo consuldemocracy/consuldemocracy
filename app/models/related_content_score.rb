684ac99855417c646b1859e60f634a28ce8ad860
Bertocq <bertocq@gmail.com>
2017-12-20 02:31:07 +0100

    Fix Related Content Score presence validations and unique composed key

--- app/models/related_content_score.rb
+++ app/models/related_content_score.rb
@@ -1,6 +1,8 @@
 class RelatedContentScore < ActiveRecord::Base
   belongs_to :related_content, touch: true, counter_cache: :related_content_scores_count
+  belongs_to :user
 
-  validates :user_id, presence: true
-  validates :related_content_id, presence: true
+  validates :user, presence: true
+  validates :related_content, presence: true
+  validates :related_content_id, uniqueness: { scope: [:user_id] }
 end
