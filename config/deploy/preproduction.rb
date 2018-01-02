d2b4397f4766d867af2ea1992ca504c3d4c407ea
Fernando Faubell Valls <ffaubell@fadrell.com>
2018-01-02 16:54:36 +0100

    Cambios en implantación preproduction.

--- config/deploy/preproduction.rb
+++ config/deploy/preproduction.rb
@@ -3,8 +3,8 @@ set :server_name, deploysecret(:server_name)
 set :db_server, deploysecret(:db_server)
 set :branch, ENV['branch'] || :stable
 set :ssh_options, port: deploysecret(:ssh_port)
-set :stage, :preproduction
-set :rails_env, :preproduction
+set :stage, :production
+set :rails_env, :production
 
 server deploysecret(:server1), user: deploysecret(:user), roles: %w(web app db importer cron background)
 # server deploysecret(:server2), user: deploysecret(:user), roles: %w(web app db importer)
