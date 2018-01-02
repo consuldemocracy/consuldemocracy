129d64308219075f6cd56828810d38e74ea7b036
Fernando Faubell Valls <ffaubell@fadrell.com>
2018-01-02 15:58:38 +0100

    Traducciones.

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -2,6 +2,6 @@ module Consul
   class Application < Rails::Application
     config.i18n.default_locale = :va
     config.i18n.available_locales = [:va, :es]
-    config.i18n.fallbacks = {'va' => 'es'}
+    #config.i18n.fallbacks = {'va' => 'es'}
   end
 end
