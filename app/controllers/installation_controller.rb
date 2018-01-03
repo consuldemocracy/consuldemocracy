327817ff3631473ab5b1d6a1b6d32f4af3181391
Bertocq <bertocq@gmail.com>
2018-01-03 21:06:37 +0100

    Update version on install controller (late)

--- app/controllers/installation_controller.rb
+++ app/controllers/installation_controller.rb
@@ -12,7 +12,7 @@ class InstallationController < ApplicationController
 
   def consul_installation_details
    {
-     release: 'v0.11'
+     release: 'v0.12'
    }.merge(features: settings_feature_flags)
   end
 
