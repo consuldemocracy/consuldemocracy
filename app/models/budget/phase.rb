c0cb6bf4ac6b57eca22ce1aefd91654a7823d772
Bertocq <bertocq@gmail.com>
2018-01-15 19:38:39 +0100

    Add Budget::Phase model, spec and factory
    Create a new Budget::Phase model that:
    * Stablishes a relation with its budget
    * Stablishes relation with two other Budget::Phases (previous and next)
    * Validates basic dates range, kind and description rules.
    * Adds scopes to get the ones enabled as well as each individual phase
    
    Create a factory that generates a basic and valid Budget::Phase
    
    Create a model spec that checks kind, date range and budget validations.

---
+++ app/models/budget/phase.rb
@@ -0,0 +1,31 @@
+class Budget
+  class Phase < ActiveRecord::Base
+
+    belongs_to :budget
+    belongs_to :next_phase, class_name: 'Budget::Phase', foreign_key: :next_phase_id
+    has_one :prev_phase, class_name: 'Budget::Phase', foreign_key: :next_phase_id
+
+    validates :budget, presence: true
+    validates :kind, presence: true, uniqueness: { scope: :budget }, inclusion: { in: Budget::PHASES }
+    validates :description, length: { maximum: Budget.description_max_length }
+    validate :dates_range_valid?
+
+    scope :enabled,           -> { where(enabled: true) }
+    scope :drafting,          -> { find_by_kind('drafting') }
+    scope :accepting,         -> { find_by_kind('accepting')}
+    scope :reviewing,         -> { find_by_kind('reviewing')}
+    scope :selecting,         -> { find_by_kind('selecting')}
+    scope :valuating,         -> { find_by_kind('valuating')}
+    scope :publishing_prices, -> { find_by_kind('publishing_prices')}
+    scope :balloting,         -> { find_by_kind('balloting')}
+    scope :reviewing_ballots, -> { find_by_kind('reviewing_ballots')}
+    scope :finished,          -> { find_by_kind('finished')}
+
+    def dates_range_valid?
+      if starts_at.present? && ends_at.present? && starts_at >= ends_at
+        errors.add(:starts_at, I18n.t('budgets.phases.errors.dates_range_invalid'))
+      end
+    end
+
+  end
+end
