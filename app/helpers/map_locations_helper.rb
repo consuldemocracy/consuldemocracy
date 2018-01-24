d8b9c37bb2afc0907dc6e439dfabc8606e6a6df4
rgarcia <voodoorai2000@gmail.com>
2018-01-24 20:35:14 +0100

    revert clean coordinates

--- app/helpers/map_locations_helper.rb
+++ app/helpers/map_locations_helper.rb
@@ -60,7 +60,7 @@ module MapLocationsHelper
       latitude_input_selector: "##{map_location_input_id(parent_class, 'latitude')}",
       longitude_input_selector: "##{map_location_input_id(parent_class, 'longitude')}",
       zoom_input_selector: "##{map_location_input_id(parent_class, 'zoom')}",
-      marker_investments_coordinates: clean_coordinates(investments_coordinates)
+      marker_investments_coordinates: investments_coordinates
     }
     options[:marker_latitude] = map_location.latitude if map_location.latitude.present?
     options[:marker_longitude] = map_location.longitude if map_location.longitude.present?
