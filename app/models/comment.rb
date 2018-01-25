070c94494e6ae7838c4b7dc6afe06fe9f7a85884
Bertocq <bertocq@gmail.com>
2018-01-26 00:27:03 +0100

    Fix linelenght over 100 on multiple files

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,8 @@ class Comment < ActiveRecord::Base
   include Graphqlable
   include Notifiable
 
-  COMMENTABLE_TYPES = %w(Debate Proposal Budget::Investment Poll Topic Legislation::Question Legislation::Annotation Legislation::Proposal).freeze
+  COMMENTABLE_TYPES = %w(Debate Proposal Budget::Investment Poll Topic Legislation::Question
+                        Legislation::Annotation Legislation::Proposal).freeze
 
   acts_as_paranoid column: :hidden_at
   include ActsAsParanoidAliases
@@ -27,7 +28,9 @@ class Comment < ActiveRecord::Base
 
   scope :for_render, -> { with_hidden.includes(user: :organization) }
   scope :with_visible_author, -> { joins(:user).where("users.hidden_at IS NULL") }
-  scope :not_as_admin_or_moderator, -> { where("administrator_id IS NULL").where("moderator_id IS NULL")}
+  scope :not_as_admin_or_moderator, -> do
+    where("administrator_id IS NULL").where("moderator_id IS NULL")
+  end
   scope :sort_by_flags, -> { order(flags_count: :desc, updated_at: :desc) }
   scope :public_for_api, -> do
     where(%{(comments.commentable_type = 'Debate' and comments.commentable_id in (?)) or
