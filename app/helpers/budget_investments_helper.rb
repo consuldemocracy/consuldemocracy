fb16677e20aef67057b8216bda21dd6bf445765e
Angel Perez <iAngel.p93@gmail.com>
2018-01-30 14:19:39 -0400

    'Current applied filters' message handles multiple filters in use

--- app/helpers/budget_investments_helper.rb
+++ app/helpers/budget_investments_helper.rb
@@ -23,4 +23,7 @@ module BudgetInvestmentsHelper
     Budget::Investment::SORTING_OPTIONS.map { |so| [t("admin.budget_investments.index.sort_by.#{so}"), so] }
   end
 
+  def budget_investments_advanced_filters(params)
+    params.map { |af| t("admin.budget_investments.index.filters.#{af}") }.join(', ')
+  end
 end
