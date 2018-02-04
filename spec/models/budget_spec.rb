d08aa05c41bf3a9e82caea04480584949d169533
Bertocq <bertocq@gmail.com>
2018-02-04 22:39:26 +0100

    Use updatable slug factory trait to sluggable concern
    Slugs should only be updated on certain conditions, we need a trait that
    meets that conditions and the name of the trait passed as a mandatory &
    named argument on the sluggable concern

--- spec/models/budget_spec.rb
+++ spec/models/budget_spec.rb
@@ -4,7 +4,7 @@ describe Budget do
 
   let(:budget) { create(:budget) }
 
-  it_behaves_like "sluggable"
+  it_behaves_like "sluggable", updatable_slug_trait: :drafting
 
   describe "name" do
     before do
