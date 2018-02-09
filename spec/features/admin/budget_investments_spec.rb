1152f95965a6676f548574417f1ae11c1d423df2
rgarcia <voodoorai2000@gmail.com>
2018-02-09 21:34:57 +0100

    Display valuator group assignments

--- spec/features/admin/budget_investments_spec.rb
+++ spec/features/admin/budget_investments_spec.rb
@@ -54,7 +54,7 @@ feature 'Admin budget investments' do
       end
     end
 
-    scenario 'Displaying assignments info' do
+    scenario 'Display admin and valuator assignments' do
       budget_investment1 = create(:budget_investment, budget: budget)
       budget_investment2 = create(:budget_investment, budget: budget)
       budget_investment3 = create(:budget_investment, budget: budget)
@@ -83,6 +83,33 @@ feature 'Admin budget investments' do
       within("#budget_investment_#{budget_investment3.id}") do
         expect(page).to have_content("Gema")
         expect(page).to have_content("No valuators assigned")
+      end
+    end
+
+    scenario 'Display valuator group assignments' do
+      budget_investment1 = create(:budget_investment, budget: @budget)
+      budget_investment2 = create(:budget_investment, budget: @budget)
+      budget_investment3 = create(:budget_investment, budget: @budget)
+
+      health_group = create(:valuator_group, name: "Health")
+      culture_group = create(:valuator_group, name: "Culture")
+
+      budget_investment1.valuator_groups << health_group
+      budget_investment2.valuator_group_ids = [health_group.id, culture_group.id]
+
+      visit admin_budget_budget_investments_path(budget_id: @budget.id)
+
+      within("#budget_investment_#{budget_investment1.id}") do
+        expect(page).to have_content("Health")
+      end
+
+      within("#budget_investment_#{budget_investment2.id}") do
+        expect(page).to have_content("Health")
+        expect(page).to have_content("Culture")
+      end
+
+      within("#budget_investment_#{budget_investment3.id}") do
+        expect(page).to have_content("No valuator groups assigned")
       end
     end
 
