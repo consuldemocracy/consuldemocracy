78258fcf15fa799a58a2d57d51a68cac23af3451
rgarcia <voodoorai2000@gmail.com>
2018-02-11 23:38:14 +0100

    Revert "Revert "Add valuator groups""
    This reverts commit 9617bc62d7ad8b1ae4c8c24c2addaf72c12d6420.

--- spec/models/valuator_spec.rb
+++ spec/models/valuator_spec.rb
@@ -15,4 +15,39 @@ describe Valuator do
       expect(valuator.description_or_email).to eq(valuator.email)
     end
   end
+
+  describe "#assigned_investment_ids" do
+
+    it "returns investments assigned to a valuator" do
+      valuator = create(:valuator)
+      investment1 = create(:budget_investment)
+      investment2 = create(:budget_investment)
+      investment3 = create(:budget_investment)
+
+      investment1.valuators << valuator
+      investment2.valuators << valuator
+
+      assigned_investment_ids = valuator.assigned_investment_ids
+      expect(assigned_investment_ids).to include investment1.id
+      expect(assigned_investment_ids).to include investment2.id
+      expect(assigned_investment_ids).to_not include investment3.id
+    end
+
+    it "returns investments assigned to a valuator group" do
+      group = create(:valuator_group)
+      valuator = create(:valuator, valuator_group: group)
+
+      investment1 = create(:budget_investment)
+      investment2 = create(:budget_investment)
+      investment3 = create(:budget_investment)
+
+      investment1.valuator_groups << group
+      investment2.valuator_groups << group
+
+      assigned_investment_ids = valuator.assigned_investment_ids
+      expect(assigned_investment_ids).to include investment1.id
+      expect(assigned_investment_ids).to include investment2.id
+      expect(assigned_investment_ids).to_not include investment3.id
+    end
+  end
 end
