6d8d44a5e3c5906b5141fea78c2886d7fa07e012
Raúl Fuentes <raul.fuentes@m2c.es>
2018-02-13 13:51:47 +0100

    Added interface and methods for edit the name of a Budget::Group if the budget is still inthe drafting phase

--- app/controllers/admin/budget_groups_controller.rb
+++ app/controllers/admin/budget_groups_controller.rb
@@ -8,10 +8,22 @@ class Admin::BudgetGroupsController < Admin::BaseController
     @groups = @budget.groups.includes(:headings)
   end
 
+  def update
+    @group = Budget::Group.by_slug(params[:id]).first
+    if @group.generate_slug?
+      params[:id] = @group.generate_slug
+    end
+    @group.update(budget_group_params)
+  end
+
   private
 
     def budget_group_params
       params.require(:budget_group).permit(:name)
+    end
+
+    def load_budget
+      @budget = Budget.find_by(slug: params[:budget_id]) || Budget.find_by(id: params[:budget_id])
     end
 
 end
