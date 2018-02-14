e9c836aaa7ddb6dfab3481406f7ef11334bc9006
Fernando Faubell Valls <ffaubell@fadrell.com>
2018-02-14 15:35:52 +0100

    Revisión de traducciones y pantallas.

--- app/helpers/budget_headings_helper.rb
+++ app/helpers/budget_headings_helper.rb
@@ -1,18 +1,26 @@
 module BudgetHeadingsHelper
 
   def budget_heading_select_options(budget)
+    budget.headings.order_by_group_name.map do |heading|
+      [heading.name_scoped_by_group, heading.id]
+    end
+  end
+
+  def custom_budget_heading_select_options(budget)
     aux = []
     groups_ids = budget.investments.where(author_id: current_user.id).map(&:group_id)
-    budget.groups.where.not(id: groups_ids).each do |g|
+    groups_allowed = budget.groups.where.not(id: groups_ids)
+    groups_allowed.each do |g|
       g.headings.each do |heading|
         aux << [heading.name_scoped_by_group, heading.id]
       end
     end
     aux
+  end
 
-    # budget.headings.order_by_group_name.map do |heading|
-    #   [heading.name_scoped_by_group, heading.id]
-    # end
+  def groups_not_allowed(budget)
+    groups_ids = budget.investments.where(author_id: current_user.id).map(&:group_id)
+    budget.groups.where(id: groups_ids)
   end
 
 end
