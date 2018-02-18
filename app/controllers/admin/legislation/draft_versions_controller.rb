1b91b43d1b624afe3e76aeaef638a9012e675b6c
Bertocq <bertocq@gmail.com>
2018-02-18 23:54:37 +0100

    Fix line lenght offenses at multiple controllers

--- app/controllers/admin/legislation/draft_versions_controller.rb
+++ app/controllers/admin/legislation/draft_versions_controller.rb
@@ -9,7 +9,8 @@ class Admin::Legislation::DraftVersionsController < Admin::Legislation::BaseCont
   def create
     if @draft_version.save
       link = legislation_process_draft_version_path(@process, @draft_version).html_safe
-      redirect_to admin_legislation_process_draft_versions_path, notice: t('admin.legislation.draft_versions.create.notice', link: link)
+      notice = t('admin.legislation.draft_versions.create.notice', link: link)
+      redirect_to admin_legislation_process_draft_versions_path, notice: notice
     else
       flash.now[:error] = t('admin.legislation.draft_versions.create.error')
       render :new
@@ -20,7 +21,8 @@ class Admin::Legislation::DraftVersionsController < Admin::Legislation::BaseCont
     if @draft_version.update(draft_version_params)
       link = legislation_process_draft_version_path(@process, @draft_version).html_safe
       notice = t('admin.legislation.draft_versions.update.notice', link: link)
-      redirect_to edit_admin_legislation_process_draft_version_path(@process, @draft_version), notice: notice
+      edit_path = edit_admin_legislation_process_draft_version_path(@process, @draft_version)
+      redirect_to edit_path, notice: notice
     else
       flash.now[:error] = t('admin.legislation.draft_versions.update.error')
       render :edit
@@ -29,7 +31,8 @@ class Admin::Legislation::DraftVersionsController < Admin::Legislation::BaseCont
 
   def destroy
     @draft_version.destroy
-    redirect_to admin_legislation_process_draft_versions_path, notice: t('admin.legislation.draft_versions.destroy.notice')
+    notice = t('admin.legislation.draft_versions.destroy.notice')
+    redirect_to admin_legislation_process_draft_versions_path, notice: notice
   end
 
   private
