haj/consul-1

Added newsletter migration

db/migrate/20180205170054_create_newsletters.rb
@@ -1,0 +1,13 @@
+class CreateNewsletters < ActiveRecord::Migration
+  def change
+    create_table :newsletters do |t|
+      t.string :subject
+      t.integer :recipient
+      t.string :from
+      t.text :body
+      t.date :sent_at, default: nil
+
+      t.timestamps null: false
+    end
+  end
+end

consuldemocracy/consuldemocracy

db/migrate/20180205170054_create_newsletters.rb
@@ -1,0 +1,13 @@
+class CreateNewsletters < ActiveRecord::Migration
+  def change
+    create_table :newsletters do |t|
+      t.string :subject
+      t.integer :segment_recipient
+      t.string :from
+      t.text :body
+      t.date :sent_at, default: nil
+
+      t.timestamps null: false
+    end
+  end
+end