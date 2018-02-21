38f7a222f5a9db13bcd8bc78bc8df61fe4063d1d
Bertocq <bertocq@gmail.com>
2018-02-21 01:02:51 +0100

    Add error message for invalid Newsletter user segment
    Instead of the name of the User Segment we'll display an error message

--- app/helpers/user_segments_helper.rb
+++ app/helpers/user_segments_helper.rb
@@ -4,4 +4,12 @@ module UserSegmentsHelper
       [t("admin.segment_recipient.#{user_segment_name}"), user_segment_name]
     end
   end
+
+  def segment_name(user_segment)
+    if user_segment && UserSegments.respond_to?(user_segment)
+      I18n.t("admin.segment_recipient.#{user_segment}")
+    else
+      I18n.t("admin.segment_recipient.invalid_recipients_segment")
+    end
+  end
 end
