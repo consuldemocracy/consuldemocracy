3d9131a963ebf852e6382933e9cc2076e42bd23d
Bertocq <bertocq@gmail.com>
2018-03-01 23:46:47 +0100

    Cleanup spec files from I18n calls

--- spec/features/admin/budget_groups_spec.rb
+++ spec/features/admin/budget_groups_spec.rb
@@ -6,25 +6,25 @@ feature 'Admin can change the groups name' do
   let(:group) { create(:budget_group, budget: budget) }
 
   background do
-
     admin = create(:administrator)
     login_as(admin.user)
   end
 
   scenario "Show button" do
     visit admin_budget_path(group.budget)
+
     within("#budget_group_#{group.id}") do
-      expect(page).to have_content(I18n.t("admin.budgets.form.edit_group" ))
+      expect(page).to have_content('Edit group')
     end
   end
 
   scenario "Change name", :js do
     visit admin_budget_path(group.budget)
     within("#budget_group_#{group.id}") do
-      click_link I18n.t("admin.budgets.form.edit_group" )
+      click_link 'Edit group'
       within("#group-form-#{group.id}") do
         fill_in 'budget_group_name', with: 'Google'
-        click_button I18n.t("admin.budgets.form.submit" )
+        click_button 'Save group'
       end
     end
 
@@ -37,10 +37,10 @@ feature 'Admin can change the groups name' do
     visit admin_budget_path(group.budget)
 
     within("#budget_group_#{group.id}") do
-      click_link I18n.t("admin.budgets.form.edit_group" )
+      click_link 'Edit group'
       within("#group-form-#{group.id}") do
         fill_in 'budget_group_name', with: 'Google'
-        click_button I18n.t("admin.budgets.form.submit" )
+        click_button 'Save group'
       end
     end
 
@@ -53,15 +53,16 @@ feature 'Admin can change the groups name' do
   scenario "Can't repeat names", :js  do
     group.budget.groups << create(:budget_group, name: 'group_name')
     visit admin_budget_path(group.budget)
+
     within("#budget_group_#{group.id}") do
-      click_link I18n.t("admin.budgets.form.edit_group" )
+      click_link 'Edit group'
       within("#group-form-#{group.id}") do
         fill_in 'budget_group_name', with: 'group_name'
-        click_button I18n.t("admin.budgets.form.submit" )
+        click_button 'Save group'
       end
     end
 
-    expect(page).to have_content(I18n.t("errors.messages.taken"))
+    expect(page).to have_content('has already been taken')
   end
 
 end
