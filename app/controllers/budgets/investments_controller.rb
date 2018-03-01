65e236065086b90328fd92bf879adc76bf1382bc
rgarcia <voodoorai2000@gmail.com>
2018-03-01 22:20:07 +0100

    Use a float smaller than 1 as a random seed
    By using a random seed value smaller than 1, we solve the previous
    situation[1] in a simpler way
    
    This test is now obsolete.
    It’s hard to write a tests to verify that even with a big seed in
    params, we will covert it to a float smaller than 1.
    
    We should refactor these `set_random_seed` methods into a nice model or
    controller concern and test it thoroughly
    
    [1]
    https://github.com/AyuntamientoMadrid/consul/commit/ba3bf11526fc6ce9c66f
    647c414946c61ff945fe

--- app/controllers/budgets/investments_controller.rb
+++ app/controllers/budgets/investments_controller.rb
@@ -111,8 +111,8 @@ module Budgets
 
       def set_random_seed
         if params[:order] == 'random' || params[:order].blank?
-          seed = params[:random_seed] || session[:random_seed] || rand(-100000..100000)
-          params[:random_seed] = Float(seed) / 1000000 rescue 0
+          seed = params[:random_seed] || session[:random_seed] || rand
+          params[:random_seed] = seed
           session[:random_seed] = params[:random_seed]
         else
           params[:random_seed] = nil
