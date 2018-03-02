237f5804d0b94281bae72fb862c5e73aaf330351
Alberto Calderón Queimadelos <bertocq@gmail.com>
2018-03-02 11:23:45 +0100

    Revert "Fix incorrect URLs for attachments (documents)"

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,7 +1,7 @@
 class Document < ActiveRecord::Base
   include DocumentsHelper
   include DocumentablesHelper
-  has_attached_file :attachment, url: "/system/:class/:prefix/:style/:basename.:extension",
+  has_attached_file :attachment, url: "/system/:class/:prefix/:style/:hash.:extension",
                                  hash_data: ":class/:style/:custom_hash_data",
                                  use_timestamp: false,
                                  hash_secret: Rails.application.secrets.secret_key_base
