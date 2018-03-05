fb20c5e3b10e7f4f0688ecfa9a4811377faa3507
Emilien <emilien@happy-dev.fr>
2018-03-05 17:25:05 +0100

    Connect external code quality services (#19)
    * Change travis notification to email
    
    * Enable :en locales for tests
    
    * Add badges in README
    
    * Send test coverage reports to Codeclimate

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,7 +1,7 @@
 module Consul
   class Application < Rails::Application
     config.i18n.default_locale = :fr
-    config.i18n.available_locales = [:fr ]
+    config.i18n.available_locales = [:fr, :en]
     config.time_zone = 'Europe/Paris'
   end
 end
