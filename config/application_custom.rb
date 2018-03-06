28f77e092a00020a9fd75c8724eb021e238dcb9a
Emilien Ah-Kiem <emilien@happy-dev.fr>
2018-03-06 13:00:33 +0100

    Fix - available locales

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,7 +1,11 @@
 module Consul
   class Application < Rails::Application
     config.i18n.default_locale = :fr
-    config.i18n.available_locales = [:fr, :en]
     config.time_zone = 'Europe/Paris'
+    if Rails.env == 'test'
+      config.i18n.available_locales = [:fr, :en]
+    else
+      config.i18n.available_locales = [:fr]
+    end
   end
 end
