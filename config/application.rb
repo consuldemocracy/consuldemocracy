0e1d5d95a7f7f3099fb246e790de1f809da6e2d0
Alberto Calderón Queimadelos <bertocq@gmail.com>
2018-03-08 17:03:38 +0100

    Revert "Make config.time_zone configurable at secrets.yml"

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Consul
 
     # Add lib to the autoload path
     config.autoload_paths << Rails.root.join('lib')
-    config.time_zone = Rails.application.secrets.time_zone
+    config.time_zone = 'Madrid'
     config.active_job.queue_adapter = :delayed_job
 
     # CONSUL specific custom overrides
