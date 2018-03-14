2ff55157532d5882d3e6976c8b491c5cb899e5e7
Raúl Fuentes <raul.fuentes@m2c.es>
2018-03-14 12:55:12 +0100

    Add test

--- spec/features/management/account_spec.rb
+++ spec/features/management/account_spec.rb
@@ -8,23 +8,32 @@ feature 'Account' do
 
   scenario "Should not allow unverified users to edit their account" do
     user = create(:user)
+
     login_managed_user(user)
+    click_link "Edit email"
 
-    visit management_root_path
+    expect(page).to have_content "No verified user logged in yet"
 
-    click_link 'Reset password via email'
+    click_link "Delete account"
 
     expect(page).to have_content "No verified user logged in yet"
   end
 
-  scenario 'Delete a user account', :js do
+  scenario 'Delete user account', :js do
     user = create(:user, :level_two)
     login_managed_user(user)
 
     visit management_account_path
 
-    click_link "Delete user"
-    accept_confirm { click_link "Delete account" }
+    within '.is-active' do
+      click_link "Delete account"
+    end
+
+    accept_alert do
+      within '#erase-account-form' do
+        click_link "Delete account"
+      end
+    end
 
     expect(page).to have_content "User account deleted."
 
@@ -102,6 +111,39 @@ feature 'Account' do
     expect(page).to have_content 'Password reseted successfully'
     expect(page).to have_css("a[href='javascript:window.print();']", text: 'Print password')
     expect(page).to have_css("div.for-print-only", text: 'another_new_password', visible: false)
+  end
+
+  scenario 'Update user email', :js do
+    user = create(:user, :level_two)
+    login_managed_user(user)
+
+    visit management_account_path
+
+    click_link "Edit email"
+
+    fill_in 'user_email', with: 'new@mail.com'
+
+    click_button "Update email"
+
+    expect(user.reload.unconfirmed_email).to eq 'new@mail.com'
+
+  end
+
+  scenario 'Update user email errors', :js do
+    user = create(:user, :level_two)
+    user1 = create(:user)
+    login_managed_user(user)
+
+    visit management_account_path
+
+    click_link "Edit email"
+
+    fill_in 'user_email', with: user1.email
+
+    click_button "Update email"
+
+    expect(page).to have_content 'has already been taken'
+
   end
 
 end
