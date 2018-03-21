2cb59260e9e71039c29bd880576e935afd70881e
María Checa <MariaCheca@users.noreply.github.com>
2018-03-21 20:16:38 +0100

    Add Budget::Investment::Statuses controller and routes

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -55,6 +55,8 @@ namespace :admin do
     resources :budget_phases, only: [:edit, :update]
   end
 
+  resources :budget_investment_statuses, only: [:index, :new, :create, :update, :edit, :destroy]
+
   resources :signature_sheets, only: [:index, :new, :create, :show]
 
   resources :banners, only: [:index, :new, :create, :edit, :update, :destroy] do
