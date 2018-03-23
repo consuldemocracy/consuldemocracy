4a14971c92b19e0c676f0b3dcf2458a35a5f4204
Claire Zuliani <clairezed@users.noreply.github.com>
2018-03-23 15:07:10 +0100

    Mobile menu (#37)
    * Adapt main menu to mobile (#5)
    
    * Add FAQ link in help page menu for mobile (#5)
    
    * Adapt admin top-bar-title for mobile  (#5)
    
    * Adapt dropdown admin menu for mobile and clean management layout  (#5)

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -2,7 +2,8 @@ module Consul
   class Application < Rails::Application
     config.i18n.default_locale = :fr
     config.time_zone = 'Europe/Paris'
-    if Rails.env == 'test'
+    # English needed for Faver dev_seeds
+    if Rails.env.test? || Rails.env.development?
       config.i18n.available_locales = [:fr, :en]
     else
       config.i18n.available_locales = [:fr]
