35c156ba7ae269cc97f894658c07c0e143b95fa3
Bertocq <bertocq@gmail.com>
2018-04-06 11:41:04 +0200

    Create setting to enable/disable attached documents
    Add setting to both seed and dev_seeds as well as a rake task to make it
    easier to set.

--- db/dev_seeds/settings.rb
+++ db/dev_seeds/settings.rb
@@ -43,6 +43,7 @@ section "Creating Settings" do
   Setting.create(key: 'feature.community', value: "true")
   Setting.create(key: 'feature.map', value: "true")
   Setting.create(key: 'feature.allow_images', value: "true")
+  Setting.create(key: 'feature.allow_attached_documents', value: "true")
   Setting.create(key: 'feature.public_stats', value: "true")
   Setting.create(key: 'feature.guides', value: nil)
 
