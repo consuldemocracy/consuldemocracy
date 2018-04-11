384b01b20b1dfd8b117a6a56cfa2c7de3280b6bf
Bertocq <bertocq@gmail.com>
2018-04-11 17:32:13 +0200

    Extract Budget Investment to_csv to its own class
    The csv generation doesn't seem like a Model concern, at least not taking
    into account the amount of lines of the method (36+). Just a simple ruby
    class that encapsulates the logic makes it easier to read and maintain as
    we increase the columns exported.. also customize in case other forks need
    different values.

--- app/controllers/admin/budget_investments_controller.rb
+++ app/controllers/admin/budget_investments_controller.rb
@@ -20,7 +20,7 @@ class Admin::BudgetInvestmentsController < Admin::BaseController
       format.html
       format.js
       format.csv do
-        send_data Budget::Investment.to_csv(@investments),
+        send_data Budget::Investment::Exporter.new(@investments).to_csv,
                   filename: 'budget_investments.csv'
       end
     end
