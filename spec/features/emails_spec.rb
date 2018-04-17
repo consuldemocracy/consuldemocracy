84b616256f02ec0f30dfa363a5d07c811f4115be
Angel Perez <iAngel.p93@gmail.com>
2018-04-17 15:56:47 -0400

    Fix spec to properly check emails are sent based on current locale

--- spec/features/emails_spec.rb
+++ spec/features/emails_spec.rb
@@ -187,6 +187,19 @@ feature 'Emails' do
     end
   end
 
+  scenario "Email depending on user's locale" do
+    visit root_path(locale: :es)
+
+    click_link 'Registrarse'
+    fill_in_signup_form
+    click_button 'Registrarse'
+
+    email = open_last_email
+    expect(email).to deliver_to('manuela@consul.dev')
+    expect(email).to have_body_text(user_confirmation_path)
+    expect(email).to have_subject('Instrucciones de confirmación')
+  end
+
   scenario "Email on unfeasible spending proposal" do
     Setting["feature.spending_proposals"] = true
 
