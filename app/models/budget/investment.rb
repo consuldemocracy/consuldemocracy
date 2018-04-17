70012320031a10e6df20751e933a7fd750406fe2
Fernando Faubell Valls <ffaubell@fadrell.com>
2018-04-17 13:16:45 +0200

    Correcciones en listas.

--- app/models/budget/investment.rb
+++ app/models/budget/investment.rb
@@ -76,10 +76,12 @@ class Budget
     scope :undecided,                   -> { where(feasibility: "undecided") }
     scope :with_supports,               -> { where('cached_votes_up > 0') }
     scope :selected,                    -> { feasible.where(selected: true) }
+    scope :selected_undecided,          -> { where(selected: true) }
     scope :compatible,                  -> { where(incompatible: false) }
     scope :incompatible,                -> { where(incompatible: true) }
     scope :winners,                     -> { selected.compatible.where(winner: true) }
     scope :unselected,                  -> { not_unfeasible.where(selected: false) }
+    scope :unselected_undecided,        -> { where(selected: false) }
     scope :last_week,                   -> { where("created_at >= ?", 7.days.ago)}
 
     scope :by_group,    ->(group_id)    { where(group_id: group_id) }
