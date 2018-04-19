c9bba03667a50fd405b14e9a8d26a1ddf55f112b
María Checa <MariaCheca@users.noreply.github.com>
2018-04-19 16:36:41 +0200

    Add condition for `max_votable_headings` and `max_supportable_headings`
    Added to model the condition for these attributes to be greater or equal to 1.

--- app/models/budget/group.rb
+++ app/models/budget/group.rb
@@ -9,6 +9,8 @@ class Budget
     validates :budget_id, presence: true
     validates :name, presence: true, uniqueness: { scope: :budget }
     validates :slug, presence: true, format: /\A[a-z0-9\-_]+\z/
+    validates :max_votable_headings, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+    validates :max_supportable_headings, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 
     scope :by_slug, ->(slug) { where(slug: slug) }
 
--- spec/models/budget/group_spec.rb
+++ spec/models/budget/group_spec.rb
@@ -1,23 +1,39 @@
 require 'rails_helper'
 
 describe Budget::Group do
-
-  let(:budget) { create(:budget) }
-
   it_behaves_like "sluggable", updatable_slug_trait: :drafting_budget
 
-  describe "name" do
-    before do
-      create(:budget_group, budget: budget, name: 'object name')
+  describe "Validations" do
+
+    let(:budget) { create(:budget) }
+    let(:group) { create(:budget_group, budget: budget) }
+
+    describe "name" do
+      before do
+        create(:budget_group, budget: budget, name: 'object name')
+      end
+
+      it "can be repeatead in other budget's groups" do
+        expect(build(:budget_group, budget: create(:budget), name: 'object name')).to be_valid
+      end
+
+      it "must be unique among all budget's groups" do
+        expect(build(:budget_group, budget: budget, name: 'object name')).not_to be_valid
+      end
     end
 
-    it "can be repeatead in other budget's groups" do
-      expect(build(:budget_group, budget: create(:budget), name: 'object name')).to be_valid
+    describe "max_supportable_headings" do
+      it "is invalid if its not greater than 1" do
+        group.max_supportable_headings = 0
+        expect(group).not_to be_valid
+      end
     end
 
-    it "must be unique among all budget's groups" do
-      expect(build(:budget_group, budget: budget, name: 'object name')).not_to be_valid
+    describe "max_votable_headings" do
+      it "is invalid if its not greater than 1" do
+        group.max_votable_headings = 0
+        expect(group).not_to be_valid
+      end
     end
   end
-
 end
