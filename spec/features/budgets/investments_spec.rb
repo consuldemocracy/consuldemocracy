833670117ea47197b0d88d9f7b7c2310919274a5
Julian Herrero <microweb10@gmail.com>
2018-04-30 13:59:57 +0700

    add test for not selected bugets investments

--- spec/features/budgets/investments_spec.rb
+++ spec/features/budgets/investments_spec.rb
@@ -925,6 +925,23 @@ feature 'Budget Investments' do
     expect(page).to have_content("This investment project has been marked as not feasible and will not go to balloting phase")
   end
 
+  scenario "Show (not selected budget investment)" do
+    user = create(:user)
+    login_as(user)
+
+    investment = create(:budget_investment,
+                        :feasible,
+                        :finished,
+                        budget: budget,
+                        group: group,
+                        heading: heading,
+                        unfeasibility_explanation: 'Local government is not competent in this matter')
+
+    visit budget_investment_path(budget_id: budget.id, id: investment.id)
+
+    expect(page).to have_content("This investment project has not been selected for balloting phase")
+  end
+
   scenario "Show (unfeasible budget investment with valuation not finished)" do
     user = create(:user)
     login_as(user)
