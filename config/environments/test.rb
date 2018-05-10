2220ec43707bdb4de7c66eb6dc5dd6a3f72dbe65
Angel Perez <iAngel.p93@gmail.com>
2018-05-10 14:49:47 -0400

    Config class caching as false, eager load as true on test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,12 +5,12 @@ Rails.application.configure do
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs. Don't rely on the data there!
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
+  config.eager_load = true
 
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
