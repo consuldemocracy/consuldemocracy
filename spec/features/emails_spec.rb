6ce6b548b07b614ff84f5f9a070d165e5a9572dd
decabeza <alberto@decabeza.es>
2018-05-10 19:33:30 +0200

    Updates i18n texts on specs

--- spec/features/emails_spec.rb
+++ spec/features/emails_spec.rb
@@ -336,7 +336,7 @@ feature 'Emails' do
       visit new_management_user_invite_path
 
       fill_in "emails", with: " john@example.com, ana@example.com,isable@example.com "
-      click_button "Send invites"
+      click_button "Send invitations"
 
       expect(page).to have_content "3 invitations have been sent."
 
