e38ec6040b85e0df1edf1dc4b7e60c1afc93ef17
decabeza <alberto@decabeza.es>
2018-05-10 19:33:30 +0200

    Updates i18n texts on specs

--- spec/support/common_actions.rb
+++ spec/support/common_actions.rb
@@ -458,7 +458,7 @@ module CommonActions
     visit new_management_user_invite_path
 
     fill_in "emails", with: "john@example.com, ana@example.com, isable@example.com"
-    click_button "Send invites"
+    click_button "Send invitations"
 
     expect(page).to have_content "3 invitations have been sent."
   end
