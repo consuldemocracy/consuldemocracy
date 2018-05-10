37d1ec0077e911af26e72f1a0cae80ac53cabe65
rgarcia <voodoorai2000@gmail.com>
2018-05-10 15:01:13 +0200

    Send newsletter in batches
    To avoid being marked as spam, we have to send emails in batches and in
    intervals
    
    We are using Madrid’s forks as reference which has 400.000 users and
    sending emails in batches of 10.000 users every 20 minutes

--- spec/models/newsletter_spec.rb
+++ spec/models/newsletter_spec.rb
@@ -63,4 +63,56 @@ describe Newsletter do
       expect(newsletter.list_of_recipient_emails).not_to include('erased_user@consul.dev')
     end
   end
+
+  describe "#deliver" do
+    let!(:proposals) { Array.new(3) { create(:proposal) } }
+    let!(:debate) { create(:debate) }
+
+    let!(:recipients) { proposals.map(&:author).map(&:email) }
+    let!(:newsletter) { create(:newsletter, segment_recipient: "proposal_authors") }
+
+    before do
+      reset_mailer
+      Delayed::Worker.delay_jobs = true
+    end
+
+    after do
+      Delayed::Worker.delay_jobs = false
+    end
+
+    it "sends an email with the newsletter to every recipient" do
+      newsletter.deliver
+
+      recipients.each do |recipient|
+        email = Mailer.newsletter(newsletter, recipient)
+        expect(email).to deliver_to(recipient)
+      end
+
+      Delayed::Job.all.map(&:invoke_job)
+      expect(ActionMailer::Base.deliveries.count).to eq(3)
+    end
+
+    it "sends emails in batches" do
+      allow(newsletter).to receive(:batch_size).and_return(1)
+
+      newsletter.deliver
+
+      expect(Delayed::Job.count).to eq(3)
+    end
+
+    it "sends batches in time intervals" do
+      allow(newsletter).to receive(:batch_size).and_return(1)
+      allow(newsletter).to receive(:batch_interval).and_return(1.second)
+      allow(newsletter).to receive(:first_batch_run_at).and_return(Time.current)
+
+      newsletter.deliver
+
+      now = newsletter.first_batch_run_at
+      expect(Delayed::Job.count).to eq(3)
+      expect(Delayed::Job.first.run_at).to eq(now)
+      expect(Delayed::Job.second.run_at).to eq(now + 1.second)
+      expect(Delayed::Job.third.run_at).to eq(now + 2.seconds)
+    end
+
+  end
 end
