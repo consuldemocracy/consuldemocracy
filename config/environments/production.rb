7aef6039cf31b7c754c534c34225e63f0e69a4b8
Senén Rodero Rodríguez <senenrodero@gmail.com>
2018-05-18 13:09:47 +0200

    Install aws-ses gem for production email delivery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,8 +60,11 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+  # Configure AWS SES (Simple Email Service)
+  config.action_mailer.delivery_method = :ses
+
   # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.  
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: Rails.application.secrets.server_name }
   config.action_mailer.asset_host = "https://#{Rails.application.secrets.server_name}"
