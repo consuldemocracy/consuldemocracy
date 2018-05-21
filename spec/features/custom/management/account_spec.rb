63ba541ac929ae01c33f5fbd3711c28aad42505b
taitus <sebastia.roig@gmail.com>
2018-05-21 18:21:24 +0200

    Add custom specs. Minor styles changes

---
+++ spec/features/custom/management/account_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature 'Account' do
+
+  background do
+    Setting["org_name"] = 'MASDEMOCRACIAEUROPA'
+    login_as_manager
+  end
+
+  scenario "Display custom menu when org_name is MASDEMOCRACIAEUROPA" do
+    user = create(:user)
+    login_managed_user(user)
+
+    visit management_root_path
+
+    expect(page).to have_content "Edit user account"
+    expect(page).to have_content "Create proposal"
+    expect(page).to have_content "Print proposals"
+    expect(page).to have_content "User's invites"
+    expect(page).not_to have_content "Users"
+    expect(page).not_to have_content "Support proposals"
+    expect(page).not_to have_content "Create spending proposals"
+    expect(page).not_to have_content "Support spending proposals"
+    expect(page).not_to have_content "Print spending proposals"
+
+  end
+
+end
