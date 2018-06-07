06f0567f294a08d1c339ed9f885391d3848b75b0
María Checa <MariaCheca@users.noreply.github.com>
2018-06-07 18:31:57 +0200

    Adds "Admin ballots" button in admin budgets index

--- spec/features/admin/budgets_spec.rb
+++ spec/features/admin/budgets_spec.rb
@@ -81,6 +81,16 @@ feature 'Admin budgets' do
       end
     end
 
+    scenario 'Admin ballots link appears if budget has a poll associated' do
+      budget = create(:budget)
+      create(:poll, budget: budget)
+
+      visit admin_budgets_path
+
+      within "#budget_#{budget.id}" do
+        expect(page).to have_link("Admin ballots")
+      end
+    end
   end
 
   context 'New' do
