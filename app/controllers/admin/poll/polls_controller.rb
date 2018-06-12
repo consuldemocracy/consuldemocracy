6fa75621814af1e12afbd529ccb9c3635c54abfe
rgarcia <voodoorai2000@gmail.com>
2018-06-12 13:41:03 +0200

    Automatically create a budget poll if it does not exist

--- app/controllers/admin/poll/polls_controller.rb
+++ app/controllers/admin/poll/polls_controller.rb
@@ -63,7 +63,7 @@ class Admin::Poll::PollsController < Admin::Poll::BaseController
 
     def poll_params
       attributes = [:name, :starts_at, :ends_at, :geozone_restricted, :results_enabled,
-                    :stats_enabled, geozone_ids: [],
+                    :stats_enabled, :budget_id, geozone_ids: [],
                     image_attributes: image_attributes]
       params.require(:poll).permit(*attributes, translation_params(Poll))
     end
