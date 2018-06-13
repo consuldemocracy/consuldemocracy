f8375a92d1096d20603e45ed72c14e73662624c5
Angel Perez <iAngel.p93@gmail.com>
2018-06-13 13:41:21 -0400

    Enable moderation abilities for Budget::Investment model

--- app/models/abilities/administrator.rb
+++ app/models/abilities/administrator.rb
@@ -21,6 +21,9 @@ module Abilities
       can :restore, Legislation::Proposal
       cannot :restore, Legislation::Proposal, hidden_at: nil
 
+      can :restore, Budget::Investment
+      cannot :restore, Budget::Investment, hidden_at: nil
+
       can :restore, User
       cannot :restore, User, hidden_at: nil
 
@@ -35,6 +38,9 @@ module Abilities
 
       can :confirm_hide, Legislation::Proposal
       cannot :confirm_hide, Legislation::Proposal, hidden_at: nil
+
+      can :confirm_hide, Budget::Investment
+      cannot :confirm_hide, Budget::Investment, hidden_at: nil
 
       can :confirm_hide, User
       cannot :confirm_hide, User, hidden_at: nil
