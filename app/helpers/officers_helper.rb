3bf60ff00d3a5403a5685d22983928bf08c2f4f7
decabeza <alberto@decabeza.es>
2018-06-20 14:37:36 +0200

    Hides sibebar menu if officer has no shifts

--- app/helpers/officers_helper.rb
+++ app/helpers/officers_helper.rb
@@ -5,7 +5,7 @@ module OfficersHelper
   end
 
   def vote_collection_shift?
-    current_user.poll_officer.officer_assignments.where(date: Time.current.to_date).any?
+    current_user.poll_officer.officer_assignments.voting_days.where(date: Time.current.to_date).any?
   end
 
   def final_recount_shift?
