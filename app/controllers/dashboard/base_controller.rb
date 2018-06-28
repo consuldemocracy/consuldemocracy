59eb1a4f478908efc9dcb23ebfc2855774b95309
Juan Salvador Pérez García <jsperezg@gmail.com>
2018-06-28 11:33:42 +0200

    Adds progress indicator

--- app/controllers/dashboard/base_controller.rb
+++ app/controllers/dashboard/base_controller.rb
@@ -1,7 +1,7 @@
 class Dashboard::BaseController < ApplicationController
   before_action :authenticate_user!
 
-  helper_method :proposal, :proposed_actions, :resources
+  helper_method :proposal, :proposed_actions, :resource, :resources, :next_goal_supports, :next_goal_progress
 
   respond_to :html
   layout 'proposals_dashboard'
@@ -18,5 +18,13 @@ class Dashboard::BaseController < ApplicationController
 
   def resources
     @resources ||= ProposalDashboardAction.resources.active_for(proposal)
+  end
+
+  def next_goal_supports
+    @next_goal_supports ||= ProposalDashboardAction.next_goal_for(proposal)&.required_supports || Setting["votes_for_proposal_success"]
+  end
+
+  def next_goal_progress
+    @next_goal_progress ||= (proposal.votes_for.size * 100) / next_goal_supports.to_i
   end
 end
