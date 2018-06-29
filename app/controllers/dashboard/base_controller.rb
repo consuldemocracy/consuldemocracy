53baf983b581c4114e9e09d878cc2f63ff08f7ea
Juan Salvador Pérez García <jsperezg@gmail.com>
2018-06-29 12:56:20 +0200

    Implements #169
    Community counter now shows the community members. For performance
    reasons this information is refresed once per hour.

--- app/controllers/dashboard/base_controller.rb
+++ app/controllers/dashboard/base_controller.rb
@@ -1,7 +1,7 @@
 class Dashboard::BaseController < ApplicationController
   before_action :authenticate_user!
 
-  helper_method :proposal, :proposed_actions, :resource, :resources, :next_goal_supports, :next_goal_progress
+  helper_method :proposal, :proposed_actions, :resource, :resources, :next_goal_supports, :next_goal_progress, :community_members_count
 
   respond_to :html
   layout 'proposals_dashboard'
@@ -9,7 +9,7 @@ class Dashboard::BaseController < ApplicationController
   private
 
   def proposal
-    @proposal ||= Proposal.find(params[:proposal_id])
+    @proposal ||= Proposal.includes(:community).find(params[:proposal_id])
   end
 
   def proposed_actions
@@ -26,5 +26,11 @@ class Dashboard::BaseController < ApplicationController
 
   def next_goal_progress
     @next_goal_progress ||= (proposal.votes_for.size * 100) / next_goal_supports.to_i
+  end
+
+  def community_members_count
+    Rails.cache.fetch("community/#{proposal.community.id}/participants_count", expires_in: 1.hour) do
+      proposal.community.participants.count
+    end
   end
 end
