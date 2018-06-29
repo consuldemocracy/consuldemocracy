54c82a53d7de32da5cc1edfda7300148b0545028
Javier Martín <javier@elretirao.net>
2018-06-29 18:46:40 +0200

    Precompile CKEditor plugins in use.
    As stated in #1196, compiling everything related to CKEditor made
    compilations slower. However, not compiling any plugins meant Travis had
    to compile them while running a test. It often resulted in a test
    failing because the time Travis took to compile the plugins the
    application uses exceeded Capybara's wait time.

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,4 +1,4 @@
 Ckeditor.setup do |config|
   config.assets_languages = Rails.application.config.i18n.available_locales.map{|l| l.to_s.downcase}
-  config.assets_plugins = []
+  config.assets_plugins = %w[copyformatting tableselection scayt wsc]
 end
