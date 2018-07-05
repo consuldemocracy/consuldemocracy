5e6498d4824b361e901339fed9d20167852166cf
María Checa <MariaCheca@users.noreply.github.com>
2018-07-05 18:40:17 +0200

    Add budget stats permissions for normal users and admins

--- app/models/abilities/everyone.rb
+++ app/models/abilities/everyone.rb
@@ -26,7 +26,7 @@ module Abilities
       can [:read], Budget::Group
       can [:read, :print, :json_data], Budget::Investment
       can :read_results, Budget, phase: "finished"
-      can :read_stats, Budget, phase: ['reviewing_ballots', 'finished']
+      can :read_stats, Budget, phase: "finished"
       can :new, DirectMessage
       can [:read, :debate, :draft_publication, :allegations, :result_publication, :proposals], Legislation::Process, published: true
       can [:read, :changes, :go_to_version], Legislation::DraftVersion
