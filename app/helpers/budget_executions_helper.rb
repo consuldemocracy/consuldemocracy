749954267de78e6d37f587fd21fdd8366fc61cbc
Angel Perez <iAngel.p93@gmail.com>
2018-07-11 12:41:48 -0400

    Use Budget::Investment#winners scope to fetch only winner investments

--- app/helpers/budget_executions_helper.rb
+++ app/helpers/budget_executions_helper.rb
@@ -3,15 +3,13 @@ module BudgetExecutionsHelper
   def winner_investments(heading)
     if params[:status].present?
       heading.investments
-             .selected
-             .sort_by_ballots
+             .winners
              .joins(:milestones)
              .distinct
              .where('budget_investment_milestones.status_id = ?', params[:status])
     else
       heading.investments
-             .selected
-             .sort_by_ballots
+             .winners
              .joins(:milestones)
              .distinct
     end
