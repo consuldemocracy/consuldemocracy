a3ef662509f945c1c26e3aba3545390e888ee254
Angel Perez <iAngel.p93@gmail.com>
2018-07-12 22:01:26 -0400

    Filtering investments are based on the latest milestone status

--- app/helpers/budget_executions_helper.rb
+++ app/helpers/budget_executions_helper.rb
@@ -6,13 +6,20 @@ module BudgetExecutionsHelper
              .winners
              .joins(:milestones)
              .distinct
-             .where('budget_investment_milestones.status_id = ?', params[:status])
+             .where(filter_investment_by_latest_milestone, params[:status])
     else
       heading.investments
              .winners
              .joins(:milestones)
              .distinct
     end
+  end
+
+  def filter_investment_by_latest_milestone
+    <<-SQL
+      (SELECT status_id FROM budget_investment_milestones
+       WHERE investment_id = budget_investments.id ORDER BY publication_date DESC LIMIT 1) = ?
+    SQL
   end
 
 end
