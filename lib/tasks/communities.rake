c21a08bc22e173ff5f0aec46fb6a06824ac3a16e
Javier Martín <javier@elretirao.net>
2018-07-17 14:50:10 +0200

    Add task to migrate existing data to communitable

--- lib/tasks/communities.rake
+++ lib/tasks/communities.rake
@@ -18,4 +18,27 @@ namespace :communities do
     end
   end
 
+  desc "Migrate communitable data to communitable columns"
+  task migrate_communitable: :environment do
+    (Proposal.all + Budget::Investment.all).each do |communitable|
+      if communitable.community_id.present?
+        community = Community.where(id: communitable.community_id).first
+
+        if community
+          community.update(
+            communitable_id:    communitable.id,
+            communitable_type:  communitable.class.name
+          )
+        else
+          warn "WARNING: The community associated to #{communitable.class.name} "\
+               "with ID #{communitable.id} no longer exists. "\
+               "Consider running `rake communities: associate_community`"
+        end
+      else
+        warn "WARNING: #{communitable.class.name} with ID #{communitable.id} "\
+             "should have an associated community. "\
+             "Consider running `rake communities: associate_community`"
+      end
+    end
+  end
 end
--- spec/lib/tasks/communities_spec.rb
+++ spec/lib/tasks/communities_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 require 'rake'
 
 describe 'Communities Rake' do
+  before do
+    Rake.application.rake_require "tasks/communities"
+    Rake::Task.define_task(:environment)
+  end
 
   describe '#associate_community' do
-
-    before do
-      Rake.application.rake_require "tasks/communities"
-      Rake::Task.define_task(:environment)
-    end
 
     let :run_rake_task do
       Rake::Task['communities:associate_community'].reenable
@@ -46,4 +45,79 @@ describe 'Communities Rake' do
 
   end
 
+  describe "#migrate_communitable" do
+    let :run_rake_task do
+      Rake::Task["communities:migrate_communitable"].reenable
+      Rake.application.invoke_task "communities:migrate_communitable"
+    end
+
+    context "Associate community to Proposal" do
+      let(:proposal) { create(:proposal) }
+      let(:community) { proposal.community }
+
+      context "Community has no communitable_id" do
+        before { community.update(communitable_id: nil, communitable_type: nil) }
+
+        it "assigns the communitable_id" do
+          run_rake_task
+          community.reload
+          expect(community.communitable_id).to eq proposal.id
+          expect(community.communitable_type).to eq "Proposal"
+        end
+      end
+
+      context "Proposal has no community_id" do
+        before { proposal.update_column(:community_id, nil) }
+
+        it "writes a warning with the proposal id" do
+          warning = /Proposal(.+)#{proposal.id}(.+)should have an associated/
+          expect { run_rake_task }.to output(warning).to_stderr
+        end
+      end
+
+      context "The proposal community doesn't exist anymore" do
+        before { community.destroy }
+
+        it "writes a warning with the proposal id" do
+          warning = /community(.+)Proposal(.+)#{proposal.id}(.+)no longer exists/
+          expect { run_rake_task }.to output(warning).to_stderr
+        end
+      end
+    end
+
+    context "Associate community to Budget Investment" do
+      let(:investment) { create(:budget_investment) }
+      let(:community) { investment.community }
+
+      context "Community has no communitable_id" do
+        before { community.update(communitable_id: nil, communitable_type: nil) }
+
+        it "assigns the communitable_id" do
+          run_rake_task
+          community.reload
+          expect(community.communitable_id).to eq investment.id
+          expect(community.communitable_type).to eq "Budget::Investment"
+        end
+      end
+
+      context "Investment has no community_id" do
+        before { investment.update_column(:community_id, nil) }
+
+        it "writes a warning with the investment id" do
+          warning = /Investment(.+)#{investment.id}(.+)should have an associated/
+          expect { run_rake_task }.to output(warning).to_stderr
+        end
+      end
+
+      context "The investment community doesn't exist anymore" do
+        before { community.destroy }
+
+        it "writes a warning with the investment id" do
+          warning = /community(.+)Investment(.+)#{investment.id}(.+)no longer exists/
+          expect { run_rake_task }.to output(warning).to_stderr
+        end
+      end
+    end
+
+  end
 end
