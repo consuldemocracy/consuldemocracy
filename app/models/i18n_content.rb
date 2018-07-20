c2c091bcb5c641ae139a5779643a049e42678ef8
Angel Perez <iAngel.p93@gmail.com>
2018-07-20 15:06:18 -0400

    Clean I18n codebase

--- app/models/i18n_content.rb
+++ app/models/i18n_content.rb
@@ -1,7 +1,7 @@
 class I18nContent < ActiveRecord::Base
 
   scope :by_key, -> (key){ where(key: key) }
-  scope :begins_with_key, -> (key){ where("key LIKE ?", "#{key}?%") }
+  scope :begins_with_key, -> (key){ where("key ILIKE ?", "#{key}?%") }
 
   validates :key, uniqueness: true
 
