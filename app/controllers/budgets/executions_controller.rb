fb24098605826d3edcdd83d9e16d52be2cb0844b
Marko Lovic <markolovic33@gmail.com>
2018-07-23 20:07:50 +0200

    Show city heading in first position on executions list

--- app/controllers/budgets/executions_controller.rb
+++ app/controllers/budgets/executions_controller.rb
@@ -16,6 +16,8 @@ module Budgets
       if params[:status].present?
         @headings = @headings.where(filter_investment_by_latest_milestone, params[:status])
       end
+
+      @headings = reorder_with_city_heading_first(@headings)
     end
 
     private
@@ -29,6 +31,20 @@ module Budgets
           (SELECT status_id FROM budget_investment_milestones
            WHERE investment_id = budget_investments.id ORDER BY publication_date DESC LIMIT 1) = ?
         SQL
+      end
+
+      def reorder_with_city_heading_first(original_headings)
+        headings = original_headings.to_a.dup
+
+        city_heading_index = headings.find_index do |heading|
+          heading.name == "Toda la ciudad"
+        end
+
+        if city_heading_index
+          headings.insert(0, headings.delete_at(city_heading_index))
+        else
+          headings
+        end
       end
   end
 end
