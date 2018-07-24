d340d801e207f03c4a1b9395321c19efe4bee12d
Angel Perez <iAngel.p93@gmail.com>
2018-07-24 14:51:01 -0400

    Fallback to default I18n value if translation is not available

--- config/initializers/i18n_translation.rb
+++ config/initializers/i18n_translation.rb
@@ -12,11 +12,12 @@ module ActionView
         if translation.present?
           Globalize.with_locale(locale) do
             string = I18nContent.where(key: key).first.value
+
             options.each do |key, value|
               string.sub! "%{#{key}}", (value || "%{#{key}}")
             end
 
-            return string.html_safe
+            return string.html_safe unless string.nil?
           end
         end
 
