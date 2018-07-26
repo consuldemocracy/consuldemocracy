ed5414e1a922b7fcf49ccd70f192b384463c61a6
rgarcia <voodoorai2000@gmail.com>
2018-07-27 00:13:29 +0200

    Add globalization javascript interface specs

--- app/helpers/site_customization_helper.rb
+++ app/helpers/site_customization_helper.rb
@@ -1,5 +1,5 @@
 module SiteCustomizationHelper
   def site_customization_display_translation?(locale)
-    I18nContentTranslation.existing_languages.include?(neutral_locale(locale)) ? "" : "display: none"
+    I18nContentTranslation.existing_languages.include?(neutral_locale(locale)) || locale == I18n.locale ? "" : "display: none"
   end
 end
