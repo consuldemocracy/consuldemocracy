12f6f06ade07ba33f4d9d0ee9e453a2b66cc6948
rgarcia <voodoorai2000@gmail.com>
2018-07-27 01:50:51 +0200

    Fix exception when using locale as a parameter in the url
    When visiting, for example, /admin/site_customization/information_texts?locale=fr
    we were getting an `UncaughtThrowError: uncaught throw :exception`
    With the following payload
    ```
    File "/aytomad/app/participa/participacion/releases/20180726231929/app/views/admin/site_customization/information_texts/_form_field.html.erb" line 5
    
    File "/aytomad/app/participa/participacion/releases/20180726231929/app/helpers/globalize_helper.rb" line 35 in block in globalize
    ```
    
    Substituting this line seems to solve it
    
    Note that we had to remove the portuguese local too, as it was giving a different
    exception due to this change. This problem, has been solved in the original
    globalization PR

--- app/models/i18n_content.rb
+++ app/models/i18n_content.rb
@@ -6,6 +6,6 @@ class I18nContent < ActiveRecord::Base
   validates :key, uniqueness: true
 
   translates :value, touch: true
-  globalize_accessors locales: [:en, :es, :fr, :nl, :val, :pt_br]
+  globalize_accessors locales: [:en, :es, :fr, :nl, :val]
 
 end
