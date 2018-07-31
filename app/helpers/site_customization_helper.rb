d33d66d3bc40e9ec0e94b4ebfbef023eb869e6bf
Angel Perez <iAngel.p93@gmail.com>
2018-07-31 13:54:18 -0400

    Extract translation logic to helper method

--- app/helpers/site_customization_helper.rb
+++ app/helpers/site_customization_helper.rb
@@ -7,6 +7,19 @@ module SiteCustomizationHelper
     site_customization_enable_translation?(locale) ? "" : "display: none;"
   end
 
+  def translation_for_locale(content, locale)
+    i18n_content = I18nContent.where(key: content.key).first
+
+    if i18n_content.present?
+      I18nContentTranslation.where(
+        i18n_content_id: i18n_content.id,
+        locale: locale
+      ).first.try(:value)
+    else
+      false
+    end
+  end
+
   def merge_translatable_field_options(options, locale)
     options.merge(
       class: "#{options[:class]} js-globalize-attribute".strip,
