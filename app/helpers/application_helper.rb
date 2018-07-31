87d92b255313b3eff5231a57f8826c3812dfed41
Juan Salvador Pérez García <jsperezg@gmail.com>
2018-07-31 12:59:02 +0200

    Fixed issue with sprockets in production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -62,7 +62,7 @@ module ApplicationHelper
   end
 
   def self.asset_data_base64(path)
-    asset = Rails.application.assets.find_asset(path)
+    asset = Rails.application.assets_manifest.find(path).first
     throw "Could not find asset '#{path}'" if asset.nil?
     base64 = Base64.encode64(asset.to_s).gsub(/\s+/, "")
     "data:#{asset.content_type};base64,#{Rack::Utils.escape(base64)}"
