a7a2efd097e7325cf876b14df690d94a2e8ac6ca
Julian Herrero <microweb10@gmail.com>
2018-08-02 18:51:19 +0200

    Make answers translatable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180813141443) do
+ActiveRecord::Schema.define(version: 20180801114529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
