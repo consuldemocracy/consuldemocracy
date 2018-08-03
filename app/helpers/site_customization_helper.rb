53e1d89fc5d2a4dedc7087bde414d01a0e0cf898
Marko Lovic <mlovicbueno@gmail.com>
2018-08-03 15:52:25 +0200

    Extract translation helper from translatable form views
    So that individual form partials don't depend on the implementation
    of how translations are deleted.

--- app/helpers/site_customization_helper.rb
+++ app/helpers/site_customization_helper.rb
@@ -1,5 +1,9 @@
 module SiteCustomizationHelper
+  def site_customization_enable_translation?(locale)
+    I18nContentTranslation.existing_languages.include?(neutral_locale(locale)) || locale == I18n.locale
+  end
+
   def site_customization_display_translation?(locale)
-    I18nContentTranslation.existing_languages.include?(neutral_locale(locale)) || locale == I18n.locale ? "" : "display: none"
+    site_customization_enable_translation?(locale) ? "" : "display: none"
   end
 end
