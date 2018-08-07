25f104d2dde5991fb88235b57767f7f12d7376ae
Angel Perez <iAngel.p93@gmail.com>
2018-08-07 17:13:27 -0400

    Fix deprecation warning for `.uniq` usage

--- app/models/i18n_content_translation.rb
+++ app/models/i18n_content_translation.rb
@@ -1,5 +1,5 @@
 class I18nContentTranslation < ActiveRecord::Base
   def self.existing_languages
-    self.select(:locale).uniq.map{ |l| l.locale.to_sym }.to_a
+    self.select(:locale).distinct.map { |l| l.locale.to_sym }.to_a
   end
 end
