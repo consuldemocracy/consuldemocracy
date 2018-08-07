129411864e5ec3a5fa759c4e581171b7ba0b6c3e
Angel Perez <iAngel.p93@gmail.com>
2018-08-07 15:23:22 -0400

    Replace occurrences of ActiveRecord::Base on new models

--- app/models/i18n_content_translation.rb
+++ app/models/i18n_content_translation.rb
@@ -1,4 +1,4 @@
-class I18nContentTranslation < ActiveRecord::Base
+class I18nContentTranslation < ApplicationRecord
   def self.existing_languages
     self.select(:locale).distinct.map { |l| l.locale.to_sym }.to_a
   end
