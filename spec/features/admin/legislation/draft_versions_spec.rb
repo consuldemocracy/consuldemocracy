05bfa193cdc55433a5b099887f505d1d02463cb6
Marko Lovic <mlovicbueno@gmail.com>
2018-08-12 21:43:47 +0200

    Make Legislation Draft Versions translatable

--- spec/features/admin/legislation/draft_versions_spec.rb
+++ spec/features/admin/legislation/draft_versions_spec.rb
@@ -7,6 +7,11 @@ feature 'Admin legislation draft versions' do
     login_as(admin.user)
   end
 
+  it_behaves_like "translatable",
+                  "legislation_draft_version",
+                  "edit_admin_legislation_process_draft_version_path",
+                  %w[title changelog]
+
   context "Feature flag" do
 
     scenario 'Disabled with a feature flag' do
@@ -53,9 +58,9 @@ feature 'Admin legislation draft versions' do
 
       click_link 'Create version'
 
-      fill_in 'legislation_draft_version_title', with: 'Version 3'
-      fill_in 'legislation_draft_version_changelog', with: 'Version 3 changes'
-      fill_in 'legislation_draft_version_body', with: 'Version 3 body'
+      fill_in 'legislation_draft_version_title_en', with: 'Version 3'
+      fill_in 'legislation_draft_version_changelog_en', with: 'Version 3 changes'
+      fill_in 'legislation_draft_version_body_en', with: 'Version 3 body'
 
       within('.end') do
         click_button 'Create version'
@@ -86,11 +91,11 @@ feature 'Admin legislation draft versions' do
 
       click_link 'Version 1'
 
-      fill_in 'legislation_draft_version_title', with: 'Version 1b'
+      fill_in 'legislation_draft_version_title_en', with: 'Version 1b'
 
       click_link 'Launch text editor'
 
-      fill_in 'legislation_draft_version_body', with: '# Version 1 body\r\n\r\nParagraph\r\n\r\n>Quote'
+      fill_in 'legislation_draft_version_body_en', with: '# Version 1 body\r\n\r\nParagraph\r\n\r\n>Quote'
 
       within('.fullscreen') do
         click_link 'Close text editor'
@@ -99,6 +104,137 @@ feature 'Admin legislation draft versions' do
       click_button 'Save changes'
 
       expect(page).to have_content 'Version 1b'
+    end
+  end
+
+  context "Translations" do
+
+    let!(:draft_version) { create(:legislation_draft_version,
+                                    title_en: "Title in English",
+                                    title_es: "Título en Español",
+                                    changelog_en: "Changes in English",
+                                    changelog_es: "Cambios en Español",
+                                    body_en: "Body in English",
+                                    body_es: "Texto en Español") }
+
+    before do
+      @edit_draft_version_url = edit_admin_legislation_process_draft_version_path(draft_version.process, draft_version)
+    end
+
+    scenario "Add a translation", :js do
+      visit @edit_draft_version_url
+
+      select "Français", from: "translation_locale"
+      fill_in 'legislation_draft_version_title_fr', with: 'Titre en Français'
+
+      click_button 'Save changes'
+
+      visit @edit_draft_version_url
+      expect(page).to have_field('legislation_draft_version_title_en', with: 'Title in English')
+
+      click_link "Español"
+      expect(page).to have_field('legislation_draft_version_title_es', with: 'Título en Español')
+
+      click_link "Français"
+      expect(page).to have_field('legislation_draft_version_title_fr', with: 'Titre en Français')
+    end
+
+    scenario "Update a translation", :js do
+      draft_version.update!(status: 'published')
+      draft_version.process.update!(title_es: 'Título de proceso')
+
+      visit @edit_draft_version_url
+
+      click_link "Español"
+      fill_in 'legislation_draft_version_title_es', with: 'Título correcto en Español'
+
+      click_button 'Save changes'
+
+      visit legislation_process_draft_version_path(draft_version.process, draft_version)
+
+      expect(page).to have_content("Title in English")
+
+      select('Español', from: 'locale-switcher')
+
+      expect(page).to have_content('Título correcto en Español')
+    end
+
+    scenario "Remove a translation", :js do
+      visit @edit_draft_version_url
+
+      click_link "Español"
+      click_link "Remove language"
+
+      expect(page).not_to have_link "Español"
+
+      click_button "Save changes"
+      visit @edit_draft_version_url
+      expect(page).not_to have_link "Español"
+    end
+
+    scenario 'Add translation through markup editor', :js do
+      visit @edit_draft_version_url
+
+      select "Français", from: "translation_locale"
+
+      click_link 'Launch text editor'
+
+      fill_in 'legislation_draft_version_body_fr', with: 'Texte en Français'
+
+      click_link 'Close text editor'
+      click_button "Save changes"
+
+      visit @edit_draft_version_url
+
+      click_link "Français"
+      click_link 'Launch text editor'
+
+      expect(page).to have_field('legislation_draft_version_body_fr', with: 'Texte en Français')
+    end
+
+    context "Globalize javascript interface" do
+
+      scenario "Highlight current locale", :js do
+        visit @edit_draft_version_url
+
+        expect(find("a.js-globalize-locale-link.is-active")).to have_content "English"
+
+        select('Español', from: 'locale-switcher')
+
+        expect(find("a.js-globalize-locale-link.is-active")).to have_content "Español"
+      end
+
+      scenario "Highlight selected locale", :js do
+        visit @edit_draft_version_url
+
+        expect(find("a.js-globalize-locale-link.is-active")).to have_content "English"
+
+        click_link "Español"
+
+        expect(find("a.js-globalize-locale-link.is-active")).to have_content "Español"
+      end
+
+      scenario "Show selected locale form", :js do
+        visit @edit_draft_version_url
+
+        expect(page).to have_field('legislation_draft_version_title_en', with: 'Title in English')
+
+        click_link "Español"
+
+        expect(page).to have_field('legislation_draft_version_title_es', with: 'Título en Español')
+      end
+
+      scenario "Select a locale and add it to the draft_version form", :js do
+        visit @edit_draft_version_url
+
+        select "Français", from: "translation_locale"
+
+        expect(page).to have_link "Français"
+
+        click_link "Français"
+
+        expect(page).to have_field('legislation_draft_version_title_fr')
+      end
     end
   end
 end
