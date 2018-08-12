9171a5cb03a31311381937862ea3a407b9ed45c3
Marko Lovic <mlovicbueno@gmail.com>
2018-08-12 21:43:47 +0200

    Make Legislation Draft Versions translatable

--- app/models/legislation/draft_version.rb
+++ app/models/legislation/draft_version.rb
@@ -4,6 +4,13 @@ class Legislation::DraftVersion < ActiveRecord::Base
   acts_as_paranoid column: :hidden_at
   include ActsAsParanoidAliases
 
+  translates :title,     touch: true
+  translates :changelog, touch: true
+  translates :body,      touch: true
+  translates :body_html,  touch: true
+  translates :toc_html,  touch: true
+  globalize_accessors locales: [:en, :es, :fr, :nl, :val, :pt_br]
+
   belongs_to :process, class_name: 'Legislation::Process', foreign_key: 'legislation_process_id'
   has_many :annotations, class_name: 'Legislation::Annotation', foreign_key: 'legislation_draft_version_id', dependent: :destroy
 
@@ -21,6 +28,13 @@ class Legislation::DraftVersion < ActiveRecord::Base
 
     self.body_html = Redcarpet::Markdown.new(renderer).render(body)
     self.toc_html = Redcarpet::Markdown.new(toc_renderer).render(body)
+
+    translations.each do |translation|
+      if translation.body_changed?
+        translation.body_html = Redcarpet::Markdown.new(renderer).render(translation.body)
+        translation.toc_html = Redcarpet::Markdown.new(toc_renderer).render(translation.body)
+      end
+    end
   end
 
   def display_title
