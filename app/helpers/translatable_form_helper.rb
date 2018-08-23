ca5f8719a8c5a15e8b3e9e03ce7165b904276a5e
Marko Lovic <mlovicbueno@gmail.com>
2018-08-23 18:28:25 +0200

    Fix helper to merge style option correctly

--- app/helpers/translatable_form_helper.rb
+++ app/helpers/translatable_form_helper.rb
@@ -15,11 +15,11 @@ module TranslatableFormHelper
   def merge_translatable_field_options(options, locale)
     options.merge(
       class: "#{options[:class]} js-globalize-attribute".strip,
-      style: display_translation?(locale),
+      style: "#{options[:style]} #{display_translation?(locale)}".strip,
       data:  options.fetch(:data, {}).merge(locale: locale),
       label_options: {
         class: "#{options.dig(:label_options, :class)} js-globalize-attribute".strip,
-        style: display_translation?(locale),
+        style: "#{options.dig(:label_options, :style)} #{display_translation?(locale)}".strip,
         data:  (options.dig(:label_options, :data) || {}) .merge(locale: locale)
       }
     )
