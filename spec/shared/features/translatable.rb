612fdb09dddeb24c044e88fb9678c6833be1b393
Javi Martín <javim@elretirao.net>
2018-08-28 17:44:46 +0200

    Share translatable specs

--- spec/shared/features/translatable.rb
+++ spec/shared/features/translatable.rb
@@ -1,5 +1,13 @@
 shared_examples "translatable" do |factory_name, path_name, fields|
-  let(:language_texts)     { { es: "en español", en: "in English", fr: "en Français" } }
+  let(:language_texts) do
+    {
+      es:      "en español",
+      en:      "in English",
+      fr:      "en Français",
+      "pt-BR": "Português"
+    }
+  end
+
   let(:translatable_class) { build(factory_name).class }
 
   let(:attributes) do
@@ -11,6 +19,105 @@ shared_examples "translatable" do |factory_name, path_name, fields|
   let(:translatable) { create(factory_name, attributes) }
   let(:path) { send(path_name, *resource_hierarchy_for(translatable)) }
   before { login_as(create(:administrator).user) }
+
+  context "Manage translations" do
+    before do
+      if translatable_class.name == "I18nContent"
+        skip "Translation handling is different for site customizations"
+      end
+    end
+
+    scenario "Add a translation", :js do
+      visit path
+
+      select "Français", from: "translation_locale"
+
+      fields.each do |field|
+        fill_in field_for(field, :fr), with: text_for(field, :fr)
+      end
+
+      click_button update_button_text
+
+      visit path
+      field = fields.sample
+
+      expect(page).to have_field(field_for(field, :en), with: text_for(field, :en))
+
+      click_link "Español"
+      expect(page).to have_field(field_for(field, :es), with: text_for(field, :es))
+
+      click_link "Français"
+      expect(page).to have_field(field_for(field, :fr), with: text_for(field, :fr))
+    end
+
+    scenario "Update a translation", :js do
+      visit path
+
+      click_link "Español"
+      field = fields.sample
+
+      fill_in field_for(field, :es), with: "Corrección de #{text_for(field, :es)}"
+
+      click_button update_button_text
+
+      visit path
+
+      expect(page).to have_content(text_for(field, :en))
+
+      select('Español', from: 'locale-switcher')
+
+      expect(page).to have_content("Corrección de #{text_for(field, :es)}")
+    end
+
+    scenario "Remove a translation", :js do
+      visit path
+
+      click_link "Español"
+      click_link "Remove language"
+
+      expect(page).not_to have_link "Español"
+
+      click_button update_button_text
+
+      visit path
+      expect(page).not_to have_link "Español"
+    end
+
+    scenario 'Change value of a translated field to blank' do
+      possible_blanks = fields.select do |field|
+        translatable.dup.tap { |duplicate| duplicate.send(:"#{field}=", '') }.valid?
+      end
+
+      skip("can't have translatable blank fields") if possible_blanks.empty?
+
+      field = possible_blanks.sample
+
+      visit path
+      expect(page).to have_content text_for(field, :en)
+
+      fill_in field_for(field, :en), with: ''
+      click_button update_button_text
+
+      visit path
+      expect(page).not_to have_content text_for(field, :en)
+    end
+
+    scenario "Add a translation for a locale with non-underscored name", :js do
+      visit path
+      field = fields.sample
+
+      select "Português", from: "translation_locale"
+      fill_in field_for(field, :pt_br), with: text_for(field, :"pt-BR")
+
+      click_button update_button_text
+
+      visit path
+
+      select('Português', from: 'locale-switcher')
+
+      expect(page).to have_content(text_for(field, :"pt-BR"))
+    end
+  end
 
   context "Globalize javascript interface" do
     scenario "Highlight current locale", :js do
@@ -35,7 +142,7 @@ shared_examples "translatable" do |factory_name, path_name, fields|
 
     scenario "Show selected locale form", :js do
       visit path
-      field = fields.last # TODO: not sure about first, last, or sample
+      field = fields.sample
 
       expect(page).to have_field(field_for(field, :en), with: text_for(field, :en))
 
@@ -53,7 +160,7 @@ shared_examples "translatable" do |factory_name, path_name, fields|
 
       click_link "Français"
 
-      expect(page).to have_field(field_for(fields.last, :fr))
+      expect(page).to have_field(field_for(fields.sample, :fr))
     end
   end
 end
@@ -69,5 +176,16 @@ def field_for(field, locale)
     "contents_content_#{translatable.key}values_#{field}_#{locale}"
   else
     "#{translatable_class.model_name.singular}_#{field}_#{locale}"
+  end
+end
+
+# FIXME: button texts should be consistent. Right now, buttons don't
+# even share the same colour.
+def update_button_text
+  case translatable_class.name
+  when "Budget::Investment::Milestone"
+    "Update milestone"
+  else
+    "Save changes"
   end
 end
