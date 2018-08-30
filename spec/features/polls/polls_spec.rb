b787e33883bd917bcfb5887af54b5882e4698ab2
Javi Martín <javim@elretirao.net>
2018-08-30 22:01:33 +0200

    Use the same system to freeze time in all specs
    This required changing the `voted_before_sign_in` slightly in order to
    change what the method returns if the user signed in and voted at the
    exact same microsecond.
    
    It doesn't affect production code because it would be impossible for the
    user to do both things at the same time.
    
    As a side effect, the method now returns what the method name suggests.
    Before this change, the correct method name would have been
    `voted_before_or_at_the_same_time_of_sign_in`.
    
    As a less desirable side effect, in the tests now we need to make sure
    at least one second passes between the moment a user votes and the
    moment a user signs in again. One microsecond wouldn't work because
    the method `travel_to` automatically sets microseconds to zero in order
    to avoid rounding issues.

--- spec/features/polls/polls_spec.rb
+++ spec/features/polls/polls_spec.rb
@@ -387,17 +387,11 @@ feature 'Polls' do
     end
   end
 
-  context 'Booth & Website' do
+  context 'Booth & Website', :with_frozen_time do
 
-    let(:poll) { create(:poll, summary: "Summary", description: "Description", starts_at: '2017-12-01', ends_at: '2018-02-01') }
+    let(:poll) { create(:poll, summary: "Summary", description: "Description") }
     let(:booth) { create(:poll_booth) }
     let(:officer) { create(:poll_officer) }
-
-    before do
-      allow(Date).to receive(:current).and_return Date.new(2018,1,1)
-      allow(Date).to receive(:today).and_return Date.new(2018,1,1)
-      allow(Time).to receive(:current).and_return Time.zone.parse("2018-01-01 12:00:00")
-    end
 
     scenario 'Already voted on booth cannot vote on website', :js do
 
