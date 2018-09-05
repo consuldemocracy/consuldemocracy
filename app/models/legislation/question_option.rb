3fb98b175272dd5dc1ad5afa62f906c244605ce3
Javi Martín <javim@elretirao.net>
2018-09-05 19:43:51 +0200

    Fix typos

--- app/models/legislation/question_option.rb
+++ app/models/legislation/question_option.rb
@@ -2,7 +2,7 @@ class Legislation::QuestionOption < ActiveRecord::Base
   acts_as_paranoid column: :hidden_at
   include ActsAsParanoidAliases
 
-  translates :value, touch: :true
+  translates :value, touch: true
   globalize_accessors
 
   belongs_to :question, class_name: 'Legislation::Question', foreign_key: 'legislation_question_id', inverse_of: :question_options
