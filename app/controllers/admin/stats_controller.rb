9029acd4409822d54831813fabd09dcf96db1223
Angel Perez <iAngel.p93@gmail.com>
2018-09-05 10:24:35 -0400

    Fix Rubocop warnings for Admin controllers [ci skip]

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -3,23 +3,28 @@ class Admin::StatsController < Admin::BaseController
   def show
     @event_types = Ahoy::Event.group(:name).count
 
-    @visits = Visit.count
-    @debates = Debate.with_hidden.count
+    @visits    = Visit.count
+    @debates   = Debate.with_hidden.count
     @proposals = Proposal.with_hidden.count
-    @comments = Comment.not_valuations.with_hidden.count
+    @comments  = Comment.not_valuations.with_hidden.count
 
-    @debate_votes = Vote.where(votable_type: 'Debate').count
+    @debate_votes   = Vote.where(votable_type: 'Debate').count
     @proposal_votes = Vote.where(votable_type: 'Proposal').count
-    @comment_votes = Vote.where(votable_type: 'Comment').count
+    @comment_votes  = Vote.where(votable_type: 'Comment').count
     @votes = Vote.count
 
-    @user_level_two = User.active.level_two_verified.count
+    @user_level_two   = User.active.level_two_verified.count
     @user_level_three = User.active.level_three_verified.count
-    @verified_users = User.active.level_two_or_three_verified.count
+    @verified_users   = User.active.level_two_or_three_verified.count
     @unverified_users = User.active.unverified.count
     @users = User.active.count
-    @user_ids_who_voted_proposals = ActsAsVotable::Vote.where(votable_type: 'Proposal').distinct.count(:voter_id)
+
+    @user_ids_who_voted_proposals = ActsAsVotable::Vote.where(votable_type: 'Proposal')
+                                                       .distinct
+                                                       .count(:voter_id)
+
     @user_ids_who_didnt_vote_proposals = @verified_users - @user_ids_who_voted_proposals
+
     @spending_proposals = SpendingProposal.count
     budgets_ids = Budget.where.not(phase: 'finished').pluck(:id)
     @budgets = budgets_ids.size
