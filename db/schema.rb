9cfa07ff1defb10f3b5051de2b2472114223b1d4
Javi Martín <javim@elretirao.net>
2018-09-07 14:24:45 +0200

    Remove unrelated schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -1305,12 +1305,6 @@ ActiveRecord::Schema.define(version: 20180813141443) do
   add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope", using: :btree
   add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope", using: :btree
 
-  create_table "web_sections", force: :cascade do |t|
-    t.text     "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "widget_cards", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
@@ -1327,6 +1321,12 @@ ActiveRecord::Schema.define(version: 20180813141443) do
     t.integer  "limit",      default: 3
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "web_sections", force: :cascade do |t|
+    t.text     "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "administrators", "users"
