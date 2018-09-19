aa801eda07982e332a44052dfff298d4493cd4ba
Papaya Labs <papayalabs@gmail.com>
2018-09-19 09:56:19 -0500

    Resolve conflicts to rebase

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180802221830) do
+ActiveRecord::Schema.define(version: 20180813141443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
