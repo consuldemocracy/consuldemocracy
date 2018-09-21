4f4769062a48ad68134317450f3d6fc7e9e30ae6
voodoorai2000 <voodoorai2000@gmail.com>
2018-09-21 15:34:40 +0200

    Use a single server by default
    Capistrano configuration is prepared to deploy to multiple servers
    
    For now assuming that we are going to use a single server for everthing (app, db, cron jobs, queue system, etc)

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@ set :ssh_options, port: deploysecret(:ssh_port)
 set :stage, :production
 set :rails_env, :production
 
-#server deploysecret(:server1), user: deploysecret(:user), roles: %w(web app db importer)
-server deploysecret(:server2), user: deploysecret(:user), roles: %w(web app db importer cron background)
-server deploysecret(:server3), user: deploysecret(:user), roles: %w(web app db importer)
-server deploysecret(:server4), user: deploysecret(:user), roles: %w(web app db importer)
+server deploysecret(:server1), user: deploysecret(:user), roles: %w(web app db importer cron background)
+#server deploysecret(:server2), user: deploysecret(:user), roles: %w(web app db importer cron background)
+#server deploysecret(:server3), user: deploysecret(:user), roles: %w(web app db importer)
+#server deploysecret(:server4), user: deploysecret(:user), roles: %w(web app db importer)
