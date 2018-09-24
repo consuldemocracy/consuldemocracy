62490c4640d9667c19e010b7870e8bc7d944d63c
decabeza <alberto@decabeza.es>
2018-09-24 13:51:08 +0200

    Adds custom javascripts folder

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -18,6 +18,6 @@ Rails.application.config.assets.precompile += %w( ie.css )
 # Loads custom images and custom fonts before app/assets/images and app/assets/fonts
 assets_path = Rails.application.config.assets.paths
 
-%w[images fonts].each do |asset|
+%w[images fonts javascripts].each do |asset|
   assets_path.insert(0, Rails.root.join("app", "assets", asset, "custom").to_s)
 end
