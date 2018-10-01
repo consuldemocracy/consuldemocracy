ba4e673057af0558b6fe4f8c37d275e058b8e82f
decabeza <alberto@decabeza.es>
2018-10-01 16:48:46 +0200

    Adds imageable to legislation proposals

--- spec/features/legislation/proposals_spec.rb
+++ spec/features/legislation/proposals_spec.rb
@@ -113,4 +113,21 @@ feature 'Legislation Proposals' do
     all("[id^='legislation_proposal_']").collect { |e| e[:id] }
   end
 
+  scenario "Create a legislation proposal with an image", :js do
+    create(:legislation_proposal, process: process)
+
+    login_as user
+
+    visit new_legislation_process_proposal_path(process)
+
+    fill_in 'Proposal title', with: 'Legislation proposal with image'
+    fill_in 'Proposal summary', with: 'Including an image on a legislation proposal'
+    imageable_attach_new_file(create(:image), Rails.root.join('spec/fixtures/files/clippy.jpg'))
+    check 'legislation_proposal_terms_of_service'
+    click_button 'Create proposal'
+
+    expect(page).to have_content 'Legislation proposal with image'
+    expect(page).to have_content 'Including an image on a legislation proposal'
+    expect(page).to have_css("img[alt='#{Legislation::Proposal.last.image.title}']")
+  end
 end
