12658ee418c54514cd685c8e37f4597ff0325f4b
Ziyan Junaideen <ziyan@jdeen.com>
2018-10-06 22:35:08 +0530

    Secret yml update + checking for presence of recipients

--- config/initializers/recipient_interceptor.rb
+++ config/initializers/recipient_interceptor.rb
@@ -1,4 +1,6 @@
-if (recipients = Rails.application.secrets.interceptor_recipients)
+recipients = Rails.application.secrets.email_interceptor_recipients
+
+if recipients.present?
   interceptor = RecipientInterceptor.new(recipients)
   Mail.register_interceptor(interceptor)
 end
