a9e980875f92432ffe099a298eba931bfa76a2f0
Ziyan Junaideen <ziyan@jdeen.com>
2018-10-06 18:29:35 +0530

    Email interceptor - staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -90,4 +90,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Mail.register_interceptor RecipientInterceptor.new(ENV['EMAIL_RECIPIENTS'])
 end
