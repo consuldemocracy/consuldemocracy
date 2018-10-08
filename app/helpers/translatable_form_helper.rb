6fc33895877c68d415d2381ed24ce2c4f57f79d5
Javi Martín <javim@elretirao.net>
2018-10-08 11:13:27 +0200

    Keep invalid translation params through requests
    We were reloading the values from the database and ignoring the
    parameters sent by the browser.

--- app/helpers/translatable_form_helper.rb
+++ app/helpers/translatable_form_helper.rb
@@ -22,7 +22,7 @@ module TranslatableFormHelper
     def translatable_fields(&block)
       @object.globalize_locales.map do |locale|
         Globalize.with_locale(locale) do
-          fields_for(:translations, @object.translations.where(locale: locale).first_or_initialize, builder: TranslationsFieldsBuilder) do |translations_form|
+          fields_for(:translations, translation_for(locale), builder: TranslationsFieldsBuilder) do |translations_form|
             @template.concat translations_form.hidden_field(
               :_destroy,
               value: !@template.enable_locale?(@object, locale),
@@ -37,6 +37,19 @@ module TranslatableFormHelper
       end.join.html_safe
     end
 
+    def translation_for(locale)
+      existing_translation_for(locale) || new_translation_for(locale)
+    end
+
+    def existing_translation_for(locale)
+      # Use `select` because `where` uses the database and so ignores
+      # the `params` sent by the browser
+      @object.translations.select { |translation| translation.locale == locale }.first
+    end
+
+    def new_translation_for(locale)
+      @object.translations.new(locale: locale)
+    end
   end
 
   class TranslationsFieldsBuilder < FoundationRailsHelper::FormBuilder
