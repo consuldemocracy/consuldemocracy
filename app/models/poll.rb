1b6c207bde8e52f07ecf74e203fa8cfd272e1b4c
Javi Martín <javim@elretirao.net>
2018-10-10 14:37:43 +0200

    Update polls translatable fields
    The `:name` attribute is still allowed in the controller because some
    forks use it when creating a poll from a budget.

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -7,7 +7,8 @@ class Poll < ActiveRecord::Base
   translates :name,        touch: true
   translates :summary,     touch: true
   translates :description, touch: true
-  include Globalizable
+  globalize_accessors
+  accepts_nested_attributes_for :translations, allow_destroy: true
 
   RECOUNT_DURATION = 1.week
 
@@ -24,7 +25,10 @@ class Poll < ActiveRecord::Base
   has_and_belongs_to_many :geozones
   belongs_to :author, -> { with_hidden }, class_name: 'User', foreign_key: 'author_id'
 
-  validates_translation :name, presence: true
+  translation_class.instance_eval do
+    validates :name, presence: true
+  end
+
   validate :date_range
 
   scope :current,  -> { where('starts_at <= ? and ? <= ends_at', Date.current.beginning_of_day, Date.current.beginning_of_day) }
