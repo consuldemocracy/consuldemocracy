4db54092e6d11e4bc2036828f6bb7c51d37876c4
decabeza <alberto@decabeza.es>
2018-10-18 01:26:07 +0200

    Removes unrelated dashboard changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,7 @@ set :deploy_to, deploysecret(:deploy_to)
 set :server_name, deploysecret(:server_name)
 set :db_server, deploysecret(:db_server)
 set :branch, :stable
+set :ssh_options, port: deploysecret(:ssh_port)
 set :stage, :production
 set :rails_env, :production
 
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,9 +1,9 @@
-set :deploy_to, -> { deploysecret(:deploy_to) }
+set :deploy_to, deploysecret(:deploy_to)
 set :server_name, deploysecret(:server_name)
 set :db_server, deploysecret(:db_server)
-set :branch, deploysecret(:branch, ENV['branch']) || :master
+set :branch, ENV['branch'] || :master
+set :ssh_options, port: deploysecret(:ssh_port)
 set :stage, :staging
-set :rails_env, deploysecret(:rails_env, 'staging').to_sym
+set :rails_env, :staging
 
 server deploysecret(:server), user: deploysecret(:user), roles: %w(web app db importer cron)
-
