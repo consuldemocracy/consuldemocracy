4b8d6b4ecc9a109fa7933571084ab95765acd635
decabeza <alberto@decabeza.es>
2018-10-20 12:01:28 +0200

    Adds configurable top right and main navigation menus

--- spec/features/site_customization/content_blocks_spec.rb
+++ spec/features/site_customization/content_blocks_spec.rb
@@ -30,4 +30,34 @@ feature "Custom content blocks" do
     expect(page).to have_content("contenido para footer")
     expect(page).not_to have_content("content for footer")
   end
+
+  scenario "main navigation left" do
+    create(:site_customization_content_block, name: "subnavigation_left", locale: "en", body: "content for left links")
+    create(:site_customization_content_block, name: "subnavigation_left", locale: "es", body: "contenido para left links")
+
+    visit "/?locale=en"
+
+    expect(page).to have_content("content for left links")
+    expect(page).not_to have_content("contenido para left links")
+
+    visit "/?locale=es"
+
+    expect(page).to have_content("contenido para left links")
+    expect(page).not_to have_content("content for left links")
+  end
+
+  scenario "main navigation right" do
+    create(:site_customization_content_block, name: "subnavigation_right", locale: "en", body: "content for right links")
+    create(:site_customization_content_block, name: "subnavigation_right", locale: "es", body: "contenido para right links")
+
+    visit "/?locale=en"
+
+    expect(page).to have_content("content for right links")
+    expect(page).not_to have_content("contenido para right links")
+
+    visit "/?locale=es"
+
+    expect(page).to have_content("contenido para right links")
+    expect(page).not_to have_content("content for left links")
+  end
 end
