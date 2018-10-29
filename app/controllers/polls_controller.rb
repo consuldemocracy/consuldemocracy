0aff04872c0cc399f8f09a4b6dfcfb8851d8f81d
Tiago P. Zini <tiagozini@gmail.com>
2018-10-29 03:12:06 -0400

    WIP

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -28,99 +28,41 @@ class PollsController < ApplicationController
 
   def answer    
     @questions = @poll.questions.for_render.sort_for_list
-    @token = poll_voter_token(@poll, current_user)
-    @poll_questions_answers = Poll::Question::Answer.where(question: @poll.questions).where.not(description: "").order(:given_order)
+    
     @answers_by_question_id = {}     
     @found_error = false
-    @answers = []
-    @questions.each do |question| 
+    @questions.each do |question|
       if(question.answers.count()==0)
         next
       end        
-      answer_id = params['answer_question_' + question.id.to_s]        
-      answer = question.answers.find_or_initialize_by(author: current_user)
-      if (answer_id)
-        answer.answer = Poll::Question::Answer.find_by_id(answer_id.to_i).title
-      else 
-        answer.answer = nil
+      answer_title = params['answer_question_' + question.id.to_s]        
+      unless (answer_title)
         @found_error = true        
       end
-      answer.touch if answer.persisted?
-      answer.record_voter_participation(@token)        
-      question.question_answers.where(question_id: question).each do |question_answer|
-        question_answer.set_most_voted
-      end
-      @answers_by_question_id[answer.question_id] = answer.answer
-      @answers.push(answer)
+      @answers_by_question_id[question.id] = answer_title              
     end
-    @commentable = @poll
-    @comment_tree = CommentTree.new(@commentable, params[:page], @current_order)
+    @token = params[:token]
 
     unless @found_error
-      @answers.each do |answer|       
-        answer.save()
+      @questions.each do |question|       
+        answer = question.answers.find_or_initialize_by(author: current_user)
+        answer.answer = params['answer_question_' + question.id.to_s]
+        answer.touch if answer.persisted?        
+        answer.save!        
+        answer.record_voter_participation(@token)        
+        question.question_answers.where(question_id: question).each do |question_answer|
+          question_answer.set_most_voted
+        end
       end
       return redirect_to poll_path(@poll), notice: t("flash.actions.save_changes.notice")      
     else
       @poll_questions_answers = Poll::Question::Answer.where(question: @poll.questions).where.not(description: "").order(:given_order)
+      @commentable = @poll
+      @comment_tree = CommentTree.new(@commentable, params[:page], @current_order)
       render :action => 'show'      
       return true
     end  
   end
-
-
-=begin
-  def answer    
-    @questions = @poll.questions.for_render.sort_for_list
-    @token = poll_voter_token(@poll, current_user)    
-    questions_without_any_answer = []
-    question_counter = 0
-    questions_answered_counter = 0    
-    answers_by_question_id = {}
-    
-    @questions.each do |question| 
-      # test if exists answer to do a choice
-      if(question.answers.count()==0)
-        next
-      end
-      if (params['answer_question_' + question.id.to_s].nil?)
-        questions_without_any_answer.insert(0, question.id)
-        answers_by_question_id[question.id] = nil
-      else        
-        answer_id = params['answer_question_' + question.id.to_s]
-        answers_by_question_id[question.id] = Poll::Question::Answer.find_by_id(answer_id.to_i).title
-      end
-    end
-
-    session[:answers_by_question_id] = answers_by_question_id
-    
-    # save answers in case of no errors
-    unless (questions_without_any_answer.any?)
-      @questions.each do |question| 
-        if(question.answers.count()==0)
-          next
-        end        
-        answer_id = params['answer_question_' + question.id.to_s]        
-        question_counter += 1
-        unless answer_id.nil?
-          questions_answered_counter+=1
-          answer = question.answers.find_or_initialize_by(author: current_user)
-          answer.answer = Poll::Question::Answer.find_by_id(answer_id.to_i).title
-          answer.touch if answer.persisted?
-          answer.save
-          answer.record_voter_participation(@token)        
-          question.question_answers.where(question_id: question).each do |question_answer|
-            question_answer.set_most_voted
-          end             
-        end
-      end    
-      return redirect_to poll_path(@poll), notice: t("flash.actions.save_changes.notice")      
-    else
-      session[:questions_without_any_answer] = questions_without_any_answer
-      return redirect_to poll_path(@poll)
-    end
-  end
-=end
 
   def stats
     @stats = Poll::Stats.new(@poll).generate
