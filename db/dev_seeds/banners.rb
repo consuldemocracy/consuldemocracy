747aec35068f2db2652a9a153182877e1c9f6ab8
decabeza <alberto@decabeza.es>
2018-10-30 18:48:53 +0100

    Improves some code format details

--- db/dev_seeds/banners.rb
+++ db/dev_seeds/banners.rb
@@ -4,11 +4,11 @@ section "Creating banners" do
     description = Faker::Lorem.sentence(word_count = 12)
     target_url = Rails.application.routes.url_helpers.proposal_path(proposal)
     banner = Banner.new(title: title,
-                            description: description,
-                            target_url: target_url,
-                            post_started_at: rand((Time.current - 1.week)..(Time.current - 1.day)),
-                            post_ended_at:   rand((Time.current - 1.day)..(Time.current + 1.week)),
-                            created_at: rand((Time.current - 1.week)..Time.current))
+                        description: description,
+                        target_url: target_url,
+                        post_started_at: rand((Time.current - 1.week)..(Time.current - 1.day)),
+                        post_ended_at:   rand((Time.current - 1.day)..(Time.current + 1.week)),
+                        created_at: rand((Time.current - 1.week)..Time.current))
     I18n.available_locales.map do |locale|
       Globalize.with_locale(locale) do
         banner.description = "Description for locale #{locale}"
