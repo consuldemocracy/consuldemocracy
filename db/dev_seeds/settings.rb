cd7bff04b499a2741bccedf98b48b32f0c3c654b
decabeza <alberto@decabeza.es>
2018-10-31 11:51:44 +0100

    Refactors social meta tags spec

--- db/dev_seeds/settings.rb
+++ db/dev_seeds/settings.rb
@@ -56,7 +56,8 @@ section "Creating Settings" do
   Setting.create(key: 'mailer_from_name', value: 'CONSUL')
   Setting.create(key: 'mailer_from_address', value: 'noreply@consul.dev')
   Setting.create(key: 'meta_title', value: 'CONSUL')
-  Setting.create(key: 'meta_description', value: 'Citizen Participation & Open Gov Application')
+  Setting.create(key: 'meta_description', value: 'Citizen participation tool for an open, '\
+                                                 'transparent and democratic government')
   Setting.create(key: 'meta_keywords', value: 'citizen participation, open government')
   Setting.create(key: 'verification_offices_url', value: 'http://oficinas-atencion-ciudadano.url/')
   Setting.create(key: 'min_age_to_participate', value: '16')
