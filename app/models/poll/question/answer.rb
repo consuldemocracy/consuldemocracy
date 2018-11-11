19f1aca2142cddd7cb861575bfd74f605b6f5fa6
Juanjo Bazán <jjbazan@gmail.com>
2018-11-11 14:54:35 +0100

    fix poll accuracy results using floats

--- app/models/poll/question/answer.rb
+++ app/models/poll/question/answer.rb
@@ -46,7 +46,7 @@ class Poll::Question::Answer < ActiveRecord::Base
   end
 
   def total_votes_percentage
-    question.answers_total_votes.zero? ? 0 : (total_votes * 100) / question.answers_total_votes
+    question.answers_total_votes.zero? ? 0 : (total_votes * 100.0) / question.answers_total_votes
   end
 
   def set_most_voted
