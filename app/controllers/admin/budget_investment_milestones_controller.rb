c4448faf70c29e371505d9cb1a8c0acdffad8a7a
Javi Martín <javim@elretirao.net>
2018-11-12 19:43:50 +0100

    Move milestones code to admin/milestones
    All milestone controllers will inherit from `AdminMilestonesController`,
    and all views will render the same content.

--- app/controllers/admin/budget_investment_milestones_controller.rb
+++ app/controllers/admin/budget_investment_milestones_controller.rb
@@ -1,77 +1,8 @@
-class Admin::BudgetInvestmentMilestonesController < Admin::BaseController
-  include Translatable
-
-  before_action :load_milestoneable, only: [:index, :new, :create, :edit, :update, :destroy]
-  before_action :load_milestone, only: [:edit, :update, :destroy]
-  before_action :load_statuses, only: [:index, :new, :create, :edit, :update]
-  helper_method :milestoneable_path
-
-  def index
-  end
-
-  def new
-    @milestone = @milestoneable.milestones.new
-  end
-
-  def create
-    @milestone = Milestone.new(milestone_params)
-    @milestone.milestoneable = @milestoneable
-    if @milestone.save
-      redirect_to milestoneable_path, notice: t('admin.milestones.create.notice')
-    else
-      render :new
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @milestone.update(milestone_params)
-      redirect_to milestoneable_path, notice: t('admin.milestones.update.notice')
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @milestone.destroy
-    redirect_to milestoneable_path, notice: t('admin.milestones.delete.notice')
-  end
+class Admin::BudgetInvestmentMilestonesController < Admin::MilestonesController
 
   private
 
-  def milestone_params
-    image_attributes = [:id, :title, :attachment, :cached_attachment, :user_id, :_destroy]
-    documents_attributes = [:id, :title, :attachment, :cached_attachment, :user_id, :_destroy]
-    attributes = [:publication_date, :budget_investment_id, :status_id,
-                  translation_params(Milestone),
-                  image_attributes: image_attributes, documents_attributes: documents_attributes]
-
-    params.require(:milestone).permit(*attributes)
-  end
-
-  def load_milestoneable
-    @milestoneable = Budget::Investment.find(params[:budget_investment_id])
-  end
-
-  def load_milestone
-    @milestone = get_milestone
-  end
-
-  def get_milestone
-    Milestone.find(params[:id])
-  end
-
-  def resource
-    get_milestone
-  end
-
-  def load_statuses
-    @statuses = Milestone::Status.all
-  end
-
-  def milestoneable_path
-    polymorphic_path([:admin, *resource_hierarchy_for(@milestone.milestoneable)])
+  def milestoneable
+    Budget::Investment.find(params[:budget_investment_id])
   end
 end
