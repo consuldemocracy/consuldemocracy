87eae5fd20aee3ab561a6797ec058b19639a4ed7
Javi Martín <javim@elretirao.net>
2018-11-15 17:10:22 +0100

    Move milestones scope to milestoneable concern

--- app/models/budget/investment.rb
+++ app/models/budget/investment.rb
@@ -83,7 +83,6 @@ class Budget
     scope :last_week,                   -> { where("created_at >= ?", 7.days.ago)}
     scope :sort_by_flags,               -> { order(flags_count: :desc, updated_at: :desc) }
     scope :sort_by_created_at,          -> { reorder(created_at: :desc) }
-    scope :with_milestones,             -> { joins(:milestones).distinct }
 
     scope :by_budget,         ->(budget)      { where(budget: budget) }
     scope :by_group,          ->(group_id)    { where(group_id: group_id) }
