619a7ca9bdab7261ff156964e15ae4f95c302487
Javi Martín <javim@elretirao.net>
2018-11-19 15:50:55 +0100

    Manage legislation process milestones

--- app/models/legislation/process.rb
+++ app/models/legislation/process.rb
@@ -1,6 +1,7 @@
 class Legislation::Process < ActiveRecord::Base
   include ActsAsParanoidAliases
   include Taggable
+  include Milestoneable
   include Documentable
   documentable max_documents_allowed: 4,
                max_file_size: 3.megabytes,
