2e081264c782b732b656781bd9fc4117bc163920
Javi Martín <javim@elretirao.net>
2018-11-22 01:29:36 +0100

    Use `Date.current` instead of `Date.today`
    Using `Date.today` was making the spec fail around midnight.

--- spec/lib/tasks/milestones_spec.rb
+++ spec/lib/tasks/milestones_spec.rb
@@ -47,7 +47,7 @@ describe "Milestones tasks" do
       expect(status.description).to eq "Good"
       expect(status.hidden_at).to be nil
       expect(status.created_at.to_date).to eq Date.yesterday
-      expect(status.updated_at.to_date).to eq Date.today
+      expect(status.updated_at.to_date).to eq Date.current
     end
 
     it "migrates milestones" do
@@ -63,7 +63,7 @@ describe "Milestones tasks" do
       expect(milestone.publication_date).to eq Date.yesterday
       expect(milestone.status_id).to eq Milestone::Status.first.id
       expect(milestone.created_at.to_date).to eq Date.yesterday
-      expect(milestone.updated_at.to_date).to eq Date.today
+      expect(milestone.updated_at.to_date).to eq Date.current
     end
 
     it "Updates the primary key sequence correctly" do
