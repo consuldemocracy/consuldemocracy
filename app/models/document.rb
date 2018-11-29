2cd4be3ffc327e8d076e6ce5710b12effe077553
voodoorai2000 <voodoorai2000@gmail.com>
2018-11-29 18:12:57 +0100

    Add http to documents parsed from Amazon S3

--- app/models/document.rb
+++ app/models/document.rb
@@ -36,7 +36,7 @@ class Document < ActiveRecord::Base
     self.attachment = if Paperclip::Attachment.default_options[:storage] == :filesystem
                         File.open(cached_attachment)
                       else
-                        URI.parse(cached_attachment)
+                        URI.parse("http:" + cached_attachment)
                       end
   end
 
