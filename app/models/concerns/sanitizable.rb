4192dcd2ce578e851f5ebd854bf2f0c8a82393f3
Senén Rodero Rodríguez <senenrodero@gmail.com>
2018-12-03 18:07:40 +0100

    Detect translatable descriptions to sanitize

--- app/models/concerns/sanitizable.rb
+++ app/models/concerns/sanitizable.rb
@@ -13,7 +13,16 @@ module Sanitizable
   protected
 
     def sanitize_description
-      self.description = WYSIWYGSanitizer.new.sanitize(description)
+      if self.class.included_modules.include?(Globalizable) &&
+         self.class.translated_attribute_names.include?(:description)
+         self.translations.each do |translation|
+           Globalize.with_locale(translation.locale) do
+             self.description = WYSIWYGSanitizer.new.sanitize(description)
+           end
+         end
+      else
+        self.description = WYSIWYGSanitizer.new.sanitize(description)
+      end
     end
 
     def sanitize_tag_list
