b685419b2862cb93cdbef67d5dc83255e00a0663
Javi Martín <javim@elretirao.net>
2018-12-11 13:49:38 +0100

    Fix "Remove obsolete model usage"
    We accidentally changed the schema when modifying an existing migration.
    It wasn't critical because we're going to remove that table in the
    future, but it resulted in conflicts for users who had already run the
    migration before its modification.
    
    This commit fixes commit 4a7f479.

--- db/schema.rb
+++ db/schema.rb
@@ -163,11 +163,14 @@ ActiveRecord::Schema.define(version: 20181016204729) do
   create_table "budget_investment_milestone_translations", force: :cascade do |t|
     t.integer  "budget_investment_milestone_id", null: false
     t.string   "locale",                         null: false
-    t.string   "title"
-    t.text     "description"
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
+    t.string   "title"
+    t.text     "description"
   end
+
+  add_index "budget_investment_milestone_translations", ["budget_investment_milestone_id"], name: "index_6770e7675fe296cf87aa0fd90492c141b5269e0b", using: :btree
+  add_index "budget_investment_milestone_translations", ["locale"], name: "index_budget_investment_milestone_translations_on_locale", using: :btree
 
   create_table "budget_investment_milestones", force: :cascade do |t|
     t.integer  "investment_id"
