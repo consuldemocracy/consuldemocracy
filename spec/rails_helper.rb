0d48fddc3b3abd4c88fc78277c4ed4f2c89b2d7b
Julian Herrero <microweb10@gmail.com>
2018-12-14 17:16:22 +0100

    change web driver options to prevent test failures

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,7 @@ end
 
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w(headless no-sandbox window-size=1200,600) }
+    chromeOptions: { args: %w(headless no-sandbox window-size=1200,1800) }
   )
 
   Capybara::Selenium::Driver.new(
