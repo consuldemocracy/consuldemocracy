c72f8a3c486cc3a697a26e3ad264427dc423fec7
Javi Martín <javim@elretirao.net>
2018-12-21 19:31:03 +0100

    Add translations for rails date order
    Not doing so caused crashes on applications which don't fall back to
    English when a translation is missing.
    
    We're adding them in a separate file so we can exclude it from crowdin
    and so translators don't translate symbols as if they were words which
    need translation.

--- spec/features/verification/level_two_verification_spec.rb
+++ spec/features/verification/level_two_verification_spec.rb
@@ -24,4 +24,19 @@ feature 'Level two verification' do
     expect(page).to have_content 'Code correct'
   end
 
+  context "In Spanish, with no fallbacks" do
+    before do
+      skip unless I18n.available_locales.include?(:es)
+      allow(I18n.fallbacks).to receive(:[]).and_return([:es])
+    end
+
+    scenario "Works normally" do
+      user = create(:user)
+      login_as(user)
+
+      visit verification_path(locale: :es)
+      verify_residence
+    end
+  end
+
 end
