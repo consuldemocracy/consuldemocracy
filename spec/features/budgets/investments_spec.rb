f3a8ef2f17c583e2d9047543a9d28d35b3f76f07
Senén Rodero Rodríguez <senenrodero@gmail.com>
2018-12-23 19:19:36 +0100

    Add translation interface feature setting
    Allow to enable/disable translation interface at frontend

--- spec/features/budgets/investments_spec.rb
+++ spec/features/budgets/investments_spec.rb
@@ -24,11 +24,16 @@ describe "Budget Investments" do
   context "Concerns" do
     it_behaves_like "notifiable in-app", Budget::Investment
     it_behaves_like "relationable", Budget::Investment
-    it_behaves_like "new_translatable",
-                    "budget_investment",
-                    "new_budget_investment_path",
-                    %w[title],
-                    { "description" => :ckeditor }
+    context "Translatable at front end" do
+      before do
+        Setting["feature.translation_interface"] = true
+      end
+      it_behaves_like "new_translatable",
+                      "budget_investment",
+                      "new_budget_investment_path",
+                      %w[title],
+                      { "description" => :ckeditor }
+    end
   end
 
   context "Load" do
