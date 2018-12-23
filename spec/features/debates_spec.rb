ce39da2782d994724a333b46707f16350b0cbe2e
Senén Rodero Rodríguez <senenrodero@gmail.com>
2018-12-23 19:19:36 +0100

    Add translation interface feature setting
    Allow to enable/disable translation interface at frontend

--- spec/features/debates_spec.rb
+++ spec/features/debates_spec.rb
@@ -12,11 +12,17 @@ feature 'Debates' do
   context "Concerns" do
     it_behaves_like 'notifiable in-app', Debate
     it_behaves_like 'relationable', Debate
-    it_behaves_like 'translatable',
-                    'debate',
-                    'edit_debate_path',
-                    %w[title],
-                    { 'description' => :ckeditor }
+    context "Translatable at front end" do
+      before do
+        Setting['feature.translation_interface'] = true
+      end
+
+      it_behaves_like 'translatable',
+                      'debate',
+                      'edit_debate_path',
+                      %w[title],
+                      { 'description' => :ckeditor }
+    end
   end
 
   scenario 'Index' do
