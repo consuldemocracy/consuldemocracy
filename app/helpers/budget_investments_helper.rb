5dd468092a8de9f969d318ee3916b01b2f287a01
Anna Anks Nowak <matisnape@wp.pl>
2018-12-30 23:41:09 +0100

    Rewrite the method to not use budget_investments_sorting_options [#2931]

--- app/helpers/budget_investments_helper.rb
+++ app/helpers/budget_investments_helper.rb
@@ -1,17 +1,14 @@
 module BudgetInvestmentsHelper
-  def budget_investments_sorting_options
-    Budget::Investment::SORTING_OPTIONS.map do |so|
-      [t("admin.budget_investments.index.sort_by.#{so}"), so]
-    end
-  end
-
   def budget_investments_advanced_filters(params)
     params.map { |af| t("admin.budget_investments.index.filters.#{af}") }.join(', ')
   end
 
   def link_to_investments_sorted_by(column)
-    sorting_option = budget_investments_sorting_options.select { |so| so[1] == column.downcase }.flatten
-    link_to t(sorting_option[0]), admin_budget_budget_investments_path(sort_by: sorting_option[1])
+    sorting_option = column.downcase
+    link_to(
+      t("admin.budget_investments.index.sort_by.#{sorting_option}"),
+      admin_budget_budget_investments_path(sort_by: sorting_option)
+    )
   end
 
   def investments_minimal_view_path
