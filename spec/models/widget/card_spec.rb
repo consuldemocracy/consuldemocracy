162b61f94c8237d9c32bb275d14549433cfb8486
Manu <nahia.desarrollo@gmail.com>
2019-01-13 16:33:54 -0500

    updated test file for cards

--- spec/models/widget/card_spec.rb
+++ spec/models/widget/card_spec.rb
@@ -26,11 +26,24 @@ describe Widget::Card do
 
     it "returns cards for the homepage body" do
       header = create(:widget_card, header: true)
-      card1 = create(:widget_card, header: false, title: "Card 1")
-      card2 = create(:widget_card, header: false, title: "Card 2")
-      card3 = create(:widget_card, header: false, title: "Card 3")
+      card1 = create(:widget_card, header: false, title: "Card 1", site_customization_page_id: 0)
+      card2 = create(:widget_card, header: false, title: "Card 2", site_customization_page_id: 0)
+      card3 = create(:widget_card, header: false, title: "Card 3", site_customization_page_id: 0)
 
       expect(Widget::Card.body).to eq([card1, card2, card3])
+    end
+  end
+
+  describe "#custom page" do
+
+    it "return cards for the custom pages" do
+      header = create(:widget_card, header: true)
+      card = create(:widget_card, header: false)
+      card1 = create(:widget_card, header: false, title: "Card 1", site_customization_page_id: 1)
+      card2 = create(:widget_card, header: false, title: "Card 2", site_customization_page_id: 1)
+      card3 = create(:widget_card, header: false, title: "Card 3", site_customization_page_id: 1)
+
+      expect(Widget::Card.page(1)).to eq([card1, card2, card3])
     end
   end
 
