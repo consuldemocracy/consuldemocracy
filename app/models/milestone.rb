3075c89b70354860866384185cb12bdb00f7a04e
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-01-17 20:02:17 +0100

    Remove monkey patch
    Since globalize version update this is no longer needed. New Globalize
    version initializes globalized_model correctly when building new
    translations.

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -13,8 +13,6 @@ class Milestone < ActiveRecord::Base
 
   validates :milestoneable, presence: true
   validates :publication_date, presence: true
-
-  before_validation :assign_model_to_translations
   validates_translation :description, presence: true, unless: -> { status_id.present? }
 
   scope :order_by_publication_date, -> { order(publication_date: :asc, created_at: :asc) }
