74a2258d3133d890f345a5b9714dea9328b8ff3b
Sandra <sandraescobedobadia@gmail.com>
2019-01-21 16:44:06 +0100

    feat: require phone to propose investment

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,7 +45,8 @@ class User < ActiveRecord::Base
   validates :phone_number, presence: true, length: { is: 9 },
                            numericality: true,
                            format: { with: /(6|7|9)[0-9]+/,
-                                     message: I18n.t('users.registrations.phone_validation') }
+                                     message: I18n.t('users.registrations.phone_validation') },
+                           if: -> { (persisted? && phone_number_was) || !persisted? }
 
   validate :validate_username_length
 
