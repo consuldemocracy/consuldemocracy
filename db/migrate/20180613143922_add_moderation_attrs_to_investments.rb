7d086b69f3b6452621f3a71a51dfd42650889883
Fernando Faubell Valls <fernando.faubell@aitana.es>
2019-01-21 09:51:06 +0100

    fix: moderation in investments.

--- db/migrate/20180613143922_add_moderation_attrs_to_investments.rb
+++ db/migrate/20180613143922_add_moderation_attrs_to_investments.rb
@@ -2,7 +2,8 @@ class AddModerationAttrsToInvestments < ActiveRecord::Migration
   def change
     change_table :budget_investments do |t|
       t.datetime :confirmed_hide_at
-      t.datetime :ignored_flag_at
+      #t.datetime :ignored_flag_at
+      t.datetime :accepted_at
       t.integer :flags_count, default: 0
     end
   end
