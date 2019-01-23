e8bab193dca86451f2d57ed20c7c395ec06d38fd
taitus <sebastia.roig@gmail.com>
2019-01-23 14:42:31 +0100

    Allow create translations without length validation when locale is distinct to default_locale

--- app/models/concerns/globalizable.rb
+++ app/models/concerns/globalizable.rb
@@ -21,7 +21,13 @@ module Globalizable
   class_methods do
     def validates_translation(method, options = {})
       validates(method, options.merge(if: lambda { |resource| resource.translations.blank? }))
-      translation_class.instance_eval { validates method, options }
+      if options.include?(:length)
+        lenght_validate = { length: options[:length] }
+        translation_class.instance_eval { validates method, lenght_validate.merge(if: lambda { |translation| translation.locale == I18n.default_locale })}
+        translation_class.instance_eval { validates method, options.reject { |key| key == :length } } if options.count > 1
+      else
+        translation_class.instance_eval { validates method, options }
+      end
     end
 
     def translation_class_delegate(method)
