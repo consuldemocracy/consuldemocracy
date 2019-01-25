5a37770fe6cd3929bc171bb96fb8d71d868868db
taitus <sebastia.roig@gmail.com>
2019-01-25 16:39:43 +0100

    Display button or informative text
    When all remote translations are enqueued we not display button but we display informative text.
    
    Co-Authored-By: alessandro <agileontheweb@gmail.com>

--- app/helpers/remote_translations_helper.rb
+++ app/helpers/remote_translations_helper.rb
@@ -5,4 +5,10 @@ module RemoteTranslationsHelper
     available_remote_locales = RemoteTranslationsCaller.new.available_remote_locales
     remote_translations.present? && available_remote_locales.include?(parse_locale(locale).to_s)
   end
+
+  def display_remote_translation_button?(remote_translations)
+    remote_translations.none? do |remote_translation|
+      RemoteTranslation.remote_translation_enqueued?(remote_translation)
+    end
+  end
 end
