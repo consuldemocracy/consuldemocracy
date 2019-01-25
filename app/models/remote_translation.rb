e174c0f01aa544f1f805ae29941a70447e6d8b0b
taitus <sebastia.roig@gmail.com>
2019-01-25 15:00:52 +0100

    Create RemoteTranslation model
    - Each RemoteTranslation is associated with a resource (through polymorphic)
      and has the locale to we want translate.
    - After create a RemoteTranslation we create a enqueue_remote_translation
      method that will be send remote translation instance to remote translation
      client

---
+++ app/models/remote_translation.rb
@@ -0,0 +1,14 @@
+class RemoteTranslation < ApplicationRecord
+
+  belongs_to :remote_translatable, polymorphic: true
+
+  validates :remote_translatable_id, presence: true
+  validates :remote_translatable_type, presence: true
+  validates :locale, presence: true
+
+  after_create :enqueue_remote_translation
+
+  def enqueue_remote_translation
+  end
+
+end
