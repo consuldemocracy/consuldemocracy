c9522b424b1d4178fc782a7dc60bff4773c9a7f4
Julian Herrero <microweb10@gmail.com>
2019-01-29 20:28:57 +0100

    Show unfeasible and unselected investments for finished budgets
    We were filtering by winners investments for finished budget without
    having in consideration other filters.
    We want the default filter to be `winners` for finished budgets.

--- app/controllers/budgets/groups_controller.rb
+++ app/controllers/budgets/groups_controller.rb
@@ -4,7 +4,7 @@ module Budgets
     load_and_authorize_resource :group, class: "Budget::Group"
 
     before_action :set_default_budget_filter, only: :show
-    has_filters %w{not_unfeasible feasible unfeasible unselected selected}, only: [:show]
+    has_filters %w[not_unfeasible feasible unfeasible unselected selected winners], only: [:show]
 
     def show
     end
