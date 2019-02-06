cfab7adf64b05e215e5601ee788bee60877ba2cf
Fernando Faubell Valls <fernando.faubell@aitana.es>
2019-02-06 17:31:29 +0100

    Estructura áreas.

--- db/arees_2019.rb
+++ db/arees_2019.rb
@@ -1,8 +1,81 @@
 Setting["feature.areas"] = 'f'
 
-areas_es = { "IDEAS PARA MEJORAR EL URBANISMO" =>
-               ["Ideas para tener un mobiliario mejor en la ciudad",
-                "Ideas para mejorar las zonas verdes",
+areas = [
+  {
+    name: {
+      val: "IDEES PER A MILLORAR L’URBANISME",
+      es: "IDEAS PARA MEJORAR EL URBANISMO"
+    },
+    subareas: [
+      {
+        val: "Idees per a tindre un mobiliari millor a la ciutat",
+        es: "Ideas para tener un mobiliario mejor en la ciudad",
+      },
+      {
+        val: "Idees per millorar les zones verdes",
+        es: "Ideas para mejorar las zonas verdes"
+      },
+      {
+        val:
+        es:
+      }
+                 "Idees per a fer més accessible la ciutat",
+      {
+        val:
+        es:
+      }
+      {
+        val:
+        es:
+      }
+
+    ]
+
+  }
+
+
+
+                 "Idees per a fer més accessible la ciutat",
+                 "Propostes de millora en temes d’il·luminació i/o contaminació lumínica",
+                 "Tens alguna proposta que responga a un problema diferent en aquest tema?"] }
+
+
+
+
+              "IDEES PER A MILLORAR LA MOBILITAT i EL TRÀNSIT" =>
+                ["Idees per a millorar en l’aparcament públic de la ciutat",
+                 "Idees per millorar el sistema de zona blava",
+                 "Idees per millorar la complicada situació de circulació a la ciutat tant per a vehicles a motor, bicicletes i vianants",
+                 "Idees per reduir la velocitat i el excés de trànsit",
+                 "Idees que milloren la temporalització dels semàfors",
+                 "Tens alguna proposta que responga a un problema diferent en aquest tema?"],
+              "IDEES PER A MILLORAR EN L’EDUCACIÓ FORMAL" =>
+                ["Millores en les instal·lacions educatives",
+                 "Idees per articular un sistema públic de guarderies infantils a la ciutat"],
+              "IDEES PER A TINDRE UNA CIUTAT MÉS NETA I SALUDABLE" =>
+                ["Mesures per a millorar la neteja viària",
+                 "Idees que milloren els problemes de convivència amb les mascotes a la ciutat",
+                 "Mesures per a la reducció de la contaminació",
+                 "Tens alguna proposta que responga a un problema diferent en aquest tema?"],
+              "IDEES PER A MILLORAR LES INSTAL·LACIONS I ELS SERVEIS MUNICIPALS" =>
+                ["Idees per a millorar l’oferta de centres específics per a persones majors, centres juvenils o infància",
+                 "Idees per a millorar els equipaments esportius"],
+              "IDEES PER A MILLORAR LA SEGURETAT CIUTADANA" =>
+                ["Idees per a millorar la sensació de seguretat ciutadana, sobretot a la perifèria"],
+              "IDEES PER RECOLZAR A LES ASSOCIACIONS I INCENTIVAR L’ACTIVITAT SOCIOCULTURAL" =>
+                ["Idees que milloren la situació de les associacions culturals, de dones, esportives, de persones majors i totes en general, a la ciutat",
+                 "Idees que milloren el sistema de subvencions municipal a les associacions."],
+              "IDEES PER A MILLORAR EL DESENVOLUPAMENT ECONÒMIC I EL FOMENT DE L’OCUPACIÓ" =>
+                ["Idees que milloren la situació del xicotet comerç",
+                 "Idees per a augmentar la varietat de comerços a la ciutat",
+                 "Idees per a millorar l’oferta de treball a la ciutat",
+                 "Idees per a incentivar la innovació i els nous projectes empresarials a la ciutat"],
+              "IDEES PER A MILLORAR LA SITUACIÓ DE LES PERSONES AMB NECESSITATS ESPECÍFIQUES" =>
+                ["Com milloraries els sistema d’ajuda a aquest col·lectiu?",
+                 "Idees per a millorar les condicions de les residències de la tercera edat.",
+                 "Idees per a millorar en els serveis social municipals"]
+            }]
+areas_es = {
                 "Ideas para hacer más accesible la ciudad",
                 "Propuestas de mejora en temas de iluminación y/o contaminación lumínica",
                 "¿Tienes alguna propuesta que responda a un problema diferente en este tema?"],
@@ -39,45 +112,10 @@ areas_es = { "IDEAS PARA MEJORAR EL URBANISMO" =>
                  "Ideas para mejorar las condiciones de las residencias de la tercera edad.",
                  "Ideas para mejorar en los servicios social municipales"]
             }
-areas_val = { "IDEES PER A MILLORAR L’URBANISME" =>
-                ["Idees per a tindre un mobiliari millor a la ciutat",
-                 "Idees per millorar les zones verdes",
-                 "Idees per a fer més accessible la ciutat",
-                 "Propostes de millora en temes d’il·luminació i/o contaminació lumínica",
-                 "Tens alguna proposta que responga a un problema diferent en aquest tema?"],
-              "IDEES PER A MILLORAR LA MOBILITAT i EL TRÀNSIT" =>
-                ["Idees per a millorar en l’aparcament públic de la ciutat",
-                 "Idees per millorar el sistema de zona blava",
-                 "Idees per millorar la complicada situació de circulació a la ciutat tant per a vehicles a motor, bicicletes i vianants",
-                 "Idees per reduir la velocitat i el excés de trànsit",
-                 "Idees que milloren la temporalització dels semàfors",
-                 "Tens alguna proposta que responga a un problema diferent en aquest tema?"],
-              "IDEES PER A MILLORAR EN L’EDUCACIÓ FORMAL" =>
-                ["Millores en les instal·lacions educatives",
-                 "Idees per articular un sistema públic de guarderies infantils a la ciutat"],
-              "IDEES PER A TINDRE UNA CIUTAT MÉS NETA I SALUDABLE" =>
-                ["Mesures per a millorar la neteja viària",
-                 "Idees que milloren els problemes de convivència amb les mascotes a la ciutat",
-                 "Mesures per a la reducció de la contaminació",
-                 "Tens alguna proposta que responga a un problema diferent en aquest tema?"],
-              "IDEES PER A MILLORAR LES INSTAL·LACIONS I ELS SERVEIS MUNICIPALS" =>
-                ["Idees per a millorar l’oferta de centres específics per a persones majors, centres juvenils o infància",
-                 "Idees per a millorar els equipaments esportius"],
-              "IDEES PER A MILLORAR LA SEGURETAT CIUTADANA" =>
-                ["Idees per a millorar la sensació de seguretat ciutadana, sobretot a la perifèria"],
-              "IDEES PER RECOLZAR A LES ASSOCIACIONS I INCENTIVAR L’ACTIVITAT SOCIOCULTURAL" =>
-                ["Idees que milloren la situació de les associacions culturals, de dones, esportives, de persones majors i totes en general, a la ciutat",
-                 "Idees que milloren el sistema de subvencions municipal a les associacions."],
-              "IDEES PER A MILLORAR EL DESENVOLUPAMENT ECONÒMIC I EL FOMENT DE L’OCUPACIÓ" =>
-                ["Idees que milloren la situació del xicotet comerç",
-                 "Idees per a augmentar la varietat de comerços a la ciutat",
-                 "Idees per a millorar l’oferta de treball a la ciutat",
-                 "Idees per a incentivar la innovació i els nous projectes empresarials a la ciutat"],
-              "IDEES PER A MILLORAR LA SITUACIÓ DE LES PERSONES AMB NECESSITATS ESPECÍFIQUES" =>
-                ["Com milloraries els sistema d’ajuda a aquest col·lectiu?",
-                 "Idees per a millorar les condicions de les residències de la tercera edat.",
-                 "Idees per a millorar en els serveis social municipals"]
-            }
+
+
+
+
 I18n.default_locale = :es
 areas_es.each do |area, sub_areas|
   new_area = Area.find_or_create_by(name: area)
