542508e9b97cac9120b1090149e9dfe496c3853e
decabeza <alberto@decabeza.es>
2019-02-12 11:53:03 +0100

    Add order to voted headings names

--- app/models/user.rb
+++ app/models/user.rb
@@ -142,7 +142,7 @@ class User < ActiveRecord::Base
   end
 
   def headings_voted_within_group(group)
-    Budget::Heading.where(id: voted_investments.by_group(group).pluck(:heading_id))
+    Budget::Heading.order("name").where(id: voted_investments.by_group(group).pluck(:heading_id))
   end
 
   def voted_investments
