67676b5f50a537d2a3da5dd5873f2629860f317c
milovanderlinden <milo@dogodigi.net>
2019-02-15 12:00:12 +0100

    Remove deprecation warning for serve_static_assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,6 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_assets = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.serve_static_file = ENV['RAILS_SERVE_STATIC_FILES'].present?
   # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,7 +18,6 @@ Rails.application.configure do
   config.allow_concurrency = false
 
   # Configure static file server for tests with Cache-Control for performance.
-  config.serve_static_assets = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.serve_static_file = ENV['RAILS_SERVE_STATIC_FILES'].present?
   # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.static_cache_control = 'public, max-age=3600'
