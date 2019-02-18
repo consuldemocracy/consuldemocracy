b3928b67539fa2031eb8c1aad8591ac24b44b79e
milovanderlinden <milo@dogodigi.net>
2019-02-18 09:07:52 +0100

    Don't seed, we do not need it

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 if [[ -v CUSTOM_SET_NAME ]]; then
     bin/rake db:migrate
-    bin/rake db:seed
     curl -s https://consul-assets-service.consulproject.nl/?set=$CUSTOM_SET_NAME | bash
 fi
