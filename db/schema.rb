edde17024462a2f206fa3b60aece052c376d5594
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-02-21 18:21:03 +0100

    Remove deprecated attributes from SiteCustomization::Page
    Some fields from SiteCustomization::page are translatable and we no
    longer need them. This commit will remove the annoying deprecation
    warning thrown by Globalize gem after gem version update.

--- db/schema.rb
+++ db/schema.rb
@@ -1274,9 +1274,6 @@ ActiveRecord::Schema.define(version: 20190411090023) do
 
   create_table "site_customization_pages", force: :cascade do |t|
     t.string   "slug",                                 null: false
-    t.string   "title"
-    t.string   "subtitle"
-    t.text     "content"
     t.boolean  "more_info_flag"
     t.boolean  "print_content_flag"
     t.string   "status",             default: "draft"
