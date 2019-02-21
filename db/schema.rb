2575f863bc9174025a006889b7daaad695b40a8a
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-02-21 17:28:09 +0100

    Remove deprecated attributes from Poll
    Some fields from Poll are translatable and we no longer need them. This
    commit will remove the annoying deprecation warning thrown by Globalize
    gem after gem version update.

--- db/schema.rb
+++ db/schema.rb
@@ -1256,13 +1256,10 @@ ActiveRecord::Schema.define(version: 20190607160900) do
   end
 
   create_table "polls", force: :cascade do |t|
-    t.string   "name"
     t.datetime "starts_at"
     t.datetime "ends_at"
     t.boolean  "published",          default: false
     t.boolean  "geozone_restricted", default: false
-    t.text     "summary"
-    t.text     "description"
     t.integer  "comments_count",     default: 0
     t.integer  "author_id"
     t.datetime "hidden_at"
