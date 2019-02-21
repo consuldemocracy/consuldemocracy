0f6c7fb5da2b038e19a79bb723358cb4a008020f
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-02-21 17:30:56 +0100

    Remove deprecated attributes from Poll:Question
    Some fields from Poll::Question are translatable and we no longer need
    them. This commit will remove the annoying deprecation warning thrown
    by Globalize gem after gem version update.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190221162450) do
+ActiveRecord::Schema.define(version: 20190221162858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1038,7 +1038,6 @@ ActiveRecord::Schema.define(version: 20190221162450) do
     t.integer  "poll_id"
     t.integer  "author_id"
     t.string   "author_visible_name"
-    t.string   "title"
     t.integer  "comments_count"
     t.datetime "hidden_at"
     t.datetime "created_at"
