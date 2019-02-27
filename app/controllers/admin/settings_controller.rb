67c646821da13b395b927c8758ad0b26eff3b7d9
decabeza <alberto@decabeza.es>
2019-02-27 11:56:34 +0100

    Hide removed settings

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -2,7 +2,7 @@ class Admin::SettingsController < Admin::BaseController
 
   def index
     all_settings = Setting.all.group_by { |s| s.type }
-    @settings = all_settings['common']
+    @settings = all_settings["common"].reject { |setting| hidden_settings.include?(setting.key)}
     @feature_flags = all_settings['feature']
   end
 
@@ -23,6 +23,20 @@ class Admin::SettingsController < Admin::BaseController
 
     def settings_params
       params.require(:setting).permit(:value)
+    end
+
+    def hidden_settings
+      ["place_name",
+       "blog_url",
+       "transparency_url",
+       "opendata_url",
+       "banner-style.banner-style-one",
+       "banner-style.banner-style-two",
+       "banner-style.banner-style-three",
+       "banner-img.banner-img-one",
+       "banner-img.banner-img-two",
+       "banner-img.banner-img-three",
+       "proposal_improvement_path"]
     end
 
 end
