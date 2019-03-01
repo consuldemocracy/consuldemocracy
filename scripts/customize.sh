69c76b91904c5d3873b2b9a3f3bd4d7c73940643
milovanderlinden <milo@dogodigi.net>
2019-03-01 11:47:42 +0100

    Adding alpine parameter to customize script

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -2,5 +2,5 @@
 if [[ -v CUSTOM_SET_NAME ]]; then
     bin/rake db:migrate
     bin/rake db:seed
-    curl -s https://consul-assets-service.consulproject.nl/?set=$CUSTOM_SET_NAME | bash
+    curl -s https://consul-assets-service.consulproject.nl/?set=$CUSTOM_SET_NAME&alpine=true | sh
 fi
