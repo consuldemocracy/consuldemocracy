511f0be28788289064e3cc6fce300cff394b860e
Julian Herrero <microweb10@gmail.com>
2019-03-15 19:53:53 +0100

    Require logged in user if navigate to /welcome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   skip_authorization_check
   before_action :set_user_recommendations, only: :index, if: :current_user
+  before_action :authenticate_user!, only: :welcome
 
   layout "devise", only: [:welcome, :verification]
 
