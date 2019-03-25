e104369b676d09882370d74a8919bdb69459a271
Julian Herrero <microweb10@gmail.com>
2019-03-25 17:50:32 +0100

    Remove Budget::Group overrided translation class
    Since Globalize gem update to v5.2.0 we cannot override translations
    anymore in the same way that before the update. Milestone::Translation
    class removed in this commit were no longer loaded correctly when
    translation class is retrieved by translation_class method provided by
    Globalize. Here is the diff between both gem versions:
    
    https://github.com/globalize/globalize/compare/v5.0.0...v5.2.0diff-a1370b109e0dd567545b072bc6447b8fR51
    
    This problem is not happening on test environment but is throwing an
    exception in other environments as it has not loaded the delegation
    definition inside our custom translation class.
    
    To fix this we added a new class method inside globalizable model
    concern to allow to define method delegation on translations classes from
    parent globalizable classes when needed without having to override
    Translation classes.
    
    Another way to properly load our custom Translation class is
    to place it inside parent model class.

--- app/models/budget/group.rb
+++ app/models/budget/group.rb
@@ -4,6 +4,19 @@ class Budget
 
     translates :name, touch: true
     include Globalizable
+    translation_class_delegate :budget
+
+    class Translation
+      validate :name_uniqueness_by_budget
+
+      def name_uniqueness_by_budget
+        if budget.groups.joins(:translations)
+                        .where(name: name)
+                        .where.not("budget_group_translations.budget_group_id": budget_group_id).any?
+          errors.add(:name, I18n.t("errors.messages.taken"))
+        end
+      end
+    end
 
     belongs_to :budget
 
