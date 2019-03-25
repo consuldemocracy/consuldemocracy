5343448c5a3d006e3b377f07cf97a54f6884c469
taitus <sebastia.roig@gmail.com>
2019-03-25 19:58:56 +0100

    Remove deprecated attributes from Budget::Phase
    Some fields from Budget::Phase are translatable and we no longer need them. This commit will remove the annoying deprecation warning thrown by Globalize gem after gem version update.

--- db/schema.rb
+++ db/schema.rb
@@ -297,8 +297,6 @@ ActiveRecord::Schema.define(version: 20190607160900) do
     t.integer  "budget_id"
     t.integer  "next_phase_id"
     t.string   "kind",                         null: false
-    t.text     "summary"
-    t.text     "description"
     t.datetime "starts_at"
     t.datetime "ends_at"
     t.boolean  "enabled",       default: true
