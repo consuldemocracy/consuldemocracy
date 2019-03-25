accc17504bc5a875b34c563277b4ab29ab58c1e7
taitus <sebastia.roig@gmail.com>
2019-03-25 19:39:34 +0100

    Fix spec models/widget/feed_spec.rb:61 as branch master.

--- app/models/widget/feed.rb
+++ app/models/widget/feed.rb
@@ -31,7 +31,7 @@ class Widget::Feed < ActiveRecord::Base
   end
 
   def processes
-    Legislation::Process.open.published.limit(limit)
+    Legislation::Process.open.published.order("created_at DESC").limit(limit)
   end
 
 end
