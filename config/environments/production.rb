2d2bf63d88cbc40e50a0409f041468d0b56cf2cd
Alberto Miedes <albertomg994@gmail.com>
2019-03-29 12:00:15 +0100

    Update environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :warn
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
@@ -65,6 +65,18 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: Rails.application.secrets.server_name }
   config.action_mailer.asset_host = "https://#{Rails.application.secrets.server_name}"
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+                 address: Rails.application.secrets.smtp_address,
+                    port: Rails.application.secrets.smtp_port,
+                  domain: Rails.application.secrets.smtp_domain,
+               user_name: Rails.application.secrets.smtp_user_name,
+                password: Rails.application.secrets.smtp_password,
+          authentication: 'login',
+    enable_starttls_auto: true,
+                     ssl: false
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
