7cefec4c083dbac3fbad89e5097b857a7bd1e5fe
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-04-01 11:44:50 +0200

    Move Translation class inside Budget::Phase
    The reason to move Translation class is explained here [1]
    
    [1] https://github.com/consul/consul/pull/3359/commits/106649a8a598fb4b1389e174fdd6e70f51767af2

--- app/models/budget/phase.rb
+++ app/models/budget/phase.rb
@@ -100,5 +100,14 @@ class Budget
         PHASE_KINDS.index(kind) >= PHASE_KINDS.index(phase)
       end
 
+    class Translation < Globalize::ActiveRecord::Translation
+      before_validation :sanitize_description
+
+      private
+
+        def sanitize_description
+          self.description = WYSIWYGSanitizer.new.sanitize(description)
+        end
+    end
   end
 end
