c2f393276af6acc99594b89a1788b6336e4fa421
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-04-01 11:39:23 +0200

    Move Translation class inside Budget::Group
    The reason to move Translation class is explained here [1]
    
    [1] https://github.com/consul/consul/pull/3359/commits/106649a8a598fb4b1389e174fdd6e70f51767af2

--- app/models/budget/group.rb
+++ app/models/budget/group.rb
@@ -40,5 +40,18 @@ class Budget
       slug.nil? || budget.drafting?
     end
 
+    class Translation < Globalize::ActiveRecord::Translation
+      delegate :budget, to: :globalized_model
+
+      validate :name_uniqueness_by_budget
+
+      def name_uniqueness_by_budget
+        if budget.groups.joins(:translations)
+                        .where(name: name)
+                        .where.not("budget_group_translations.budget_group_id": budget_group_id).any?
+          errors.add(:name, I18n.t("errors.messages.taken"))
+        end
+      end
+    end
   end
 end
