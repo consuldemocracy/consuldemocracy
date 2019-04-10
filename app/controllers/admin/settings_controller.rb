856e6cb31b7b539e253bb47948a2cfee8c8ed533
taitus <sebastia.roig@gmail.com>
2019-04-10 14:04:56 +0200

    Add content to the Remote Census Configuration tab
     - Render remote census configuration content on settings index.
     - Update type method from Setting
       On Admin::SettingsController#index we are using 'all_settings' to
       group all settings by 'type' method.
       'type' method return the first part of key when split by '.'
       To allow use by example: all_settings["remote_census.general"]
       and recover only settings related with this key we have added new
       'elsif' on 'type' method.

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -14,6 +14,9 @@ class Admin::SettingsController < Admin::BaseController
     @participation_processes_settings = all_settings["process"]
     @map_configuration_settings = all_settings["map"]
     @proposals_settings = all_settings["proposals"]
+    @remote_census_general_settings = all_settings["remote_census.general"]
+    @remote_census_request_settings = all_settings["remote_census.request"]
+    @remote_census_response_settings = all_settings["remote_census.response"]
     @uploads_settings = all_settings["uploads"]
   end
 
