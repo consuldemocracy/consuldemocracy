902365acfc342d0703f2b0ffd24d1bfc42babc8a
taitus <sebastia.roig@gmail.com>
2019-04-16 16:20:34 +0200

    Allow new call to CustomCensusApi on CensusCaller
    When Setting["feature.remote_census"] is active, we allow and
    ability the calls to new CustomCensusAPI througth CensusCaller

--- lib/census_caller.rb
+++ lib/census_caller.rb
@@ -1,7 +1,11 @@
 class CensusCaller
 
   def call(document_type, document_number)
-    response = CensusApi.new.call(document_type, document_number)
+    if Setting["feature.remote_census"].present?
+      response = CustomCensusApi.new.call(document_type, document_number)
+    else
+      response = CensusApi.new.call(document_type, document_number)
+    end
     response = LocalCensus.new.call(document_type, document_number) unless response.valid?
 
     response
