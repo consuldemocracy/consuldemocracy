3ffaa3f9709b028c7b7176dbe8d9047e88afb3c8
taitus <sebastia.roig@gmail.com>
2019-04-17 14:12:14 +0200

    Verification::Residence allow to use new CustomCensusAPI
    Verification::Residence allow send params :date_of_birth and
    :postal_code to CensusCaller call method.

--- spec/features/verification/residence_spec.rb
+++ spec/features/verification/residence_spec.rb
@@ -21,6 +21,25 @@ feature "Residence" do
     expect(page).to have_content "Residence verified"
   end
 
+  scenario "Verify resident throught CustomCensusApi" do
+    Setting["feature.remote_census"] = true
+    user = create(:user)
+    login_as(user)
+
+    visit account_path
+    click_link "Verify my account"
+
+    fill_in "residence_document_number", with: "12345678Z"
+    select "DNI", from: "residence_document_type"
+    select_date "31-December-1980", from: "residence_date_of_birth"
+    fill_in "residence_postal_code", with: "28013"
+    check "residence_terms_of_service"
+    click_button "Verify residence"
+
+    expect(page).to have_content "Residence verified"
+    Setting["feature.remote_census"] = nil
+  end
+
   scenario "Residence form use min age to participate" do
     min_age = (Setting["min_age_to_participate"] = 16).to_i
     underage = min_age - 1
