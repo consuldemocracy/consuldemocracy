c6a8aa1301321f294bc3c3499522d22ef297780d
Javi Martín <javim@elretirao.net>
2019-04-26 20:28:46 +0200

    Replace `attribute_changed?` in `before` callbacks
    This method is deprecated in Rails 5.1 because its behavior will be
    different in `before` and `after` callbacks.
    
    Here we're replacing the deprecated `attribute_changed?` and
    `attribute_was` with `will_save_change_to_attribute?` and
    `attribute_in_database` during `before_save` callbacks.
    
    https://github.com/rails/rails/pull/32835/

--- app/models/budget/investment.rb
+++ app/models/budget/investment.rb
@@ -396,7 +396,7 @@ class Budget
     private
 
       def set_denormalized_ids
-        self.group_id = heading&.group_id if heading_id_changed?
+        self.group_id = heading&.group_id if will_save_change_to_heading_id?
         self.budget_id ||= heading&.group&.budget_id
       end
 
