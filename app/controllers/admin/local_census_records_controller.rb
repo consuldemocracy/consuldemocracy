d942ba455e6016d2ae26983ae239ad6beb763b6a
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-05-08 18:55:31 +0200

    Add new and create actions

--- app/controllers/admin/local_census_records_controller.rb
+++ app/controllers/admin/local_census_records_controller.rb
@@ -5,4 +5,21 @@ class Admin::LocalCensusRecordsController < Admin::BaseController
     @local_census_records = @local_census_records.search(params[:search])
     @local_census_records = @local_census_records.page(params[:page])
   end
+
+  def create
+    @local_census_record = LocalCensusRecord.new(local_census_record_params)
+    if @local_census_record.save
+      redirect_to admin_local_census_records_path,
+        notice: t("admin.local_census_records.create.notice")
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def local_census_record_params
+      attributes = [:document_type, :document_number, :date_of_birth, :postal_code]
+      params.require(:local_census_record).permit(*attributes)
+    end
 end
