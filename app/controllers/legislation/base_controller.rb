b1551bc816a3cf885a943081d4618b09367d04b0
lalo <abelardogilm@gmail.com>
2019-05-09 13:48:11 +0200

    Add process PeopleProposals index page

--- app/controllers/legislation/base_controller.rb
+++ app/controllers/legislation/base_controller.rb
@@ -6,4 +6,11 @@ class Legislation::BaseController < ApplicationController
   def legislation_proposal_votes(proposals)
     @legislation_proposal_votes = current_user ? current_user.legislation_proposal_votes(proposals) : {}
   end
+
+  def legislation_people_proposal_votes(people_proposals)
+    @legislation_proposal_votes = {}
+    if current_user
+      @legislation_proposal_votes = current_user.legislation_people_proposal_votes(people_proposals)
+    end
+  end
 end
