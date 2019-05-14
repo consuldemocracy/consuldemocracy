527e06938a4754e296bb60fbf15b5f779dffe4fc
taitus <sebastia.roig@gmail.com>
2019-05-14 11:49:34 +0200

    Allow any configuration of required fields
    - Add the methods "parse_date_of_birth" and
    "parse_postal_code" to recover the correct
    position of their values depending on the
    configuration of the remote census.

--- app/models/signature_sheet.rb
+++ app/models/signature_sheet.rb
@@ -23,8 +23,8 @@ class SignatureSheet < ActiveRecord::Base
   def verify_signatures
     parsed_required_fields_to_verify_groups.each do |required_fields_to_verify|
       document_number = required_fields_to_verify[0]
-      date_of_birth = required_fields_to_verify[1]
-      postal_code = required_fields_to_verify[2]
+      date_of_birth = parse_date_of_birth(required_fields_to_verify)
+      postal_code = parse_postal_code(required_fields_to_verify)
 
       signature = signatures.where(document_number: document_number,
                                    date_of_birth: date_of_birth,
@@ -40,5 +40,23 @@ class SignatureSheet < ActiveRecord::Base
 
   def signable_found
     errors.add(:signable_id, :not_found) if errors.messages[:signable].present?
+  end
+
+  private
+
+  def parse_date_of_birth(required_fields_to_verify)
+    return required_fields_to_verify[1] if Setting["remote_census_request.alias_date_of_birth"].present?
+    nil
+  end
+
+  def parse_postal_code(required_fields_to_verify)
+    if Setting["remote_census_request.alias_date_of_birth"].present? &&
+        Setting["remote_census_request.alias_postal_code"].present?
+      return required_fields_to_verify[2]
+    elsif Setting["remote_census_request.alias_postal_code"].present?
+      return required_fields_to_verify[1]
+    else
+      return nil
+    end
   end
 end
