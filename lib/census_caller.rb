437069ebf44c67ad12c221747776304e3e16955d
voodoorai2000 <voodoorai2000@gmail.com>
2019-05-16 20:36:10 +0200

    Cleanup LocalCensus usage

--- lib/census_caller.rb
+++ lib/census_caller.rb
@@ -2,8 +2,6 @@ class CensusCaller
 
   def call(document_type, document_number)
     #response = CensusApi.new.call(document_type, document_number)
-    response = LocalCensus.new.call(document_type, document_number) unless response.valid?
-
-    response
+    LocalCensus.new.call(document_type, document_number)
   end
 end
