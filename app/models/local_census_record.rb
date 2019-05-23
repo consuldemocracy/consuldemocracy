d1587ecc6202b3eeb98e8a974a88095c80b7e732
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-05-23 12:51:16 +0200

    Add sanitization to clear trailing whitespaces

--- app/models/local_census_record.rb
+++ app/models/local_census_record.rb
@@ -1,8 +1,18 @@
 class LocalCensusRecord < ApplicationRecord
+  before_validation :sanitize
+
   validates :document_number, presence: true
   validates :document_type, presence: true
   validates :date_of_birth, presence: true
   validates :postal_code, presence: true
 
   scope :search,  -> (terms) { where("document_number ILIKE ?", "%#{terms}%") }
+
+  private
+
+    def sanitize
+      self.document_type   = self.document_type&.strip
+      self.document_number = self.document_number&.strip
+      self.postal_code     = self.postal_code&.strip
+    end
 end
