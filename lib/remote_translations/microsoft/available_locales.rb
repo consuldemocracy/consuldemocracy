b01977f74a74562cd515633433f4b93909359e79
taitus <sebastia.roig@gmail.com>
2019-05-24 18:22:24 +0200

    Fix houncibot offenses.

--- lib/remote_translations/microsoft/available_locales.rb
+++ lib/remote_translations/microsoft/available_locales.rb
@@ -1,7 +1,7 @@
-require 'net/https'
-require 'uri'
-require 'cgi'
-require 'json'
+require "net/https"
+require "uri"
+require "cgi"
+require "json"
 
 module RemoteTranslations::Microsoft::AvailableLocales
 
@@ -24,22 +24,22 @@ module RemoteTranslations::Microsoft::AvailableLocales
 
   private
 
-  def remote_available_locales
-    host = 'https://api.cognitive.microsofttranslator.com'
-    path = '/languages?api-version=3.0'
+    def remote_available_locales
+      host = "https://api.cognitive.microsofttranslator.com"
+      path = "/languages?api-version=3.0"
 
-    uri = URI (host + path)
+      uri = URI (host + path)
 
-    request = Net::HTTP::Get.new(uri)
-    request['Ocp-Apim-Subscription-Key'] = Rails.application.secrets.microsoft_api_key
+      request = Net::HTTP::Get.new(uri)
+      request["Ocp-Apim-Subscription-Key"] = Rails.application.secrets.microsoft_api_key
 
-    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-      http.request (request)
+      response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == "https") do |http|
+        http.request (request)
+      end
+
+      result = response.body.force_encoding("utf-8")
+
+      JSON.parse(result)["translation"]
     end
-
-    result = response.body.force_encoding("utf-8")
-
-    JSON.parse(result)["translation"]
-  end
 
 end
--- lib/remote_translations/microsoft/sentences_parser.rb
+++ lib/remote_translations/microsoft/sentences_parser.rb
@@ -1,11 +1,12 @@
 module RemoteTranslations::Microsoft::SentencesParser
 
   def detect_split_position(text)
-    minimum_valid_index = text.size - RemoteTranslations::Microsoft::Client::CHARACTERS_LIMIT_PER_REQUEST
-    valid_point = text[minimum_valid_index..text.size].index('.')
-    valid_whitespace = text[minimum_valid_index..text.size].index(' ')
+    limit = RemoteTranslations::Microsoft::Client::CHARACTERS_LIMIT_PER_REQUEST
+    minimum_valid_index = text.size - limit
+    valid_point = text[minimum_valid_index..text.size].index(".")
+    valid_whitespace = text[minimum_valid_index..text.size].index(" ")
 
-    split_position = get_split_position(valid_point, valid_whitespace, minimum_valid_index)
+    get_split_position(valid_point, valid_whitespace, minimum_valid_index)
   end
 
   def get_split_position(valid_point, valid_whitespace, minimum_valid_index)
