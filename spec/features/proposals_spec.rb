4be4d967100615fffffbf6b623e8d5fed5be89c4
Julian Herrero <microweb10@gmail.com>
2019-05-24 16:53:40 +0200

    Don't show order links in selected proposals list

--- spec/features/proposals_spec.rb
+++ spec/features/proposals_spec.rb
@@ -997,6 +997,22 @@ feature "Proposals" do
       expect(page).not_to have_link "Recommended"
       expect(page).not_to have_link "See more recommendations"
     end
+
+    scenario "do not show order links in selected proposals list" do
+      visit proposals_path
+
+      expect(page).to have_css  "ul.submenu"
+      expect(page).to have_link "most active"
+      expect(page).to have_link "highest rated"
+      expect(page).to have_link "newest"
+
+      click_link "Selected proposals"
+
+      expect(page).not_to have_css  "ul.submenu"
+      expect(page).not_to have_link "most active"
+      expect(page).not_to have_link "highest rated"
+      expect(page).not_to have_link "newest"
+    end
   end
 
   context "Search" do
