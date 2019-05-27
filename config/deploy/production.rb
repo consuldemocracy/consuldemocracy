6e8595ced2a06073b011bc9f7b627b5924a4be7e
Filip Muki Dobranic <filipdobranic@gmail.com>
2019-05-27 15:26:35 +0200

    change production branch

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,10 @@
-set :branch, :master
+set :deploy_to, deploysecret(:deploy_to)
+set :server_name, deploysecret(:server_name)
+set :db_server, deploysecret(:db_server)
+set :branch, "consul-sos"
+set :ssh_options, port: deploysecret(:ssh_port)
+set :stage, :production
+set :rails_env, :production
 
 server deploysecret(:server1), user: deploysecret(:user), roles: %w[web app db importer cron background]
 #server deploysecret(:server2), user: deploysecret(:user), roles: %w(web app db importer cron background)
