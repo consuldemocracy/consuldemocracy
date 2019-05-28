8f6939979991735755b3677c5979c52fb411230a
Javi Martín <javim@elretirao.net>
2019-05-28 16:40:52 +0200

    Add version to migration files
    These migrations were backported after upgrading to Rails 5.

--- db/migrate/20170628211547_add_ballot_line_counter_cached_to_budget_ballots.rb
+++ db/migrate/20170628211547_add_ballot_line_counter_cached_to_budget_ballots.rb
@@ -1,4 +1,4 @@
-class AddBallotLineCounterCachedToBudgetBallots < ActiveRecord::Migration
+class AddBallotLineCounterCachedToBudgetBallots < ActiveRecord::Migration[4.2]
   def change
     add_column :budget_ballots, :ballot_lines_count, :integer, default: 0
   end
--- db/migrate/20171008154106_add_slug_to_polls.rb
+++ db/migrate/20171008154106_add_slug_to_polls.rb
@@ -1,4 +1,4 @@
-class AddSlugToPolls < ActiveRecord::Migration
+class AddSlugToPolls < ActiveRecord::Migration[4.2]
   def change
     add_column :polls, :slug, :string
   end
