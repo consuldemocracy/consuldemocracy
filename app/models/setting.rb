javierm/consul

Sort settings alphabetically

app/models/setting.rb
@@ -47,87 +47,87 @@ class Setting < ActiveRecord::Base
 
     def defaults
       {
+        "banner-img.banner-img-one":                                "Banner image 1",
+        "banner-img.banner-img-three":                              "Banner image 3",
+        "banner-img.banner-img-two":                                "Banner image 2",
+        "banner-style.banner-style-one":                            "Banner style 1",
+        "banner-style.banner-style-three":                          "Banner style 3",
+        "banner-style.banner-style-two":                            "Banner style 2",
+        "blog_url":                                                 "https://diario.madrid.es/decidemadrid/",
+        "comments_body_max_length":                                 1000,
+        "direct_message_max_per_day":                               3, # For proposal notifications
+        "email_domain_for_officials":                               '', # Users with this email domain (including subdomains) will automatically be marked as level 1 officials
+        "facebook_handle":                                          "Abriendo-Madrid-1475577616080350",
+        "feature.allow_attached_documents":                         true,
+        "feature.allow_images":                                     true,
+        "feature.budgets":                                          true,
+        "feature.community":                                        true,
+        "feature.debates":                                          true,
+        "feature.facebook_login":                                   true,
+        "feature.featured_proposals":                               true,
+        "feature.google_login":                                     true,
+        "feature.guides":                                           nil,
+        "feature.help_page":                                        true,
+        "feature.homepage.widgets.feeds.debates":                   true,
+        "feature.homepage.widgets.feeds.processes":                 true,
+        "feature.homepage.widgets.feeds.proposals":                 true,
+        "feature.legislation":                                      true,
+        "feature.map":                                              nil,
+        "feature.polls":                                            true,
+        "feature.proposals":                                        true,
+        "feature.public_stats":                                     true,
+        "feature.signature_sheets":                                 true,
+        "feature.spending_proposal_features.final_voting_allowed":  true,
+        "feature.spending_proposal_features.open_results_page":     nil,
+        "feature.spending_proposal_features.phase1":                true,
+        "feature.spending_proposal_features.phase2":                nil,
+        "feature.spending_proposal_features.phase3":                nil,
+        "feature.spending_proposal_features.valuation_allowed":     nil,
+        "feature.spending_proposal_features.voting_allowed":        nil,
+        "feature.spending_proposals":                               nil,
+        "feature.twitter_login":                                    true,
+        "feature.user.recommendations":                             true,
+        "feature.user.recommendations_on_debates":                  true,
+        "feature.user.recommendations_on_proposals":                true,
+        "feature.user.skip_verification":                           'true',
+        "featured_proposals_number":                                3,
+        "instagram_handle":                                         "decidemadrid",
+        "mailer_from_address":                                      'noreply@consul.dev',
+        "mailer_from_name":                                         'CONSUL',
+        "map_latitude":                                             40.4332002,
+        "map_longitude":                                            -3.7009591,
+        "map_zoom":                                                 10,
+        "max_ratio_anon_votes_on_debates":                          50,
+        "max_votes_for_debate_edit":                                1000,
+        "max_votes_for_proposal_edit":                              1000,
+        "meta_description":                                         nil,
+        "meta_keywords":                                            nil,
+        "meta_title":                                               nil,
+        "min_age_to_participate":                                   16,
+        "min_age_to_verify":                                        16,
+        "months_to_archive_proposals":                              12,
         "official_level_1_name":                                    "Empleados públicos", # For the moderation console
         "official_level_2_name":                                    "Organización Municipal",
         "official_level_3_name":                                    "Directores generales",
         "official_level_4_name":                                    "Concejales",
         "official_level_5_name":                                    "Alcaldesa",
-        "max_ratio_anon_votes_on_debates":                          50,
-        "max_votes_for_debate_edit":                                1000,
-        "max_votes_for_proposal_edit":                              1000,
-        "comments_body_max_length":                                 1000,
-        "proposal_code_prefix":                                     'MAD',
-        "votes_for_proposal_success":                               53726,
-        "months_to_archive_proposals":                              12,
-        "email_domain_for_officials":                               '', # Users with this email domain (including subdomains) will automatically be marked as level 1 officials
-        "per_page_code_head":                                       '',
+        "opendata_url":                                             "http://datos.madrid.es/",
+        "org_name":                                                 "Decide Madrid",
         "per_page_code_body":                                       '',
+        "per_page_code_head":                                       '',
+        "place_name":                                               "Madrid",
+        "proposal_code_prefix":                                     'MAD',
+        "proposal_improvement_path":                                nil,
+        "proposal_notification_minimum_interval_in_days":           3,
+        "related_content_score_threshold":                          -0.3,
+        "telegram_handle":                                          nil,
+        "transparency_url":                                         "http://transparencia.madrid.es/",
         "twitter_handle":                                           "abriendomadrid",
         "twitter_hashtag":                                          "#decidemadrid",
-        "facebook_handle":                                          "Abriendo-Madrid-1475577616080350",
-        "youtube_handle":                                           "channel/UCFmaChI9quIY7lwHplnacfg",
-        "telegram_handle":                                          nil,
-        "instagram_handle":                                         "decidemadrid",
-        "blog_url":                                                 "https://diario.madrid.es/decidemadrid/",
-        "transparency_url":                                         "http://transparencia.madrid.es/",
-        "opendata_url":                                             "http://datos.madrid.es/",
         "url":                                                      "https://decide.madrid.es", # Public-facing URL of the app.
-        "org_name":                                                 "Decide Madrid",
-        "place_name":                                               "Madrid",
-        "meta_title":                                               nil,
-        "meta_description":                                         nil,
-        "meta_keywords":                                            nil,
-        "feature.debates":                                          true,
-        "feature.proposals":                                        true,
-        "feature.featured_proposals":                               true,
-        "feature.spending_proposals":                               nil,
-        "feature.polls":                                            true,
-        "feature.twitter_login":                                    true,
-        "feature.facebook_login":                                   true,
-        "feature.google_login":                                     true,
-        "feature.public_stats":                                     true,
-        "feature.budgets":                                          true,
-        "feature.signature_sheets":                                 true,
-        "feature.legislation":                                      true,
-        "feature.user.recommendations":                             true,
-        "feature.user.recommendations_on_debates":                  true,
-        "feature.user.recommendations_on_proposals":                true,
-        "feature.community":                                        true,
-        "feature.map":                                              nil,
-        "feature.allow_images":                                     true,
-        "feature.allow_attached_documents":                         true,
-        "feature.guides":                                           nil,
-        "feature.help_page":                                        true,
-        "feature.spending_proposal_features.phase1":                true,
-        "feature.spending_proposal_features.phase2":                nil,
-        "feature.spending_proposal_features.phase3":                nil,
-        "feature.spending_proposal_features.voting_allowed":        nil,
-        "feature.spending_proposal_features.final_voting_allowed":  true,
-        "feature.spending_proposal_features.open_results_page":     nil,
-        "feature.spending_proposal_features.valuation_allowed":     nil,
-        "banner-style.banner-style-one":                            "Banner style 1",
-        "banner-style.banner-style-two":                            "Banner style 2",
-        "banner-style.banner-style-three":                          "Banner style 3",
-        "banner-img.banner-img-one":                                "Banner image 1",
-        "banner-img.banner-img-two":                                "Banner image 2",
-        "banner-img.banner-img-three":                              "Banner image 3",
-        "proposal_notification_minimum_interval_in_days":           3,
-        "direct_message_max_per_day":                               3, # For proposal notifications
-        "mailer_from_name":                                         'CONSUL',
-        "mailer_from_address":                                      'noreply@consul.dev',
         "verification_offices_url":                                 'http://www.madrid.es/portales/munimadrid/es/Inicio/El-Ayuntamiento/Atencion-al-ciudadano/Oficinas-de-Atencion-al-Ciudadano?vgnextfmt=default&vgnextchannel=5b99cde2e09a4310VgnVCM1000000b205a0aRCRD',
-        "min_age_to_participate":                                   16,
-        "min_age_to_verify":                                        16,
-        "featured_proposals_number":                                3,
-        "proposal_improvement_path":                                nil,
-        "map_latitude":                                             40.4332002,
-        "map_longitude":                                            -3.7009591,
-        "map_zoom":                                                 10,
-        "related_content_score_threshold":                          -0.3,
-        "feature.user.skip_verification":                           'true',
-        "feature.homepage.widgets.feeds.proposals":                 true,
-        "feature.homepage.widgets.feeds.debates":                   true,
-        "feature.homepage.widgets.feeds.processes":                 true
+        "votes_for_proposal_success":                               53726,
+        "youtube_handle":                                           "channel/UCFmaChI9quIY7lwHplnacfg"
       }
     end
   end

consuldemocracy/consuldemocracy

app/models/setting.rb
@@ -43,6 +43,51 @@ class Setting < ApplicationRecord
 
     def defaults
       {
+        "comments_body_max_length": 1000,
+        "dashboard.emails": nil,
+        "direct_message_max_per_day": 3,
+        # Users with this email domain will automatically be marked as level 1 officials
+        # Emails under the domain's subdomains will also be included
+        "email_domain_for_officials": "",
+        "facebook_handle": nil,
+        "feature.allow_attached_documents": true,
+        "feature.allow_images": true,
+        "feature.community": true,
+        "feature.facebook_login": true,
+        "feature.featured_proposals": nil,
+        "feature.google_login": true,
+        "feature.help_page": true,
+        "feature.map": nil,
+        "feature.public_stats": true,
+        "feature.signature_sheets": true,
+        "feature.twitter_login": true,
+        "feature.user.recommendations": true,
+        "feature.user.recommendations_on_debates": true,
+        "feature.user.recommendations_on_proposals": true,
+        "feature.user.skip_verification": "true",
+        "featured_proposals_number": 3,
+        "homepage.widgets.feeds.debates": true,
+        "homepage.widgets.feeds.processes": true,
+        "homepage.widgets.feeds.proposals": true,
+        "hot_score_period_in_days": 31,
+        # Code to be included at the top (inside <body>) of every page
+        "html.per_page_code_body": "",
+        # Code to be included at the top (inside <head>) of every page (useful for tracking)
+        "html.per_page_code_head": "",
+        "instagram_handle": nil,
+        "mailer_from_address": "noreply@consul.dev",
+        "mailer_from_name": "CONSUL",
+        "map.latitude": 51.48,
+        "map.longitude": 0.0,
+        "map.zoom": 10,
+        "max_ratio_anon_votes_on_debates": 50,
+        "max_votes_for_debate_edit": 1000,
+        "max_votes_for_proposal_edit": 1000,
+        "meta_description": nil,
+        "meta_keywords": nil,
+        "meta_title": nil,
+        "min_age_to_participate": 16,
+        "months_to_archive_proposals": 12,
         # Names for the moderation console, as a hint for moderators
         # to know better how to assign users with official positions
         "official_level_1_name": "Cargo oficial 1",
@@ -50,75 +95,30 @@ class Setting < ApplicationRecord
         "official_level_3_name": "Cargo oficial 3",
         "official_level_4_name": "Cargo oficial 4",
         "official_level_5_name": "Cargo oficial 5",
-        "max_ratio_anon_votes_on_debates": 50,
-        "max_votes_for_debate_edit": 1000,
-        "max_votes_for_proposal_edit": 1000,
-        "comments_body_max_length": 1000,
-        "proposal_code_prefix": "CONSUL",
-        "votes_for_proposal_success": 53726,
-        "months_to_archive_proposals": 12,
-        # Users with this email domain will automatically be marked as level 1 officials
-        # Emails under the domain's subdomains will also be included
-        "email_domain_for_officials": "",
-        # Code to be included at the top (inside <head>) of every page (useful for tracking)
-        "html.per_page_code_head": "",
-        # Code to be included at the top (inside <body>) of every page
-        "html.per_page_code_body": "",
-        "twitter_handle": nil,
-        "twitter_hashtag": nil,
-        "facebook_handle": nil,
-        "youtube_handle": nil,
-        "telegram_handle": nil,
-        "instagram_handle": nil,
-        "url": "http://example.com", # Public-facing URL of the app.
         # CONSUL installation's organization name
         "org_name": "CONSUL",
-        "meta_title": nil,
-        "meta_description": nil,
-        "meta_keywords": nil,
-        "process.debates": true,
-        "process.proposals": true,
-        "process.polls": true,
         "process.budgets": true,
+        "process.debates": true,
         "process.legislation": true,
-        "feature.featured_proposals": nil,
-        "feature.twitter_login": true,
-        "feature.facebook_login": true,
-        "feature.google_login": true,
-        "feature.public_stats": true,
-        "feature.signature_sheets": true,
-        "feature.user.recommendations": true,
-        "feature.user.recommendations_on_debates": true,
-        "feature.user.recommendations_on_proposals": true,
-        "feature.user.skip_verification": "true",
-        "feature.community": true,
-        "feature.map": nil,
-        "feature.allow_images": true,
-        "feature.allow_attached_documents": true,
-        "feature.help_page": true,
+        "process.polls": true,
+        "process.proposals": true,
+        "proposal_code_prefix": "CONSUL",
         "proposal_notification_minimum_interval_in_days": 3,
-        "direct_message_max_per_day": 3,
-        "mailer_from_name": "CONSUL",
-        "mailer_from_address": "noreply@consul.dev",
-        "min_age_to_participate": 16,
-        "featured_proposals_number": 3,
-        "map.latitude": 51.48,
-        "map.longitude": 0.0,
-        "map.zoom": 10,
-        "related_content_score_threshold": -0.3,
-        "homepage.widgets.feeds.proposals": true,
-        "homepage.widgets.feeds.debates": true,
-        "homepage.widgets.feeds.processes": true,
-        "hot_score_period_in_days": 31,
-        "proposals.successful_proposal_id": nil,
-        "proposals.poll_short_title": nil,
+        "proposals.email_description": nil,
+        "proposals.email_short_title": nil,
         "proposals.poll_description": nil,
         "proposals.poll_link": nil,
-        "proposals.email_short_title": nil,
-        "proposals.email_description": nil,
-        "proposals.poster_short_title": nil,
+        "proposals.poll_short_title": nil,
         "proposals.poster_description": nil,
-        "dashboard.emails": nil
+        "proposals.poster_short_title": nil,
+        "proposals.successful_proposal_id": nil,
+        "related_content_score_threshold": -0.3,
+        "telegram_handle": nil,
+        "twitter_handle": nil,
+        "twitter_hashtag": nil,
+        "url": "http://example.com", # Public-facing URL of the app.
+        "votes_for_proposal_success": 53726,
+        "youtube_handle": nil
       }
     end
   end