javierm/consul

Simplify code seeding settings

db/seeds.rb
@@ -5,114 +5,90 @@ if Administrator.count == 0 && !Rails.env.test?
   admin.create_administrator
 end
 
-Setting["official_level_1_name"] = "Empleados públicos" # For the moderation console
-Setting["official_level_2_name"] = "Organización Municipal"
-Setting["official_level_3_name"] = "Directores generales"
-Setting["official_level_4_name"] = "Concejales"
-Setting["official_level_5_name"] = "Alcaldesa"
+default_settings = {
+  "official_level_1_name":                                    "Empleados públicos", # For the moderation console
+  "official_level_2_name":                                    "Organización Municipal",
+  "official_level_3_name":                                    "Directores generales",
+  "official_level_4_name":                                    "Concejales",
+  "official_level_5_name":                                    "Alcaldesa",
+  "max_ratio_anon_votes_on_debates":                          50,
+  "max_votes_for_debate_edit":                                1000,
+  "max_votes_for_proposal_edit":                              1000,
+  "comments_body_max_length":                                 1000,
+  "proposal_code_prefix":                                     'MAD',
+  "votes_for_proposal_success":                               53726,
+  "months_to_archive_proposals":                              12,
+  "email_domain_for_officials":                               '', # Users with this email domain (including subdomains) will automatically be marked as level 1 officials
+  "per_page_code_head":                                       '',
+  "per_page_code_body":                                       '',
+  "twitter_handle":                                           "abriendomadrid",
+  "twitter_hashtag":                                          "#decidemadrid",
+  "facebook_handle":                                          "Abriendo-Madrid-1475577616080350",
+  "youtube_handle":                                           "channel/UCFmaChI9quIY7lwHplnacfg",
+  "telegram_handle":                                          nil,
+  "instagram_handle":                                         "decidemadrid",
+  "blog_url":                                                 "https://diario.madrid.es/decidemadrid/",
+  "transparency_url":                                         "http://transparencia.madrid.es/",
+  "opendata_url":                                             "http://datos.madrid.es/",
+  "url":                                                      "https://decide.madrid.es", # Public-facing URL of the app.
+  "org_name":                                                 "Decide Madrid",
+  "place_name":                                               "Madrid",
+  "meta_title":                                               nil,
+  "meta_description":                                         nil,
+  "meta_keywords":                                            nil,
+  "feature.debates":                                          true,
+  "feature.proposals":                                        true,
+  "feature.featured_proposals":                               true,
+  "feature.spending_proposals":                               nil,
+  "feature.polls":                                            true,
+  "feature.twitter_login":                                    true,
+  "feature.facebook_login":                                   true,
+  "feature.google_login":                                     true,
+  "feature.public_stats":                                     true,
+  "feature.budgets":                                          true,
+  "feature.signature_sheets":                                 true,
+  "feature.legislation":                                      true,
+  "feature.user.recommendations":                             true,
+  "feature.user.recommendations_on_debates":                  true,
+  "feature.user.recommendations_on_proposals":                true,
+  "feature.community":                                        true,
+  "feature.map":                                              nil,
+  "feature.allow_images":                                     true,
+  "feature.allow_attached_documents":                         true,
+  "feature.guides":                                           nil,
+  "feature.help_page":                                        true,
+  "feature.spending_proposal_features.phase1":                true,
+  "feature.spending_proposal_features.phase2":                nil,
+  "feature.spending_proposal_features.phase3":                nil,
+  "feature.spending_proposal_features.voting_allowed":        nil,
+  "feature.spending_proposal_features.final_voting_allowed":  true,
+  "feature.spending_proposal_features.open_results_page":     nil,
+  "feature.spending_proposal_features.valuation_allowed":     nil,
+  "banner-style.banner-style-one":                            "Banner style 1",
+  "banner-style.banner-style-two":                            "Banner style 2",
+  "banner-style.banner-style-three":                          "Banner style 3",
+  "banner-img.banner-img-one":                                "Banner image 1",
+  "banner-img.banner-img-two":                                "Banner image 2",
+  "banner-img.banner-img-three":                              "Banner image 3",
+  "proposal_notification_minimum_interval_in_days":           3,
+  "direct_message_max_per_day":                               3, # For proposal notifications
+  "mailer_from_name":                                         'CONSUL',
+  "mailer_from_address":                                      'noreply@consul.dev',
+  "verification_offices_url":                                 'http://www.madrid.es/portales/munimadrid/es/Inicio/El-Ayuntamiento/Atencion-al-ciudadano/Oficinas-de-Atencion-al-Ciudadano?vgnextfmt=default&vgnextchannel=5b99cde2e09a4310VgnVCM1000000b205a0aRCRD',
+  "min_age_to_participate":                                   16,
+  "min_age_to_verify":                                        16,
+  "featured_proposals_number":                                3,
+  "proposal_improvement_path":                                nil,
+  "map_latitude":                                             40.4332002,
+  "map_longitude":                                            -3.7009591,
+  "map_zoom":                                                 10,
+  "related_content_score_threshold":                          -0.3,
+  "feature.user.skip_verification":                           'true',
+  "feature.homepage.widgets.feeds.proposals":                 true,
+  "feature.homepage.widgets.feeds.debates":                   true,
+  "feature.homepage.widgets.feeds.processes":                 true
+}
 
-Setting["max_ratio_anon_votes_on_debates"] = 50
-
-Setting["max_votes_for_debate_edit"] = 1000
-
-Setting["max_votes_for_proposal_edit"] = 1000
-
-Setting['comments_body_max_length'] = 1000
-
-Setting["proposal_code_prefix"] = 'MAD'
-
-Setting["votes_for_proposal_success"] = 53726
-
-Setting["months_to_archive_proposals"] = 12
-
-Setting["email_domain_for_officials"] = '' # Users with this email domain (including subdomains) will automatically be marked as level 1 officials
-
-Setting['per_page_code_head'] = ''
-
-Setting['per_page_code_body'] = ''
-
-Setting["twitter_handle"] = "abriendomadrid"
-Setting["twitter_hashtag"] = "#decidemadrid"
-Setting["facebook_handle"] = "Abriendo-Madrid-1475577616080350"
-Setting["youtube_handle"] = "channel/UCFmaChI9quIY7lwHplnacfg"
-Setting["telegram_handle"] = nil
-Setting["instagram_handle"] = "decidemadrid"
-Setting["blog_url"] = "https://diario.madrid.es/decidemadrid/"
-Setting["transparency_url"] = "http://transparencia.madrid.es/"
-Setting["opendata_url"] = "http://datos.madrid.es/"
-
-Setting["url"] = "https://decide.madrid.es" # Public-facing URL of the app.
-
-Setting["org_name"] = "Decide Madrid"
-
-Setting["place_name"] = "Madrid"
-
-Setting["meta_title"] = nil
-Setting["meta_description"] = nil
-Setting["meta_keywords"] = nil
-
-Setting['feature.debates'] = true
-Setting['feature.proposals'] = true
-Setting['feature.featured_proposals'] = true
-Setting['feature.spending_proposals'] = nil
-Setting['feature.polls'] = true
-Setting['feature.twitter_login'] = true
-Setting['feature.facebook_login'] = true
-Setting['feature.google_login'] = true
-Setting['feature.public_stats'] = true
-Setting['feature.budgets'] = true
-Setting['feature.signature_sheets'] = true
-Setting['feature.legislation'] = true
-Setting['feature.user.recommendations'] = true
-Setting['feature.user.recommendations_on_debates'] = true
-Setting['feature.user.recommendations_on_proposals'] = true
-Setting['feature.community'] = true
-Setting['feature.map'] = nil
-Setting['feature.allow_images'] = true
-Setting['feature.allow_attached_documents'] = true
-Setting['feature.guides'] = nil
-Setting['feature.help_page'] = true
-
-Setting['feature.spending_proposal_features.phase1'] = true
-Setting['feature.spending_proposal_features.phase2'] = nil
-Setting['feature.spending_proposal_features.phase3'] = nil
-Setting['feature.spending_proposal_features.voting_allowed'] = nil
-Setting['feature.spending_proposal_features.final_voting_allowed'] = true
-Setting['feature.spending_proposal_features.open_results_page'] = nil
-Setting['feature.spending_proposal_features.valuation_allowed'] = nil
-
-Setting['banner-style.banner-style-one']   = "Banner style 1"
-Setting['banner-style.banner-style-two']   = "Banner style 2"
-Setting['banner-style.banner-style-three'] = "Banner style 3"
-
-Setting['banner-img.banner-img-one']   = "Banner image 1"
-Setting['banner-img.banner-img-two']   = "Banner image 2"
-Setting['banner-img.banner-img-three'] = "Banner image 3"
-
-
-Setting['proposal_notification_minimum_interval_in_days'] = 3
-Setting['direct_message_max_per_day'] = 3 # For proposal notifications
-
-Setting['mailer_from_name'] = 'CONSUL'
-Setting['mailer_from_address'] = 'noreply@consul.dev'
-
-Setting['verification_offices_url'] = 'http://www.madrid.es/portales/munimadrid/es/Inicio/El-Ayuntamiento/Atencion-al-ciudadano/Oficinas-de-Atencion-al-Ciudadano?vgnextfmt=default&vgnextchannel=5b99cde2e09a4310VgnVCM1000000b205a0aRCRD'
-Setting['min_age_to_participate'] = 16
-Setting['min_age_to_verify'] = 16
-
-Setting['featured_proposals_number'] = 3
-
-Setting['proposal_improvement_path'] = nil
-
-Setting['map_latitude'] = 40.4332002
-Setting['map_longitude'] = -3.7009591
-Setting['map_zoom'] = 10
-
-Setting['related_content_score_threshold'] = -0.3
-
-Setting["feature.user.skip_verification"] = 'true'
-
-Setting['feature.homepage.widgets.feeds.proposals'] = true
-Setting['feature.homepage.widgets.feeds.debates'] = true
-Setting['feature.homepage.widgets.feeds.processes'] = true
-
+default_settings.each do |name, value|
+  Setting[name] = value
+end

consuldemocracy/consuldemocracy

db/seeds.rb
@@ -7,114 +7,94 @@ if Administrator.count == 0 && !Rails.env.test?
   admin.create_administrator
 end
 
-# Names for the moderation console, as a hint for moderators
-# to know better how to assign users with official positions
-Setting["official_level_1_name"] = "Cargo oficial 1"
-Setting["official_level_2_name"] = "Cargo oficial 2"
-Setting["official_level_3_name"] = "Cargo oficial 3"
-Setting["official_level_4_name"] = "Cargo oficial 4"
-Setting["official_level_5_name"] = "Cargo oficial 5"
+default_settings = {
+  # Names for the moderation console, as a hint for moderators
+  # to know better how to assign users with official positions
+  "official_level_1_name": "Cargo oficial 1",
+  "official_level_2_name": "Cargo oficial 2",
+  "official_level_3_name": "Cargo oficial 3",
+  "official_level_4_name": "Cargo oficial 4",
+  "official_level_5_name": "Cargo oficial 5",
+  "max_ratio_anon_votes_on_debates": 50,
+  "max_votes_for_debate_edit": 1000,
+  "max_votes_for_proposal_edit": 1000,
+  "comments_body_max_length": 1000,
+  "proposal_code_prefix": "CONSUL",
+  "votes_for_proposal_success": 53726,
+  "months_to_archive_proposals": 12,
+  # Users with this email domain will automatically be marked as level 1 officials
+  # Emails under the domain's subdomains will also be included
+  "email_domain_for_officials": "",
+  # Code to be included at the top (inside <head>) of every page (useful for tracking)
+  "html.per_page_code_head": "",
+  # Code to be included at the top (inside <body>) of every page
+  "html.per_page_code_body": "",
+  "twitter_handle": nil,
+  "twitter_hashtag": nil,
+  "facebook_handle": nil,
+  "youtube_handle": nil,
+  "telegram_handle": nil,
+  "instagram_handle": nil,
+  "url": "http://example.com", # Public-facing URL of the app.
+  # CONSUL installation's organization name
+  "org_name": "CONSUL",
+  "meta_title": nil,
+  "meta_description": nil,
+  "meta_keywords": nil,
+  "process.debates": true,
+  "process.proposals": true,
+  "process.polls": true,
+  "process.budgets": true,
+  "process.legislation": true,
+  "feature.featured_proposals": nil,
+  "feature.twitter_login": true,
+  "feature.facebook_login": true,
+  "feature.google_login": true,
+  "feature.public_stats": true,
+  "feature.signature_sheets": true,
+  "feature.user.recommendations": true,
+  "feature.user.recommendations_on_debates": true,
+  "feature.user.recommendations_on_proposals": true,
+  "feature.user.skip_verification": "true",
+  "feature.community": true,
+  "feature.map": nil,
+  "feature.allow_images": true,
+  "feature.allow_attached_documents": true,
+  "feature.help_page": true,
+  "proposal_notification_minimum_interval_in_days": 3,
+  "direct_message_max_per_day": 3,
+  "mailer_from_name": "CONSUL",
+  "mailer_from_address": "noreply@consul.dev",
+  "min_age_to_participate": 16,
+  "featured_proposals_number": 3,
+  "map.latitude": 51.48,
+  "map.longitude": 0.0,
+  "map.zoom": 10,
+  "related_content_score_threshold": -0.3,
+  "homepage.widgets.feeds.proposals": true,
+  "homepage.widgets.feeds.debates": true,
+  "homepage.widgets.feeds.processes": true,
+  "hot_score_period_in_days": 31,
+  "proposals.successful_proposal_id": nil,
+  "proposals.poll_short_title": nil,
+  "proposals.poll_description": nil,
+  "proposals.poll_link": nil,
+  "proposals.email_short_title": nil,
+  "proposals.email_description": nil,
+  "proposals.poster_short_title": nil,
+  "proposals.poster_description": nil,
+  "dashboard.emails": nil
+}
 
-Setting["max_ratio_anon_votes_on_debates"] = 50
-
-Setting["max_votes_for_debate_edit"] = 1000
-
-Setting["max_votes_for_proposal_edit"] = 1000
-
-Setting["comments_body_max_length"] = 1000
-
-Setting["proposal_code_prefix"] = "CONSUL"
-
-Setting["votes_for_proposal_success"] = 53726
-
-Setting["months_to_archive_proposals"] = 12
-
-# Users with this email domain will automatically be marked as level 1 officials
-# Emails under the domain's subdomains will also be included
-Setting["email_domain_for_officials"] = ""
-
-# Code to be included at the top (inside <head>) of every page (useful for tracking)
-Setting["html.per_page_code_head"] = ""
-
-# Code to be included at the top (inside <body>) of every page
-Setting["html.per_page_code_body"] = ""
-
-Setting["twitter_handle"] = nil
-Setting["twitter_hashtag"] = nil
-Setting["facebook_handle"] = nil
-Setting["youtube_handle"] = nil
-Setting["telegram_handle"] = nil
-Setting["instagram_handle"] = nil
-
-Setting["url"] = "http://example.com" # Public-facing URL of the app.
-
-# CONSUL installation's organization name
-Setting["org_name"] = "CONSUL"
-
-Setting["meta_title"] = nil
-Setting["meta_description"] = nil
-Setting["meta_keywords"] = nil
-
-Setting["process.debates"] = true
-Setting["process.proposals"] = true
-Setting["process.polls"] = true
-Setting["process.budgets"] = true
-Setting["process.legislation"] = true
-
-Setting["feature.featured_proposals"] = nil
-Setting["feature.twitter_login"] = true
-Setting["feature.facebook_login"] = true
-Setting["feature.google_login"] = true
-Setting["feature.public_stats"] = true
-Setting["feature.signature_sheets"] = true
-Setting["feature.user.recommendations"] = true
-Setting["feature.user.recommendations_on_debates"] = true
-Setting["feature.user.recommendations_on_proposals"] = true
-Setting["feature.user.skip_verification"] = "true"
-Setting["feature.community"] = true
-Setting["feature.map"] = nil
-Setting["feature.allow_images"] = true
-Setting["feature.allow_attached_documents"] = true
-Setting["feature.help_page"] = true
-
-Setting["proposal_notification_minimum_interval_in_days"] = 3
-Setting["direct_message_max_per_day"] = 3
-
-Setting["mailer_from_name"] = "CONSUL"
-Setting["mailer_from_address"] = "noreply@consul.dev"
-
-Setting["min_age_to_participate"] = 16
-
-Setting["featured_proposals_number"] = 3
-
-Setting["map.latitude"] = 51.48
-Setting["map.longitude"] = 0.0
-Setting["map.zoom"] = 10
-
-Setting["related_content_score_threshold"] = -0.3
-
-Setting["homepage.widgets.feeds.proposals"] = true
-Setting["homepage.widgets.feeds.debates"] = true
-Setting["homepage.widgets.feeds.processes"] = true
-
-Setting["hot_score_period_in_days"] = 31
+default_settings.each do |name, value|
+  Setting[name] = value
+end
 
 WebSection.create(name: "homepage")
 WebSection.create(name: "debates")
 WebSection.create(name: "proposals")
 WebSection.create(name: "budgets")
 WebSection.create(name: "help_page")
-
-Setting["proposals.successful_proposal_id"] = nil
-Setting["proposals.poll_short_title"] = nil
-Setting["proposals.poll_description"] = nil
-Setting["proposals.poll_link"] = nil
-Setting["proposals.email_short_title"] = nil
-Setting["proposals.email_description"] = nil
-Setting["proposals.poster_short_title"] = nil
-Setting["proposals.poster_description"] = nil
-
-Setting["dashboard.emails"] = nil
 
 # Default custom pages
 load Rails.root.join("db", "pages.rb")