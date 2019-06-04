d7a1e07b4d85b84c04f1e43a2a74d8bbb9e48437
Alberto Miedes <albertomg994@gmail.com>
2019-06-04 15:08:02 +0200

    Load census entities info on boot

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,3 +1,30 @@
+
+census_entities = []
+
+CSV.foreach(Rails.root.join("config", "entidades_censo.csv"), headers: true, col_sep: "\;") do |row|
+  census_entities.append({
+    ine_code: row[0],
+    nif: row[1],
+    city_council: row[2],
+    postal_code: row[3],
+    entity: row[4],
+    entity_code: row[5],
+    city_council_name: row[6]
+  })
+end
+
+CENSUS_ENTITIES = census_entities.freeze
+postal_codes = CENSUS_ENTITIES.map { |entity| entity[:postal_code] }.uniq
+
+census_dictionary = {}
+
+postal_codes.each do |code|
+  census_dictionary[code] = CENSUS_ENTITIES.select { |entity| entity[:postal_code] == code }
+                                           .map { |e| e[:entity_code] }.uniq
+end
+
+CENSUS_DICTIONARY = census_dictionary.freeze
+
 module Consul
   class Application < Rails::Application
     config.i18n.default_locale = :es
