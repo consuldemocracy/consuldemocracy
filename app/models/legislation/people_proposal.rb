744824e26fdec15911e97b19d11268ffb3dd7f09
voodoorai2000 <voodoorai2000@gmail.com>
2019-06-06 11:04:46 +0200

    Remove deprecated reference to documentable method
    We have done a little refactoring of documents. Now document settings are part of the admin interface[1].
    
    [1] https://github.com/consul/consul/pull/3585/

--- app/models/legislation/people_proposal.rb
+++ app/models/legislation/people_proposal.rb
@@ -14,9 +14,6 @@ class Legislation::PeopleProposal < ActiveRecord::Base
   include Imageable
   include Randomizable
 
-  documentable max_documents_allowed: 3,
-               max_file_size: 3.megabytes,
-               accepted_content_types: [ "application/pdf" ]
   accepts_nested_attributes_for :documents, allow_destroy: true
 
   acts_as_votable
