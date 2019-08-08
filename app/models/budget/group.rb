59478ef46188c9929d36bbd6bc558744dca304b1
Javi Martín <javim@elretirao.net>
2019-08-08 18:27:41 +0200

    Remove duplicate Translation classes
    We accidentally added them twice when rebasing the translations branch.

--- app/models/budget/group.rb
+++ app/models/budget/group.rb
@@ -39,19 +39,5 @@ class Budget
     def generate_slug?
       slug.nil? || budget.drafting?
     end
-
-    class Translation < Globalize::ActiveRecord::Translation
-      delegate :budget, to: :globalized_model
-
-      validate :name_uniqueness_by_budget
-
-      def name_uniqueness_by_budget
-        if budget.groups.joins(:translations)
-                        .where(name: name)
-                        .where.not("budget_group_translations.budget_group_id": budget_group_id).any?
-          errors.add(:name, I18n.t("errors.messages.taken"))
-        end
-      end
-    end
   end
 end
