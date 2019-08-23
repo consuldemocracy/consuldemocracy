dad4badbc30ef9046e53a8924a6b05a4a7bcd23c
Julian Herrero <microweb10@gmail.com>
2019-08-23 17:48:21 +0700

    Replace Rollbar with Airbrake + Errbit

---
+++ config/initializers/errbit.rb
@@ -0,0 +1,13 @@
+Airbrake.configure do |config|
+  config.host = Rails.application.secrets.errbit_host
+  config.project_id = Rails.application.secrets.errbit_project_id
+  config.project_key = Rails.application.secrets.errbit_project_key
+
+  config.environment = Rails.env
+  config.ignore_environments = %w[development test]
+end
+
+Airbrake.add_filter do |notice|
+  ignorables = %w[ActiveRecord::RecordNotFound]
+  notice.ignore! if ignorables.include? notice[:errors].first[:type]
+end
