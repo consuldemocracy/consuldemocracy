21fcdba8c874d8270b8414655a556ce927da1307
Javi Martín <javim@elretirao.net>
2019-10-01 16:25:02 +0200

    Remove obsolete method
    This method wasn't used since commit 24dde9c3.

--- app/helpers/valuation_helper.rb
+++ app/helpers/valuation_helper.rb
@@ -13,21 +13,6 @@ module ValuationHelper
     ValuatorGroup.order("name ASC").collect { |g| [g.name, "group_#{g.id}"] }
   end
 
-  def assigned_valuators_info(valuators)
-    case valuators.size
-    when 0
-      t("valuation.budget_investments.index.no_valuators_assigned")
-    when 1
-      "<span title=\"#{t("valuation.budget_investments.index.valuators_assigned", count: 1)}\">".html_safe +
-        valuators.first.name +
-      "</span>".html_safe
-    else
-      "<span title=\"".html_safe + valuators.map(&:name).join(", ") + "\">".html_safe +
-        t("valuation.budget_investments.index.valuators_assigned", count: valuators.size) +
-      "</span>".html_safe
-    end
-  end
-
   def explanation_field(field)
     simple_format_no_tags_no_sanitize(safe_html_with_links(field.html_safe)) if field.present?
   end
