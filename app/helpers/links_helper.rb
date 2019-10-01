86a205c8fcf3cdbc9f5149ba3bd1edd84c09b95d
Javi Martín <javim@elretirao.net>
2019-10-01 22:19:20 +0200

    Extract link to verify account to a mehtod
    We were using the same text and the same route in many places.

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -12,4 +12,8 @@ module LinksHelper
   def link_to_signup(options = {})
     link_to t("users.signup"), new_user_registration_path, options
   end
+
+  def link_to_verify_account
+    link_to t("users.verify_account"), verification_path
+  end
 end
