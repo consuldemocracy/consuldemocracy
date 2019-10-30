df4f8810c5e30add875b8f53688679e33431f02f
Julian Herrero <microweb10@gmail.com>
2019-10-30 20:17:39 +0700

    Define `force_ssl` configuration in secrets file
    this is usually configured in the production.rb file (which is under
    version control), the natural place to configure it is the secrets.yml
    file.
    
    Until now we were using the capistrano shared folder, but that's a bit
    inconvenient since changes we've done to the production.rb file (like
    changing eager_load_paths when we upgraded to Rails 5) won't take effect
    after a deployment.

--- config/environments/preproduction.rb
+++ config/environments/preproduction.rb
@@ -47,7 +47,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # Configure force_ssl in secrets.yml
+  config.force_ssl = Rails.application.secrets.force_ssl
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,8 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # Configure force_ssl in secrets.yml
+  config.force_ssl = Rails.application.secrets.force_ssl
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
