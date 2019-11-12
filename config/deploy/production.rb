1f4f70b4a9d06c1c4603fc5e200506d422dc2f2d
Javi Martín <javim@elretirao.net>
2019-11-12 18:09:16 +0100

    Remove redundant stage settings
    These files are only loaded when the stage already has the value set
    inside them, so we don't need to set it again.
    
    On the other hand, the `rails_env` setting is configured in the
    `config/deploy.rb` file.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,8 +3,6 @@ set :server_name, deploysecret(:server_name)
 set :db_server, deploysecret(:db_server)
 set :branch, :master
 set :ssh_options, port: deploysecret(:ssh_port)
-set :stage, :production
-set :rails_env, :production
 
 server deploysecret(:server1), user: deploysecret(:user), roles: %w[web app db importer cron background]
 #server deploysecret(:server2), user: deploysecret(:user), roles: %w(web app db importer cron background)
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,5 @@ set :server_name, deploysecret(:server_name)
 set :db_server, deploysecret(:db_server)
 set :branch, ENV["branch"] || :master
 set :ssh_options, port: deploysecret(:ssh_port)
-set :stage, :staging
-set :rails_env, :staging
 
 server deploysecret(:server), user: deploysecret(:user), roles: %w[web app db importer cron]
