6e00842b0ed350b29579f5ffe40b935edd87bdbb
Julian Herrero <microweb10@gmail.com>
2019-12-18 16:06:04 +0700

    Don't include disabled processes in sitemap

--- spec/lib/tasks/sitemap_spec.rb
+++ spec/lib/tasks/sitemap_spec.rb
@@ -5,36 +5,78 @@ describe "rake sitemap:create", type: :feature do
 
   before do
     File.delete(file) if File.exist?(file)
-
     Rake::Task["sitemap:create"].reenable
-    Rake.application.invoke_task("sitemap:create")
   end
 
-  it "generates a sitemap" do
-    expect(file).to exist
+  describe "when processes are enabled" do
+    before { Rake.application.invoke_task("sitemap:create") }
+
+    it "generates a sitemap" do
+      expect(file).to exist
+    end
+
+    it "generates a valid sitemap" do
+      sitemap = Nokogiri::XML(File.open(file))
+      expect(sitemap.errors).to be_empty
+    end
+
+    it "generates a sitemap with expected and valid URLs" do
+      sitemap = File.read(file)
+
+      # Static pages
+      expect(sitemap).to include(faq_path)
+      expect(sitemap).to include(help_path)
+      expect(sitemap).to include(how_to_use_path)
+
+      # Dynamic URLs
+      expect(sitemap).to include(polls_path)
+      expect(sitemap).to include(budgets_path)
+      expect(sitemap).to include(debates_path)
+      expect(sitemap).to include(proposals_path)
+      expect(sitemap).to include(legislation_processes_path)
+
+      expect(sitemap).to have_content("0.7", count: 5)
+      expect(sitemap).to have_content("daily", count: 5)
+    end
   end
 
-  it "generates a valid sitemap" do
-    sitemap = Nokogiri::XML(File.open(file))
-    expect(sitemap.errors).to be_empty
-  end
+  describe "when processes are not enabled" do
+    before do
+      Setting["process.debates"] = nil
+      Setting["process.proposals"] = nil
+      Setting["process.budgets"] = nil
+      Setting["process.polls"] = nil
+      Setting["process.legislation"] = nil
 
-  it "generates a sitemap with expected and valid URLs" do
-    sitemap = File.read(file)
+      Rake.application.invoke_task("sitemap:create")
+    end
 
-    # Static pages
-    expect(sitemap).to include(faq_path)
-    expect(sitemap).to include(help_path)
-    expect(sitemap).to include(how_to_use_path)
+    it "generates a sitemap" do
+      expect(file).to exist
+    end
 
-    # Dynamic URLs
-    expect(sitemap).to include(polls_path)
-    expect(sitemap).to include(budgets_path)
-    expect(sitemap).to include(debates_path)
-    expect(sitemap).to include(proposals_path)
-    expect(sitemap).to include(legislation_processes_path)
+    it "generates a valid sitemap" do
+      sitemap = Nokogiri::XML(File.open(file))
+      expect(sitemap.errors).to be_empty
+    end
 
-    expect(sitemap).to have_content("0.7", count: 5)
-    expect(sitemap).to have_content("daily", count: 5)
+    it "generates a sitemap with expected and valid URLs" do
+      sitemap = File.read(file)
+
+      # Static pages
+      expect(sitemap).to include(faq_path)
+      expect(sitemap).to include(help_path)
+      expect(sitemap).to include(how_to_use_path)
+
+      # Dynamic URLs
+      expect(sitemap).not_to include(polls_path)
+      expect(sitemap).not_to include(budgets_path)
+      expect(sitemap).not_to include(debates_path)
+      expect(sitemap).not_to include(proposals_path)
+      expect(sitemap).not_to include(legislation_processes_path)
+
+      expect(sitemap).not_to have_content("0.7")
+      expect(sitemap).not_to have_content("daily")
+    end
   end
 end
