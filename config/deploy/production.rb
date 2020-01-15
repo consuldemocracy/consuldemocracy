34310070a04e3c05d4833f61c4368155f9821071
Filip Muki Dobranic <filipdobranic@gmail.com>
2020-01-15 15:08:30 +0100

    oooops

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,10 +1,10 @@
-set :deploy_to, deploysecret(:deploy_to)
-set :server_name, deploysecret(:server_name)
-set :db_server, deploysecret(:db_server)
-set :branch, "consul-sos"
-set :ssh_options, port: deploysecret(:ssh_port)
-set :stage, :production
-set :rails_env, :production
+# set :deploy_to, deploysecret(:deploy_to)
+# set :server_name, deploysecret(:server_name)
+# set :db_server, deploysecret(:db_server)
+# set :branch, "consul-sos"
+# set :ssh_options, port: deploysecret(:ssh_port)
+# set :stage, :production
+# set :rails_env, :production
 
 server deploysecret(:server1), user: deploysecret(:user), roles: %w[web app db importer cron background]
 #server deploysecret(:server2), user: deploysecret(:user), roles: %w(web app db importer cron background)
