fa76eda1b9c9311917f6e02946ea4ced9c028ba8
Filip Muki Dobranic <filipdobranic@gmail.com>
2020-01-16 13:26:45 +0100

    set default locale to bs-BA

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,16 @@
 module Consul
   class Application < Rails::Application
+    config.i18n.default_locale = 'bs-BA'
+    # available_locales = %w(ar de en es fa fr gl he it nl pl pt-BR sq sv val zh-CN zh-TW sl sl-SI)
+    available_locales = %w(bs-BA en)
+    config.i18n.available_locales = available_locales
+    config.i18n.fallbacks = {
+      'bs-BA' => 'en'
+      # 'fr' => 'es',
+      # 'gl' => 'es',
+      # 'it' => 'es',
+      # 'pt-BR' => 'es',
+      # 'sl-SI' => 'sl',
+    }
   end
 end
