8aaadbaf5d4081438653bfb23d4733ef37ebf9ad
Filip Muki Dobranic <filipdobranic@gmail.com>
2020-02-13 20:22:21 +0100

    add serbian locale

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,11 +1,11 @@
 module Consul
   class Application < Rails::Application
-    config.i18n.default_locale = 'bs'
+    config.i18n.default_locale = 'sr'
     # available_locales = %w(ar de en es fa fr gl he it nl pl pt-BR sq sv val zh-CN zh-TW sl sl-SI)
-    available_locales = %w(bs en)
+    available_locales = %w(sr en)
     config.i18n.available_locales = available_locales
     config.i18n.fallbacks = {
-      'bs-BA' => 'en'
+      'sr' => 'en'
       # 'fr' => 'es',
       # 'gl' => 'es',
       # 'it' => 'es',
