04bd6bbec55ec9663548faa2a6db3897202510a0
Xisco Bravo <xisco@an2.es>
2020-03-13 10:54:51 +0100

    Actualizada configuración de los idiomas
    El castellano es el único idioma disponible, por tanto también el idioma por defecto

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,9 @@
 module Consul
   class Application < Rails::Application
+    # Idioma por defecto
+    config.i18n.default_locale = :es
+
+    # Idiomas disponibles
+    available_locales = [:es]
   end
 end
