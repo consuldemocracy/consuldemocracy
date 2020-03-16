f1bd5fb1f00ddd23816529a2d2831f0a6261cb5f
Julian Herrero <microweb10@gmail.com>
2020-03-16 12:54:00 +0100

    Include duration in budgets table

--- app/components/admin/budget_phases/phases_component.rb
+++ app/components/admin/budget_phases/phases_component.rb
@@ -11,15 +11,7 @@ class Admin::BudgetPhases::PhasesComponent < ApplicationComponent
       budget.phases.order(:id)
     end
 
-    def start_date(phase)
-      formatted_date(phase.starts_at) if phase.starts_at.present?
-    end
-
-    def end_date(phase)
-      formatted_date(phase.ends_at - 1.second) if phase.ends_at.present?
-    end
-
-    def formatted_date(time)
-      time_tag(time, format: :datetime)
+    def dates(phase)
+      Admin::Budgets::DurationComponent.new(phase).dates
     end
 end
