f4aefcf4fd4e272c09e80f3a74653aadd4a09f2c
Gabriel Iturra <gabrieliturrab@ug.uchile.cl>
2020-03-16 23:09:49 -0300

    cambiar idioma a español2

--- config/application.rb
+++ config/application.rb
@@ -20,33 +20,9 @@ module Consul
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :es
     available_locales = [
-      "ar",
-      "bs",
-      "cs",
-      "da",
-      "de",
-      "el",
       "en",
-      "es",
-      "fa",
-      "fr",
-      "gl",
-      "he",
-      "hr",
-      "id",
-      "it",
-      "nl",
-      "pl",
-      "pt-BR",
-      "ru",
-      "sl",
-      "sq",
-      "so",
-      "sv",
-      "tr",
-      "val",
-      "zh-CN",
-      "zh-TW"]
+      "es"
+    ]
     config.i18n.available_locales = available_locales
     config.i18n.fallbacks = {
       "fr"    => "es",
