3de7d05f143f70c619dad0385566c5405780bbff
Gabriel Iturra <gabrieliturrab@ug.uchile.cl>
2020-03-16 23:07:15 -0300

    cambiar idioma a español

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Consul
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :es
     available_locales = [
       "ar",
       "bs",
