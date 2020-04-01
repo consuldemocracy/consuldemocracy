28b9beaf71c9ff4ae5db036afc72e069c307d7ae
Xisco Bravo <xisco@an2.es>
2020-04-01 17:56:33 +0200

    Corregido error al actualizar la columna 'email' en usuarios

--- db/migrate/20170620114954_change_users_email_null.rb
+++ db/migrate/20170620114954_change_users_email_null.rb
@@ -1,5 +1,5 @@
 class ChangeUsersEmailNull < ActiveRecord::Migration[4.2]
   def change
-    change_column :users, :email, :string, null: nil
+    change_column :users, :email, :string, null: true
   end
 end
