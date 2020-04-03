2332d698ab37460f0849353013943123da135830
Xisco Bravo <xisco@an2.es>
2020-04-03 12:12:55 +0200

    Modificada clase CensusCaller para incluir la llamada al censo con CensusvaApi

--- lib/census_caller.rb
+++ lib/census_caller.rb
@@ -3,7 +3,8 @@ class CensusCaller
     if Setting["feature.remote_census"].present?
       response = RemoteCensusApi.new.call(document_type, document_number, date_of_birth, postal_code)
     else
-      response = CensusApi.new.call(document_type, document_number)
+      # response = CensusApi.new.call(document_type, document_number)
+      response = CensusvaApi.new(document_type, document_number)
     end
     response = LocalCensus.new.call(document_type, document_number) unless response.valid?
 
