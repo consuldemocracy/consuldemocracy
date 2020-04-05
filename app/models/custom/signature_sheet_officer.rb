b5e756331bd886441a10fa46da6b9c1affeb7d5f
Xisco Bravo <xisco@an2.es>
2020-04-05 15:42:58 +0200

    Añadido modelo SignatureSheetOfficer

---
+++ app/models/custom/signature_sheet_officer.rb
@@ -0,0 +1,5 @@
+class SignatureSheetOfficer < ActiveRecord::Base
+  belongs_to :user
+  delegate :name, :email, :name_and_email, to: :user
+  validates :user_id, presence: true, uniqueness: true
+end
