073466ce2c2ef62044b0d269f8241a9cacfb3094
decabeza <alberto@decabeza.es>
2020-04-08 12:58:14 +0200

    Show heading name and amount on single heading new form

--- spec/features/budgets/investments_spec.rb
+++ spec/features/budgets/investments_spec.rb
@@ -894,6 +894,8 @@ describe "Budget Investments" do
 
       visit new_budget_investment_path(budget)
 
+      expect(page).to have_content("#{heading.name} (#{budget.formatted_heading_price(heading)})")
+
       expect(page).to have_selector("input[name=\"budget_investment[heading_id]\"][value=\"#{heading.id}\"]",
                                      visible: false)
 
@@ -925,6 +927,8 @@ describe "Budget Investments" do
       login_as(author)
 
       visit new_budget_investment_path(budget)
+
+      expect(page).not_to have_content("#{heading.name} (#{budget.formatted_heading_price(heading)})")
 
       within("#budget_investment_heading_id") do
         expect(page).to have_selector("option[value='#{heading.id}']")
