4dd4666a3b9bcdb23b6617f36f7b6b621450a5b1
Xisco Bravo <xisco@an2.es>
2020-04-08 16:24:37 +0200

    Actualizadas ramas a utilizar del repositorio en el entorno de producción y preproducción
    En la configuración de capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :branch, :master
+set :branch, :"master-valladolid"
 
 server deploysecret(:server1), user: deploysecret(:user), roles: %w[web app db importer cron background]
 #server deploysecret(:server2), user: deploysecret(:user), roles: %w(web app db importer cron background)
