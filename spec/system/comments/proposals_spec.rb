f60d307a59085534a0069920536f9c51408136da
taitus <sebastia.roig@gmail.com>
2020-04-22 17:48:08 +0200

    Mitigate recurrent flaky specs for voting comments

--- spec/system/comments/proposals_spec.rb
+++ spec/system/comments/proposals_spec.rb
@@ -505,6 +505,11 @@ describe "Commenting proposals" do
 
       within("#comment_#{comment.id}_votes") do
         find(".in_favor a").click
+
+        within(".in_favor") do
+          expect(page).to have_content "1"
+        end
+
         find(".in_favor a").click
 
         within(".in_favor") do
