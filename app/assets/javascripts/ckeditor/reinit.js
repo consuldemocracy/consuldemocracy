wema0/consul

Upgrade Turbolinks to version 5.0.1
We didn't upgrade Turbolinks when we upgraded to Rails 5 so we didn't
upgrade too many things at the same time, and postponed it... until now
:).

app/assets/javascripts/ckeditor/reinit.js
@@ -1,4 +1,4 @@
-$(document).bind("page:change", function() {
+$(document).bind("turbolinks:render", function() {
   if (typeof(CKEDITOR) != "undefined"){
     for(name in CKEDITOR.instances){
       try{CKEDITOR.replace(name);}catch(err){};

consuldemocracy/consuldemocracy

app/assets/javascripts/ckeditor/reinit.js
@@ -1,4 +1,4 @@
-$(document).on("page:change", function() {
+$(document).on("turbolinks:render", function() {
   if (typeof(CKEDITOR) != "undefined"){
     for(name in CKEDITOR.instances){
       try{CKEDITOR.replace(name);}catch(err){};