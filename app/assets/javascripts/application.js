13f0adcba3cb3391529feac4645ca6afe65ce9bc
Xisco Bravo <xisco@an2.es>
2020-05-01 18:41:57 +0200

    Añadida configuración para obtener la ruta por defecto en los archivos de tipo javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require ava-config
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui/widgets/datepicker
