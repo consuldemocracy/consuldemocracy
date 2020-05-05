418b42af0b3ee3bfb56cfe5ecf951d5e37a9f060
Xisco Bravo <xisco@an2.es>
2020-05-05 19:55:13 +0200

    Modificada configuración de capistrano para utilizar password al conectarse con ssh
    En todos los entornos disponibles

--- config/deploy/preproduction.rb
+++ config/deploy/preproduction.rb
@@ -1,4 +1,6 @@
 set :branch, :"development-valladolid"
 
-server deploysecret(:server1), user: deploysecret(:user), roles: %w[web app db importer cron background]
+ask(:password, nil, echo: false)
+
+server deploysecret(:server1), user: deploysecret(:user), password: fetch(:password), roles: %w[web app db importer cron background]
 # server deploysecret(:server2), user: deploysecret(:user), roles: %w[web app db importer]
