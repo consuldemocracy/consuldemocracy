f641169917bbf8dde0192d18854ea679ffd683fc
David Rodriguez <drodriguez@divisait.com>
2020-06-12 16:23:06 +0200

    Prevenimos la entrada por login y clave de usuarios de participacion

--- app/models/custom/user.rb
+++ app/models/custom/user.rb
@@ -22,4 +22,14 @@ class User < ApplicationRecord
   def signature_sheet_officer?
     signature_sheet_officer.present?
   end
+
+  # Queremos evitar que podamos autenticarnos como usuarios de participacion
+  # externos... que la clave es una de mentira...
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    puts conditions
+    login = conditions.delete(:login)
+    where(conditions.to_hash).find_by(["lower(email) = ? AND origin_participacion is NULL",login.downcase]) ||
+    where(conditions.to_hash).find_by(["username = ? AND origin_participacion is NULL", login])
+  end
 end
