c6c511fdaf3b81ecf43c3e6d33d17e363c845541
Senén Rodero Rodríguez <senenrodero@gmail.com>
2020-06-17 17:07:20 +0200

    Use hidden input to store serialized form
    Data attributes dynamically added to a page are lost when using browser
    history so when user navigates back or forward to a page with a form with
    unsaved changes the form serialization was done again but this time with
    modified and unpersisted values so application thinks form wasn't changed.
    By using a hidden input to store form serialized data there is no data loss
    when using browser history and we can keep warning the user about unsaved
    changes between the content original loaded (persisted) and the restored
    content after using history back.

--- app/assets/javascripts/watch_form_changes.js
+++ app/assets/javascripts/watch_form_changes.js
@@ -9,7 +9,8 @@
     },
     hasChanged: function() {
       return App.WatchFormChanges.forms().is(function() {
-        return $(this).serialize() !== $(this).data("watchChanges");
+        var id = "#" + App.WatchFormChanges.trackInputId(this);
+        return $(this).serialize() !== $(id).val();
       });
     },
     checkChanges: function() {
@@ -19,14 +20,30 @@
         return true;
       }
     },
+    trackInputId: function(form) {
+      return form.id + "_watch_form_changes";
+    },
+    serialize: function(form) {
+      var id = App.WatchFormChanges.trackInputId(form);
+      var input = $("<input>").attr({ id: id, type: "hidden", value: $(form).serialize() });
+      $(input).insertBefore(form);
+    },
     initialize: function() {
       if (App.WatchFormChanges.forms().length === 0 || App.WatchFormChanges.msg() === undefined) {
         return;
       }
-      $(document).off("turbolinks:before-visit").on("turbolinks:before-visit", App.WatchFormChanges.checkChanges);
-      App.WatchFormChanges.forms().each(function() {
-        $(this).data("watchChanges", $(this).serialize());
+
+      App.WatchFormChanges.forms().each(function(_, form) {
+        // Serialize form directly if there is not ckeditors within form
+        if ($(form).find(".html-area").length === 0) {
+          App.WatchFormChanges.serialize(form);
+          return;
+        }
+
+        App.WatchFormChanges.wait_for_html_editors(form);
       });
     }
   };
+
+  $(document).on("turbolinks:before-visit", App.WatchFormChanges.checkChanges);
 }).call(this);
---
+++ spec/system/watch_form_changes_spec.rb
@@ -0,0 +1,77 @@
+require "rails_helper"
+
+describe "Watch form changes", :js do
+  let(:prompt) { "You have unsaved changes. Do you confirm to leave the page?" }
+
+  before do
+    admin = create(:administrator)
+    login_as(admin.user)
+  end
+
+  describe "when form does not include html editor" do
+    let(:legislation_process) { create(:legislation_process, title: "An example legislation process") }
+    let(:legislation_draft_version) do
+      create(:legislation_draft_version, title: "Version 1", process: legislation_process)
+    end
+    let(:path) do
+      edit_admin_legislation_process_draft_version_path(legislation_process, legislation_draft_version)
+    end
+
+    context "should ask for confirmation before leaving the current page" do
+      scenario "when any form input was modified" do
+        visit path
+
+        fill_in "Changes", with: "Version 1b"
+        dismiss_confirm(prompt) do
+          click_link "Proposals", match: :first
+        end
+
+        expect(page).to have_current_path(path)
+      end
+
+      scenario "when any form input was modified and page was restored from browser history" do
+        visit path
+
+        fill_in "Changes", with: "Version 1b"
+        accept_confirm(prompt) do
+          click_link "Proposals", match: :first
+        end
+
+        expect(page).to have_text(:h2, "Proposals")
+
+        go_back
+
+        expect(page).to have_content legislation_process.title
+
+        accept_confirm(prompt) do
+          click_link "Proposals", match: :first
+        end
+
+        expect(page).to have_text(:h2, "Proposals")
+      end
+    end
+
+    context "should not ask for confirmation before leaving the current page" do
+      scenario "when users restores original form data" do
+        visit path
+
+        fill_in "Changes", with: "Version 1b"
+        accept_confirm(prompt) do
+          click_link "Proposals", match: :first
+        end
+
+        expect(page).to have_text(:h2, "Proposals")
+
+        go_back
+
+        expect(page).to have_content legislation_process.title
+
+        fill_in "Changes", with: legislation_draft_version.changelog
+
+        click_link "Proposals", match: :first
+
+        expect(page).to have_text(:h2, "Proposals")
+      end
+    end
+  end
+end
