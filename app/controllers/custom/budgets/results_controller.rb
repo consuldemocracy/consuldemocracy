1d791508ebcb8c15eefa85be4c6392b421715383
Andos Asesores <soporte@an2.es>
2020-07-21 03:42:43 +0200

    Modificado orden de las partidas de los resultados en los presupuestos por id

--- app/controllers/custom/budgets/results_controller.rb
+++ app/controllers/custom/budgets/results_controller.rb
@@ -2,10 +2,16 @@ require_dependency Rails.root.join("app", "controllers", "budgets", "results_con
 
 module Budgets
   class ResultsController < ApplicationController
+    def show
+      authorize! :read_results, @budget
+      @investments = Budget::Result.new(@budget, @heading).investments
+      @headings = @budget.headings.order(:id)
+    end
+
     private
       def load_heading
         if @budget.present?
-          headings = @budget.headings.order(:name)
+          headings = @budget.headings.order(:id)
           @heading = headings.find_by_slug_or_id(params[:heading_id]) || headings.first
         end
       end
