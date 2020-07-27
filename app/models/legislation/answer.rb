ba0a346a12bf4b8144c5d8ecf810bd8744a84425
Julian Herrero <microweb10@gmail.com>
2020-07-27 16:47:52 +0700

    Do not delete users when deleting legislation answers
    When a legislation process is deleted, everything related will be
    deleted, including the answers. This 'dependen: :destroy' was causing
    that admin users accounts were being accidentally deleted. Even when
    they were logged in they were logged out and not able to log in again.

--- app/models/legislation/answer.rb
+++ app/models/legislation/answer.rb
@@ -5,7 +5,7 @@ class Legislation::Answer < ApplicationRecord
   belongs_to :question, foreign_key: "legislation_question_id", inverse_of: :answers, counter_cache: true
   belongs_to :question_option, foreign_key: "legislation_question_option_id",
                                inverse_of: :answers, counter_cache: true
-  belongs_to :user, dependent: :destroy, inverse_of: :legislation_answers
+  belongs_to :user, inverse_of: :legislation_answers
 
   validates :question, presence: true, uniqueness: { scope: :user_id }
   validates :question_option, presence: true
