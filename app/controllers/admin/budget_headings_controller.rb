constructors/consul

Add approval voting to budgets

app/controllers/admin/budget_headings_controller.rb
@@ -63,7 +63,7 @@ class Admin::BudgetHeadingsController < Admin::BaseController
     end
 
     def budget_heading_params
-      valid_attributes = [:price, :population, :allow_custom_content, :latitude, :longitude]
+      valid_attributes = [:price, :population, :allow_custom_content, :latitude, :longitude, :max_votes]
       params.require(:budget_heading).permit(*valid_attributes, translation_params(Budget::Heading))
     end
 end

db/migrate/20190418114431_add_approval_voting_fields.rb
@@ -1,0 +1,6 @@
+class AddApprovalVotingFields < ActiveRecord::Migration[4.2]
+  def change
+    add_column :budgets, :voting_style, :string, default: "knapsack"
+    add_column :budget_headings, :max_votes, :integer, default: 1
+  end
+end

spec/models/budget/heading_spec.rb
@@ -323,4 +323,13 @@ describe Budget::Heading do
       expect(Budget::Heading.allow_custom_content).to eq [translated_heading]
     end
   end
+
+  describe "#max_votes" do
+    it "must be at least 1" do
+      expect(build(:budget_heading, max_votes: 1)).to be_valid
+      expect(build(:budget_heading, max_votes: 10)).to be_valid
+      expect(build(:budget_heading, max_votes: -1)).not_to be_valid
+      expect(build(:budget_heading, max_votes: 0)).not_to be_valid
+    end
+  end
 end

consuldemocracy/consuldemocracy

Co-Authored-By: Javi Martín <javim@elretirao.net>

app/controllers/admin/budget_headings_controller.rb
@@ -63,7 +63,7 @@ class Admin::BudgetHeadingsController < Admin::BaseController
     end
 
     def budget_heading_params
-      valid_attributes = [:price, :population, :allow_custom_content, :latitude, :longitude]
+      valid_attributes = [:price, :population, :allow_custom_content, :latitude, :longitude, :max_ballot_lines]
       params.require(:budget_heading).permit(*valid_attributes, translation_params(Budget::Heading))
     end
 end

db/migrate/20190418114431_add_approval_voting_fields.rb
@@ -1,0 +1,6 @@
+class AddApprovalVotingFields < ActiveRecord::Migration[4.2]
+  def change
+    add_column :budgets, :voting_style, :string, default: "knapsack"
+    add_column :budget_headings, :max_ballot_lines, :integer, default: 1
+  end
+end

spec/models/budget/heading_spec.rb
@@ -323,4 +323,13 @@ describe Budget::Heading do
       expect(Budget::Heading.allow_custom_content).to eq [translated_heading]
     end
   end
+
+  describe "#max_ballot_lines" do
+    it "must be at least 1" do
+      expect(build(:budget_heading, max_ballot_lines: 1)).to be_valid
+      expect(build(:budget_heading, max_ballot_lines: 10)).to be_valid
+      expect(build(:budget_heading, max_ballot_lines: -1)).not_to be_valid
+      expect(build(:budget_heading, max_ballot_lines: 0)).not_to be_valid
+    end
+  end
 end