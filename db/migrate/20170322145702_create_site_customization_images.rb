574a5d00679a3512c217fcb4c846ef13ec397e98
taitus <sebastia.roig@gmail.com>
2020-08-31 12:31:08 +0200

    Update old migrations to maintain consistency
    After merge "Bump paperclip from 5.2.1 to 6.1.0 #3905" as indicated in
    the release_notes columns type for attachments are changed from integer
    to bigint.
    For this reason we edit old migrations so that they continue to
    generate integer as before.

--- db/migrate/20170322145702_create_site_customization_images.rb
+++ db/migrate/20170322145702_create_site_customization_images.rb
@@ -2,7 +2,10 @@ class CreateSiteCustomizationImages < ActiveRecord::Migration[4.2]
   def change
     create_table :site_customization_images do |t|
       t.string :name, null: false
-      t.attachment :image
+      t.string :image_file_name
+      t.string :image_content_type
+      t.integer :image_file_size
+      t.datetime :image_updated_at
       t.timestamps null: false
     end
 
--- db/migrate/20170627113331_create_images.rb
+++ db/migrate/20170627113331_create_images.rb
@@ -1,14 +1,13 @@
 class CreateImages < ActiveRecord::Migration[4.2]
-  def up
+  def change
     create_table :images do |t|
       t.references :imageable, polymorphic: true, index: true
       t.string :title, limit: 80
       t.timestamps null: false
+      t.string :attachment_file_name
+      t.string :attachment_content_type
+      t.integer :attachment_file_size
+      t.datetime :attachment_updated_at
     end
-    add_attachment :images, :attachment
-  end
-
-  def down
-    remove_attachment :images, :attachment
   end
 end
