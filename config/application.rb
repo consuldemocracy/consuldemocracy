905ac48bb9fd727a189bca427902b47f27c793df
Javi Martín <javim@elretirao.net>
2020-09-05 14:31:58 +0200

    Raise an exception when an asset is not found
    This is the default in Rails 5.1 applications. If we want to use an
    asset in the public folder, we need to add the `public_folder: true`
    option, making it clear that we don't expect the asset to be in the
    asset pipeline.
    
    Since we don't use `asset_path` to reference assets in the public
    folder, we can safely disable the `unknown_asset_fallback` option.

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module Consul
 
     # Overwrite Rails 5.1 defaults and use the options we used in Rails 5.0
     config.action_view.form_with_generates_remote_forms = false
-    config.assets.unknown_asset_fallback = true
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
