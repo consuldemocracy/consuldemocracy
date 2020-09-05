f42effe9feca125da3d4a4cb1a43e9da4dd26141
Javi Martín <javim@elretirao.net>
2020-09-05 18:49:00 +0200

    Use Rails 5.2 defaults and overwrite them
    We can remove the `new_framework_defaults_5_2` file by using Rails 5.2
    default options and overwriting the ones we haven't enabled.
    
    We're disabling `use_authenticated_message_encryption` because, even if
    we don't use it, some CONSUL installations might be using it, and
    enabling this options would make it harder to decrypt existing encrypted
    messages.
    
    And we're disabling `cache_versioning` until we verify our cache keeps
    working and expires as expected on production environments, particularly
    for stats.

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Consul
   class Application < Rails::Application
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     # Keep belongs_to fields optional by default, because that's the way
     # Rails 4 models worked
@@ -16,6 +16,14 @@ module Consul
 
     # Use local forms with `form_with`, so it works like `form_for`
     config.action_view.form_with_generates_remote_forms = false
+
+    # Keep disabling cache versioning until we verify it's compatible
+    # with `:dalli_store` and with the way we cache stats
+    config.active_record.cache_versioning = false
+
+    # Keep using AES-256-CBC for message encryption in case it's used
+    # in any CONSUL installations
+    config.active_support.use_authenticated_message_encryption = false
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
