111d8484db44f0ff358f8d9d82940ff299ae56a2
Michael Weber <michael.weber@stud-mail.uni-wuerzburg.de>
2020-10-07 10:22:36 +0200

    Fix 500 error on proposal votes

--- app/controllers/custom/proposals_controller.rb
+++ app/controllers/custom/proposals_controller.rb
@@ -60,6 +60,7 @@ class ProposalsController < ApplicationController
   end
 
   def vote
+    @follow = Follow.find_or_create_by!(user: current_user, followable: @proposal)
     @proposal.register_vote(current_user, "yes")
     set_proposal_votes(@proposal)
   end
@@ -76,6 +77,7 @@ class ProposalsController < ApplicationController
   end
 
   def vote_featured
+    @follow = Follow.find_or_create_by!(user: current_user, followable: @proposal)
     @proposal.register_vote(current_user, "yes")
     set_featured_proposal_votes(@proposal)
   end
