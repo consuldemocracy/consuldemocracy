a8b2352d4a31bc47ea34e81d636f59b2302db764
Michael Weber <michael.weber@stud-mail.uni-wuerzburg.de>
2020-10-15 22:05:25 +0200

    Hide dashboard from users

---
+++ app/models/custom/abilities/common.rb
@@ -0,0 +1,124 @@
+module Abilities
+  class Common
+    include CanCan::Ability
+
+    def initialize(user)
+      merge Abilities::Everyone.new(user)
+
+      can [:read, :update], User, id: user.id
+
+      can :read, Debate
+      can :update, Debate do |debate|
+        debate.editable_by?(user)
+      end
+
+      can :read, Proposal
+      can :update, Proposal do |proposal|
+        proposal.editable_by?(user)
+      end
+      can :publish, Proposal do |proposal|
+        proposal.draft? && proposal.author.id == user.id && !proposal.retired?
+      end
+      #can :dashboard, Proposal do |proposal|
+      #  proposal.author.id == user.id
+      #end
+      can :manage_polls, Proposal do |proposal|
+        proposal.author.id == user.id
+      end
+      can :manage_mailing, Proposal do |proposal|
+        proposal.author.id == user.id
+      end
+      can :manage_poster, Proposal do |proposal|
+        proposal.author.id == user.id
+      end
+
+      can :results, Poll do |poll|
+        poll.related&.author&.id == user.id
+      end
+
+      can [:retire_form, :retire], Proposal, author_id: user.id
+
+      can :read, Legislation::Proposal
+      cannot [:edit, :update], Legislation::Proposal do |proposal|
+        proposal.editable_by?(user)
+      end
+      can [:retire_form, :retire], Legislation::Proposal, author_id: user.id
+
+      can :create, Comment
+      can :create, Debate
+      can [:create, :created], Proposal
+      can :create, Legislation::Proposal
+
+      can :suggest, Debate
+      can :suggest, Proposal
+      can :suggest, Legislation::Proposal
+      can :suggest, Tag
+
+      can [:flag, :unflag], Comment
+      cannot [:flag, :unflag], Comment, user_id: user.id
+
+      can [:flag, :unflag], Debate
+      cannot [:flag, :unflag], Debate, author_id: user.id
+
+      can [:flag, :unflag], Proposal
+      cannot [:flag, :unflag], Proposal, author_id: user.id
+
+      can [:flag, :unflag], Legislation::Proposal
+      cannot [:flag, :unflag], Legislation::Proposal, author_id: user.id
+
+      can [:flag, :unflag], Budget::Investment
+      cannot [:flag, :unflag], Budget::Investment, author_id: user.id
+
+      can [:create, :destroy], Follow
+
+      can [:destroy], Document do |document|
+        document.documentable&.author_id == user.id
+      end
+
+      can [:destroy], Image, imageable: { author_id: user.id }
+
+      can [:create, :destroy], DirectUpload
+
+      unless user.organization?
+        can :vote, Debate
+        can :vote, Comment
+      end
+
+      if user.level_two_or_three_verified?
+        can :vote, Proposal, &:published?
+        can :vote_featured, Proposal
+
+        can :vote, Legislation::Proposal
+        can :vote_featured, Legislation::Proposal
+        can :create, Legislation::Answer
+
+        can :create, Budget::Investment,               budget: { phase: "accepting" }
+        can :edit, Budget::Investment,                 budget: { phase: "accepting" }, author_id: user.id
+        can :update, Budget::Investment,               budget: { phase: "accepting" }, author_id: user.id
+        can :suggest, Budget::Investment,              budget: { phase: "accepting" }
+        can :destroy, Budget::Investment,              budget: { phase: ["accepting", "reviewing"] }, author_id: user.id
+        can :vote, Budget::Investment,                 budget: { phase: "selecting" }
+
+        can [:show, :create], Budget::Ballot,          budget: { phase: "balloting" }
+        can [:create, :destroy], Budget::Ballot::Line, budget: { phase: "balloting" }
+
+        can :create, DirectMessage
+        can :show, DirectMessage, sender_id: user.id
+
+        can :answer, Poll do |poll|
+          poll.answerable_by?(user)
+        end
+        can :answer, Poll::Question do |question|
+          question.answerable_by?(user)
+        end
+      end
+
+      can [:create, :show], ProposalNotification, proposal: { author_id: user.id }
+
+      can [:create], Topic
+      can [:update, :destroy], Topic, author_id: user.id
+
+      can :disable_recommendations, [Debate, Proposal]
+    end
+  end
+end
---
+++ app/models/custom/abilities/moderation.rb
@@ -0,0 +1,79 @@
+module Abilities
+  class Moderation
+    include CanCan::Ability
+
+    def initialize(user)
+      merge Abilities::Common.new(user)
+
+      can :dashboard, Proposal do |proposal|
+        proposal.author.id == user.id
+      end
+
+      can :read, Organization
+      can(:verify, Organization) { |o| !o.verified? }
+      can(:reject, Organization) { |o| !o.rejected? }
+
+      can :read, Comment
+
+      can :hide, Comment, hidden_at: nil
+      cannot :hide, Comment, user_id: user.id
+
+      can :ignore_flag, Comment, ignored_flag_at: nil, hidden_at: nil
+      cannot :ignore_flag, Comment, user_id: user.id
+
+      can :moderate, Comment
+      cannot :moderate, Comment, user_id: user.id
+
+      can :hide, Debate, hidden_at: nil
+      cannot :hide, Debate, author_id: user.id
+
+      can :ignore_flag, Debate, ignored_flag_at: nil, hidden_at: nil
+      cannot :ignore_flag, Debate, author_id: user.id
+
+      can :moderate, Debate
+      cannot :moderate, Debate, author_id: user.id
+
+      can :hide, Proposal, hidden_at: nil
+
+      can :ignore_flag, Proposal, ignored_flag_at: nil, hidden_at: nil
+      cannot :ignore_flag, Proposal, author_id: user.id
+
+      can :moderate, Proposal
+
+      can :hide, Legislation::Proposal, hidden_at: nil
+      cannot :hide, Legislation::Proposal, author_id: user.id
+
+      can :ignore_flag, Legislation::Proposal, ignored_flag_at: nil, hidden_at: nil
+      cannot :ignore_flag, Legislation::Proposal, author_id: user.id
+
+      can :moderate, Legislation::Proposal
+      cannot :moderate, Legislation::Proposal, author_id: user.id
+
+      can :hide, User
+      cannot :hide, User, id: user.id
+
+      can :block, User
+      cannot :block, User, id: user.id
+
+      can :hide, ProposalNotification, hidden_at: nil
+      cannot :hide, ProposalNotification, author_id: user.id
+
+      can :ignore_flag, ProposalNotification, ignored_at: nil, hidden_at: nil
+      cannot :ignore_flag, ProposalNotification, author_id: user.id
+
+      can :moderate, ProposalNotification
+      cannot :moderate, ProposalNotification, author_id: user.id
+
+      can :index, ProposalNotification
+
+      can :hide, Budget::Investment, hidden_at: nil
+      cannot :hide, Budget::Investment, author_id: user.id
+
+      can :ignore_flag, Budget::Investment, ignored_flag_at: nil, hidden_at: nil
+      cannot :ignore_flag, Budget::Investment, author_id: user.id
+
+      can :moderate, Budget::Investment
+      cannot :moderate, Budget::Investment, author_id: user.id
+    end
+  end
+end
