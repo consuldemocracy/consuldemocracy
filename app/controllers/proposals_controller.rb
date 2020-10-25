9bd012f83c303631cb0610483a6909411844f6eb
anks <matisnape@wp.pl>
2020-10-25 14:22:41 +0100

    Hide retired proposals from related content proposals  (Merge pull request #4196)
    Co-authored-by: Anna Anks Nowak <matisnape@users.noreply.github.com>

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -27,8 +27,7 @@ class ProposalsController < ApplicationController
     super
     @notifications = @proposal.notifications
     @notifications = @proposal.notifications.not_moderated
-    @related_contents = Kaminari.paginate_array(@proposal.relationed_contents)
-                                .page(params[:page]).per(5)
+    @related_contents = Kaminari.paginate_array(@proposal.relationed_contents).page(params[:page]).per(5)
 
     if request.path != proposal_path(@proposal)
       redirect_to proposal_path(@proposal), status: :moved_permanently
