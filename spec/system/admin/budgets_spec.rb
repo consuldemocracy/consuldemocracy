31b2217e8aa31e57a48c5fea8bd40540e6b3c410
decabeza <alberto@decabeza.es>
2020-11-05 12:12:13 +0100

    Allow to delete a budget with staff assigned

--- spec/system/admin/budgets_spec.rb
+++ spec/system/admin/budgets_spec.rb
@@ -454,6 +454,24 @@ describe "Admin budgets" do
       expect(page).to have_content("You cannot delete a budget that has an associated poll")
       expect(page).to have_content("There is 1 budget")
     end
+
+    scenario "Allow to delete a budget with administrators or valuators assigned", :js do
+      admin = create(:administrator)
+      valuator = create(:valuator)
+
+      budget = create(:budget, administrators: [admin], valuators: [valuator])
+
+      visit admin_budgets_path
+
+      within "#budget_#{budget.id}" do
+        click_link "Delete budget"
+      end
+
+      accept_confirm
+
+      expect(page).to have_content("Budget deleted successfully")
+      expect(page).not_to have_content(budget.name)
+    end
   end
 
   context "Edit" do
