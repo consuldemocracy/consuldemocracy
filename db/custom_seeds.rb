4eca919457c9d3dbe15dccbe620ffc2c9f9ed93f
Javi Martín <javim@elretirao.net>
2020-11-05 23:33:17 +0100

    Add link to the weather in Santiago de Compostela
    The same way it's done in similar pages.

--- db/custom_seeds.rb
+++ db/custom_seeds.rb
@@ -20,3 +20,24 @@ I18n.available_locales.each do |locale|
     )
   end
 end
+
+lg_weather = { es: "cas", gl: "gal" }
+
+I18n.available_locales.each do |locale|
+  next if SiteCustomization::ContentBlock.find_by(locale: locale, name: "footer")
+
+  I18n.with_locale(locale) do
+    SiteCustomization::ContentBlock.create!(
+      locale: locale,
+      name: "footer",
+      body: %Q(
+        <li class="inline-block">
+          <a href="http://www.santiagodecompostela.gal/tempo.php?lg=#{lg_weather[locale]}" title="#{I18n.t("sites.weather.title")}">
+            <span class="show-for-sr">#{I18n.t("sites.weather.title")}</span>
+            <span class="icon-otempo" aria-hidden="true"></span>
+          </a>
+        </li>
+      )
+    )
+  end
+end
