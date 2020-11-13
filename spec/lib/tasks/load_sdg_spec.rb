00e91c40b954c31eaa55ebc8e497a2a47401a95e
Senén Rodero Rodríguez <senenrodero@gmail.com>
2020-11-13 17:00:09 +0100

    Add SDG targets to seeds
    Co-authored-by: Javi Martín <javim@elretirao.net>

--- spec/lib/tasks/load_sdg_spec.rb
+++ spec/lib/tasks/load_sdg_spec.rb
@@ -7,22 +7,28 @@ describe "rake db:load_sdg" do
     Rake.application.invoke_task("db:load_sdg")
   end
 
-  it "populates empty databases correctly" do
+  it "populates empty databases and assigns targets correctly" do
     SDG::Goal.destroy_all
 
     run_rake_task
 
     expect(SDG::Goal.count).to eq 17
+    expect(SDG::Target.count).to eq 169
+    expect(SDG::Target["17.1"].goal.code).to eq 17
   end
 
   it "does not create additional records on populated databases" do
     expect(SDG::Goal.count).to eq 17
+    expect(SDG::Target.count).to eq 169
 
     goal_id = SDG::Goal.last.id
+    target_id = SDG::Target.last.id
 
     run_rake_task
 
     expect(SDG::Goal.count).to eq 17
+    expect(SDG::Target.count).to eq 169
     expect(SDG::Goal.last.id).to eq goal_id
+    expect(SDG::Target.last.id).to eq target_id
   end
 end
