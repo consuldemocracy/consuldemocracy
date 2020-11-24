5611f589097710285ad7361e7aa6c17d9681d277
Senén Rodero Rodríguez <senenrodero@gmail.com>
2020-11-24 17:06:54 +0100

    Add SDG page to create local targets

--- app/controllers/sdg_management/local_targets_controller.rb
+++ app/controllers/sdg_management/local_targets_controller.rb
@@ -1,7 +1,30 @@
 class SDGManagement::LocalTargetsController < SDGManagement::BaseController
+  include Translatable
+
   LocalTarget = ::SDG::LocalTarget
 
   def index
     @local_targets = LocalTarget.all.sort
   end
+
+  def new
+    @local_target = LocalTarget.new
+  end
+
+  def create
+    @local_target = LocalTarget.new(local_target_params)
+
+    if @local_target.save
+      redirect_to sdg_management_local_targets_path, notice: t("sdg_management.local_targets.create.notice")
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def local_target_params
+      translations_attributes = translation_params(LocalTarget)
+      params.require(:sdg_local_target).permit(:code, :target_id, translations_attributes)
+    end
 end
--- spec/system/sdg_management/local_targets_spec.rb
+++ spec/system/sdg_management/local_targets_spec.rb
@@ -15,6 +15,7 @@ describe "Local Targets", :js do
 
       expect(page).to have_title "SDG content - Local Targets"
       within("table") { expect(page).to have_content "Affordable food for everyone" }
+      expect(page).to have_link "Create local target", href: new_sdg_management_local_target_path
     end
 
     scenario "Show local targets grouped by target" do
@@ -28,6 +29,34 @@ describe "Local Targets", :js do
       expect(target_1.title).to appear_before(target_1_local_target.title)
       expect(target_1_local_target.title).to appear_before(target_2.title)
       expect(target_2.title).to appear_before(target_2_local_target.title)
+    end
+  end
+
+  describe "Create" do
+    scenario "Shows succesful notice when form is fullfilled correctly" do
+      visit new_sdg_management_local_target_path
+
+      target = SDG::Target["1.1"]
+      select "#{target.code} #{target.title}", from: "Target"
+      fill_in "Code", with: "1.1.1"
+      fill_in "Title", with: "Local target title"
+      fill_in "Description", with: "Local target description"
+      click_button "Create local target"
+
+      expect(page).to have_content("Local target created successfully")
+      expect(page).to have_content("1.1.1")
+    end
+
+    scenario "Shows form errors when not valid" do
+      visit new_sdg_management_local_target_path
+
+      target = SDG::Target["2.3"]
+      code_and_title = "#{target.code} #{target.title}"
+      select code_and_title, from: "Target"
+      click_button "Create local target"
+
+      expect(page).to have_content("errors prevented this local target from being saved.")
+      expect(page).to have_select("Target", selected: code_and_title)
     end
   end
 end
