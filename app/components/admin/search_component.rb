a9427f5971a474192233c7a58a8fbea1dde27605
Javi Martín <javim@elretirao.net>
2020-12-03 16:19:57 +0100

    Allow custom options in search form component
    So we keep the same API as the form_tag method.

--- app/components/admin/search_component.rb
+++ app/components/admin/search_component.rb
@@ -1,14 +1,19 @@
 class Admin::SearchComponent < ApplicationComponent
-  attr_reader :url, :label
+  attr_reader :url, :label, :form_options
 
-  def initialize(url:, label:)
+  def initialize(url:, label:, **form_options)
     @url = url
     @label = label
+    @form_options = form_options
   end
 
   private
 
     def search_terms
       params[:search]
+    end
+
+    def options
+      { method: :get, role: "search" }.merge(form_options)
     end
 end
