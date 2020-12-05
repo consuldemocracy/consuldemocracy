38583b7b81c82528c4117ecb4d7d282264c4ca2e
Javi Martín <javim@elretirao.net>
2020-12-05 17:10:03 +0100

    Use a method to generate a link of list
    Generally speaking, I'm not a big fan of helpers, but there are methods
    which IMHO qualify as helpers when:
    
    * They do not deal with application objects but mainly strings and
      arrays
    * They return text or an HTML tag
    * Their logic is simple and splitting it into several methods is not
      necessary
    
    Many Rails helpers, like `tag`, follow these principles.

---
+++ app/helpers/link_list_helper.rb
@@ -0,0 +1,13 @@
+module LinkListHelper
+  def link_list(*links, **options)
+    return if links.compact.empty?
+
+    tag.ul(options) do
+      safe_join(links.compact.map do |text, url, **link_options|
+        tag.li do
+          link_to text, url, link_options
+        end
+      end)
+    end
+  end
+end
