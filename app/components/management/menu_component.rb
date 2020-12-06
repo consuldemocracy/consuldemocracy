ad6e61bbc59d84c29e909ca0be25452f53e1dfb2
Javi Martín <javim@elretirao.net>
2020-12-06 14:00:09 +0100

    Remove `menu_` prefix in management menu methods
    Now that we're in a component and the method names won't conficts with
    the ones in other modules/classes, we can remove the prefix.

--- app/components/management/menu_component.rb
+++ app/components/management/menu_component.rb
@@ -4,46 +4,46 @@ class Management::MenuComponent < ApplicationComponent
 
   private
 
-    def menu_users?
+    def users?
       ["users", "email_verifications", "document_verifications"].include?(controller_name)
     end
 
-    def menu_edit_password_email?
+    def edit_password_email?
       controller_name == "account" && action_name == "edit_password_email"
     end
 
-    def menu_edit_password_manually?
+    def edit_password_manually?
       controller_name == "account" && action_name == "edit_password_manually"
     end
 
-    def menu_create_proposal?
+    def create_proposal?
       controller_name == "proposals" && action_name == "new"
     end
 
-    def menu_support_proposal?
+    def support_proposal?
       controller_name == "proposals" && action_name == "index"
     end
 
-    def menu_print_proposals?
+    def print_proposals?
       controller_name == "proposals" && action_name == "print"
     end
 
-    def menu_create_investments?
+    def create_investments?
       (controller_name == "budget_investments" && action_name == "new") ||
         (controller_name == "budgets" && action_name == "create_investments")
     end
 
-    def menu_support_investments?
+    def support_investments?
       (controller_name == "budget_investments" && action_name == "index") ||
         (controller_name == "budgets" && action_name == "support_investments")
     end
 
-    def menu_print_investments?
+    def print_investments?
       (controller_name == "budget_investments" && action_name == "print") ||
         (controller_name == "budgets" && action_name == "print_investments")
     end
 
-    def menu_user_invites?
+    def user_invites?
       controller_name == "user_invites"
     end
 end
