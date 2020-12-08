5590ecaaa6929fba3b14c36c7054bf166b75f888
Javi Martín <javim@elretirao.net>
2020-12-08 13:16:33 +0100

    Extract methods to generate SDG management menu

--- app/components/sdg_management/menu_component.rb
+++ app/components/sdg_management/menu_component.rb
@@ -3,6 +3,14 @@ class SDGManagement::MenuComponent < ApplicationComponent
 
   private
 
+    def links
+      [goals_link]
+    end
+
+    def goals_link
+      [t("sdg_management.menu.sdg_content"), sdg_management_goals_path, sdg?, class: "goals-link"]
+    end
+
     def sdg?
       %w[goals targets local_targets].include?(controller_name)
     end
