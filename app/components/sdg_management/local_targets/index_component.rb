smart-green-city-hassfurt/consul

Add local_targets index to the administration

app/components/sdg_management/local_targets/index_component.rb
@@ -1,0 +1,23 @@
+class SDGManagement::LocalTargets::IndexComponent < ApplicationComponent
+  include SDGManagement::Header
+
+  attr_reader :local_targets
+
+  def initialize(local_targets)
+    @local_targets = local_targets
+  end
+
+  private
+
+    def title
+      SDG::LocalTarget.model_name.human(count: 2).titleize
+    end
+
+    def attribute_name(attribute)
+      SDG::Target.human_attribute_name(attribute)
+    end
+
+    def header_id(object)
+      "#{dom_id(object)}_header"
+    end
+end

consuldemocracy/consuldemocracy

app/components/sdg_management/local_targets/index_component.rb
@@ -1,0 +1,23 @@
+class SDGManagement::LocalTargets::IndexComponent < ApplicationComponent
+  include SDGManagement::Header
+
+  attr_reader :local_targets
+
+  def initialize(local_targets)
+    @local_targets = local_targets
+  end
+
+  private
+
+    def title
+      SDG::LocalTarget.model_name.human(count: 2).titleize
+    end
+
+    def attribute_name(attribute)
+      SDG::LocalTarget.human_attribute_name(attribute)
+    end
+
+    def header_id(object)
+      "#{dom_id(object)}_header"
+    end
+end