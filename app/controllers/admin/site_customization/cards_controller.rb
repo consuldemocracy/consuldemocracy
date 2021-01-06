0762c668551ccaf16ff5044eebf0e7b6c20a3e5e
Javi Martín <javim@elretirao.net>
2021-01-06 15:11:25 +0100

    Don't skip authorization check managing cards
    I'm not sure why we were skipping this check.

--- app/controllers/admin/site_customization/cards_controller.rb
+++ app/controllers/admin/site_customization/cards_controller.rb
@@ -1,5 +1,4 @@
 class Admin::SiteCustomization::CardsController < Admin::SiteCustomization::BaseController
-  skip_authorization_check
   load_and_authorize_resource :page, class: "::SiteCustomization::Page"
   load_and_authorize_resource :card, through: :page, class: "Widget::Card"
 
