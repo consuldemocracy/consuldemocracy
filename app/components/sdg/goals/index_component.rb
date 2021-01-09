4072735187bb48323eb2d3b15868f260204cd61e
Javi Martín <javim@elretirao.net>
2021-01-09 19:19:29 +0100

    Add icons to SDG index

--- app/components/sdg/goals/index_component.rb
+++ app/components/sdg/goals/index_component.rb
@@ -5,4 +5,18 @@ class SDG::Goals::IndexComponent < ApplicationComponent
   def initialize(goals)
     @goals = goals
   end
+
+  private
+
+    def goal_links
+      goals.map { |goal| goal_link(goal) }
+    end
+
+    def goal_link(goal)
+      [icon(goal), sdg_goal_path(goal.code)]
+    end
+
+    def icon(goal)
+      render SDG::Goals::IconComponent.new(goal)
+    end
 end
