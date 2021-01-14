68b32f2a84a8e1521114c216e2c0a9c11f9abee4
Javi Martín <javim@elretirao.net>
2021-01-14 14:45:01 +0100

    Remove duplication creating web sections in seeds

---
+++ db/web_sections.rb
@@ -0,0 +1,5 @@
+WebSection.where(name: "homepage").first_or_create!
+WebSection.where(name: "debates").first_or_create!
+WebSection.where(name: "proposals").first_or_create!
+WebSection.where(name: "budgets").first_or_create!
+WebSection.where(name: "help_page").first_or_create!
