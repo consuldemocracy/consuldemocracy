5a42d65d5f77c9aad3bdf7425fe639647cbcd0f2
Javi Martín <javim@elretirao.net>
2021-01-14 16:42:58 +0100

    Make it possible to add banners to the SDG section

--- db/web_sections.rb
+++ db/web_sections.rb
@@ -1,3 +1,3 @@
-%w[homepage debates proposals budgets help_page].each do |section|
+%w[homepage debates proposals budgets help_page sdg].each do |section|
   WebSection.where(name: section).first_or_create!
 end
