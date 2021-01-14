javierm/consul

Add SDG phases cards management
These cards will be displayed in the SDG homepage.

Note there seems to be a strange behavior in cancancan. If we define
these rules:

can :manage, Widget::Card, page_type: "SDG::Phase"
can :manage, Widget::Card

The expected behavior is the first rule will always be ignored because
the second one overwrites it. However, when creating a new card with
`load_and_authorize_resource` will automatically add `page_type:
"SDG::Phase"`.

Similarly, if we do something like:

can :manage, Widget::Card, id: 3
can :manage, Widget::Card

Then the new card will have `3` as an ID.

Maybe upgrading cancancan solves the issue; we haven't tried it. For now
we're defining a different rule when creating widget cards.

app/controllers/sdg_management/cards_controller.rb
@@ -1,0 +1,12 @@
+class SDGManagement::CardsController < SDGManagement::BaseController
+  include Admin::Widget::CardsActions
+
+  load_and_authorize_resource :page, class: "SDG::Phase", id_param: "sdg_phase_id"
+  load_and_authorize_resource :card, through: :page, class: "Widget::Card"
+
+  private
+
+    def index_path
+      sdg_management_homepage_path
+    end
+end

app/models/abilities/sdg/manager.rb
@@ -6,5 +6,7 @@ class Abilities::SDG::Manager
 
     can :read, ::SDG::Target
     can :manage, ::SDG::LocalTarget
+    can [:read, :update, :destroy], Widget::Card, page_type: "SDG::Phase"
+    can(:create, Widget::Card) { |card| card.page_type == "SDG::Phase" }
   end
 end

spec/models/abilities/sdg/manager_spec.rb
@@ -13,4 +13,9 @@ describe "Abilities::SDG::Manager" do
   it { should_not be_able_to(:read, SDG::Manager) }
   it { should_not be_able_to(:create, SDG::Manager) }
   it { should_not be_able_to(:delete, SDG::Manager) }
+
+  it { should_not be_able_to(:update, create(:widget_card)) }
+  it { should be_able_to(:update, create(:widget_card, page: SDG::Phase.sample)) }
+  it { should_not be_able_to(:create, build(:widget_card)) }
+  it { should be_able_to(:create, build(:widget_card, page: SDG::Phase.sample)) }
 end

spec/system/sdg_management/homepage_spec.rb
@@ -16,5 +16,37 @@ describe "SDG homepage configuration", :js do
 
       expect(page).to have_title "SDG content - Homepage configuration"
     end
+
+    scenario "Create card" do
+      visit sdg_management_homepage_path
+      click_link "Create planning card"
+
+      within(".translatable-fields") { fill_in "Title", with: "My planning card" }
+      click_button "Create card"
+
+      within(".planning-cards") do
+        expect(page).to have_content "My planning card"
+      end
+
+      within(".sensitization-cards") do
+        expect(page).to have_content "There are no cards for this phase"
+      end
+    end
+
+    scenario "Update card" do
+      create(:widget_card, page: SDG::Phase["monitoring"], title: "My monitoring card")
+
+      visit sdg_management_homepage_path
+      within(".monitoring-cards") { click_link "Edit" }
+
+      within(".translatable-fields") { fill_in "Title", with: "Updated monitoring card" }
+      click_button "Save card"
+
+      within(".monitoring-cards") do
+        expect(page).to have_css "tbody tr", count: 1
+        expect(page).to have_content "Updated monitoring card"
+        expect(page).not_to have_content "My monitoring card"
+      end
+    end
   end
 end

consuldemocracy/consuldemocracy

app/controllers/sdg_management/cards_controller.rb
@@ -1,0 +1,13 @@
+class SDGManagement::CardsController < SDGManagement::BaseController
+  include Admin::Widget::CardsActions
+  helper_method :index_path
+
+  load_and_authorize_resource :phase, class: "SDG::Phase", id_param: "sdg_phase_id"
+  load_and_authorize_resource :card, through: :phase, class: "Widget::Card"
+
+  private
+
+    def index_path
+      sdg_management_homepage_path
+    end
+end

app/models/abilities/sdg/manager.rb
@@ -6,5 +6,7 @@ class Abilities::SDG::Manager
 
     can :read, ::SDG::Target
     can :manage, ::SDG::LocalTarget
+    can [:read, :update, :destroy], Widget::Card, cardable_type: "SDG::Phase"
+    can(:create, Widget::Card) { |card| card.cardable_type == "SDG::Phase" }
   end
 end

spec/models/abilities/sdg/manager_spec.rb
@@ -13,4 +13,9 @@ describe "Abilities::SDG::Manager" do
   it { should_not be_able_to(:read, SDG::Manager) }
   it { should_not be_able_to(:create, SDG::Manager) }
   it { should_not be_able_to(:delete, SDG::Manager) }
+
+  it { should_not be_able_to(:update, create(:widget_card)) }
+  it { should be_able_to(:update, create(:widget_card, cardable: SDG::Phase.sample)) }
+  it { should_not be_able_to(:create, build(:widget_card)) }
+  it { should be_able_to(:create, build(:widget_card, cardable: SDG::Phase.sample)) }
 end

spec/system/sdg_management/homepage_spec.rb
@@ -16,5 +16,37 @@ describe "SDG homepage configuration", :js do
 
       expect(page).to have_title "SDG content - Homepage configuration"
     end
+
+    scenario "Create card" do
+      visit sdg_management_homepage_path
+      click_link "Create planning card"
+
+      within(".translatable-fields") { fill_in "Title", with: "My planning card" }
+      click_button "Create card"
+
+      within(".planning-cards") do
+        expect(page).to have_content "My planning card"
+      end
+
+      within(".sensitization-cards") do
+        expect(page).to have_content "There are no cards for this phase"
+      end
+    end
+
+    scenario "Update card" do
+      create(:widget_card, cardable: SDG::Phase["monitoring"], title: "My monitoring card")
+
+      visit sdg_management_homepage_path
+      within(".monitoring-cards") { click_link "Edit" }
+
+      within(".translatable-fields") { fill_in "Title", with: "Updated monitoring card" }
+      click_button "Save card"
+
+      within(".monitoring-cards") do
+        expect(page).to have_css "tbody tr", count: 1
+        expect(page).to have_content "Updated monitoring card"
+        expect(page).not_to have_content "My monitoring card"
+      end
+    end
   end
 end