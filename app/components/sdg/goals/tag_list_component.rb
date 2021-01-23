7b3fcf6cb580129b64d0e0d1bba244be61f41367
Javi Martín <javim@elretirao.net>
2021-01-23 15:51:23 +0100

    Split goal tag list component
    This way we'll be able to add targets as well. Besides, having two
    classes in the CSS selector will allows to overwrite styles such as
    `.debate-show ul li`.
    
    On the other hand, we need to add a stylesheet for the filters as well.

--- app/components/sdg/goals/tag_list_component.rb
+++ app/components/sdg/goals/tag_list_component.rb
@@ -1,24 +1,18 @@
 class SDG::Goals::TagListComponent < ApplicationComponent
-  attr_reader :record_or_name, :limit
-  delegate :link_list, to: :helpers
-
-  def initialize(record_or_name, limit: nil)
-    @record_or_name = record_or_name
-    @limit = limit
-  end
-
-  def render?
-    process.enabled?
-  end
+  include SDG::TagList
 
   private
 
+    def record
+      record_or_name if record_or_name.respond_to?(:sdg_goals)
+    end
+
     def links
-      [*goal_links, see_more_link]
+      [*goal_links, see_more_link(goals)]
     end
 
     def goal_links
-      goals.limit(limit).map do |goal|
+      goals.order(:code).limit(limit).map do |goal|
         [
           render(SDG::Goals::IconComponent.new(goal)),
           index_by_goal(goal),
@@ -28,42 +22,14 @@ class SDG::Goals::TagListComponent < ApplicationComponent
     end
 
     def goals
-      if record_or_name.respond_to?(:sdg_goals)
-        record_or_name.sdg_goals.order(:code)
-      else
-        SDG::Goal.order(:code)
-      end
-    end
-
-    def see_more_link
-      return unless limit && count_out_of_limit > 0
-
-      [
-        "#{count_out_of_limit}+",
-        polymorphic_path(record_or_name),
-        class: "more-goals", title: t("sdg.goals.filter.more", count: count_out_of_limit)
-      ]
+      record&.sdg_goals || SDG::Goal.all
     end
 
     def index_by_goal(goal)
-      polymorphic_path(model, advanced_search: { goal: goal.code })
+      index_by(goal: goal.code)
     end
 
-    def filter_text(goal)
-      t("sdg.goals.filter.link",
-        resources: model.model_name.human(count: :other),
-        code: goal.code)
-    end
-
-    def count_out_of_limit
-      goals.size - limit
-    end
-
-    def model
-      process.name.constantize
-    end
-
-    def process
-      @process ||= SDG::ProcessEnabled.new(record_or_name)
+    def i18n_namespace
+      "goals"
     end
 end
