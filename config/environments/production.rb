c470add0ff8b9df39a6074fba36b9cb678610f01
Senén Rodero Rodríguez <senenrodero@gmail.com>
2021-01-25 21:42:32 +0100

    Configure rails cache to connect with AWS Memcached cluster

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,11 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store, { value_max_bytes: 2000000 }
+  if Rails.application.secrets.aws_memcached_configuration_endpoint.present?
+    endpoint    = Rails.application.secrets.aws_memcached_configuration_endpoint
+    elasticache = Dalli::ElastiCache.new(endpoint)
+    config.cache_store = :dalli_store, elasticache.servers, { expires_in: 1.day, compress: true}
+  end
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -61,7 +61,11 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store
+  if Rails.application.secrets.aws_memcached_configuration_endpoint.present?
+    endpoint    = Rails.application.secrets.aws_memcached_configuration_endpoint
+    elasticache = Dalli::ElastiCache.new(endpoint)
+    config.cache_store = :dalli_store, elasticache.servers, { expires_in: 1.day, compress: true}
+  end
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
