5b6551f6d7139d2f399fa1dee8ff34775de0bb3b
Javi Martín <javim@elretirao.net>
2021-02-15 23:45:57 +0100

    Extract method to get the notifications link text

--- app/components/layout/notification_item_component.rb
+++ app/components/layout/notification_item_component.rb
@@ -4,4 +4,14 @@ class Layout::NotificationItemComponent < ApplicationComponent
   def initialize(user)
     @user = user
   end
+
+  private
+
+    def text
+      if user.notifications.unread.count > 0
+        t("layouts.header.notification_item.new_notifications", count: user.notifications_count)
+      else
+        t("layouts.header.notification_item.no_notifications")
+      end
+    end
 end
