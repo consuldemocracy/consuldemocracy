f9ecc583ef6905bdffcab49d8c11a57007867db6
Javi Martín <javim@elretirao.net>
2021-02-18 22:09:48 +0100

    Extract methods to show a phase start/end

--- app/components/admin/budget_phases/phases_component.rb
+++ app/components/admin/budget_phases/phases_component.rb
@@ -10,4 +10,16 @@ class Admin::BudgetPhases::PhasesComponent < ApplicationComponent
     def phases
       budget.phases.order(:id)
     end
+
+    def start_date(phase)
+      formatted_date(phase.starts_at)
+    end
+
+    def end_date(phase)
+      formatted_date(phase.ends_at)
+    end
+
+    def formatted_date(time)
+      l(time.to_date) if time.present?
+    end
 end
