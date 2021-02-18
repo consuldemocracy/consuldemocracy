46fd740027dc56e8fb3af1e74311c40909caeac2
IsaacMorenosutilo <isaac.moreno@sagasoluciones.com>
2021-02-18 14:13:30 +0100

    Configuración del correo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,17 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :address              => 'mail.sagasoluciones.com',
+    :port                 => 2525,
+    :domain               => 'sagasoluciones.com',
+    :user_name            => 'mailserver-ocms@sagasoluciones.com',
+    :password             => 'M@ailServer17',
+    :authentication => :plain,
+    :enable_starttls_auto => true,
+    :ssl => false
+  }
 end
