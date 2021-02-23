consuldev/consul

Refactor participatory budgets in draft mode
Previously the draft mode was a phase of the PB, but that had some
limitations.

Now the phase drafting disappears and therefore the PB can have the
status published or not published (in draft mode).

That will give more flexibility in order to navigate through the
different phases and see how it looks for administrators before
publishing the PB and everybody can see.

By default, the PB is always created in draft mode, so it gives you
the flexibility to adjust and modify anything before publishing it.

app/controllers/admin/budgets_controller.rb
@@ -14,6 +14,7 @@ class Admin::BudgetsController < Admin::BaseController
   end
 
   def show
+    render :edit
   end
 
   def new
@@ -22,6 +23,11 @@ class Admin::BudgetsController < Admin::BaseController
 
   def edit
     load_staff
+  end
+
+  def publish
+    @budget.publish!
+    redirect_to admin_budget_path(@budget), notice: t("admin.budgets.publish.notice")
   end
 
   def calculate_winners
@@ -70,6 +76,7 @@ class Admin::BudgetsController < Admin::BaseController
       descriptions = Budget::Phase::PHASE_KINDS.map { |p| "description_#{p}" }.map(&:to_sym)
       valid_attributes = [:phase,
                           :currency_symbol,
+                          :published,
                           administrator_ids: [],
                           valuator_ids: []
       ] + descriptions

consuldemocracy/consuldemocracy

app/controllers/admin/budgets_controller.rb
@@ -15,12 +15,18 @@ class Admin::BudgetsController < Admin::BaseController
   end
 
   def show
+    render :edit
   end
 
   def new
   end
 
   def edit
+  end
+
+  def publish
+    @budget.publish!
+    redirect_to edit_admin_budget_path(@budget), notice: t("admin.budgets.publish.notice")
   end
 
   def calculate_winners
@@ -42,9 +48,9 @@ class Admin::BudgetsController < Admin::BaseController
   end
 
   def create
-    @budget = Budget.new(budget_params)
+    @budget = Budget.new(budget_params.merge(published: false))
     if @budget.save
-      redirect_to admin_budget_path(@budget), notice: t("admin.budgets.create.notice")
+      redirect_to edit_admin_budget_path(@budget), notice: t("admin.budgets.create.notice")
     else
       render :new
     end