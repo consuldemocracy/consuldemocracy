grupoInnovaris/consul

Render header on sdg index page
We no longer show the static header when we have created a
custom header.

app/components/sdg/goals/index_component.rb
@@ -1,10 +1,11 @@
 class SDG::Goals::IndexComponent < ApplicationComponent
-  attr_reader :goals, :phases
+  attr_reader :goals, :phases, :header
   delegate :link_list, to: :helpers
 
-  def initialize(goals, phases)
+  def initialize(goals, phases, header)
     @goals = goals
     @phases = phases
+    @header = header
   end
 
   private

spec/components/sdg/goals/index_component_spec.rb
@@ -3,16 +3,29 @@ require "rails_helper"
 describe SDG::Goals::IndexComponent, type: :component do
   let!(:goals) { SDG::Goal.all }
   let!(:phases) { SDG::Phase.all }
-  let!(:component) { SDG::Goals::IndexComponent.new(goals, phases) }
+  let!(:component) { SDG::Goals::IndexComponent.new(goals, phases, nil) }
 
   before do
     Setting["feature.sdg"] = true
   end
 
-  it "renders a heading" do
-    render_inline component
+  describe "renders a heading" do
+    it "render static header" do
+      render_inline component
 
-    expect(page).to have_css "h1", exact_text: "Sustainable Development Goals"
+      expect(page).to have_css "h1", exact_text: "Sustainable Development Goals"
+    end
+
+    it "render a custom header" do
+      sdg_web_section = WebSection.find_by!(name: "sdg")
+      header = create(:widget_card, cardable: sdg_web_section)
+      component = SDG::Goals::IndexComponent.new(goals, phases, header)
+
+      render_inline component
+
+      expect(page).to have_content header.title
+      expect(page).not_to have_css "h1", exact_text: "Sustainable Development Goals"
+    end
   end
 
   it "renders phases" do

consuldemocracy/consuldemocracy

app/components/sdg/goals/index_component.rb
@@ -1,9 +1,10 @@
 class SDG::Goals::IndexComponent < ApplicationComponent
-  attr_reader :goals, :phases
+  attr_reader :goals, :header, :phases
   delegate :link_list, to: :helpers
 
-  def initialize(goals, phases)
+  def initialize(goals, header:, phases:)
     @goals = goals
+    @header = header
     @phases = phases
   end
 

spec/components/sdg/goals/index_component_spec.rb
@@ -3,16 +3,29 @@ require "rails_helper"
 describe SDG::Goals::IndexComponent, type: :component do
   let!(:goals) { SDG::Goal.all }
   let!(:phases) { SDG::Phase.all }
-  let!(:component) { SDG::Goals::IndexComponent.new(goals, phases) }
+  let!(:component) { SDG::Goals::IndexComponent.new(goals, header: nil, phases: phases) }
 
   before do
     Setting["feature.sdg"] = true
   end
 
-  it "renders a heading" do
-    render_inline component
+  describe "header" do
+    it "renders the default header when a custom one is not defined" do
+      render_inline component
 
-    expect(page).to have_css "h1", exact_text: "Sustainable Development Goals"
+      expect(page).to have_css "h1", exact_text: "Sustainable Development Goals"
+    end
+
+    it "renders a custom header" do
+      sdg_web_section = WebSection.find_by!(name: "sdg")
+      header = create(:widget_card, cardable: sdg_web_section)
+      component = SDG::Goals::IndexComponent.new(goals, header: header, phases: phases)
+
+      render_inline component
+
+      expect(page).to have_content header.title
+      expect(page).not_to have_css "h1", exact_text: "Sustainable Development Goals"
+    end
   end
 
   it "renders phases" do