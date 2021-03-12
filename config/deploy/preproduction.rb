eeb3d443a6db666c34d386f36e2e2396891132a6
aayala63 <antonio.ayala@lorca.es>
2021-03-12 10:20:34 +0100

    Change initial css of Lorca

--- config/deploy/preproduction.rb
+++ config/deploy/preproduction.rb
@@ -1,4 +1,4 @@
-set :branch, ENV["branch"] || :master
+set :branch, ENV["branch"] || :stable
 
 server deploysecret(:server1), user: deploysecret(:user), roles: %w[web app db importer cron background]
 server deploysecret(:server2), user: deploysecret(:user), roles: %w[web app db importer]
