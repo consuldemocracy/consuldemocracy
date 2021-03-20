c690635b6c1c58b8df3c109e255b102fe062f5ff
Jakub Krzus <jakub.krzus@gmail.com>
2021-03-20 19:03:46 +0000

    conditions translations in seed are being generated

--- db/pages/conditions.rb
+++ db/pages/conditions.rb
@@ -1,8 +1,17 @@
-if SiteCustomization::Page.find_by(slug: "conditions").nil?
-  page = SiteCustomization::Page.new(slug: "conditions", status: "published")
-  page.print_content_flag = true
+def generate_content(page)
   page.title = I18n.t("pages.conditions.title")
   page.subtitle = I18n.t("pages.conditions.subtitle")
   page.content = "<p>#{I18n.t("pages.conditions.description")}</p>"
   page.save!
+end
+
+if SiteCustomization::Page.find_by(slug: "conditions").nil?
+  page = SiteCustomization::Page.new(slug: "conditions", status: "published")
+  page.print_content_flag = true
+  generate_content(page)
+  I18n.available_locales.each do |locale|
+    I18n.locale = locale
+    translation = page.translations.build(locale: locale)
+    generate_content(translation)
+  end
 end
