44bec30bf8d8583d2b4c0bef1da5c870df616722
Jakub Krzus <jakub.krzus@gmail.com>
2021-03-20 19:07:29 +0000

    privacy translations in seed are being generated

--- db/pages/privacy.rb
+++ db/pages/privacy.rb
@@ -1,7 +1,16 @@
-if SiteCustomization::Page.find_by(slug: "privacy").nil?
-  page = SiteCustomization::Page.new(slug: "privacy", status: "published")
-  page.print_content_flag = true
+def generate_content(page)
   page.title = I18n.t("pages.privacy.title")
   page.content = I18n.t("pages.privacy.subtitle")
   page.save!
+end
+
+if SiteCustomization::Page.find_by(slug: "privacy").nil?
+  page = SiteCustomization::Page.new(slug: "privacy", status: "published")
+  page.print_content_flag = true
+  generate_content(page)
+  I18n.available_locales.each do |locale|
+    I18n.locale = locale
+    translation = page.translations.build(locale: locale)
+    generate_content(translation)
+  end
 end
