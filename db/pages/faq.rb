2781fbac9636a14d5f2926026a3d035f3ab31e2d
Jakub Krzus <jakub.krzus@gmail.com>
2021-03-20 17:51:57 +0000

    faq translations in seed are being generated

--- db/pages/faq.rb
+++ db/pages/faq.rb
@@ -1,6 +1,14 @@
-if SiteCustomization::Page.find_by(slug: "faq").nil?
-  page = SiteCustomization::Page.new(slug: "faq", status: "published")
+def generate_content(page)
   page.title = I18n.t("pages.help.faq.page.title")
   page.content = "<p>#{I18n.t("pages.help.faq.page.description")}</p>"
   page.save!
+end
+if SiteCustomization::Page.find_by(slug: "faq").nil?
+  page = SiteCustomization::Page.new(slug: "faq", status: "published")
+  generate_content(page)
+  I18n.available_locales.each do |locale|
+    I18n.locale = locale
+    translation = page.translations.build(locale: locale)
+    generate_content(translation)
+  end
 end
