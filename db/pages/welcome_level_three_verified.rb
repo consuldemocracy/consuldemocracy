465725aa9009800728cb45a597c4a05f299dd086
Jakub Krzus <jakub.krzus@gmail.com>
2021-03-20 19:13:27 +0000

    welcome_level_three_verified translations in seed are being generated

--- db/pages/welcome_level_three_verified.rb
+++ db/pages/welcome_level_three_verified.rb
@@ -1,5 +1,4 @@
-if SiteCustomization::Page.find_by(slug: "welcome_level_three_verified").nil?
-  page = SiteCustomization::Page.new(slug: "welcome_level_three_verified", status: "published")
+def generate_content(page)
   page.title = I18n.t("welcome.welcome.title")
 
   page.content = "<p>#{I18n.t("welcome.welcome.user_permission_info")}</p>
@@ -16,4 +15,14 @@ if SiteCustomization::Page.find_by(slug: "welcome_level_three_verified").nil?
 
                   <p><a href='/'>#{I18n.t("welcome.welcome.start_using_consul")}</a></p>"
   page.save!
+end
+
+if SiteCustomization::Page.find_by(slug: "welcome_level_three_verified").nil?
+  page = SiteCustomization::Page.new(slug: "welcome_level_three_verified", status: "published")
+  generate_content(page)
+  I18n.available_locales.each do |locale|
+    I18n.locale = locale
+    translation = page.translations.build(locale: locale)
+    generate_content(translation)
+  end
 end
