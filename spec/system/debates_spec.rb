0b7014b7d72a93ffac0d8495a9fbaf1668905980
Javi Martín <javim@elretirao.net>
2021-03-27 00:51:26 +0100

    Explicitly disable JS when testing JS injection
    CKEditor already comes with JS injection protection, so potential
    attackers will most likely disable CKEditor.

--- spec/system/debates_spec.rb
+++ spec/system/debates_spec.rb
@@ -263,7 +263,7 @@ describe "Debates" do
     expect(page).to have_content error_message
   end
 
-  scenario "JS injection is prevented but safe html is respected" do
+  scenario "JS injection is prevented but safe html is respected", :no_js do
     author = create(:user)
     login_as(author)
 
@@ -297,7 +297,7 @@ describe "Debates" do
     expect(page).to have_link("www.example.org", href: "http://www.example.org")
   end
 
-  scenario "JS injection is prevented but autolinking is respected" do
+  scenario "JS injection is prevented but autolinking is respected", :no_js do
     author = create(:user)
     js_injection_string = "<script>alert('hey')</script> <a href=\"javascript:alert('surprise!')\">click me<a/> http://example.org"
     login_as(author)
