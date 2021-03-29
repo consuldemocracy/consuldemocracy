be6390cc71e4e21130491724d4881a2634f0f4e9
taitus <sebastia.roig@gmail.com>
2021-03-29 13:13:11 +0200

    Allow to create an investment with documents
    In the Management section when creating an investment we were not passing the
    document attributes, so we were never able to associate documents.
    
    Make the nested_documentable spec compatible with the Management section.

--- app/controllers/management/budgets/investments_controller.rb
+++ app/controllers/management/budgets/investments_controller.rb
@@ -1,6 +1,7 @@
 class Management::Budgets::InvestmentsController < Management::BaseController
   include Translatable
   include ImageAttributes
+  include DocumentAttributes
   include MapLocationAttributes
   include FeatureFlags
   feature_flag :budgets
@@ -61,6 +62,7 @@ class Management::Budgets::InvestmentsController < Management::BaseController
     def investment_params
       attributes = [:external_url, :heading_id, :tag_list, :organization_name, :location, :skip_map,
                     image_attributes: image_attributes,
+                    documents_attributes: document_attributes,
                     map_location_attributes: map_location_attributes]
       params.require(:budget_investment).permit(attributes, translation_params(Budget::Investment))
     end
