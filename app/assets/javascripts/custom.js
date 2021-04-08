b34f76686215dcd63bc37b88e4188c751ed52e2e
aletaeka <rayuela09>
2021-04-08 09:37:14 +0200

    Intento de modificar imagen de portada según idioma  através de javascript

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -5,3 +5,12 @@
 //
 //
 
+$(document).ready(function () {
+
+  var theLanguage = $('html').attr('lang');
+
+  if ($(theLanguage) == "en") {
+    $(".home-page .jumbo.highlight img.margin").attr("src", 'assets/images/custom/banner-home-en.png');
+  }
+
+});
