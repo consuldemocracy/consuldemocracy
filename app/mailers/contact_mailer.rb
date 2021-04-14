01cc5e28f69a6caa34a8ce31ec4af1c62673f207
alu0101037530 <alu0101037530@ull.edu.es>
2021-04-14 09:33:41 +0100

    Mail fixed

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,4 @@
 class ContactMailer < ApplicationMailer
-    default from: proc { "#{Setting["mailer_from_name"]} <#{Setting["mailer_from_address"]}>" }
-
     def contact_send(params)
         @parameters=params
         mail(to:'innconsul@grupoinnovaris.com', subject:@parameters[:subject])
