00d92b896364d1ffc4ad48649096969369af3e25
IsaacMorenosutilo <isaac.moreno@sagasoluciones.com>
2021-04-15 10:27:07 +0200

    Task 3242 Consul-idiomas

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,6 +1,6 @@
 module Consul
   class Application < Rails::Application
     config.i18n.default_locale = :es
-    config.i18n.available_locales = [:es, :en]
+    config.i18n.available_locales = [:es]
   end
 end
