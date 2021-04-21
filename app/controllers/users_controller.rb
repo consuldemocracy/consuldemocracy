744490f2e4ebf70b2299abb0e9f2a2bc5d9b5a58
Victor Paz <victorpazrdgz@gmail.com>
2021-04-21 17:52:48 +0100

    fixed load autorized

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   has_filters %w[proposals participants participants_d participants_p debates budget_investments comments follows], only: :show
 
   #load_and_authorize_resource
-  #load_and_authorize_resource :except => :edit
-  skip_load_and_authorize_resource :only => :edit
+  load_and_authorize_resource except: [:edit]
+#   skip_load_and_authorize_resource :only => :edit
   skip_authorization_check # JHH: Tener esto controlado
   helper_method :author?
   helper_method :valid_interests_access?
