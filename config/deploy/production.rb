9412e14e917f04af1222a0a319058a1017940944
saga <isaac.moreno@sagasoluciones.com>
2021-04-28 11:32:29 +0000

    Cambios mimemagic

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,3 @@
 set :branch, ENV["branch"] || :master
 
 server deploysecret(:server1), user: deploysecret(:user), roles: %w[web app db importer cron background]
-#server deploysecret(:server2), user: deploysecret(:user), roles: %w(web app db importer cron background)
-#server deploysecret(:server3), user: deploysecret(:user), roles: %w(web app db importer)
-#server deploysecret(:server4), user: deploysecret(:user), roles: %w(web app db importer)
