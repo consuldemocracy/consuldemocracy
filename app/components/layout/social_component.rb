3b00f3c1415e6f4ac0326f5984b3311856613f9f
Javi Martín <javim@elretirao.net>
2021-05-06 19:24:43 +0200

    Remove duplication in social icons in footer

--- app/components/layout/social_component.rb
+++ app/components/layout/social_component.rb
@@ -1,3 +1,15 @@
 class Layout::SocialComponent < ApplicationComponent
   delegate :content_block, to: :helpers
+
+  private
+
+    def sites
+      {
+        twitter: "https://twitter.com",
+        facebook: "https://www.facebook.com",
+        youtube: "https://www.youtube.com",
+        telegram: "https://www.telegram.me",
+        instragram: "https://www.instagram.com"
+      }
+    end
 end
