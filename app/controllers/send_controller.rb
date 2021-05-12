2688b24d1a1295f083587a5d2ea09371e3b74bc5
alu0101037530 <alu0101037530@ull.edu.es>
2021-05-12 13:04:13 +0100

    Contact form

--- app/controllers/send_controller.rb
+++ app/controllers/send_controller.rb
@@ -1,14 +1,18 @@
 class SendController < ApplicationController
   skip_authorization_check
   layout "contact"
-  
+
   def index
   end
 
   def create
-    @params=params
-    ContactMailer.contact_send(params).deliver
-    flash[:notice]= "Formulario enviado"
-    redirect_to root_path
+    if params[:terms_of_service]!="1"
+      render :index, notice: 'Debe aceptar las condiciones'
+    else
+      @params=params
+      ContactMailer.contact_send(params).deliver
+      flash[:notice]= "Formulario enviado"
+      redirect_to root_path
+    end
   end
 end
