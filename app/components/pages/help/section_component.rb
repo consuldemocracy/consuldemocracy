5f922520547af033f1be0eaa9339f43e8910ba61
Javi Martín <javim@elretirao.net>
2021-05-18 15:50:34 +0200

    Fix exception using locales with no help images
    In commit 905ac48bb we activated exceptions when assets were not found,
    in order to detect places where we were trying to load non-existent
    images.
    
    We got an exception for that reason: we were loading images based on the
    current locale, but for some locales there was no images.
    
    We're now using fallbacks and loading another image when the original
    one isn't available.
    
    Note we're copying the English images to images with a generic name for
    the case where there's no fallback with an image. We're copying the
    files instead of using symbolic links to make sure they can be
    overwritten independently in other CONSUL installations.
    
    Also note we're updating the HTML so the section gets the ID instead of
    the header. That way the system test is simple.

--- app/components/pages/help/section_component.rb
+++ app/components/pages/help/section_component.rb
@@ -1,8 +1,19 @@
 class Pages::Help::SectionComponent < ApplicationComponent
-  attr_reader :section, :image_path
+  attr_reader :section
 
-  def initialize(section, image_path = nil)
+  def initialize(section)
     @section = section
-    @image_path = image_path
+  end
+
+  def image_path
+    locale_with_image = Array(I18n.fallbacks[I18n.locale]).find do |locale|
+      AssetFinder.find_asset("help/#{section}_#{locale}.png")
+    end
+
+    if locale_with_image
+      "help/#{section}_#{locale_with_image}.png"
+    elsif AssetFinder.find_asset("help/#{section}.png")
+      "help/#{section}.png"
+    end
   end
 end
