b99ce2af455144aee766450f130418a381a04374
Javi Martín <javim@elretirao.net>
2021-06-02 01:46:50 +0200

    Remove duplication in timeline component
    This way adding new steps will be easier.

--- app/components/admin/budgets_wizard/creation_timeline_component.rb
+++ app/components/admin/budgets_wizard/creation_timeline_component.rb
@@ -1,13 +1,13 @@
 class Admin::BudgetsWizard::CreationTimelineComponent < ApplicationComponent
-  attr_reader :step
+  attr_reader :current_step
 
-  def initialize(step)
-    @step = step
+  def initialize(current_step)
+    @current_step = current_step
   end
 
   private
 
-    def step_groups?
-      step == "groups"
+    def steps
+      %w[budget groups]
     end
 end
