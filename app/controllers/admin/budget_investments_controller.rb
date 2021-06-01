ac26472fe2adc5864c8d5af9a50ea6c41527aa32
angel.delafuente <angel.delafuente@getronics.com>
2021-06-01 23:49:42 +0200

    UPDATE: SDSDAVA-494 - Vista y edición de las propuestas desde el apartado de editar en la administración

--- app/controllers/admin/budget_investments_controller.rb
+++ app/controllers/admin/budget_investments_controller.rb
@@ -2,6 +2,9 @@ class Admin::BudgetInvestmentsController < Admin::BaseController
   include FeatureFlags
   include CommentableActions
   include Translatable
+  include FlagActions
+  include RandomSeed
+  include ImageAttributes
 
   feature_flag :budgets
 
@@ -84,7 +87,10 @@ class Admin::BudgetInvestmentsController < Admin::BaseController
     def budget_investment_params
       attributes = [:external_url, :heading_id, :administrator_id, :tag_list,
                     :valuation_tag_list, :incompatible, :visible_to_valuators, :selected,
-                    :milestone_tag_list, valuator_ids: [], valuator_group_ids: []]
+                    :milestone_tag_list, :organization_name, :location, :skip_map,
+                    image_attributes: image_attributes,
+                    documents_attributes: [:id, :title, :attachment, :cached_attachment, :user_id, :_destroy],
+                    map_location_attributes: [:latitude, :longitude, :zoom], valuator_ids: [], valuator_group_ids: []]
       params.require(:budget_investment).permit(attributes, translation_params(Budget::Investment))
     end
 
