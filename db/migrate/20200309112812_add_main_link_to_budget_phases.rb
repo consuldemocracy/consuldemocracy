Ajuntament-de-Palma/consul

Add main link to each phase of the budget
Co-authored-by: decabeza <alberto@decabeza.es>

db/migrate/20200309112812_add_main_link_to_budget_phases.rb
@@ -1,0 +1,6 @@
+class AddMainLinkToBudgetPhases < ActiveRecord::Migration[5.0]
+  def change
+    add_column :budget_phases, :main_link_text, :string
+    add_column :budget_phases, :main_link_url, :string
+  end
+end

spec/system/admin/budget_phases_spec.rb
@@ -42,5 +42,18 @@ describe "Admin budget phases" do
         expect(page).not_to have_content "Accepting projects"
       end
     end
+
+    scenario "Show CTA link in public site if added" do
+      visit edit_admin_budget_budget_phase_path(budget, budget.current_phase)
+      expect(page).to have_content "Main call to action (optional)"
+
+      fill_in "Text on the link", with: "Link on the phase"
+      fill_in "The link takes you to (add a link)", with: "https://consulproject.org"
+      click_button "Save changes"
+
+      visit budgets_path
+
+      expect(page).to have_link("Link on the phase", href: "https://consulproject.org")
+    end
   end
 end

consuldemocracy/consuldemocracy

db/migrate/20200309112812_add_main_link_to_budget_phases.rb
@@ -1,0 +1,6 @@
+class AddMainLinkToBudgetPhases < ActiveRecord::Migration[5.0]
+  def change
+    add_column :budget_phase_translations, :main_link_text, :string
+    add_column :budget_phases, :main_link_url, :string
+  end
+end

spec/system/admin/budget_phases_spec.rb
@@ -55,5 +55,21 @@ describe "Admin budget phases" do
 
       expect(page).to have_content "Changes saved"
     end
+
+    scenario "shows CTA link in public site if added" do
+      visit edit_admin_budget_budget_phase_path(budget, budget.current_phase)
+
+      expect(page).to have_content "Main call to action (optional)"
+
+      fill_in "Text on the link", with: "Link on the phase"
+      fill_in "The link takes you to (add a link)", with: "https://consulproject.org"
+      click_button "Save changes"
+
+      expect(page).to have_content("Changes saved")
+
+      visit budgets_path
+
+      expect(page).to have_link("Link on the phase", href: "https://consulproject.org")
+    end
   end
 end