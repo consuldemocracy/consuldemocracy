e82977dd9c2b81db276d7c1e2aed3074e2f520ca
csc-va <csc@ava.es>
2021-06-17 15:53:23 +0200

    Menu y voto online usuarios bloqueados para solo presencial

---
+++ users_helper.rb
@@ -0,0 +1,80 @@
+module UsersHelper
+  def humanize_document_type(document_type)
+    case document_type
+    when "1"
+      t "verification.residence.new.document_type.spanish_id"
+    when "2"
+      t "verification.residence.new.document_type.passport"
+    when "3"
+      t "verification.residence.new.document_type.residence_card"
+    end
+  end
+
+  def comment_commentable_title(comment)
+    commentable = comment.commentable
+    if commentable.nil?
+      deleted_commentable_text(comment)
+    elsif commentable.hidden?
+      content_tag(:del, commentable.title) + " " +
+      content_tag(:span, "(" + deleted_commentable_text(comment) + ")", class: "small")
+    else
+      link_to(commentable.title, comment)
+    end
+  end
+
+  def deleted_commentable_text(comment)
+    case comment.commentable_type
+    when "Proposal"
+      t("users.show.deleted_proposal")
+    when "Debate"
+      t("users.show.deleted_debate")
+    when "Budget::Investment"
+      t("users.show.deleted_budget_investment")
+    else
+      t("users.show.deleted")
+    end
+  end
+
+  def current_administrator?
+    current_user&.administrator?
+  end
+
+  def current_moderator?
+    current_user&.moderator?
+  end
+
+  def current_valuator?
+    current_user&.valuator?
+  end
+
+  def current_manager?
+    current_user&.manager?
+  end
+
+  def current_poll_officer?
+    current_user&.poll_officer?
+  end
+  
+  def current_signature_sheet_officer?
+    current_user&.signature_sheet_officer?
+  end
+  
+  def current_consultant?
+    current_user&.consultant?
+  end
+
+  def show_admin_menu?(user = nil)
+    unless namespace == "officing"
+      current_administrator? || current_moderator? || current_valuator? || current_manager? ||
+        (user&.administrator?) || current_poll_officer? || current_signature_sheet_officer? || current_consultant?
+    end
+  end
+
+  def interests_title_text(user)
+    if current_user == user
+      t("account.show.public_interests_my_title_list")
+    else
+      t("account.show.public_interests_user_title_list")
+    end
+  end
+end
