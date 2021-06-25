108de86da574b7e4a93015f682b3bed0b6c67893
taitus <sebastia.roig@gmail.com>
2021-06-25 13:12:40 +0200

    Add link_url validation for cards when are header cards
    Add link_url presence validation only when link_text is provided only for header cards.
    
    In this case it makes sense to allow creating a "header card" without link_url, since
    we can show the header without link text and without link url and it still does its
    function.

--- spec/models/widget/card_spec.rb
+++ spec/models/widget/card_spec.rb
@@ -25,6 +25,30 @@ describe Widget::Card do
         expect(card).not_to be_valid
       end
     end
+
+    context "header cards" do
+      it "is valid with no link_url and no link_text" do
+        header = build(:widget_card, :header, link_text: nil, link_url: nil)
+
+        expect(header).to be_valid
+      end
+
+      it "is not valid with link_text and no link_url" do
+        header = build(:widget_card, :header, link_text: "link text", link_url: nil)
+
+        expect(header).not_to be_valid
+        expect(header.errors.count).to be 1
+        expect(header.errors[:link_url].count).to be 1
+        expect(header.errors[:link_url].first).to eq "can't be blank"
+      end
+
+      it "is valid if link_text and link_url are both provided" do
+        header = build(:widget_card, :header, link_text: "Text link",
+                                              link_url: "https://consulproject.org")
+
+        expect(header).to be_valid
+      end
+    end
   end
 
   describe "#header" do
