0750166d777a30c8b65de21818d814306e63ea6d
Javi Martín <javim@elretirao.net>
2021-06-28 14:41:33 +0200

    Extract methods in locale switcher

--- app/components/layout/locale_switcher_component.rb
+++ app/components/layout/locale_switcher_component.rb
@@ -1,3 +1,13 @@
 class Layout::LocaleSwitcherComponent < ApplicationComponent
   delegate :name_for_locale, :current_path_with_query_params, to: :helpers
+
+  def render?
+    locales.size > 1
+  end
+
+  private
+
+    def locales
+      I18n.available_locales
+    end
 end
