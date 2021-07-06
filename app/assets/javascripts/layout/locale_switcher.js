Ayuntamiento-de-Andujar/consul

Use a custom dropdown widget to switch languages
As mentioned in commit 5214d89c8, using the `change` event of a `select`
field to automatically change location causes accessibility issues to
keyboard users.

So we're implementing our own widget but with a better keyboard
experience.

This is generally a bad idea; the first rule of ARIA is "don't use
ARIA", and we're using a mix of Foundation's code and our custom code to
provide the same functionality as native select fields do.

However, it's better than submitting the form on change.

An alternative here would be to add a submit button. However, with a
submit button users would have to first select an option and then click
the button, which is an extra step. It wouldn't be that big of a deal,
but consider the scenario where users can't read the characters used in
the current language. People will see a button with unfamiliar symbols
and will have to guess that clicking the button will allow them to
change the language they've selected, which could be tricky. As far as
they know, they could be pressing a button to download an app in the
selected language.

So it's important to make it possible for users to switch to their
preferred language just by clicking on its name.

The widget we're adding could be tricky for screen reader users. When
their screen reader announces something in a language they don't
understand, will they guess this is a widget to change the language? Not
all screen readers support the roles we're using and so they might not
announce the control is a menu. In this case, a `<select>` tag at least
is universally supported and it'll be easier for users to realize
they're inside a form to change the language.

I'm also not sure whether the "menuitem" role Foundation provides is the
right role here. I didn't find an obviously better option.

Given these potential issues, we might need to improve the widget in the
future.

Note we're adding a `keydown` event handler to open the menu when the
down arr

app/assets/javascripts/layout/locale_switcher.js
@@ -1,0 +1,47 @@
+(function() {
+  "use strict";
+  App.LocaleSwitcher = {
+    initialize: function() {
+      $(".locale-switcher").each(function() {
+        var button = $(this).find("[role='menuitem']:first");
+
+        $(this).find("p").on("click", function(event) {
+          event.stopPropagation();
+          button.focus();
+        });
+
+        button.on("keydown", function(event) {
+          if (event.keyCode === 40) {
+            event.preventDefault();
+            event.stopPropagation();
+            $(this).trigger($.Event("keydown", { keyCode: 39 }));
+          }
+        });
+
+        $(this).on("show.zf.dropdownMenu", function() {
+          $(this).find("[aria-selected='true']")[0].scrollIntoView({ block: "nearest" });
+        });
+
+        $(this).find("ul ul").on("keydown", function(event) {
+          App.LocationChanger.focusElementStartingWith(String.fromCharCode(event.which));
+        });
+      });
+    },
+    focusElementStartingWith: function(character) {
+      var active_item = $(document.activeElement).parent();
+
+      var match = App.LocationChanger.firstStartingWith(active_item.nextAll(), character) ||
+        App.LocationChanger.firstStartingWith(active_item.siblings(), character);
+
+      if (match) {
+        match.querySelector("a").focus();
+      }
+    },
+    firstStartingWith: function(elements, character) {
+      return elements.filter(function() {
+        return $(this).text().trim()[0].toUpperCase() === character.toUpperCase();
+      })[0];
+    }
+  };
+}).call(this);
+

consuldemocracy/consuldemocracy

app/assets/javascripts/layout/locale_switcher.js
@@ -1,0 +1,47 @@
+(function() {
+  "use strict";
+  App.LocaleSwitcher = {
+    initialize: function() {
+      $(".locale-switcher").each(function() {
+        var button = $(this).find("[role='menuitem']:first");
+
+        $(this).find("p").on("click", function(event) {
+          event.stopPropagation();
+          button.focus();
+        });
+
+        button.on("keydown", function(event) {
+          if (event.keyCode === 40) {
+            event.preventDefault();
+            event.stopPropagation();
+            $(this).trigger($.Event("keydown", { keyCode: 39 }));
+          }
+        });
+
+        $(this).on("show.zf.dropdownMenu", function() {
+          $(this).find("[aria-current='true'] a").focus()[0].scrollIntoView({ block: "nearest" });
+        });
+
+        $(this).find("ul ul").on("keydown", function(event) {
+          App.LocaleSwitcher.focusElementStartingWith(String.fromCharCode(event.which));
+        });
+      });
+    },
+    focusElementStartingWith: function(character) {
+      var active_item = $(document.activeElement).parent();
+
+      var match = App.LocaleSwitcher.firstStartingWith(active_item.nextAll(), character) ||
+        App.LocaleSwitcher.firstStartingWith(active_item.siblings(), character);
+
+      if (match) {
+        match.querySelector("a").focus();
+      }
+    },
+    firstStartingWith: function(elements, character) {
+      return elements.filter(function() {
+        return $(this).text().trim()[0].toUpperCase() === character.toUpperCase();
+      })[0];
+    }
+  };
+}).call(this);
+