a181052f0da9ed2ba8b2767a5cf2ee957bfd9bdd
Javi Martín <javim@elretirao.net>
2021-07-11 21:25:57 +0200

    Move nested images partial to a component

---
+++ app/components/images/nested_component.rb
@@ -0,0 +1,20 @@
+class Images::NestedComponent < ApplicationComponent
+  attr_reader :f, :image_fields
+  delegate :imageable_humanized_accepted_content_types, :imageable_max_file_size, to: :helpers
+
+  def initialize(f, image_fields: :image)
+    @f = f
+    @image_fields = image_fields
+  end
+
+  private
+
+    def imageable
+      f.object
+    end
+
+    def imageables_note
+      t "images.form.note", accepted_content_types: imageable_humanized_accepted_content_types,
+        max_file_size: imageable_max_file_size
+    end
+end
--- app/helpers/imageables_helper.rb
+++ app/helpers/imageables_helper.rb
@@ -14,9 +14,4 @@ module ImageablesHelper
   def imageable_humanized_accepted_content_types
     Setting.accepted_content_types_for("images").join(", ")
   end
-
-  def imageables_note(_imageable)
-    t "images.form.note", accepted_content_types: imageable_humanized_accepted_content_types,
-                          max_file_size: imageable_max_file_size
-  end
 end
