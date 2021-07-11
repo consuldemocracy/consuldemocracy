c9113041c0b6d05f18ab511107e222692cf2966c
Javi Martín <javim@elretirao.net>
2021-07-11 21:22:11 +0200

    Move nested documents partial to a component

--- app/helpers/documentables_helper.rb
+++ app/helpers/documentables_helper.rb
@@ -1,8 +1,4 @@
 module DocumentablesHelper
-  def max_documents_allowed(documentable)
-    documentable.class.max_documents_allowed
-  end
-
   def max_file_size(documentable_class)
     documentable_class.max_file_size / Numeric::MEGABYTE
   end
@@ -13,15 +9,5 @@ module DocumentablesHelper
 
   def documentable_humanized_accepted_content_types(documentable_class)
     Setting.accepted_content_types_for("documents").join(", ")
-  end
-
-  def documentables_note(documentable)
-    t "documents.form.note", max_documents_allowed: max_documents_allowed(documentable),
-                             accepted_content_types: documentable_humanized_accepted_content_types(documentable.class),
-                             max_file_size: max_file_size(documentable.class)
-  end
-
-  def max_documents_allowed?(documentable)
-    documentable.documents.count >= documentable.class.max_documents_allowed
   end
 end
