507fa7c5f5ceb1af5b4d347ccf71f5632bfcbd50
Javi Martín <javim@elretirao.net>
2021-07-25 00:20:57 +0200

    Remove duplication in attachable fields components
    These classes were almost identical.

--- app/components/documents/fields_component.rb
+++ app/components/documents/fields_component.rb
@@ -1,6 +1,5 @@
 class Documents::FieldsComponent < ApplicationComponent
   attr_reader :f
-  delegate :current_user, to: :helpers
 
   def initialize(f)
     @f = f
@@ -10,36 +9,5 @@ class Documents::FieldsComponent < ApplicationComponent
 
     def document
       f.object
-    end
-
-    def file_name
-      document.attachment_file_name
-    end
-
-    def destroy_link
-      if !document.persisted? && document.cached_attachment.present?
-        link_to t("documents.form.delete_button"), "#", class: "delete remove-cached-attachment"
-      else
-        link_to_remove_association document.new_record? ? t("documents.form.cancel_button") : t("documents.form.delete_button"), f, class: "delete remove-document"
-      end
-    end
-
-    def file_field
-      klass = document.persisted? || document.cached_attachment.present? ? " hide" : ""
-      f.file_field :attachment,
-        label_options: { class: "button hollow #{klass}" },
-        accept: accepted_content_types_extensions,
-        class: "js-document-attachment",
-        data: { url: direct_upload_path }
-    end
-
-    def direct_upload_path
-      direct_uploads_path("direct_upload[resource_type]": document.documentable_type,
-                          "direct_upload[resource_id]": document.documentable_id,
-                          "direct_upload[resource_relation]": "documents")
-    end
-
-    def accepted_content_types_extensions
-      Setting.accepted_content_types_for("documents").map { |content_type| ".#{content_type}" }.join(",")
     end
 end
--- app/components/images/fields_component.rb
+++ app/components/images/fields_component.rb
@@ -1,52 +1,8 @@
 class Images::FieldsComponent < ApplicationComponent
   attr_reader :f, :imageable
-  delegate :current_user, :render_image, to: :helpers
 
   def initialize(f, imageable:)
     @f = f
     @imageable = imageable
   end
-
-  private
-
-    def image
-      f.object
-    end
-
-    def file_name
-      image.attachment_file_name
-    end
-
-    def destroy_link
-      if !image.persisted? && image.cached_attachment.present?
-        link_to t("images.form.delete_button"), "#", class: "delete remove-cached-attachment"
-      else
-        link_to_remove_association t("images.form.delete_button"), f, class: "delete remove-image"
-      end
-    end
-
-    def file_field
-      klass = image.persisted? || image.cached_attachment.present? ? " hide" : ""
-      f.file_field :attachment,
-        label_options: { class: "button hollow #{klass}" },
-        accept: accepted_content_types_extensions,
-        class: "js-image-attachment",
-        data: { url: direct_upload_path }
-    end
-
-    def direct_upload_path
-      direct_uploads_path("direct_upload[resource_type]": imageable.class.name,
-                          "direct_upload[resource_id]": imageable.id,
-                          "direct_upload[resource_relation]": "image")
-    end
-
-    def accepted_content_types_extensions
-      Setting.accepted_content_types_for("images").map do |content_type|
-        if content_type == "jpg"
-          ".jpg,.jpeg"
-        else
-          ".#{content_type}"
-        end
-      end.join(",")
-    end
 end
