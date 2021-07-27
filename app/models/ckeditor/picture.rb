ca7f2bc9d54f30254539e93a26681130a11394fd
Javi Martín <javim@elretirao.net>
2021-07-28 00:57:12 +0200

    Fix deleted file case in CKEditor
    We were getting an error when browsing the server if one file had been
    deleted.

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -5,10 +5,10 @@ class Ckeditor::Picture < Ckeditor::Asset
   validates :storage_data, file_content_type: { allow: /^image\/.*/ }, file_size: { less_than: 2.megabytes }
 
   def url_content
-    rails_representation_url(storage_data.variant(resize: "800>").processed, only_path: true)
+    rails_representation_url(storage_data.variant(resize: "800>"), only_path: true)
   end
 
   def url_thumb
-    rails_representation_url(storage_data.variant(resize: "118x100").processed, only_path: true)
+    rails_representation_url(storage_data.variant(resize: "118x100"), only_path: true)
   end
 end
