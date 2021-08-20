c78494c100c985a14815c52e2b79916bab574801
Javi Martín <javim@elretirao.net>
2021-08-21 00:29:07 +0200

    Extract method to get toggle selection path
    This way we simplify the view code.
    
    Since now we only use the `budget` method in one place, we're removing
    it.

--- app/components/admin/budget_investments/toggle_selection_component.rb
+++ app/components/admin/budget_investments/toggle_selection_component.rb
@@ -8,7 +8,16 @@ class Admin::BudgetInvestments::ToggleSelectionComponent < ApplicationComponent
 
   private
 
-    def budget
-      investment.budget
+    def path
+      toggle_selection_admin_budget_budget_investment_path(
+        investment.budget,
+        investment,
+        filter: params[:filter],
+        sort_by: params[:sort_by],
+        min_total_supports: params[:min_total_supports],
+        max_total_supports: params[:max_total_supports],
+        advanced_filters: params[:advanced_filters],
+        page: params[:page]
+      )
     end
 end
