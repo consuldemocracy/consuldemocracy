cf0d8258edbd6324ac6400ba4c14f24da6bd4b30
Javi Martín <javim@elretirao.net>
2021-08-21 00:49:16 +0200

    Use abilities to allow toggling investment selection
    We were checking it in the view, meaning that it was possible to toggle
    the selection by sending a custom request even when the investment
    wasn't feasible.

--- spec/models/abilities/administrator_spec.rb
+++ spec/models/abilities/administrator_spec.rb
@@ -115,6 +115,10 @@ describe Abilities::Administrator do
   it { should_not be_able_to(:admin_update, finished_investment) }
   it { should_not be_able_to(:valuate, finished_investment) }
   it { should_not be_able_to(:comment_valuation, finished_investment) }
+
+  it { should be_able_to(:toggle_selection, create(:budget_investment, :feasible, :finished)) }
+  it { should_not be_able_to(:toggle_selection, create(:budget_investment, :feasible, :open)) }
+  it { should_not be_able_to(:toggle_selection, create(:budget_investment, :unfeasible, :finished)) }
   it { should_not be_able_to(:toggle_selection, finished_investment) }
 
   it { should be_able_to(:destroy, proposal_image) }
