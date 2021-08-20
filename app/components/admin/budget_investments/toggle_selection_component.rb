95f36ed52fb0c4385852ba7578b4fe9e19a8465d
Javi Martín <javim@elretirao.net>
2021-08-21 00:44:08 +0200

    Simplify code to toggle investment selection
    This way it'll be easier to change the link/button used to toggle the
    selection.
    
    Note that the conditions in the view seem to be different because we no
    longer include the `selected?` condition when rendering the link/button.
    However, an investment can only be selected if it's feasible and its
    valuation is finished, so writing something like this would have been
    redundant:
    
    ```ruby
    can?(:toggle_selection, investment) &&
      (selected? || investment.feasible? && investment.valuation_finished?)
    ```
    
    The reason why the previous code was using the `selected?` condition was
    to check whether to render the link/button to select or to deselect an
    investment. We're now doing that in the Ruby part of the component.

--- app/components/admin/budget_investments/toggle_selection_component.rb
+++ app/components/admin/budget_investments/toggle_selection_component.rb
@@ -1,12 +1,25 @@
 class Admin::BudgetInvestments::ToggleSelectionComponent < ApplicationComponent
   attr_reader :investment
   use_helpers :can?
+  delegate :selected?, to: :investment
 
   def initialize(investment)
     @investment = investment
   end
 
   private
+
+    def text
+      if selected?
+        selected_text
+      else
+        t("admin.budget_investments.index.select")
+      end
+    end
+
+    def selected_text
+      t("admin.budget_investments.index.selected")
+    end
 
     def path
       toggle_selection_admin_budget_budget_investment_path(
@@ -19,5 +32,13 @@ class Admin::BudgetInvestments::ToggleSelectionComponent < ApplicationComponent
         advanced_filters: params[:advanced_filters],
         page: params[:page]
       )
+    end
+
+    def html_class
+      if selected?
+        "button small expanded"
+      else
+        "button small hollow expanded"
+      end
     end
 end
