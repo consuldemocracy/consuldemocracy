790170a27c842cfba6b211cccf4bebd55d0a6edf
Javi Martín <javim@elretirao.net>
2021-09-19 16:14:34 +0200

    Use keyword arguments in tag methods
    The interface of this method has changed and uses keyword arguments
    instead of a hash of options. This change will be particularly
    significant when upgrading to Ruby 3.

--- app/helpers/translatable_form_helper.rb
+++ app/helpers/translatable_form_helper.rb
@@ -32,7 +32,7 @@ module TranslatableFormHelper
 
       def fields_for_locale(locale)
         fields_for_translation(@translations[locale]) do |translations_form|
-          @template.tag.div translations_options(translations_form.object, locale) do
+          @template.tag.div **translations_options(translations_form.object, locale) do
             @template.concat translations_form.hidden_field(
               :_destroy,
               value: !@template.enabled_locale?(translations_form.object.globalized_model, locale),
