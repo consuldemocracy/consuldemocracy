Cathman1990/consul

Use a switch control to enable/disable features
We were using buttons with the "Enable" and "Disable" texts to
enable/disable settings. However, when machine learning settings were
introduced in commit 4d27bbeba, a switch control was introduced to
enable/disable them.

In order to keep the interface consistent, we're now using switch
controls in other sections where settings are enabled/disabled. We can
even use the same code in the machine learning settings as well.

We're also removing the confirmation dialog to enable/disable a setting,
since the dialog is really annoying when changing several settings and
this action can be undone immediately. The only setting which might need
a confirmation is the "Skip user verification" one; we might add it in
the future. Removing the confirmation here doesn't make things worse,
though; the "Are you sure?" confirmation dialog was also pretty useless
and users would most likely blindly accept it.

Note Capybara doesn't support finding a button by its `aria-labelledby`
atrribute. Ideally we'd write `click_button "Participatory budgeting"`
instead of `click_button "Yes"`, since from the user's point of view the
"Yes" or "No" texts aren't button labels but indicators of the status of
the setting. This makes the code a little brittle since tests would pass
even if the element referenced by `aria-labelledby` didn't exist.

spec/components/admin/settings/featured_settings_form_component_spec.rb
@@ -1,0 +1,55 @@
+require "rails_helper"
+
+describe Admin::Settings::FeaturedSettingsFormComponent do
+  let(:setting) { create(:setting, key: "feature.goodnes") }
+  let(:component) { Admin::Settings::FeaturedSettingsFormComponent.new(setting) }
+
+  it "includes an aria-labelledby attribute" do
+    render_inline component
+
+    expect(page).to have_button count: 1
+    expect(page).to have_css "button[aria-labelledby='title_setting_#{setting.id}']"
+  end
+
+  it "includes an aria-describedby attribute" do
+    render_inline component
+
+    expect(page).to have_css "button[aria-describedby='description_setting_#{setting.id}']"
+  end
+
+  describe "aria-pressed attribute" do
+    it "is true when the setting is enabled" do
+      setting.update!(value: "active")
+
+      render_inline component
+
+      expect(page).to have_css "button[aria-pressed='true']"
+    end
+
+    it "is false when the setting is disabled" do
+      setting.update!(value: "")
+
+      render_inline component
+
+      expect(page).to have_css "button[aria-pressed='false']"
+    end
+  end
+
+  describe "button text" do
+    it "reflects the status when the setting is enabled" do
+      setting.update!(value: "active")
+
+      render_inline component
+
+      expect(page).to have_button "Yes"
+    end
+
+    it "reflects the status when the setting is disabled" do
+      setting.update!(value: "")
+
+      render_inline component
+
+      expect(page).to have_button "No"
+    end
+  end
+end

consuldemocracy/consuldemocracy

spec/components/admin/settings/featured_settings_form_component_spec.rb
@@ -1,0 +1,55 @@
+require "rails_helper"
+
+describe Admin::Settings::FeaturedSettingsFormComponent do
+  let(:setting) { create(:setting, key: "feature.goodness") }
+  let(:component) { Admin::Settings::FeaturedSettingsFormComponent.new(setting) }
+
+  it "includes an aria-labelledby attribute" do
+    render_inline component
+
+    expect(page).to have_button count: 1
+    expect(page).to have_css "button[aria-labelledby='title_setting_#{setting.id}']"
+  end
+
+  it "includes an aria-describedby attribute" do
+    render_inline component
+
+    expect(page).to have_css "button[aria-describedby='description_setting_#{setting.id}']"
+  end
+
+  describe "aria-pressed attribute" do
+    it "is true when the setting is enabled" do
+      setting.update!(value: "active")
+
+      render_inline component
+
+      expect(page).to have_css "button[aria-pressed='true']"
+    end
+
+    it "is false when the setting is disabled" do
+      setting.update!(value: "")
+
+      render_inline component
+
+      expect(page).to have_css "button[aria-pressed='false']"
+    end
+  end
+
+  describe "button text" do
+    it "reflects the status when the setting is enabled" do
+      setting.update!(value: "active")
+
+      render_inline component
+
+      expect(page).to have_button "Yes"
+    end
+
+    it "reflects the status when the setting is disabled" do
+      setting.update!(value: "")
+
+      render_inline component
+
+      expect(page).to have_button "No"
+    end
+  end
+end