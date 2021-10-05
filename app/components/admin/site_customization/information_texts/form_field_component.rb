c7230e668f3fde30bc8fb21e75194b8496ccb680
Javi Martín <javim@elretirao.net>
2021-10-05 20:05:46 +0200

    Refactor method to get custom texts translations
    It was a bit confusing to return `false` in the method and then handle
    this case in the view.

--- app/components/admin/site_customization/information_texts/form_field_component.rb
+++ app/components/admin/site_customization/information_texts/form_field_component.rb
@@ -10,10 +10,16 @@ class Admin::SiteCustomization::InformationTexts::FormFieldComponent < Applicati
   private
 
     def text
+      database_text || i18n_text
+    end
+
+    def database_text
       if i18n_content.present?
-        I18nContentTranslation.find_by(i18n_content_id: i18n_content.id, locale: locale)&.value
-      else
-        false
+        i18n_content.translations.find_by(locale: locale)&.value
       end
+    end
+
+    def i18n_text
+      t(i18n_content.key, locale: locale)
     end
 end
