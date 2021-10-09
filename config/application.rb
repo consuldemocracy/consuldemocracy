366aed6434924f7c3a6ac4864f0d7d5969aae94f
Chkoun <chkoun@protonmail.com>
2021-10-09 19:40:26 +0200

    Set Default language to Arabic

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module Consul
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :ar
     available_locales = [
       "ar",
       "bg",
