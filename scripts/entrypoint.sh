e96c2b80c42f8b078ae03c7d3f8832e244758670
raoulkramer <47716558+raoulkramer@users.noreply.github.com>
2021-10-28 16:03:19 +0200

    pre-existing server.pid
    with this solution we don't need to amend the docker-compose.yml file.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -16,5 +16,12 @@ chgrp -R -h "$USER_GID" "$BUNDLE_PATH"
 /usr/bin/sudo -EH -u consul "$@"
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /var/www/consul/tmp/pids/server.pid
+set -e
+
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
 
