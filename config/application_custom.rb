2ba482a39627696db5ffa93499923fa8b56d6598
Chkoun <chkoun@protonmail.com>
2021-11-05 01:39:35 +0100

    Default languages and Fallbacks
    Berber to be added once the translation is completed

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,11 @@
 module Consul
   class Application < Rails::Application
+    config.i18n.default_locale = :ar
+    config.i18n.available_locales = [:ar, :en, :fr]
+    config.i18n.fallbacks = {
+      "ar"    => "fr",
+      "fr" => "en",
+      "en" => "fr"
+    }
   end
 end
