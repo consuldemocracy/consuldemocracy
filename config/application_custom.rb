8123943f94e65a40bca26987de0c9e62e6a201b4
Chkoun <chkoun@protonmail.com>
2021-11-05 15:37:34 +0100

    Default languages and fallbacks

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,11 @@
 module Consul
   class Application < Rails::Application
+    config.i18n.default_locale = :ar
+    config.i18n.available_locales = [:ar, :fr, :en]
+    config.i18n.fallbacks = {
+      "ar"    => "fr",
+      "fr" => "ar",
+      "en" => "fr"
+    }
   end
 end
