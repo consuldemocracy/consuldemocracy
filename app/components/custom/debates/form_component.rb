0859292ba54f766d02070408942f99e698a56a5d
Chkoun <chkoun@protonmail.com>
2021-11-07 14:12:22 +0100

    Start a debate layout

---
+++ app/components/custom/debates/form_component.rb
@@ -0,0 +1,5 @@
+class Debates::FormComponent < ApplicationComponent; end
+require_dependency Rails.root.join("app", "components", "debates", "form_component").to_s
+class Debates::FormComponent
+  # Your custom logic here
+end
---
+++ app/components/custom/debates/new_component.rb
@@ -0,0 +1,5 @@
+class Debates::NewComponent < ApplicationComponent; end
+require_dependency Rails.root.join("app", "components", "debates", "new_component").to_s
+class Debates::NewComponent
+  # Your custom logic here
+end
