chkoun/consul

Add setting to allow remove investments supports

app/models/setting.rb
@@ -102,6 +102,7 @@ class Setting < ApplicationRecord
         "feature.graphql_api": true,
         "feature.sdg": true,
         "feature.machine_learning": false,
+        "feature.allow_remove_investments_supports": false,
         "homepage.widgets.feeds.debates": true,
         "homepage.widgets.feeds.processes": true,
         "homepage.widgets.feeds.proposals": true,

spec/components/budgets/investments/votes_component_spec.rb
@@ -2,6 +2,10 @@ require "rails_helper"
 
 describe Budgets::Investments::VotesComponent do
   describe "vote link" do
+    before do
+      Setting["feature.allow_remove_investments_supports"] = false
+    end
+
     context "when investment shows votes" do
       let(:investment) { create(:budget_investment, title: "Renovate sidewalks in Main Street") }
       let(:component) { Budgets::Investments::VotesComponent.new(investment) }
@@ -25,7 +29,20 @@ describe Budgets::Investments::VotesComponent do
         expect(page).to have_button "Support", disabled: true
       end
 
-      it "shows the button to remove support when users have supported the investment" do
+      it "does not shows the button to remove support when setting is disabled" do
+        user = create(:user)
+        user.up_votes(investment)
+        sign_in(user)
+
+        render_inline component
+
+        expect(page).not_to have_button count: 1, disabled: :all
+        expect(page).not_to have_button "Remove your support"
+        expect(page).not_to have_button "Remove your support to Renovate sidewalks in Main Street"
+      end
+
+      it "shows the button to remove support when users when setting is enabled" do
+        Setting["feature.allow_remove_investments_supports"] = true
         user = create(:user)
         user.up_votes(investment)
         sign_in(user)

spec/system/budgets/budgets_spec.rb
@@ -404,6 +404,7 @@ describe "Budgets" do
     end
 
     scenario "Show supports only if the support has not been removed" do
+      Setting["feature.allow_remove_investments_supports"] = true
       voter = create(:user, :level_two)
       budget = create(:budget, phase: "selecting")
       investment = create(:budget_investment, :selected, budget: budget)

spec/system/budgets/investments_spec.rb
@@ -1230,6 +1230,7 @@ describe "Budget Investments" do
     end
 
     scenario "Remove a support from show view" do
+      Setting["feature.allow_remove_investments_supports"] = true
       investment = create(:budget_investment, budget: budget)
 
       login_as(author)
@@ -1251,6 +1252,7 @@ describe "Budget Investments" do
     end
 
     scenario "Remove a support from index view" do
+      Setting["feature.allow_remove_investments_supports"] = true
       investment = create(:budget_investment, budget: budget)
 
       login_as(author)

spec/system/management/budget_investments_spec.rb
@@ -334,6 +334,7 @@ describe "Budget Investments" do
     end
 
     scenario "Remove support on behalf of someone else in index view" do
+      Setting["feature.allow_remove_investments_supports"] = true
       create(:budget_investment, heading: heading)
 
       login_managed_user(user)
@@ -354,6 +355,7 @@ describe "Budget Investments" do
     end
 
     scenario "Remove support on behalf of someone else in show view" do
+      Setting["feature.allow_remove_investments_supports"] = true
       create(:budget_investment, heading: heading, title: "Don't support me!")
 
       login_managed_user(user)

consuldemocracy/consuldemocracy

app/models/setting.rb
@@ -102,6 +102,7 @@ class Setting < ApplicationRecord
         "feature.graphql_api": true,
         "feature.sdg": true,
         "feature.machine_learning": false,
+        "feature.remove_investments_supports": false,
         "homepage.widgets.feeds.debates": true,
         "homepage.widgets.feeds.processes": true,
         "homepage.widgets.feeds.proposals": true,

spec/components/budgets/investments/votes_component_spec.rb
@@ -25,16 +25,31 @@ describe Budgets::Investments::VotesComponent do
         expect(page).to have_button "Support", disabled: true
       end
 
-      it "shows the button to remove support when users have supported the investment" do
-        user = create(:user)
-        user.up_votes(investment)
-        sign_in(user)
+      describe "button to remove support" do
+        let(:user) { create(:user) }
 
-        render_inline component
+        before do
+          user.up_votes(investment)
+          sign_in(user)
+        end
 
-        expect(page).to have_button count: 1, disabled: :all
-        expect(page).to have_button "Remove your support"
-        expect(page).to have_button "Remove your support to Renovate sidewalks in Main Street"
+        it "is shown when the setting is enabled" do
+          Setting["feature.remove_investments_supports"] = true
+
+          render_inline component
+
+          expect(page).to have_button count: 1, disabled: :all
+          expect(page).to have_button "Remove your support"
+          expect(page).to have_button "Remove your support to Renovate sidewalks in Main Street"
+        end
+
+        it "is not shown when the setting is disabled" do
+          Setting["feature.remove_investments_supports"] = false
+
+          render_inline component
+
+          expect(page).not_to have_button disabled: :all
+        end
       end
     end
   end

spec/system/budgets/budgets_spec.rb
@@ -404,6 +404,7 @@ describe "Budgets" do
     end
 
     scenario "Show supports only if the support has not been removed" do
+      Setting["feature.remove_investments_supports"] = true
       voter = create(:user, :level_two)
       budget = create(:budget, phase: "selecting")
       investment = create(:budget_investment, :selected, budget: budget)

spec/system/budgets/investments_spec.rb
@@ -1230,6 +1230,7 @@ describe "Budget Investments" do
     end
 
     scenario "Remove a support from show view" do
+      Setting["feature.remove_investments_supports"] = true
       investment = create(:budget_investment, budget: budget)
 
       login_as(author)
@@ -1251,6 +1252,7 @@ describe "Budget Investments" do
     end
 
     scenario "Remove a support from index view" do
+      Setting["feature.remove_investments_supports"] = true
       investment = create(:budget_investment, budget: budget)
 
       login_as(author)

spec/system/management/budget_investments_spec.rb
@@ -334,6 +334,7 @@ describe "Budget Investments" do
     end
 
     scenario "Remove support on behalf of someone else in index view" do
+      Setting["feature.remove_investments_supports"] = true
       create(:budget_investment, heading: heading)
 
       login_managed_user(user)
@@ -354,6 +355,7 @@ describe "Budget Investments" do
     end
 
     scenario "Remove support on behalf of someone else in show view" do
+      Setting["feature.remove_investments_supports"] = true
       create(:budget_investment, heading: heading, title: "Don't support me!")
 
       login_managed_user(user)