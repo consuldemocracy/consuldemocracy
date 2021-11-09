60c00291d31ae0fd2fb3503c24482f8cffe49145
Chkoun <chkoun@protonmail.com>
2021-11-09 15:59:20 +0100

    Account Activity V1

---
+++ app/components/custom/users/public_activity_component.rb
@@ -0,0 +1,5 @@
+class Users::PublicActivityComponent < ApplicationComponent; end
+require_dependency Rails.root.join("app", "components", "users", "public_activity_component").to_s
+class Users::PublicActivityComponent
+  # Your custom logic here
+end
