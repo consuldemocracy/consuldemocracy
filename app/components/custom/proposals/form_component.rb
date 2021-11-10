7cf698ae556e2fc12ea38993d2971c95f768159d
Chkoun <chkoun@protonmail.com>
2021-11-10 17:05:38 +0100

    New proposal layout

---
+++ app/components/custom/proposals/form_component.rb
@@ -0,0 +1,5 @@
+class Proposals::FormComponent < ApplicationComponent; end
+require_dependency Rails.root.join("app", "components", "proposals", "form_component").to_s
+class Proposals::FormComponent
+  # Your custom logic here
+end
---
+++ app/components/custom/proposals/new_component.rb
@@ -0,0 +1,5 @@
+class Proposals::NewComponent < ApplicationComponent; end
+require_dependency Rails.root.join("app", "components", "proposals", "new_component").to_s
+class Proposals::NewComponent
+  # Your custom logic here
+end
