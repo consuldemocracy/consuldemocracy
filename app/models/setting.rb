a4cdee54e34d4c970ee6448dadb88d75338f2f44
Chkoun <chkoun@protonmail.com>
2021-11-15 07:02:47 +0100

    Update setting.rb

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -132,7 +132,7 @@ class Setting < ApplicationRecord
         "uploads.images.min_width": 0,
         "uploads.images.min_height": 475,
         "uploads.images.max_size": 1,
-        "uploads.images.content_types": "image/png",
+        "uploads.images.content_types": "image/jpeg",
         "uploads.documents.max_amount": 3,
         "uploads.documents.max_size": 3,
         "uploads.documents.content_types": "application/pdf",
