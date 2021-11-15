5669aa0b2a3152239a5299ea4dfcefdd86da647a
Chkoun <chkoun@protonmail.com>
2021-11-15 05:59:17 +0100

    Homepage Layout v2

---
+++ app/components/custom/widget/feeds/participation_component.rb
@@ -0,0 +1,5 @@
+class Widget::Feeds::ParticipationComponent < ApplicationComponent; end
+require_dependency Rails.root.join("app", "components", "widget", "feeds", "participation_component").to_s
+class Widget::Feeds::ParticipationComponent
+  # Your custom logic here
+end
