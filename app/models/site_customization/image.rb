e428b1710ed6af52868c7e56d472bc9023837978
Chkoun <chkoun@protonmail.com>
2021-11-16 05:11:28 +0100

    png

--- app/models/site_customization/image.rb
+++ app/models/site_customization/image.rb
@@ -14,7 +14,7 @@ class SiteCustomization::Image < ApplicationRecord
   has_attachment :image
 
   validates :name, presence: true, uniqueness: true, inclusion: { in: VALID_IMAGES.keys }
-  validates_attachment_content_type :image, content_type: ["image/png"]
+  validates_attachment_content_type :image, content_type: ["image/png", "image/jpeg"]
   validate :check_image
 
   def self.all_images
