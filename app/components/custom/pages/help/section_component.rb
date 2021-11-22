f299f72b8ee5e1c91c97ee5b800eb7640967d1c2
Chkoun <chkoun@protonmail.com>
2021-11-22 18:29:15 +0100

    Help Page v1

---
+++ app/components/custom/pages/help/section_component.rb
@@ -0,0 +1,5 @@
+class Pages::Help::SectionComponent < ApplicationComponent; end
+require_dependency Rails.root.join("app", "components", "pages", "help", "section_component").to_s
+class Pages::Help::SectionComponent
+  # Your custom logic here
+end
