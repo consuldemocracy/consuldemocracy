826ab55dff044ed6929a2dffebfcda5e2dd07d66
Finn Heemeyer <fh@tpwd.de>
2022-01-13 21:27:26 +0100

    Add castVoteOnComment mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,6 @@ module Types
     field :add_comment_to_poll, mutation: Mutations::AddCommentToPoll, authenticate: true
     field :add_comment_to_debate, mutation: Mutations::AddCommentToDebate, authenticate: true
     field :start_debate, mutation: Mutations::StartDebate, authenticate: true
+    field :cast_vote_on_comment, mutation: Mutations::CastVoteOnComment, authenticate: true
   end
 end
