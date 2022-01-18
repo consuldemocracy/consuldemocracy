98d2998ba006b718bc4d66e66543070e39530927
Finn Heemeyer <fh@tpwd.de>
2022-01-18 13:23:52 +0100

    Sort mutations in MutattionType

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,10 @@
 module Types
   class MutationType < Types::BaseObject
-    field :provide_answer_to_poll_question, mutation: Mutations::ProvideAnswerToPollQuestion, authenticate: true
-    field :add_comment_to_poll, mutation: Mutations::AddCommentToPoll, authenticate: true
     field :add_comment_to_debate, mutation: Mutations::AddCommentToDebate, authenticate: true
-    field :start_debate, mutation: Mutations::StartDebate, authenticate: true
+    field :add_comment_to_poll, mutation: Mutations::AddCommentToPoll, authenticate: true
     field :cast_vote_on_comment, mutation: Mutations::CastVoteOnComment, authenticate: true
+    field :provide_answer_to_poll_question, mutation: Mutations::ProvideAnswerToPollQuestion, authenticate: true
+    field :start_debate, mutation: Mutations::StartDebate, authenticate: true
     field :submit_proposal, mutation: Mutations::SubmitProposal, authenticate: true
     field :support_proposal, mutation: Mutations::SupportProposal, authenticate: true
   end
