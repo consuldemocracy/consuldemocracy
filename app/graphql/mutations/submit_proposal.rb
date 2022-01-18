b46b6655cb3a2ee119c2274639dbac8d86e0d350
Finn Heemeyer <fh@tpwd.de>
2022-01-18 10:55:26 +0100

    Finish submitProposal mutation

--- app/graphql/mutations/submit_proposal.rb
+++ app/graphql/mutations/submit_proposal.rb
@@ -1,40 +1,47 @@
-# module Mutations
-#   class SubmitProposal < BaseMutation
-#     argument :title, String, required: true, validates: { allow_blank: false }
-#     argument :description, String, required: true, validates: { allow_blank: false }
-#     argument :tag_list, String, required: false
-#     argument :terms_of_service, Boolean, required: true
+module Mutations
+  class SubmitProposal < BaseMutation
+    argument :title, String, required: true, validates: { allow_blank: false }
+    argument :description, String, required: true, validates: { allow_blank: false }
+    argument :summary, String, required: true, validates: {
+      length: { maximum: 200 },
+      allow_blank: false
+    }
+    argument :terms_of_service, Boolean, required: true
 
-#     # TODO: Add file uploads
+    argument :geozone_id, ID, required: false
+    argument :video_url, String, required: false
+    argument :tag_list, String, required: false
 
-#     type Types::ProposalType
+    # TODO: Add file uploads
 
-#     def resolve(title:, description:, summary:, terms_of_service:, geozone_id: nil, video_url: nil, tag_list: nil)
-#       begin
-#         Proposal.create!({
-#           video_url: video_url,
-#           geozone_id: geozone_id,
-#           # TODO: Add map feature
-#           # map_location_attributes: {
-#           #   latitude: "",
-#           #   longitude: "",
-#           #   zoom:""
-#           # },
-#           translations_attributes: {
-#             '0': {
-#               locale: context[:current_resource].locale,
-#               title: title,
-#               description: description,
-#               summary: summary
-#             }
-#           },
-#           author: context[:current_resource],
-#           tag_list: tag_list,
-#           terms_of_service: terms_of_service
-#         })
-#       rescue ActiveRecord::RecordInvalid => e
-#         raise GraphQL::ExecutionError, "#{e.message}"
-#       end
-#     end
-#   end
-# end
+    type Types::ProposalType
+
+    def resolve(title:, description:, summary:, terms_of_service:, geozone_id: nil, video_url: nil, tag_list: nil)
+      begin
+        Proposal.create!({
+          # TODO: Add map feature
+          # map_location_attributes: {
+          #   latitude: "",
+          #   longitude: "",
+          #   zoom:""
+          # },
+          translations_attributes: {
+            '0': {
+              locale: context[:current_resource].locale,
+              title: title,
+              description: description,
+              summary: summary
+            }
+          },
+          terms_of_service: terms_of_service,
+          geozone_id: geozone_id,
+          tag_list: tag_list,
+          video_url: video_url,
+          author: context[:current_resource]
+        })
+      rescue ActiveRecord::RecordInvalid => e
+        raise GraphQL::ExecutionError, e.message
+      end
+    end
+  end
+end
