37a6b5f01cebe782d799d5d48fe5ca7acfcd9b55
Finn Heemeyer <fh@tpwd.de>
2022-01-20 17:41:11 +0100

    Remove comment on startPollForProposal mutation

--- app/graphql/mutations/start_poll_for_proposal.rb
+++ app/graphql/mutations/start_poll_for_proposal.rb
@@ -23,31 +23,6 @@ module Mutations
 
         poll.save!
         poll
-        # poll = Poll.new({
-        #   name: attributes
-        #   starts_at: "19/01/2022",
-        #   ends_at: "24/01/2022",
-        #   description: "We need your opinons, m8tis and m8tiños",
-        #   questions_attributes: {
-        #     :"0" => {
-        #       author_id: "50",
-        #       proposal_id: "91",
-        #       title: "What the fuck?",
-        #       question_answers_attributes: {
-        #         :"0" => {
-        #           given_order: "2",
-        #           title: "Yeah man",
-        #           description: "Yes bro, indeed"
-        #         },
-        #         :"1" => {
-        #           given_order: "3",
-        #           title: "No, bre",
-        #           description: "Nah nah brataniño"
-        #         }
-        #       }
-        #     }
-        #   }
-        # }
       rescue ActiveRecord::RecordInvalid => e
         raise GraphQL::ExecutionError, e.message
       end
