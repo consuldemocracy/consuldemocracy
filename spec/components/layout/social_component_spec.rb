consuldemocracy/consuldemocracy

commit 3b00f3c1415e6f4ac0326f5984b3311856613f9f

Remove duplication in social icons in footer

app/components/layout/social_component.rb
@@ -1,3 +1,15 @@
 class Layout::SocialComponent < ApplicationComponent
   delegate :content_block, to: :helpers
+
+  private
+
+    def sites
+      {
+        twitter: "https://twitter.com",
+        facebook: "https://www.facebook.com",
+        youtube: "https://www.youtube.com",
+        telegram: "https://www.telegram.me",
+        instragram: "https://www.instagram.com"
+      }
+    end
 end

app/components/layout/social_component.html.erb
@@ -1,51 +1,18 @@
 <div class="small-12 medium-4 column social">
   <div class="text-right">
     <ul>
-      <% if setting["twitter_handle"] %>
-        <li>
-          <%= link_to "https://twitter.com/#{setting["twitter_handle"]}", target: "_blank",
-                       title: t("shared.go_to_page") + t("social.twitter", org: setting["org_name"]) + t("shared.target_blank") do %>
-                          <span class="show-for-sr"><%= t("social.twitter", org: setting["org_name"]) %></span>
-                          <span class="icon-twitter" aria-hidden="true"></span>
-          <% end %>
-        </li>
+      <% sites.each do |name, url| %>
+        <% if setting["#{name}_handle"] %>
+          <li>
+            <%= link_to "#{url}/#{setting["#{name}_handle"]}", target: "_blank",
+              title: t("shared.go_to_page") + t("social.#{name}", org: setting["org_name"]) + t("shared.target_blank") do %>
+              <span class="show-for-sr"><%= t("social.#{name}", org: setting["org_name"]) %></span>
+              <span class="icon-<%= name %>" aria-hidden="true"></span>
+            <% end %>
+          </li>
+        <% end %>
       <% end %>
-      <% if setting["facebook_handle"] %>
-        <li>
-          <%= link_to "https://www.facebook.com/#{setting["facebook_handle"]}/", target: "_blank",
-                      title: t("shared.go_to_page") + t("social.facebook", org: setting["org_name"]) + t("shared.target_blank") do %>
-                      <span class="show-for-sr"><%= t("social.facebook", org: setting["org_name"]) %></span>
-                      <span class="icon-facebook" aria-hidden="true"></span>
-          <% end %>
-        </li>
-      <% end %>
-      <% if setting["youtube_handle"] %>
-        <li>
-          <%= link_to "https://www.youtube.com/#{setting["youtube_handle"]}", target: "_blank",
-                      title: t("shared.go_to_page") + t("social.youtube", org: setting["org_name"]) + t("shared.target_blank") do %>
-                      <span class="show-for-sr"><%= t("social.youtube", org: setting["org_name"]) %></span>
-                      <span class="icon-youtube" aria-hidden="true"></span>
-          <% end %>
-        </li>
-      <% end %>
-      <% if setting["telegram_handle"] %>
-        <li>
-          <%= link_to "https://www.telegram.me/#{setting["telegram_handle"]}", target: "_blank",
-                      title: t("shared.go_to_page") + t("social.telegram", org: setting["org_name"]) + t("shared.target_blank") do %>
-                      <span class="show-for-sr"><%= t("social.telegram", org: setting["org_name"]) %></span>
-                      <span class="icon-telegram" aria-hidden="true"></span>
-          <% end %>
-        </li>
-      <% end %>
-      <% if setting["instagram_handle"] %>
-        <li>
-          <%= link_to "https://www.instagram.com/#{setting["instagram_handle"]}", target: "_blank",
-                      title: t("shared.go_to_page") + t("social.instagram", org: setting["org_name"]) + t("shared.target_blank") do %>
-                      <span class="show-for-sr"><%= t("social.instagram", org: setting["org_name"]) %></span>
-                      <span class="icon-instagram" aria-hidden="true"></span>
-          <% end %>
-        </li>
-      <% end %>
+
       <%= raw content_block("footer", I18n.locale) %>
     </ul>
   </div>

commit 291b4780502a1b993722e899c24683be55a405eb

Fix typo for "instagram"
This typo was accidentally introduced in commit 3b00f3c141.

app/components/layout/social_component.rb
@@ -13,7 +13,7 @@ class Layout::SocialComponent < ApplicationComponent
         facebook: "https://www.facebook.com",
         youtube: "https://www.youtube.com",
         telegram: "https://www.telegram.me",
-        instragram: "https://www.instagram.com"
+        instagram: "https://www.instagram.com"
       }.select { |name, _| setting["#{name}_handle"].present? }
     end
 

spec/components/layout/social_component_spec.rb
@@ -3,11 +3,21 @@ require "rails_helper"
 describe Layout::SocialComponent do
   describe "#render?" do
     it "renders when a social setting is present" do
-      Setting["twitter_handle"] = "myhandle"
+      Setting["twitter_handle"] = "my_twitter_handle"
+      Setting["facebook_handle"] = "my_facebook_handle"
+      Setting["youtube_handle"] = "my_youtube_handle"
+      Setting["telegram_handle"] = "my_telegram_handle"
+      Setting["instagram_handle"] = "my_instagram_handle"
+      Setting["org_name"] = "CONSUL"
 
       render_inline Layout::SocialComponent.new
 
       expect(page).to have_css "ul"
+      expect(page).to have_link "CONSUL Twitter", href: "https://twitter.com/my_twitter_handle"
+      expect(page).to have_link "CONSUL Facebook", href: "https://www.facebook.com/my_facebook_handle"
+      expect(page).to have_link "CONSUL YouTube", href: "https://www.youtube.com/my_youtube_handle"
+      expect(page).to have_link "CONSUL Telegram", href: "https://www.telegram.me/my_telegram_handle"
+      expect(page).to have_link "CONSUL Instagram", href: "https://www.instagram.com/my_instagram_handle"
     end
 
     it "renders when a content block is present" do