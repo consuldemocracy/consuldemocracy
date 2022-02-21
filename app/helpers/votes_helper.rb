javierm/consul

Remove votes query optimizations
Just like we did in commit 0214184b2d for investments, we're removing
some possible optimizations (we don't have any benchmarks proving they
affect performance at all) in order to simplify the code.

The investement votes component `delegate` code was accidentally left
but isn't used since commit 0214184b2, so we're removing it now that
we're removing the `voted_for?` helper method.

app/helpers/votes_helper.rb
@@ -13,8 +13,8 @@ module VotesHelper
     end
   end
 
-  def css_classes_for_vote(votes, votable)
-    case votes[votable.id]
+  def css_classes_for_vote(votable)
+    case current_user&.voted_as_when_voting_on(votable)
     when true
       { in_favor: "voted", against: "no-voted" }
     when false
@@ -22,9 +22,5 @@ module VotesHelper
     else
       { in_favor: "", against: "" }
     end
-  end
-
-  def voted_for?(votes, votable)
-    votes[votable.id]
   end
 end

consuldemocracy/consuldemocracy

app/helpers/votes_helper.rb
@@ -13,8 +13,8 @@ module VotesHelper
     end
   end
 
-  def css_classes_for_vote(votes, votable)
-    case votes[votable.id]
+  def css_classes_for_vote(votable)
+    case current_user&.voted_as_when_voted_for(votable)
     when true
       { in_favor: "voted", against: "no-voted" }
     when false
@@ -22,9 +22,5 @@ module VotesHelper
     else
       { in_favor: "", against: "" }
     end
-  end
-
-  def voted_for?(votes, votable)
-    votes[votable.id]
   end
 end