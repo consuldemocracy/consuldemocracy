69b552dc4c035922db89a1e81e84bb166610c011
Julian Herrero <microweb10@gmail.com>
2022-03-09 12:22:43 +0700

    Configure Errbit

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -5,30 +5,15 @@ Airbrake.configure do |config|
 
   config.environment = Rails.env
   config.ignore_environments = %w[development test]
-
-  if config.host.blank? || config.project_id.blank? || config.project_key.blank?
-    config.ignore_environments += [Rails.env]
-  end
 end
 
 Airbrake.add_filter do |notice|
-  ignorables = %w[ActiveRecord::RecordNotFound]
+  ignorables = [
+    "ActiveRecord::RecordNotFound",
+    "ActionController::RoutingError",
+    "FeatureFlags::FeatureDisabled",
+    "AbstractController::ActionNotFound",
+    "SignalException"
+  ]
   notice.ignore! if ignorables.include? notice[:errors].first[:type]
-end
-
-if Rails.application.secrets.errbit_self_hosted_ssl.present?
-  # Patch from: https://mensfeld.pl/2016/05/setting-up-errbit-reporter-airbrake-v5-gem-to-work-with-self-signed-https-certificate/
-  module Patches
-    module Airbrake
-      module SyncSender
-        def build_https(uri)
-          super.tap do |req|
-            req.verify_mode = OpenSSL::SSL::VERIFY_NONE
-          end
-        end
-      end
-    end
-  end
-
-  Airbrake::SyncSender.prepend(::Patches::Airbrake::SyncSender)
 end
