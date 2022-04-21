5e121b346a0e32b081050e065278cd7d8af8b487
patricijab <patricija.brecko@gmail.com>
2022-04-21 10:33:22 +0200

    create past budgets list for welcome page

--- app/controllers/custom/welcome_controller.rb
+++ app/controllers/custom/welcome_controller.rb
@@ -3,11 +3,15 @@ require_dependency Rails.root.join("app", "controllers", "welcome_controller").t
 class WelcomeController < ApplicationController
   skip_authorization_check
 
-  before_action :load_budgets, only: [:index]
+  before_action :load_budgets, :load_past_budgets, only: [:index]
 
   layout "devise", only: [:verification]
 
   def load_budgets
     @budgets = Budget.where("id > -1");
+  end
+
+  def load_past_budgets 
+    @past_budgets = Budget.finished
   end
 end
