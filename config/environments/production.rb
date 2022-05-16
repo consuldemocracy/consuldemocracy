e3f28e2460f0ceaa84333ac5455fadd91be549e4
Filip Muki Dobranić <filipdobranic@gmail.com>
2022-05-16 13:24:31 +0200

    implement s3 storage for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,4 +98,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # store files on s3
+  config.active_storage.service = :s3
 end
