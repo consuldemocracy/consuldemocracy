5382b84addeeefd026ac3b06712b91bf2545a8c5
patricijab <patricija.brecko@gmail.com>
2022-05-20 17:31:25 +0200

    hacky bugfix - edited images_helper

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -9,9 +9,10 @@ module ImagesHelper
     image.persisted? ? "persisted-image" : "cached-image"
   end
 
-  def render_image(image, version, show_caption = true)
+  def render_image(image, version, show_caption = true, open_in_new_tab = false)
     render "images/image", image: image,
                            version: (version if image.persisted?),
-                           show_caption: show_caption
+                           show_caption: show_caption,
+                           open_in_new_tab: open_in_new_tab
   end
 end
