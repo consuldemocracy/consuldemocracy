consuldemocracy/consuldemocracy

commit e76735031420fef1cc60db9176e7ecd8ba27f43e

Fixed bug when creating admin tags
Minor fix

Implemented specs for tags_controller and tag model

Code review

app/controllers/admin/tags_controller.rb
@@ -1,17 +1,23 @@
 class Admin::TagsController < Admin::BaseController
   before_action :find_tag, only: [:update, :destroy]
+  before_action :tags, only: [:index, :create]
 
   respond_to :html, :js
 
   def index
-    @tags = Tag.category.page(params[:page])
-    @tag  = Tag.category.new
+    @tag = Tag.category.new
   end
 
   def create
-    Tag.find_or_create_by!(name: tag_params["name"]).update!(kind: "category")
+    @tag = Tag.find_or_initialize_by(tag_params)
 
-    redirect_to admin_tags_path
+    save_and_update = @tag.save && @tag.update!(kind: "category")
+
+    if save_and_update
+      redirect_to admin_tags_path
+    else
+      render :index
+    end
   end
 
   def destroy
@@ -20,6 +26,9 @@ class Admin::TagsController < Admin::BaseController
   end
 
   private
+    def tags
+      @tags ||= Tag.category.page(params[:page])
+    end
 
     def tag_params
       params.require(:tag).permit(:name)

spec/system/admin/tags_spec.rb
@@ -105,4 +105,15 @@ describe "Admin tags", :admin do
 
     expect(page).to have_content "Soon a category"
   end
+
+  scenario "Create shows validation error when tag name is empty" do
+    visit admin_tags_path
+
+    within("form.new_tag") do
+      fill_in "tag_name", with: ""
+      click_button "Create topic"
+    end
+
+    expect(page).to have_content "can't be blank"
+  end
 end

commit f5de0a6102616cf0669a3040e6ca5acc0b2bf332

Add empty line after access modifier
Rubocop was complaining about Layout/EmptyLinesAroundAccessModifier in
the tags controller.

This issue was introduced in commit e76735031. Unfortunately, it looks
like Pronto doesn't detect this issue because the access modifier was
already there; only the lines below it were introduced in that pull
request.

app/controllers/admin/tags_controller.rb
@@ -26,6 +26,7 @@ class Admin::TagsController < Admin::BaseController
   end
 
   private
+
     def tags
       @tags ||= Tag.category.page(params[:page])
     end