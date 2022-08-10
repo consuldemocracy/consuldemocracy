0c629776685a70c5b9ad2da66dee39ee1a0991c4
Javi Martín <javim@elretirao.net>
2022-08-11 01:43:45 +0200

    Extract method to define permissions
    This way we simplify the ERB code.
    
    Due to the bug mentioned in the previous commit, we're keeping the
    original code instead of using `can?` to check permissions.

--- app/components/account/permissions_list_component.rb
+++ app/components/account/permissions_list_component.rb
@@ -4,4 +4,15 @@ class Account::PermissionsListComponent < ApplicationComponent
   def initialize(user)
     @user = user
   end
+
+  private
+
+    def permissions
+      {
+        t("verification.user_permission_debates") => true,
+        t("verification.user_permission_proposal") => true,
+        t("verification.user_permission_support_proposal") => user.level_two_or_three_verified?,
+        t("verification.user_permission_votes") => user.level_three_verified?
+      }
+    end
 end
