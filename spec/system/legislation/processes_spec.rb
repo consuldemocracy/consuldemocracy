9a0aabb6b59da37280137f963b41c0e3400f871d
decabeza <alberto@decabeza.es>
2022-08-24 18:58:19 +0200

    Add links to legislation processes dates

--- spec/system/legislation/processes_spec.rb
+++ spec/system/legislation/processes_spec.rb
@@ -142,6 +142,41 @@ describe "Legislation" do
       end
     end
 
+    scenario "Participation phases have a link to the corresponding phase" do
+      travel_to "01/06/2020".to_date
+      process = create(:legislation_process, debate_start_date: "01/05/2020",
+                       debate_end_date: "30/05/2020",
+                       proposals_phase_start_date: "01/06/2020",
+                       proposals_phase_end_date: "30/06/2020",
+                       draft_publication_date: "20/05/2020",
+                       allegations_start_date: "01/06/2020",
+                       allegations_end_date: "05/06/2020",
+                       result_publication_date: "01/07/2020")
+
+      phase_debate = ["Debate (0)", "01 May 2020 - 30 May 2020", "LOCKED"].join("\n")
+      phase_draft = ["Draft publication", "20 May 2020", "PUBLISHED"].join("\n")
+      phase_proposals = ["Proposals (0)", "01 Jun 2020 - 30 Jun 2020", "ACTIVE"].join("\n")
+      phase_comments = ["Comments (0)", "01 Jun 2020 - 05 Jun 2020", "ACTIVE"].join("\n")
+      phase_result = ["Final result publication", "01 Jul 2020", "COMING SOON"].join("\n")
+
+      visit legislation_processes_path
+
+      within("#legislation_process_#{process.id} .legislation-calendar") do
+        expect(page).to have_selector("a", text: phase_debate)
+        expect(page).to have_selector("a", text: phase_draft)
+        expect(page).to have_selector("a", text: phase_proposals)
+        expect(page).to have_selector("a", text: phase_comments)
+        expect(page).to have_selector("a", text: phase_result)
+        expect(page).to have_link(href: debate_legislation_process_path(process))
+        expect(page).to have_link(href: draft_publication_legislation_process_path(process))
+        expect(page).to have_link(href: proposals_legislation_process_path(process))
+        expect(page).to have_link(href: allegations_legislation_process_path(process))
+        expect(page).to have_link(href: result_publication_legislation_process_path(process))
+      end
+
+      travel_back
+    end
+
     scenario "Filtering processes" do
       create(:legislation_process, title: "Process open")
       create(:legislation_process, :past, title: "Process past")
