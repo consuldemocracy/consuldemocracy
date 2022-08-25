2408504fd77666fcb6da5740a01b750828e9e944
Javi Martín <javim@elretirao.net>
2022-08-25 18:01:50 +0200

    Bump capistrano from 3.16.0 to 3.17.1
    Bumps [capistrano](https://github.com/capistrano/capistrano) from 3.16.0 to 3.17.1.
    - [Release notes](https://github.com/capistrano/capistrano/releases)
    - [Commits](https://github.com/capistrano/capistrano/compare/v3.16.0...v3.17.1)
    
    ---
    updated-dependencies:
    - dependency-name: capistrano
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "~> 3.16.0"
+lock "~> 3.17.1"
 
 def deploysecret(key)
   @deploy_secrets_yml ||= YAML.load_file("config/deploy-secrets.yml")[fetch(:stage).to_s]
