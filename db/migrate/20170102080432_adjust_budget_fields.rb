5b7a969964a9d1a1600c273dc6fecf128834be01
Javi Martín <javim@elretirao.net>
2022-08-25 21:29:23 +0200

    Fix Rails/ReversibleMigration rule offenses
    We forgot to do so in commit fc757428e, when we upgraded rubocop-rails.
    In this upgrade, this rule was changed to include the `change_column`
    method.
    
    Just like we did in commit df959b74f, I'm raising an
    `ActiveRecord::IrreversibleMigration` exception in migrations done
    before version 1.0.0, since making all of them reversible would be too
    much work for little benefit.

--- db/migrate/20170102080432_adjust_budget_fields.rb
+++ db/migrate/20170102080432_adjust_budget_fields.rb
@@ -1,5 +1,9 @@
 class AdjustBudgetFields < ActiveRecord::Migration[4.2]
-  def change
+  def up
     change_column :budgets, :phase, :string, limit: 40, default: "accepting"
+  end
+
+  def down
+    fail ActiveRecord::IrreversibleMigration
   end
 end
--- db/migrate/20180220211105_change_newsletter_segment_recipient_to_string.rb
+++ db/migrate/20180220211105_change_newsletter_segment_recipient_to_string.rb
@@ -1,5 +1,9 @@
 class ChangeNewsletterSegmentRecipientToString < ActiveRecord::Migration[4.2]
-  def change
+  def up
     change_column :newsletters, :segment_recipient, :string, null: false
+  end
+
+  def down
+    fail ActiveRecord::IrreversibleMigration
   end
 end
