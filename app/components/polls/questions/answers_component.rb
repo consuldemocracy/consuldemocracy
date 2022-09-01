0ec4d52ed146f72fece701b3e6feae7123742299
Senén Rodero Rodríguez <15726+Senen@users.noreply.github.com>
2022-09-01 09:48:48 +0200

    Simplify `Polls::Questions::AnswersComponent` view

--- app/components/polls/questions/answers_component.rb
+++ app/components/polls/questions/answers_component.rb
@@ -6,26 +6,23 @@ class Polls::Questions::AnswersComponent < ApplicationComponent
     @question = question
   end
 
-  def answers_by_question_id
-    if params[:answer]
-      { question.id => params[:answer] }
-    else
-      stored_answers_by_question_id
-    end
+  def already_answered?(question_answer)
+    user_answers.find_by(answer: question_answer.title).present?
   end
 
   def voted_before_sign_in?
-    question.answers.where(author: current_user).any? do |vote|
+    user_answers.any? do |vote|
       vote.updated_at < current_user.current_sign_in_at
     end
   end
 
+  def question_answers
+    question.question_answers
+  end
+
   private
 
-    def stored_answers_by_question_id
-      poll_answers = ::Poll::Answer.by_question(question.poll.question_ids).by_author(current_user&.id)
-      poll_answers.each_with_object({}) do |answer, answers_by_question_id|
-        answers_by_question_id[answer.question_id] = answer.answer
-      end
+    def user_answers
+      @user_answers ||= question.answers.by_author(current_user)
     end
 end
