ab1659e786ad70942bdec481e3b2e165808ab501
decabeza <alberto@decabeza.es>
2022-09-09 13:20:55 +0200

    Add read more links to poll question component

--- app/components/polls/questions/question_component.rb
+++ app/components/polls/questions/question_component.rb
@@ -4,4 +4,10 @@ class Polls::Questions::QuestionComponent < ApplicationComponent
   def initialize(question:)
     @question = question
   end
+
+  def answers_read_more_links
+    safe_join(question.answers_with_read_more.map do |answer|
+      link_to answer.title, "#answer_#{answer.id}"
+    end, ", ")
+  end
 end
