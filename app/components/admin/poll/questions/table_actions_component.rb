3088371b0670b8b332d36df9bae0cd43a9c131bc
Javi Martín <javim@elretirao.net>
2022-09-18 17:51:18 +0200

    Remove duplication in poll actions tables
    We were using the same logic in four different places, so we're creating
    a new class to handle that logic.
    
    Note that I didn't find a way to delegate the `content` method to a
    `Admin::TableActionsComponent`, so we're delegating the `action` method
    instead. That means we need to create a method returning an
    `Admin::TableActionsComponent`. We also need to cache this object;
    otherwise we were getting an error when calling `actions.action` from
    the `Admin::Poll::Questions::TableActionsComponent`.

--- app/components/admin/poll/questions/table_actions_component.rb
+++ app/components/admin/poll/questions/table_actions_component.rb
@@ -1,14 +1,7 @@
 class Admin::Poll::Questions::TableActionsComponent < ApplicationComponent
   attr_reader :question
-  delegate :can?, to: :helpers
 
   def initialize(question)
     @question = question
   end
-
-  private
-
-    def actions
-      [:edit, :destroy].select { |action| can?(action, question) }
-    end
 end
