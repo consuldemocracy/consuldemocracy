5740c04d0f089bd3711ab549fb3ac596ec37d244
Javi Martín <javim@elretirao.net>
2022-09-28 13:28:31 +0200

    Use different cache namespaces for different tenants
    Since records in different tenants can have the same ID, they can share
    the same `cache_key`, and so we need a namespace to differentiate them.
    Without them, records from one tenant could expire the cache of a record
    from another tenant.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use a different cache store in production.
-  config.cache_store = :mem_cache_store
+  config.cache_store = :mem_cache_store, { namespace: proc { Tenant.current_schema }}
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
