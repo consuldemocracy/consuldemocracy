e6e703941d997554ff1ef4e91b8a99d3cc95a940
Javi Martín <javim@elretirao.net>
2022-09-28 13:28:31 +0200

    Use different cache namespaces for different tenants
    Since records in different tenants can have the same ID, they can share
    the same `cache_key`, and so we need a namespace to differentiate them.
    Without them, records from one tenant could expire the cache of a record
    from another tenant.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store
+    config.cache_store = :memory_store, { namespace: proc { Tenant.current_schema }}
     config.public_file_server.headers = {
       "Cache-Control" => "public, max-age=#{2.days.to_i}"
     }
