7b791472fd025f6acb734ac286ce899b6d6cb0c5
Javi Martín <javim@elretirao.net>
2022-09-30 23:24:34 +0200

    Use a custom method to detect the current tenant
    The subdomain elevator we were using, which is included in apartment,
    didn't work on hosts already including a subdomain (like
    demo.consul.dev, for instance). In those cases, we would manually add
    the subdomain to the list of excluded subdomains. Since these subdomains
    will be different for different CONSUL installations, it meant each
    installation had to customize the code. Furthermore, existing
    installations using subdomains would stop working.
    
    So we're using a custom method to find the current tenant, based on the
    host defined in `default_url_options`.
    
    In order to avoid any side-effects on single-tenant applications, we're
    adding a new configuration option to enable multitenancy
    
    We're enabling two ways to handle this configuration option:
    
    a) Change the application_custom.rb file, which is under version control
    b) Change the secrets.yml file, which is not under version control
    
    This way people prefering to handle configuration options through
    version control can do so, while people who prefer handling
    configuration options through te secrets.yml file can do so as well.
    
    We're also disabling the super-annoying warnings mentioning there are no
    tenants which we got every time we run migrations on single-tenant
    applications. These messages will only be enabled when the multitenancy
    feature is enabled too. For this reason, we're also disabling the
    multitenancy feature in the development environment by default.

--- spec/models/tenant_spec.rb
+++ spec/models/tenant_spec.rb
@@ -1,6 +1,138 @@
 require "rails_helper"
 
 describe Tenant do
+  describe ".resolve_host" do
+    before do
+      allow(ActionMailer::Base).to receive(:default_url_options).and_return({ host: "consul.dev" })
+    end
+
+    it "returns nil for empty hosts" do
+      expect(Tenant.resolve_host("")).to be nil
+      expect(Tenant.resolve_host(nil)).to be nil
+    end
+
+    it "returns nil for IP addresses" do
+      expect(Tenant.resolve_host("127.0.0.1")).to be nil
+    end
+
+    it "returns nil using development and test domains" do
+      expect(Tenant.resolve_host("localhost")).to be nil
+      expect(Tenant.resolve_host("lvh.me")).to be nil
+      expect(Tenant.resolve_host("example.com")).to be nil
+      expect(Tenant.resolve_host("www.example.com")).to be nil
+    end
+
+    it "treats lvh.me as localhost" do
+      expect(Tenant.resolve_host("jupiter.lvh.me")).to eq "jupiter"
+      expect(Tenant.resolve_host("www.lvh.me")).to be nil
+    end
+
+    it "returns nil for the default host" do
+      expect(Tenant.resolve_host("consul.dev")).to be nil
+    end
+
+    it "ignores the www prefix" do
+      expect(Tenant.resolve_host("www.consul.dev")).to be nil
+    end
+
+    it "returns subdomains when present" do
+      expect(Tenant.resolve_host("saturn.consul.dev")).to eq "saturn"
+    end
+
+    it "ignores the www prefix when subdomains are present" do
+      expect(Tenant.resolve_host("www.saturn.consul.dev")).to eq "saturn"
+    end
+
+    it "returns nested additional subdomains" do
+      expect(Tenant.resolve_host("europa.jupiter.consul.dev")).to eq "europa.jupiter"
+    end
+
+    it "ignores the www prefix in additional nested subdomains" do
+      expect(Tenant.resolve_host("www.europa.jupiter.consul.dev")).to eq "europa.jupiter"
+    end
+
+    it "does not ignore www if it isn't the prefix" do
+      expect(Tenant.resolve_host("wwwsaturn.consul.dev")).to eq "wwwsaturn"
+      expect(Tenant.resolve_host("saturn.www.consul.dev")).to eq "saturn.www"
+    end
+
+    it "returns the host as a subdomain" do
+      expect(Tenant.resolve_host("consul.dev.consul.dev")).to eq "consul.dev"
+    end
+
+    it "returns nested subdomains containing the host" do
+      expect(Tenant.resolve_host("saturn.consul.dev.consul.dev")).to eq "saturn.consul.dev"
+    end
+
+    it "returns full domains when they don't contain the host" do
+      expect(Tenant.resolve_host("unrelated.dev")).to eq "unrelated.dev"
+      expect(Tenant.resolve_host("mercury.anotherconsul.dev")).to eq "mercury.anotherconsul.dev"
+    end
+
+    it "ignores the www prefix in full domains" do
+      expect(Tenant.resolve_host("www.unrelated.dev")).to eq "unrelated.dev"
+      expect(Tenant.resolve_host("www.mercury.anotherconsul.dev")).to eq "mercury.anotherconsul.dev"
+    end
+
+    context "multitenancy disabled" do
+      before { allow(Rails.application.config).to receive(:multitenancy).and_return(false) }
+
+      it "always returns nil" do
+        expect(Tenant.resolve_host("saturn.consul.dev")).to be nil
+        expect(Tenant.resolve_host("jupiter.lvh.me")).to be nil
+      end
+    end
+
+    context "default host contains subdomains" do
+      before do
+        allow(ActionMailer::Base).to receive(:default_url_options).and_return({ host: "demo.consul.dev" })
+      end
+
+      it "ignores subdomains already present in the default host" do
+        expect(Tenant.resolve_host("demo.consul.dev")).to be nil
+      end
+
+      it "ignores the www prefix" do
+        expect(Tenant.resolve_host("www.demo.consul.dev")).to be nil
+      end
+
+      it "returns additional subdomains" do
+        expect(Tenant.resolve_host("saturn.demo.consul.dev")).to eq "saturn"
+      end
+
+      it "ignores the www prefix in additional subdomains" do
+        expect(Tenant.resolve_host("www.saturn.demo.consul.dev")).to eq "saturn"
+      end
+
+      it "returns nested additional subdomains" do
+        expect(Tenant.resolve_host("europa.jupiter.demo.consul.dev")).to eq "europa.jupiter"
+      end
+
+      it "ignores the www prefix in additional nested subdomains" do
+        expect(Tenant.resolve_host("www.europa.jupiter.demo.consul.dev")).to eq "europa.jupiter"
+      end
+
+      it "does not ignore www if it isn't the prefix" do
+        expect(Tenant.resolve_host("wwwsaturn.demo.consul.dev")).to eq "wwwsaturn"
+        expect(Tenant.resolve_host("saturn.www.demo.consul.dev")).to eq "saturn.www"
+      end
+    end
+
+    context "default host is similar to development and test domains" do
+      before do
+        allow(ActionMailer::Base).to receive(:default_url_options).and_return({ host: "mylvh.me" })
+      end
+
+      it "returns nil for the default host" do
+        expect(Tenant.resolve_host("mylvh.me")).to be nil
+      end
+
+      it "returns subdomains when present" do
+        expect(Tenant.resolve_host("neptune.mylvh.me")).to eq "neptune"
+      end
+    end
+  end
+
   describe "validations" do
     let(:tenant) { build(:tenant) }
 
