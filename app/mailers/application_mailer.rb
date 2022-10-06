3da175ebbdef039346fd62011813457967537f27
Javi Martín <javim@elretirao.net>
2022-10-06 13:08:35 +0200

    Set the asset host based on default_url_options
    We were duplicating the asset host and the URL host in all environments,
    but we can make it so the asset host uses the URL host unless we
    specifically set it.
    
    Note that, inside the `ApplicationMailer`, the `root_url` method already
    uses `default_url_options` to generate the URL.
    
    In the rare case of CONSUL installations who have changed the asset
    host, this change has no effect since they'll get a conflict in the
    environment files when upgrading and they'll choose to use their own
    asset host.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -4,4 +4,9 @@ class ApplicationMailer < ActionMailer::Base
   helper :mailer
   default from: proc { "#{Setting["mailer_from_name"]} <#{Setting["mailer_from_address"]}>" }
   layout "mailer"
+  before_action :set_asset_host
+
+  def set_asset_host
+    self.asset_host ||= root_url.delete_suffix("/")
+  end
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -45,7 +45,6 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = { host: "test" }
-  config.action_mailer.asset_host = "http://consul.test"
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
