525a975cc09b80d7ce20f8da45ae637e71c7f29a
Javi Martín <javim@elretirao.net>
2022-10-07 19:48:32 +0200

    Use a different attachments folder per tenant
    While this is not strictly necessary, it can help moving the data of one
    tenant to a different server or removing it.
    
    Note we're using subfolders inside the `tenants` subfolder. If we simply
    used subfolders with the schema names, if the schema names were, for
    instance, language codes like `es`, `en`, `it`, ... they would conflict
    with the default subfolders used by Active Storage.

---
+++ spec/models/attachable_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+describe Attachable do
+  it "stores tenant attachments in a folder for the tenant" do
+    create(:tenant, subdomain: "image-master")
+
+    Tenant.switch("image-master") do
+      tenant_image = create(:image, attachment: fixture_file_upload("clippy.jpg"))
+
+      expect(tenant_image.file_path).to include "storage/tenants/image-master/"
+    end
+
+    image = create(:image, attachment: fixture_file_upload("clippy.jpg"))
+
+    expect(image.file_path).to include "storage/"
+    expect(image.file_path).not_to include "image-master"
+  end
+end
