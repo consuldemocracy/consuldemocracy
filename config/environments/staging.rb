a08743bc3f314cad18b01b4d8cf0cf66cd35bb80
taitus <sebastia.roig@gmail.com>
2022-10-24 12:59:31 +0200

    Add s3 configuration

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -119,6 +119,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.active_storage.service = :s3
 end
 
 require Rails.root.join("config", "environments", "custom", "staging")
