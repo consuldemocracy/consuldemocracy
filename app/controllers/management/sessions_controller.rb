15bffe4f5f54e98085c803e63ed4e933d0cdbb3a
Javi Martín <javim@elretirao.net>
2022-11-03 00:10:28 +0100

    Allow rendering different views per tenant
    Sometimes it might be convenient to use completely different views for
    different tenants. For example, a certain tenant might use a footer that
    has nothing to do with the default one.
    
    For these cases, instead of adding `case Tenant.current_schema`
    conditions to the view, it might be tidier to use a different file.
    
    For this purpose, we're using Rails variants [1], which means that a
    tenant named `mytenant` will use a template ending with
    `.html+mytenant.erb` if it's available.
    
    This works with components too, but has a limitation: when using the
    `custom/` folder to add ERB files for a tenant, the default tenant ERB
    file needs to be added to the `custom/` folder as well; if there aren't
    changes to this file, a symbolic link will do.
    
    For example, if we're writing a custom `admin/action_component` view for
    the tenant `milky-way` but don't need to change this file for the
    default tenant:
    
    1. Create `app/components/custom/admin/action_component.rb` according to
       the components customizations documentation [2]
    2. Create the custom view for the `milky-way` tenant and save it under
       `app/components/custom/admin/action_component.html+milky-way.erb`
    3. Enter the `app/components/custom/admin/` folder and run `ln -s
       ../../admin/action_component.html.erb`
    
    We're also adding some controller tests. Since Rails doesn't load the
    middleware during controller tests, we're stubbing the `current_schema`
    method directly instead of changing the subdomain of the request.
    
    [1] https://guides.rubyonrails.org/v6.0/layouts_and_rendering.html#the-variants-option
    [2] https://docs.consulproject.org/docs/english-documentation/customization/components

--- app/controllers/management/sessions_controller.rb
+++ app/controllers/management/sessions_controller.rb
@@ -1,6 +1,7 @@
 require "manager_authenticator"
 
 class Management::SessionsController < ActionController::Base
+  include TenantVariants
   include GlobalizeFallbacks
   include AccessDeniedHandler
   default_form_builder ConsulFormBuilder
