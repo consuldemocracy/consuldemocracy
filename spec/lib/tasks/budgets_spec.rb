javierm/consul

Allow specifying the tenant in budget tasks
The `budgets:email:selected` and `budgets:email:unselected` tasks are
supposed to be run manually because they only make sense at a specific
point during the life of a budget.

However, they would only run on the default tenant, and it was
impossible to run them on a different tenant.

So we're introducing an argument in the rake task accepting the name of
the tenant whose users we want to send emails to.

spec/lib/tasks/budgets_spec.rb
@@ -13,6 +13,20 @@ describe "budget tasks" do
       expect(ActionMailer::Base.deliveries.count).to eq 1
       expect(ActionMailer::Base.deliveries.last.to).to eq ["selectme@consul.dev"]
     end
+
+    it "accepts specifying the tenant" do
+      create(:budget_investment, :selected, author: create(:user, email: "default@consul.dev"))
+      create(:tenant, subdomain: "different")
+
+      Tenant.switch("different") do
+        create(:budget_investment, :selected, author: create(:user, email: "different@consul.dev"))
+      end
+
+      Rake.application.invoke_task("budgets:email:selected[different]")
+
+      expect(ActionMailer::Base.deliveries.count).to eq 1
+      expect(ActionMailer::Base.deliveries.last.to).to eq ["different@consul.dev"]
+    end
   end
 
   describe "rake budgets:email:unselected" do
@@ -26,6 +40,20 @@ describe "budget tasks" do
 
       expect(ActionMailer::Base.deliveries.count).to eq 1
       expect(ActionMailer::Base.deliveries.last.to).to eq ["ignorme@consul.dev"]
+    end
+
+    it "accepts specifying the tenant" do
+      create(:budget_investment, author: create(:user, email: "default@consul.dev"))
+      create(:tenant, subdomain: "different")
+
+      Tenant.switch("different") do
+        create(:budget_investment, author: create(:user, email: "different@consul.dev"))
+      end
+
+      Rake.application.invoke_task("budgets:email:unselected[different]")
+
+      expect(ActionMailer::Base.deliveries.count).to eq 1
+      expect(ActionMailer::Base.deliveries.last.to).to eq ["different@consul.dev"]
     end
   end
 end

consuldemocracy/consuldemocracy

spec/lib/tasks/budgets_spec.rb
@@ -13,6 +13,20 @@ describe "budget tasks" do
       expect(ActionMailer::Base.deliveries.count).to eq 1
       expect(ActionMailer::Base.deliveries.last.to).to eq ["selectme@consul.dev"]
     end
+
+    it "accepts specifying the tenant" do
+      create(:budget_investment, :selected, author: create(:user, email: "default@consul.dev"))
+      create(:tenant, schema: "different")
+
+      Tenant.switch("different") do
+        create(:budget_investment, :selected, author: create(:user, email: "different@consul.dev"))
+      end
+
+      Rake.application.invoke_task("budgets:email:selected[different]")
+
+      expect(ActionMailer::Base.deliveries.count).to eq 1
+      expect(ActionMailer::Base.deliveries.last.to).to eq ["different@consul.dev"]
+    end
   end
 
   describe "rake budgets:email:unselected" do
@@ -26,6 +40,20 @@ describe "budget tasks" do
 
       expect(ActionMailer::Base.deliveries.count).to eq 1
       expect(ActionMailer::Base.deliveries.last.to).to eq ["ignorme@consul.dev"]
+    end
+
+    it "accepts specifying the tenant" do
+      create(:budget_investment, author: create(:user, email: "default@consul.dev"))
+      create(:tenant, schema: "different")
+
+      Tenant.switch("different") do
+        create(:budget_investment, author: create(:user, email: "different@consul.dev"))
+      end
+
+      Rake.application.invoke_task("budgets:email:unselected[different]")
+
+      expect(ActionMailer::Base.deliveries.count).to eq 1
+      expect(ActionMailer::Base.deliveries.last.to).to eq ["different@consul.dev"]
     end
   end
 end