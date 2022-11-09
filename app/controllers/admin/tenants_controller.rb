javierm/consul

Add an interface to manage tenants
Note we aren't allowing to delete a tenant because it would delete all
its data, so this action is a very dangerous one. We might need to add a
warning when creating a tenant, indicating the tenant cannot be
destroyed. We can also add an action to delete a tenant which forces the
admin to write the name of the tenant before deleting it and with a big
warning about the danger of this operation.

For now, we're letting administrators of the "main" (default) tenant to
create other tenants. However, we're only allowing to manage tenants
when the multitenancy configuration option is enabled. This way the
interface won't get in the way on single-tenant applications.

We've thought about creating a new role to manage tenants or a new URL
out of the admin area. We aren't doing so for simplicity purposes and
because we want to keep CONSUL working the same way it has for
single-tenant installations, but we might change it in the future.
There's also the fact that by default we create one user with a known
password, and if by default we create a new role and a new user to
handle tenants, the chances of people forgetting to change the password
of one of these users increases dramatically, particularly if they
aren't using multitenancy.

app/controllers/admin/tenants_controller.rb
@@ -1,0 +1,35 @@
+class Admin::TenantsController < Admin::BaseController
+  load_and_authorize_resource
+
+  def index
+    @tenants = @tenants.order(:name)
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+    if @tenant.save
+      redirect_to admin_tenants_path, notice: t("admin.tenants.create.notice")
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @tenant.update(tenant_params)
+      redirect_to admin_tenants_path, notice: t("admin.tenants.update.notice")
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def tenant_params
+      params.require(:tenant).permit(:name, :subdomain)
+    end
+end

app/models/tenant.rb
@@ -39,12 +39,16 @@ class Tenant < ApplicationRecord
   end
 
   def self.current_host
-    if default?
+    host_for(current_schema)
+  end
+
+  def self.host_for(schema)
+    if schema == "public"
       default_host
     elsif default_host == "localhost"
-      "#{current_schema}.lvh.me"
+      "#{schema}.lvh.me"
     else
-      "#{current_schema}.#{default_host}"
+      "#{schema}.#{default_host}"
     end
   end
 
@@ -64,6 +68,10 @@ class Tenant < ApplicationRecord
     ["public"].union(Apartment.tenant_names).each do |subdomain|
       switch(subdomain, &block)
     end
+  end
+
+  def host
+    self.class.host_for(subdomain)
   end
 
   private

spec/models/abilities/administrator_spec.rb
@@ -164,4 +164,37 @@ describe Abilities::Administrator do
   it { should be_able_to(:destroy, SDG::Manager) }
 
   it { should be_able_to(:manage, Widget::Card) }
+
+  describe "tenants" do
+    context "with multitenancy disabled" do
+      before { allow(Rails.application.config).to receive(:multitenancy).and_return(false) }
+
+      it { should_not be_able_to :create, Tenant }
+      it { should_not be_able_to :read, Tenant }
+      it { should_not be_able_to :update, Tenant }
+      it { should_not be_able_to :destroy, Tenant }
+    end
+
+    context "with multitenancy enabled" do
+      before { allow(Rails.application.config).to receive(:multitenancy).and_return(true) }
+
+      it { should be_able_to :create, Tenant }
+      it { should be_able_to :read, Tenant }
+      it { should be_able_to :update, Tenant }
+      it { should_not be_able_to :destroy, Tenant }
+
+      it "does not allow admin from other tenants to manage tenants " do
+        create(:tenant, subdomain: "subsidiary")
+
+        Tenant.switch("subsidiary") do
+          admin = create(:administrator).user
+
+          expect(admin).not_to be_able_to :create, Tenant
+          expect(admin).not_to be_able_to :read, Tenant
+          expect(admin).not_to be_able_to :update, Tenant
+          expect(admin).not_to be_able_to :destroy, Tenant
+        end
+      end
+    end
+  end
 end

spec/system/admin/tenants_spec.rb
@@ -1,0 +1,45 @@
+require "rails_helper"
+
+describe "Tenants", :admin do
+  before { allow(Tenant).to receive(:default_host).and_return("localhost") }
+
+  scenario "Create" do
+    visit admin_root_path
+
+    within("#side_menu") do
+      click_link "Settings"
+      click_link "Multitenancy"
+    end
+
+    click_link "Create tenant"
+    fill_in "Subdomain", with: "earth"
+    fill_in "Name", with: "Earthlings"
+    click_button "Create tenant"
+
+    expect(page).to have_content "Tenant created successfully"
+
+    within("tr", text: "earth") { click_link "View" }
+
+    expect(current_host).to eq "http://earth.lvh.me"
+    expect(page).to have_current_path root_path
+    expect(page).to have_link "Sign in"
+  end
+
+  scenario "Update" do
+    create(:tenant, subdomain: "moon")
+
+    visit admin_tenants_path
+    within("tr", text: "moon") { click_link "Edit" }
+
+    fill_in "Subdomain", with: "the-moon"
+    click_button "Update tenant"
+
+    expect(page).to have_content "Tenant updated successfully"
+
+    within("tr", text: "the-moon") { click_link "View" }
+
+    expect(current_host).to eq "http://the-moon.lvh.me"
+    expect(page).to have_current_path root_path
+    expect(page).to have_link "Sign in"
+  end
+end

consuldemocracy/consuldemocracy

app/controllers/admin/tenants_controller.rb
@@ -1,0 +1,35 @@
+class Admin::TenantsController < Admin::BaseController
+  load_and_authorize_resource
+
+  def index
+    @tenants = @tenants.order(:name)
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+    if @tenant.save
+      redirect_to admin_tenants_path, notice: t("admin.tenants.create.notice")
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @tenant.update(tenant_params)
+      redirect_to admin_tenants_path, notice: t("admin.tenants.update.notice")
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def tenant_params
+      params.require(:tenant).permit(:name, :schema)
+    end
+end

app/models/tenant.rb
@@ -40,12 +40,16 @@ class Tenant < ApplicationRecord
   end
 
   def self.current_host
-    if default?
+    host_for(current_schema)
+  end
+
+  def self.host_for(schema)
+    if schema == "public"
       default_host
     elsif default_host == "localhost"
-      "#{current_schema}.lvh.me"
+      "#{schema}.lvh.me"
     else
-      "#{current_schema}.#{default_host}"
+      "#{schema}.#{default_host}"
     end
   end
 
@@ -65,6 +69,10 @@ class Tenant < ApplicationRecord
     ["public"].union(Apartment.tenant_names).each do |schema|
       switch(schema, &block)
     end
+  end
+
+  def host
+    self.class.host_for(schema)
   end
 
   private

spec/models/abilities/administrator_spec.rb
@@ -164,4 +164,37 @@ describe Abilities::Administrator do
   it { should be_able_to(:destroy, SDG::Manager) }
 
   it { should be_able_to(:manage, Widget::Card) }
+
+  describe "tenants" do
+    context "with multitenancy disabled" do
+      before { allow(Rails.application.config).to receive(:multitenancy).and_return(false) }
+
+      it { should_not be_able_to :create, Tenant }
+      it { should_not be_able_to :read, Tenant }
+      it { should_not be_able_to :update, Tenant }
+      it { should_not be_able_to :destroy, Tenant }
+    end
+
+    context "with multitenancy enabled" do
+      before { allow(Rails.application.config).to receive(:multitenancy).and_return(true) }
+
+      it { should be_able_to :create, Tenant }
+      it { should be_able_to :read, Tenant }
+      it { should be_able_to :update, Tenant }
+      it { should_not be_able_to :destroy, Tenant }
+
+      it "does not allow administrators from other tenants to manage tenants " do
+        create(:tenant, schema: "subsidiary")
+
+        Tenant.switch("subsidiary") do
+          admin = create(:administrator).user
+
+          expect(admin).not_to be_able_to :create, Tenant
+          expect(admin).not_to be_able_to :read, Tenant
+          expect(admin).not_to be_able_to :update, Tenant
+          expect(admin).not_to be_able_to :destroy, Tenant
+        end
+      end
+    end
+  end
 end

spec/system/admin/tenants_spec.rb
@@ -1,0 +1,45 @@
+require "rails_helper"
+
+describe "Tenants", :admin do
+  before { allow(Tenant).to receive(:default_host).and_return("localhost") }
+
+  scenario "Create" do
+    visit admin_root_path
+
+    within("#side_menu") do
+      click_link "Settings"
+      click_link "Multitenancy"
+    end
+
+    click_link "Create tenant"
+    fill_in "Subdomain", with: "earth"
+    fill_in "Name", with: "Earthlings"
+    click_button "Create tenant"
+
+    expect(page).to have_content "Tenant created successfully"
+
+    within("tr", text: "earth") { click_link "View" }
+
+    expect(current_host).to eq "http://earth.lvh.me"
+    expect(page).to have_current_path root_path
+    expect(page).to have_link "Sign in"
+  end
+
+  scenario "Update" do
+    create(:tenant, schema: "moon")
+
+    visit admin_tenants_path
+    within("tr", text: "moon") { click_link "Edit" }
+
+    fill_in "Subdomain", with: "the-moon"
+    click_button "Update tenant"
+
+    expect(page).to have_content "Tenant updated successfully"
+
+    within("tr", text: "the-moon") { click_link "View" }
+
+    expect(current_host).to eq "http://the-moon.lvh.me"
+    expect(page).to have_current_path root_path
+    expect(page).to have_link "Sign in"
+  end
+end