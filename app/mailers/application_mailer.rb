fcfb7f753e4d61b8e55f096448e6345f7d5c64aa
Javi Martín <javim@elretirao.net>
2022-11-28 19:40:50 +0100

    Allow rendering different email views per tenant
    We were assigning variants in a controller, in the context of a request.
    However, when sending emails, there is no request and no controller is
    involved, so we also need to set the variant in the ApplicationMailer
    class.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,6 +3,7 @@ class ApplicationMailer < ActionMailer::Base
   default from: proc { "#{Setting["mailer_from_name"]} <#{Setting["mailer_from_address"]}>" }
   layout "mailer"
   before_action :set_asset_host
+  before_action :set_variant
   after_action :set_smtp_settings
 
   def default_url_options
@@ -11,6 +12,10 @@ class ApplicationMailer < ActionMailer::Base
 
   def set_asset_host
     self.asset_host ||= root_url.delete_suffix("/")
+  end
+
+  def set_variant
+    lookup_context.variants = [Tenant.current_schema.to_sym]
   end
 
   def set_smtp_settings
