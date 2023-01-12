4477aa73f22b664b62878cb1c684ccace7a25c89
Javi Martín <javim@elretirao.net>
2023-01-12 18:38:34 +0100

    Use link_list to render admin login items
    This way it's easier to customize this menu.

--- app/components/layout/admin_login_items_component.rb
+++ app/components/layout/admin_login_items_component.rb
@@ -1,6 +1,6 @@
 class Layout::AdminLoginItemsComponent < ApplicationComponent
   attr_reader :user
-  delegate :show_admin_menu?, to: :helpers
+  delegate :link_list, :show_admin_menu?, to: :helpers
 
   def initialize(user)
     @user = user
@@ -9,4 +9,41 @@ class Layout::AdminLoginItemsComponent < ApplicationComponent
   def render?
     show_admin_menu?(user)
   end
+
+  private
+
+    def admin_links
+      [
+        (admin_link if user.administrator?),
+        (moderation_link if user.administrator? || user.moderator?),
+        (valuation_link if feature?(:budgets) && (user.administrator? || user.valuator?)),
+        (management_link if user.administrator? || user.manager?),
+        (officing_link if user.poll_officer? && Poll.current.any?),
+        (sdg_management_link if feature?(:sdg) && (user.administrator? || user.sdg_manager?))
+      ]
+    end
+
+    def admin_link
+      [t("layouts.header.administration"), admin_root_path]
+    end
+
+    def moderation_link
+      [t("layouts.header.moderation"), moderation_root_path]
+    end
+
+    def valuation_link
+      [t("layouts.header.valuation"), valuation_root_path]
+    end
+
+    def management_link
+      [t("layouts.header.management"), management_sign_in_path]
+    end
+
+    def officing_link
+      [t("layouts.header.officing"), officing_root_path]
+    end
+
+    def sdg_management_link
+      [t("sdg_management.header.title"), sdg_management_root_path]
+    end
 end
