d5f440c9bbeeb4c07bee3c9cb583184860a321f1
Javi Martín <javim@elretirao.net>
2023-01-22 17:14:20 +0100

    Simplify header in custom pages admin index
    Now that we're using a component, we can reuse the Header module.

--- app/components/admin/site_customization/pages/index_component.rb
+++ app/components/admin/site_customization/pages/index_component.rb
@@ -1,7 +1,14 @@
 class Admin::SiteCustomization::Pages::IndexComponent < ApplicationComponent
+  include Header
   attr_reader :pages
 
   def initialize(pages)
     @pages = pages
   end
+
+  private
+
+    def title
+      t("admin.site_customization.pages.index.title")
+    end
 end
