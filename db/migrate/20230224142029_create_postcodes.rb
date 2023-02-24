7693378d32e9b23f823f11b56412be15df2fbba5
CoslaJohn <johnmunro@cosla.gov.uk>
2023-02-24 15:56:26 +0100

    updated rake tasks to remove spaces from postcodes and leading/trailing from ward names

--- db/migrate/20230224142029_create_postcodes.rb
+++ db/migrate/20230224142029_create_postcodes.rb
@@ -1,9 +1,8 @@
 class CreatePostcodes < ActiveRecord::Migration[6.0]
   def change
     create_table :postcodes do |t|
-      t.string :postcode
-      t.string :ward
-      t.integer :geozone_id
+
+      t.timestamps
     end
   end
 end
