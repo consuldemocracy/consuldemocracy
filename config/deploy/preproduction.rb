b115b0cc8d7d1bece2ccd5b17c4bd40d771de937
Javi Martín <javim@elretirao.net>
2023-02-27 17:03:20 +0100

    Use only one server when deploying to preproduction
    It doesn't make much sense that by default we use one server on
    production on two servers on preproduction.
    
    Note we're keeping `server1` instead of using just `server` in order to
    keep compatibility with existing installation.

--- config/deploy/preproduction.rb
+++ config/deploy/preproduction.rb
@@ -1,4 +1,3 @@
 set :branch, ENV["branch"] || :master
 
 server deploysecret(:server1), user: deploysecret(:user), roles: %w[web app db importer cron background]
-server deploysecret(:server2), user: deploysecret(:user), roles: %w[web app db importer]
