dabaa1e7c16d29ff25aa94a131fa853b485ba7f7
CoslaJohn <johnmunro@cosla.gov.uk>
2023-03-02 12:06:13 +0100

    Added document_type 4 young_scot to allowed local census document types

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_23_120526) do
+ActiveRecord::Schema.define(version: 2023_03_02_092141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
