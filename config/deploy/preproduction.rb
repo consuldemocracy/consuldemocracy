8aa113a47c833a9606906f44c8c0e174b09c2d8d
Javi Martín <javim@elretirao.net>
2023-03-21 19:01:26 +0100

    Unify deploy secret keys for the main server
    We were using `server` on staging but `server1` and `server2` on
    preproduction and production.
    
    The reason behind it is we've always used one server on staging but
    sometimes we've used several servers on preproduction and production.
    
    However, this is a bit of a mess on installations which have only one
    server on preproduction or production and need to use the `server` key
    for the staging environments but `server1` for other environments.
    
    So, in order to keep compatibility with existing Consul installations,
    we're now allowing either `server` or `server1` on any environment.

--- config/deploy/preproduction.rb
+++ config/deploy/preproduction.rb
@@ -1,3 +1,3 @@
 set :branch, ENV["branch"] || :master
 
-server deploysecret(:server1), user: deploysecret(:user), roles: %w[web app db importer cron background]
+server main_deploy_server, user: deploysecret(:user), roles: %w[web app db importer cron background]
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :branch, ENV["branch"] || :master
 
-server deploysecret(:server1), user: deploysecret(:user), roles: %w[web app db importer cron background]
+server main_deploy_server, user: deploysecret(:user), roles: %w[web app db importer cron background]
 #server deploysecret(:server2), user: deploysecret(:user), roles: %w(web app db importer cron background)
 #server deploysecret(:server3), user: deploysecret(:user), roles: %w(web app db importer)
 #server deploysecret(:server4), user: deploysecret(:user), roles: %w(web app db importer)
