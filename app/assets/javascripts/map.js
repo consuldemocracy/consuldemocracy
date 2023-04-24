a0f2ffc8cdd299fbcbbd12c10476c213a59b2401
Javi Martín <javim@elretirao.net>
2023-04-24 18:05:22 +0200

    Extract function to create a map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -15,9 +15,8 @@
       App.Map.maps = [];
     },
     initializeMap: function(element) {
-      var addMarkerInvestments, centerData, createMarker, editable, markerData,
-        map, mapCenterLatLng, marker, markerIcon, moveOrPlaceMarker,
-        removeMarker, removeMarkerSelector;
+      var addMarkerInvestments, createMarker, editable, markerData, map, marker,
+        markerIcon, moveOrPlaceMarker, removeMarker, removeMarkerSelector;
       App.Map.cleanInvestmentCoordinates(element);
       removeMarkerSelector = $(element).data("marker-remove-selector");
       addMarkerInvestments = $(element).data("marker-investments-coordinates");
@@ -61,9 +60,7 @@
         App.Map.updateFormfields(map, marker);
       };
 
-      centerData = App.Map.centerData(element);
-      mapCenterLatLng = new L.LatLng(centerData.lat, centerData.long);
-      map = L.map(element.id, { scrollWheelZoom: false }).setView(mapCenterLatLng, centerData.zoom);
+      map = App.Map.leafletMap(element);
       App.Map.maps.push(map);
       App.Map.addAttribution(map);
 
@@ -89,6 +86,14 @@
           }
         });
       }
+    },
+    leafletMap: function(element) {
+      var centerData, mapCenterLatLng;
+
+      centerData = App.Map.centerData(element);
+      mapCenterLatLng = new L.LatLng(centerData.lat, centerData.long);
+
+      return L.map(element.id, { scrollWheelZoom: false }).setView(mapCenterLatLng, centerData.zoom);
     },
     attributionPrefix: function() {
       return '<a href="https://leafletjs.com" title="A JavaScript library for interactive maps">Leaflet</a>';
