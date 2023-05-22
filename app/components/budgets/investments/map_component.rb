727e849fd4cfde22a9d30f91c7d538c2478d5efb
Javi Martín <javim@elretirao.net>
2023-05-22 15:40:46 +0200

    Move investments sidebar map view to a component
    This way we remove some logic from the (huge) investments controller
    class.

---
+++ app/components/budgets/investments/map_component.rb
@@ -0,0 +1,22 @@
+class Budgets::Investments::MapComponent < ApplicationComponent
+  attr_reader :heading, :investments
+
+  def initialize(investments, heading:)
+    @investments = investments
+    @heading = heading
+  end
+
+  def render?
+    map_location&.available?
+  end
+
+  private
+
+    def map_location
+      MapLocation.from_heading(heading) if heading.present?
+    end
+
+    def coordinates
+      MapLocation.where(investment: investments).map(&:json_data)
+    end
+end
