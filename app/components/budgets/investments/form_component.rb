d92e6ef37766fbc6f52ab02daae6836440a5e4ee
CoslaDeploy <consul@cosla.gov.uk>
2023-06-12 14:32:11 +0000

    moved form with video from component
    repeat of previous

--- app/components/budgets/investments/form_component.rb
+++ app/components/budgets/investments/form_component.rb
@@ -1,4 +1,9 @@
-class Budgets::Investments::FormComponent < ApplicationComponent
+class Budgets::Investments::FormComponent < ApplicationComponent; end
+
+require_dependency Rails.root.join("app", "components", "budgets", "investments", "form_component").to_s
+
+class Budgets::Investments::FormComponent
+
   include TranslatableFormHelper
   include GlobalizeHelper
   attr_reader :investment, :url
