2222cd7e8865b430cb8b5c1cdc397464b0c376ca
karim-semmoud <karim.semmoud@netoum.com>
2023-06-16 00:25:44 +0200

    Bump font-awesome-sass from 5.15.1 to 6.4.0

--- config/initializers/sass.rb
+++
@@ -1,9 +0,0 @@
-# TODO: we could remove it once we upgrade Font Awesome, although
-# we might need to add it again if we upgrade Sass
-SassC::Engine.class_eval do
-  alias_method :original_initialize, :initialize
-
-  def initialize(template, options = {})
-    original_initialize(template, options.merge(quiet_deps: true))
-  end
-end
