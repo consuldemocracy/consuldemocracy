661e6218e9016cc207fb2e7cae20e414b3d976f6
Avi-ADAM <aviadam.segel@gmail.com>
2023-06-21 02:50:52 +0300

    custom style

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :application, "consul"
 set :deploy_to, deploysecret(:deploy_to)
 set :ssh_options, port: deploysecret(:ssh_port)
 
-set :repo_url, "https://github.com/consul/consul.git"
+set :repo_url, "https://github.com/Avi-ADAM/consul.git"
 
 set :revision, `git rev-parse --short #{fetch(:branch)}`.strip
 
