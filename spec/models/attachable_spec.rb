5f29680186046666cbd78a8b60d11bfd28a66e61
Julian Herrero <microweb10@gmail.com>
2023-08-07 13:35:41 +0200

    Do not validate the attachment file size ...
    when the attachment has not changed

--- spec/models/attachable_spec.rb
+++ spec/models/attachable_spec.rb
@@ -14,4 +14,48 @@ describe Attachable do
 
     expect(build(:image).file_path).to include "storage/tenants/image-master/"
   end
+
+  context "file size validation" do
+    it "is not applied when the image attachment has not changed" do
+      image = create(:image, :proposal_image)
+
+      expect(image.valid?).to be(true)
+
+      Setting["uploads.images.max_size"] = 0.1
+
+      expect(image.valid?).to be(true)
+    end
+
+    it "is applied when the image attachment changes" do
+      image = create(:image, :proposal_image)
+
+      expect(image.valid?).to be(true)
+
+      Setting["uploads.images.max_size"] = 0.1
+      image.attachment = Rack::Test::UploadedFile.new("spec/fixtures/files/clippy.png")
+
+      expect(image.valid?).to be(false)
+    end
+
+    it "is not applied when the document attachment has not changed" do
+      document = create(:document, :proposal_document)
+
+      expect(document.valid?).to be(true)
+
+      Setting["uploads.documents.max_size"] = 0.1
+
+      expect(document.valid?).to be(true)
+    end
+
+    it "is applied when the document attachment changes" do
+      document = create(:document, :proposal_document)
+
+      expect(document.valid?).to be(true)
+
+      Setting["uploads.documents.max_size"] = 0.1
+      document.attachment = Rack::Test::UploadedFile.new("spec/fixtures/files/clippy.pdf")
+
+      expect(document.valid?).to be(false)
+    end
+  end
 end
