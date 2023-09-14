c2f03f869ad893b75eb6082ebddd6c80240a21c9
taitus <sebastia.roig@gmail.com>
2023-09-14 10:06:04 +0200

    Add new footer content block
    Include a new content block called 'footer_legal' for additional legal footer items.

--- app/models/site_customization/content_block.rb
+++ app/models/site_customization/content_block.rb
@@ -1,5 +1,5 @@
 class SiteCustomization::ContentBlock < ApplicationRecord
-  VALID_BLOCKS = %w[top_links footer subnavigation_left subnavigation_right].freeze
+  VALID_BLOCKS = %w[top_links footer footer_legal subnavigation_left subnavigation_right].freeze
 
   validates :locale, presence: true, inclusion: { in: I18n.available_locales.map(&:to_s) }
   validates :name, presence: true, uniqueness: { scope: :locale }, inclusion: { in: ->(*) { VALID_BLOCKS }}
