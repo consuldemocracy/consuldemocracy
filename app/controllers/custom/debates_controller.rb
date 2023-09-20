ec03127710b4549a0161501677356a7b2ef3e865
ssnioka <simonas.snioka@gmail.com>
2023-09-20 14:13:32 +0300

    Remove debate creation for users

--- app/controllers/custom/debates_controller.rb
+++ app/controllers/custom/debates_controller.rb
@@ -1,77 +1,89 @@
 class DebatesController < ApplicationController
-    include FeatureFlags
-    include CommentableActions
-    include FlagActions
-    include Translatable
-  
-    before_action :authenticate_user!, except: [:index, :show]
-    before_action :set_view, only: :index
-    before_action :debates_recommendations, only: :index, if: :current_user
-  
-    feature_flag :debates
-  
-    invisible_captcha only: [:create, :update], honeypot: :subtitle
-  
-    has_orders ->(c) { Debate.debates_orders(c.current_user) }, only: :index
-    has_orders %w[most_voted newest oldest], only: :show
-  
-    load_and_authorize_resource
-    helper_method :resource_model, :resource_name
-    respond_to :html, :js
-  
-    def index_customization
-      @featured_debates = @debates.featured
+  include FeatureFlags
+  include CommentableActions
+  include FlagActions
+  include Translatable
+
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_view, only: :index
+  before_action :debates_recommendations, only: :index, if: :current_user
+
+  feature_flag :debates
+
+  invisible_captcha only: [:create, :update], honeypot: :subtitle
+
+  has_orders ->(c) { Debate.debates_orders(c.current_user) }, only: :index
+  has_orders %w[newest most_voted oldest], only: :show
+
+  load_and_authorize_resource
+  helper_method :resource_model, :resource_name, :is_admin?
+  respond_to :html, :js
+
+  def index_customization
+    @featured_debates = @debates.featured
+  end
+
+  def is_admin?
+    current_user&.administrator?
+  end
+
+  def new
+    if is_admin?
+      # Allow admin to access the debate creation page
+      @debate = Debate.new
+    else
+      redirect_to debates_path, alert: t("unauthorized.default")
     end
-  
-    def show
-      super
-      redirect_to debate_path(@debate), status: :moved_permanently if request.path != debate_path(@debate)
-    end
-  
-    def vote
-      @debate.register_vote(current_user, params[:value])
-    end
-  
-    def unmark_featured
-      @debate.update!(featured_at: nil)
-      redirect_to debates_path
-    end
-  
-    def mark_featured
-      @debate.update!(featured_at: Time.current)
-      redirect_to debates_path
-    end
-  
-    def disable_recommendations
-      if current_user.update(recommended_debates: false)
-        redirect_to debates_path, notice: t("debates.index.recommendations.actions.success")
-      else
-        redirect_to debates_path, error: t("debates.index.recommendations.actions.error")
-      end
-    end
-  
-    private
-  
-      def debate_params
-        params.require(:debate).permit(allowed_params)
-      end
-  
-      def allowed_params
-        [:tag_list, :terms_of_service, :related_sdg_list, translation_params(Debate)]
-      end
-  
-      def resource_model
-        Debate
-      end
-  
-      def set_view
-        @view = (params[:view] == "minimal") ? "minimal" : "default"
-      end
-  
-      def debates_recommendations
-        if Setting["feature.user.recommendations_on_debates"] && current_user.recommended_debates
-          @recommended_debates = Debate.recommendations(current_user).sort_by_random.limit(3)
-        end
-      end
   end
   
+  def show
+    super
+    redirect_to debate_path(@debate), status: :moved_permanently if request.path != debate_path(@debate)
+  end
+
+  def vote
+    @debate.register_vote(current_user, params[:value])
+  end
+
+  def unmark_featured
+    @debate.update!(featured_at: nil)
+    redirect_to debates_path
+  end
+
+  def mark_featured
+    @debate.update!(featured_at: Time.current)
+    redirect_to debates_path
+  end
+
+  def disable_recommendations
+    if current_user.update(recommended_debates: false)
+      redirect_to debates_path, notice: t("debates.index.recommendations.actions.success")
+    else
+      redirect_to debates_path, error: t("debates.index.recommendations.actions.error")
+    end
+  end
+
+  private
+
+    def debate_params
+      params.require(:debate).permit(allowed_params)
+    end
+
+    def allowed_params
+      [:tag_list, :terms_of_service, :related_sdg_list, translation_params(Debate)]
+    end
+
+    def resource_model
+      Debate
+    end
+
+    def set_view
+      @view = (params[:view] == "minimal") ? "minimal" : "default"
+    end
+
+    def debates_recommendations
+      if Setting["feature.user.recommendations_on_debates"] && current_user.recommended_debates
+        @recommended_debates = Debate.recommendations(current_user).sort_by_random.limit(3)
+      end
+    end
+end
---
+++ app/models/custom/debate.rb
@@ -0,0 +1,184 @@
+require "numeric"
+
+class Debate < ApplicationRecord
+  include Flaggable
+  include Taggable
+  include Conflictable
+  include Measurable
+  include Sanitizable
+  include Searchable
+  include Filterable
+  include HasPublicAuthor
+  include Graphqlable
+  include Relationable
+  include Notifiable
+  include Randomizable
+  include SDG::Relatable
+
+  acts_as_votable
+  acts_as_paranoid column: :hidden_at
+  include ActsAsParanoidAliases
+
+  translates :title, touch: true
+  translates :description, touch: true
+  include Globalizable
+
+  belongs_to :author, -> { with_hidden }, class_name: "User", inverse_of: :debates
+  belongs_to :geozone
+  has_many :comments, as: :commentable, inverse_of: :commentable
+
+  validates_translation :title, presence: true, length: { in: 4..Debate.title_max_length }
+  validates_translation :description, presence: true
+  validate :description_length
+  validates :author, presence: true
+
+  validates :terms_of_service, acceptance: { allow_nil: false }, on: :create
+
+  before_save :calculate_hot_score, :calculate_confidence_score
+
+  scope :for_render,               -> { includes(:tags) }
+  scope :sort_by_hot_score,        -> { reorder(hot_score: :desc) }
+  scope :sort_by_confidence_score, -> { reorder(confidence_score: :desc) }
+  scope :sort_by_created_at,       -> { reorder(created_at: :desc) }
+  scope :sort_by_most_commented,   -> { reorder(comments_count: :desc) }
+  scope :sort_by_relevance,        -> { all }
+  scope :sort_by_flags,            -> { order(flags_count: :desc, updated_at: :desc) }
+  scope :sort_by_recommendations,  -> { order(cached_votes_total: :desc) }
+  scope :last_week,                -> { where("created_at >= ?", 7.days.ago) }
+  scope :featured,                 -> { where.not(featured_at: nil) }
+  scope :public_for_api,           -> { all }
+
+  visitable class_name: "Visit"
+
+  attr_accessor :link_required
+
+  def self.recommendations(user)
+    tagged_with(user.interests, any: true).where.not(author_id: user.id)
+  end
+
+  def searchable_translations_definitions
+    { title => "A",
+      description => "D" }
+  end
+
+  def searchable_values
+    {
+      author.username => "B",
+      tag_list.join(" ") => "B",
+      geozone&.name => "B"
+    }.merge!(searchable_globalized_values)
+  end
+
+  def self.search(terms)
+    pg_search(terms)
+  end
+
+  def to_param
+    "#{id}-#{title}".parameterize
+  end
+
+  def likes
+    cached_votes_up
+  end
+
+  def dislikes
+    cached_votes_down
+  end
+
+  def total_votes
+    cached_votes_total
+  end
+
+  def votes_score
+    cached_votes_score
+  end
+
+  def total_anonymous_votes
+    cached_anonymous_votes_total
+  end
+
+  def editable?
+    total_votes <= Setting["max_votes_for_debate_edit"].to_i
+  end
+
+  def editable_by?(user)
+    editable? && author == user
+  end
+
+  def register_vote(user, vote_value)
+    if votable_by?(user)
+      transaction do
+        if user.unverified? && !user.voted_for?(self)
+          Debate.increment_counter(:cached_anonymous_votes_total, id)
+        end
+
+        vote_by(voter: user, vote: vote_value)
+      end
+    end
+  end
+
+  def votable_by?(user)
+    return false unless user
+
+    total_votes <= 100 ||
+      !user.unverified? ||
+      Setting["max_ratio_anon_votes_on_debates"].to_i == 100 ||
+      anonymous_votes_ratio < Setting["max_ratio_anon_votes_on_debates"].to_i ||
+      user.voted_for?(self)
+  end
+
+  def anonymous_votes_ratio
+    return 0 if cached_votes_total == 0
+
+    (cached_anonymous_votes_total.to_f / cached_votes_total) * 100
+  end
+
+  def after_commented
+    save # update cache when it has a new comment
+  end
+
+  def calculate_hot_score
+    self.hot_score = ScoreCalculator.hot_score(self)
+  end
+
+  def calculate_confidence_score
+    self.confidence_score = ScoreCalculator.confidence_score(cached_votes_total,
+                                                             cached_votes_up)
+  end
+
+  def after_hide
+    tags.each { |t| t.decrement_custom_counter_for("Debate") }
+  end
+
+  def after_restore
+    tags.each { |t| t.increment_custom_counter_for("Debate") }
+  end
+
+  def featured?
+    featured_at.present?
+  end
+
+  def self.debates_orders(user)
+    orders = %w[created_at]
+
+    if Setting["feature.user.recommendations_on_debates"] && user&.recommended_debates
+      orders << "recommendations"
+    end
+
+    orders
+  end
+
+  def description_length
+    real_description_length = ActionView::Base.full_sanitizer.sanitize(description.to_s).squish.length
+
+    if real_description_length < Debate.description_min_length
+      errors.add(:description, :too_short, count: Debate.description_min_length)
+      translation.errors.add(:description, :too_short, count: Debate.description_min_length)
+    end
+
+    if real_description_length > Debate.description_max_length
+      errors.add(:description, :too_long, count: Debate.description_max_length)
+      translation.errors.add(:description, :too_long, count: Debate.description_max_length)
+    end
+  end
+end
