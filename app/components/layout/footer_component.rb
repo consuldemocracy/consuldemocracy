05345fb43c54d632bfd18405c0707c150b28a805
Javi Martín <javim@elretirao.net>
2023-10-08 22:15:38 +0200

    Extract methods in footer component
    This way it'll be easier to change and refactor this code.

--- app/components/layout/footer_component.rb
+++ app/components/layout/footer_component.rb
@@ -4,4 +4,16 @@ class Layout::FooterComponent < ApplicationComponent
   def footer_legal_content_block
     content_block("footer_legal")
   end
+
+  private
+
+    def open_source_link
+      link_to(t("layouts.footer.open_source"), t("layouts.footer.open_source_url"), target: "_blank",
+                                                                                    rel: "nofollow")
+    end
+
+    def repository_link
+      link_to(t("layouts.footer.consul"), t("layouts.footer.consul_url"), target: "_blank",
+                                                                          rel: "nofollow")
+    end
 end
