b5c13d63d1b329f12ce7cdcedc0d0b879a870594
jensconsul <145969776+jensconsul@users.noreply.github.com>
2023-10-11 14:59:24 +0200

    Add information about the project to admin index
    This way it'll be easier for people to know how to contact the Consul
    Democracy Foundation.
    
    Co-Authored-By: Javi Martín <javim@elretirao.net>

--- app/components/admin/dashboard/index_component.rb
+++ app/components/admin/dashboard/index_component.rb
@@ -4,4 +4,18 @@ class Admin::Dashboard::IndexComponent < ApplicationComponent
   def title
     t("admin.dashboard.index.title")
   end
+
+  private
+
+    def info
+      sanitize t("admin.dashboard.index.info")
+    end
+
+    def email_link
+      mail_to "info@consulfoundation.org"
+    end
+
+    def website_link
+      link_to "https://consuldemocracy.org", "https://consuldemocracy.org"
+    end
 end
