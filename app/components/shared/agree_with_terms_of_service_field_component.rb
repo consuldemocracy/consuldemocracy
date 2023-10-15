c2710de5fcdce2465ee1b7b3117079e08d3f6fab
Javi Martín <javim@elretirao.net>
2023-10-15 16:39:01 +0200

    Extract method to open links in a new window
    This way we can slightly simplify the code.

--- app/components/shared/agree_with_terms_of_service_field_component.rb
+++ app/components/shared/agree_with_terms_of_service_field_component.rb
@@ -1,5 +1,6 @@
 class Shared::AgreeWithTermsOfServiceFieldComponent < ApplicationComponent
   attr_reader :form
+  delegate :new_window_link_to, to: :helpers
 
   def initialize(form)
     @form = form
@@ -9,8 +10,7 @@ class Shared::AgreeWithTermsOfServiceFieldComponent < ApplicationComponent
 
     def label
       t("form.accept_terms",
-        policy: link_to(t("form.policy"), "/privacy", target: "_blank", title: t("shared.target_blank")),
-        conditions: link_to(t("form.conditions"), "/conditions", target: "_blank",
-                                                                 title: t("shared.target_blank")))
+        policy: new_window_link_to(t("form.policy"), "/privacy"),
+        conditions: new_window_link_to(t("form.conditions"), "/conditions"))
     end
 end
