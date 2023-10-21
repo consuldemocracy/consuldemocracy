cc50f2905667c29f45cb94b2480f10a941ef559b
ssnioka <simonas.snioka@gmail.com>
2023-10-21 15:04:10 +0300

    add lithuanian translations

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,5 +1,8 @@
 module Consul
   class Application < Rails::Application
+    config.i18n.available_locales = [:lt, :en]
     config.i18n.default_locale = :en
+    config.time_zone = "Europe/Vilnius"
+
   end
 end
