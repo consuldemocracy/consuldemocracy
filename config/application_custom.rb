6f2bcbf2d950f38af5eaa0c4e564b8c5845c17e7
ssnioka <simonas.snioka@gmail.com>
2023-10-21 15:21:42 +0300

    Edit default locale

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,7 +1,7 @@
 module Consul
   class Application < Rails::Application
     config.i18n.available_locales = [:lt, :en]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :lt
     config.time_zone = "Europe/Vilnius"
 
   end
