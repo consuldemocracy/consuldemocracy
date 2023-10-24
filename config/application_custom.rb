5859a7ba9e52a807d1c1b69cf0782b20c1135914
ssnioka <simonas.snioka@gmail.com>
2023-10-24 14:29:38 +0300

    Edit timezone, fix debate creation bug

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,8 +1,8 @@
 module Consul
   class Application < Rails::Application
+    config.time_zone = "Vilnius"
     config.i18n.available_locales = [:lt, :en]
     config.i18n.default_locale = :lt
-    config.time_zone = "Europe/Vilnius"
 
   end
 end
