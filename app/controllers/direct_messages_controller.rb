54b6e1ea24b03e5690eba6045956fe3151c6217a
Javi Martín <javim@elretirao.net>
2023-11-24 14:36:51 +0100

    Use explicit paths in direct messages URLs
    This way it's easier to search the code for places using them.

--- app/controllers/direct_messages_controller.rb
+++ app/controllers/direct_messages_controller.rb
@@ -14,7 +14,9 @@ class DirectMessagesController < ApplicationController
     if @direct_message.save
       Mailer.direct_message_for_receiver(@direct_message).deliver_later
       Mailer.direct_message_for_sender(@direct_message).deliver_later
-      redirect_to [@receiver, @direct_message], notice: I18n.t("flash.actions.create.direct_message")
+
+      redirect_to user_direct_message_path(@receiver, @direct_message),
+                  notice: I18n.t("flash.actions.create.direct_message")
     else
       render :new
     end
