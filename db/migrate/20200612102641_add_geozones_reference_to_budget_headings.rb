d169f3b2db883b36c8fe0117d9bc94105f24779b
Julian Herrero <microweb10@gmail.com>
2024-01-22 11:04:21 +0100

    Fix specs (db_schema)

--- db/migrate/20200612102641_add_geozones_reference_to_budget_headings.rb
+++ db/migrate/20200612102641_add_geozones_reference_to_budget_headings.rb
@@ -1,5 +1,7 @@
 class AddGeozonesReferenceToBudgetHeadings < ActiveRecord::Migration[5.0]
   def change
-    add_reference :budget_headings, :geozone, index: true, foreign_key: true
+    unless column_exists? :budget_headings, :geozone_id
+      add_reference :budget_headings, :geozone, index: true, foreign_key: true
+    end
   end
 end
