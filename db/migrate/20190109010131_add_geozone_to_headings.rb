558e7b3a9cfe52f494c09edd04a0d7f322c483bd
Julian Herrero <microweb10@gmail.com>
2024-02-01 14:59:30 +0100

    Add condition to avoid exception when migrating the database

--- db/migrate/20190109010131_add_geozone_to_headings.rb
+++ db/migrate/20190109010131_add_geozone_to_headings.rb
@@ -1,5 +1,7 @@
 class AddGeozoneToHeadings < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :budget_headings, :geozone, index: true, foreign_key: true
+  unless column_exists? :budget_headings, :geozone_id
+    def change
+      add_reference :budget_headings, :geozone, index: true, foreign_key: true
+    end
   end
 end
