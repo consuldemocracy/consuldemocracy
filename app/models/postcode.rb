9ca227bf6766a2137fb128d9d980dd4da4deeba8
CoslaJohn <johnmunro@cosla.gov.uk>
2024-02-02 10:44:45 +0000

    Create postcode model and migration to add fields
     Changes to be committed:
    	new file:   app/models/postcode.rb
    	new file:   db/migrate/20230302092141_create_postcodes.rb

---
+++ app/models/postcode.rb
@@ -0,0 +1,20 @@
+class Postcode < ApplicationRecord
+  include Graphqlable
+
+  has_many :geozones
+  validates :postcode, presence: true
+  scope :public_for_api, -> { all }
+
+  def self.names
+    Postcode.pluck(:postcode)
+  end
+
+  def safe_to_destroy?
+    Postcode.reflect_on_all_associations(:has_many).all? do |association|
+      association.klass.where(name: self).empty?
+    end
+  end
+
+  def human_name
+  end
+end
