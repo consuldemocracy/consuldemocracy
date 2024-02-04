2ec985bd73e899cf99d03cd30879c1b7161b22d5
Max Stevens <maxstevens2708@gmail.com>
2024-02-04 13:04:37 +0100

    Update subnavigation_component

---
+++ tenants/base/app/components/custom/layout/subnavigation_component.rb
@@ -0,0 +1,6 @@
+class Layout::SubnavigationComponent < ApplicationComponent; end
+
+require_dependency Rails.root.join("app", "components", "layout", "subnavigation_component").to_s
+
+class Admin::TableActionsComponent
+end
