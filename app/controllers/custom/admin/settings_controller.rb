5165b7b8830d81e307494d48e796d9dd7027d124
Max Stevens <maxstevens2708@gmail.com>
2024-02-04 12:46:09 +0100

    Add participation_processes_navbar_settings

---
+++ app/controllers/custom/admin/settings_controller.rb
@@ -0,0 +1,16 @@
+require_dependency Rails.root.join("app", "controllers", "admin", "settings_controller").to_s
+
+class Admin::SettingsController
+  alias :old_index :index
+
+  def index
+    old_index
+
+    all_settings = Setting.all.group_by(&:type)
+    @participation_processes_navbar_settings = all_settings["process_navbar"]
+
+    # See if this code gets executed
+    puts "This is the new index method"
+    puts all_settings
+  end
+end
