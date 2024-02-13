122cbc9c52eee144715665166fb2215b85f383a5
Max Stevens <maxstevens2708@gmail.com>
2024-02-13 12:13:43 +0100

    Update main.js script

--- app/assets/javascripts/custom/main.js
+++ app/assets/javascripts/custom/main.js
@@ -1,9 +1,22 @@
-const all_links = document.querySelectorAll("a");
+window.addEventListener("load", function () {
+  const allLinks = document.querySelectorAll("a");
+  openInNewTab(allLinks);
 
-for (let i = 0; i < all_links.length; i++) {
-  const a = all_links[i];
-  if (a.hostname != location.hostname) {
-    a.rel = "noopener noreferrer";
-    a.target = "_blank";
+  const mutationObserver = new MutationObserver(() => {
+    const allLinks = document.querySelectorAll("a");
+    openInNewTab(allLinks);
+  });
+
+  const html = document.querySelector("html");
+  mutationObserver.observe(html, { childList: true, subtree: true });
+});
+
+function openInNewTab(links) {
+  for (let i = 0; i < links.length; i++) {
+    const a = links[i];
+    if (a.hostname != location.hostname) {
+      a.rel = "noopener noreferrer";
+      a.target = "_blank";
+    }
   }
 }
