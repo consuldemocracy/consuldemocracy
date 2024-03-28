e1e5efe34b869f0166d6dca3786ee5342f73fb8d
Javi Martín <javim@elretirao.net>
2024-03-28 03:08:33 +0100

    Wrap parameters by default in ActionController
    As mentioned in the Rails configuration documentation [1] (note the link
    points to the configuration guide for Rails 7.1, but only because the
    documentation for this option wasn't as good in the configuration guide
    for Rails 7.0; the behavior hasn't changed between these two versions),
    this was done in the `wrap_parameters` initializer but now it can be
    done using a new default configuration option.
    
    [1] https://guides.rubyonrails.org/v7.1/configuring.html#config-action-controller-wrap-parameters-by-default

--- config/initializers/new_framework_defaults_7_0.rb
+++ config/initializers/new_framework_defaults_7_0.rb
@@ -73,7 +73,7 @@ Rails.application.config.action_controller.raise_on_open_redirects = true
 # Enable parameter wrapping for JSON.
 # Previously this was set in an initializer. It's fine to keep using that initializer if you've customized it.
 # To disable parameter wrapping entirely, set this config to `false`.
-# Rails.application.config.action_controller.wrap_parameters_by_default = true
+Rails.application.config.action_controller.wrap_parameters_by_default = true
 
 # Specifies whether generated namespaced UUIDs follow the RFC 4122 standard for namespace IDs provided as a
 # `String` to `Digest::UUID.uuid_v3` or `Digest::UUID.uuid_v5` method calls.
