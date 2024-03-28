a8047a96c89986655e899a50496baa11171184d8
Javi Martín <javim@elretirao.net>
2024-03-28 22:26:15 +0100

    Include a hidden field in multiple file inputs
    This doesn't really affect us because we don'thave any multiple file
    inputs in the application, but we're enabling it because it's the new
    default configuration option.

--- config/initializers/new_framework_defaults_7_0.rb
+++ config/initializers/new_framework_defaults_7_0.rb
@@ -107,7 +107,7 @@ Rails.application.config.action_dispatch.return_only_request_media_type_on_conte
 # Active Storage `has_many_attached` relationships will default to replacing the current collection instead of appending to it.
 # Thus, to support submitting an empty collection, the `file_field` helper will render an hidden field `include_hidden` by default when `multiple_file_field_include_hidden` is set to `true`.
 # See https://guides.rubyonrails.org/configuring.html#config-active-storage-multiple-file-field-include-hidden for more information.
-# Rails.application.config.active_storage.multiple_file_field_include_hidden = true
+Rails.application.config.active_storage.multiple_file_field_include_hidden = true
 
 # ** Please read carefully, this must be configured in config/application.rb (NOT this file) **
 # Disables the deprecated #to_s override in some Ruby core classes
