f30fa29994e0b650e86f6a2d4a0216d82d95cb36
Julian Herrero <microweb10@gmail.com>
2024-03-28 13:45:06 +0100

    Allow running specs scenarios using the Rails cache
    Cache is by default disabled on every non-production environment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -193,6 +193,11 @@ RSpec.configure do |config|
     savon.unmock!
   end
 
+  config.before(:each, :with_cache) do
+    allow(Rails).to receive(:cache).and_return(ActiveSupport::Cache.lookup_store(:memory_store))
+    Rails.cache.clear
+  end
+
   # Allows RSpec to persist some state between runs in order to support
   # the `--only-failures` and `--next-failure` CLI options.
   config.example_status_persistence_file_path = "spec/examples.txt"
