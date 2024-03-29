b5b03e44960ef031e2297bc03052cff95df7e8a8
Javi Martín <javim@elretirao.net>
2024-03-29 16:37:38 +0100

    Base staging configuration on the production one
    There was only one different line between these two lines, and we really
    want these two files to be as similar as possible, so testing on a
    staging server we get the same results we'll get on production.
    
    This duplication made it easy to forget to update the staging.rb file
    when updating the production.rb, particularly when upgrading to a new
    version of Rails, which automatically changes the production.rb file but
    not the staging.rb one.
    
    So, now that we're getting ready to upgrade to Rails 7.0, we're basing
    the staging configuration on the production one, just like we based the
    preproduction configuration on the staging one in commit 7b91adb16.

--- config/environments/custom/production.rb
+++ config/environments/custom/production.rb
@@ -1,4 +1,8 @@
 Rails.application.configure do
   # Overwrite settings for the production environment or add your own
-  # custom settings for this environment.
+  # custom settings for this environment. Note these changes will
+  # also affect the staging and preproduction environments, so edit the
+  # `config/environments/custom/staging.rb` and/or
+  # `config/environments/custom/preproduction.rb` files if you don't
+  # want these changes to be applied there as well.
 end
