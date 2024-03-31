df17bd1354820c484ffc2e0ece2b045d4d0dec61
Javi Martín <javim@elretirao.net>
2024-03-31 18:34:09 +0200

    Ask confirmation to delete pages from the edit page
    We were already doing that when deleting pages from the index page, and
    we also ask for confirmation in almost every page in the admin section.

--- spec/system/admin/site_customization/pages_spec.rb
+++ spec/system/admin/site_customization/pages_spec.rb
@@ -93,7 +93,9 @@ describe "Admin custom pages", :admin do
     custom_page = create(:site_customization_page, title: "An example custom page")
     visit edit_admin_site_customization_page_path(custom_page)
 
-    click_button "Delete page"
+    accept_confirm "Are you sure? This action will delete \"An example custom page\" and can't be undone." do
+      click_button "Delete page"
+    end
 
     expect(page).not_to have_content "An example custom page"
     expect(page).not_to have_content "example-page"
