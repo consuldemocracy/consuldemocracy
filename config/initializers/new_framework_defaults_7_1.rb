00be1cadbbc07af7763baf6f8dda742efa1aa85d
Javi Martín <javim@elretirao.net>
2024-04-15 04:23:39 +0200

    Use Rails 7.1 default options for SQLite
    It doesn't really affect us because we don't use SQLite.

--- config/initializers/new_framework_defaults_7_1.rb
+++ config/initializers/new_framework_defaults_7_1.rb
@@ -66,7 +66,7 @@ Rails.application.config.active_record.run_commit_callbacks_on_first_saved_insta
 # For example, it is possible to create an index for a non existing column.
 # See https://www.sqlite.org/quirks.html#double_quoted_string_literals_are_accepted for more details.
 #++
-# Rails.application.config.active_record.sqlite3_adapter_strict_strings_by_default = true
+Rails.application.config.active_record.sqlite3_adapter_strict_strings_by_default = true
 
 ###
 # Disable deprecated singular associations names.
