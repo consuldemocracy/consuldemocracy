7f0503c56dbf385941dc81c785a421ac55283400
Javi Martín <javim@elretirao.net>
2024-04-15 03:38:14 +0200

    TODO: check whether we need `pool: false` or everything still works (see guide to upgrade from Rails 7.0 to Rails 7.1)
    https://guides.rubyonrails.org/v7.1/upgrading_ruby_on_rails.html#memcachestore-and-rediscachestore-now-use-connection-pooling-by-default

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   config.cache_store = :mem_cache_store, { namespace: proc { Tenant.current_schema }}
+  # config.cache_store = :mem_cache_store, "cache.example.com", { pool: false }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
