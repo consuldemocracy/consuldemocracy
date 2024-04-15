028cf2abc5274266a966aca8dbaa37a90b2d7e7f
Javi Martín <javim@elretirao.net>
2024-04-15 14:29:09 +0200

    Use SQLCommenter format to format tags in Query Logs
    Don't really care about the format, so we'll use the new default one.

--- config/initializers/new_framework_defaults_7_1.rb
+++ config/initializers/new_framework_defaults_7_1.rb
@@ -98,7 +98,7 @@ Rails.application.config.active_support.raise_on_invalid_cache_expiration_time =
 # (https://open-telemetry.github.io/opentelemetry-sqlcommenter/), or using the legacy format.
 # Options are `:legacy` and `:sqlcommenter`.
 #++
-# Rails.application.config.active_record.query_log_tags_format = :sqlcommenter
+Rails.application.config.active_record.query_log_tags_format = :sqlcommenter
 
 ###
 # Specify the default serializer used by `MessageEncryptor` and `MessageVerifier`
