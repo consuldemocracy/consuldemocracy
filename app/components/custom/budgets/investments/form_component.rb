5a37671c238623e2d839cbc30e7a07d845d92a2a
CoslaJohn <johnmunro@cosla.gov.uk>
2024-04-18 09:34:34 +0000

    Adding Summary field to Investment Form

---
+++ app/components/custom/budgets/investments/form_component.rb
@@ -0,0 +1,24 @@
+class Budgets::Investments::FormComponent < ApplicationComponent; end
+
+require_dependency Rails.root.join("app", "components", "custom", "budgets", "investments", "form_component").to_s
+class Budgets::Investments::FormComponent < ApplicationComponent
+  include TranslatableFormHelper
+  include GlobalizeHelper
+  attr_reader :investment, :url
+  delegate :current_user, :budget_heading_select_options, :suggest_data, to: :helpers
+
+  def initialize(investment, url:)
+    @investment = investment
+    @url = url
+  end
+
+  private
+
+    def budget
+      investment.budget
+    end
+
+    def categories
+      Tag.category.order(:name)
+    end
+end
