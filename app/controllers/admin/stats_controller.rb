646dccca0ab26abd7a9808cfd4e567ad8f68ae80
Javi Martín <javim@elretirao.net>
2024-04-24 19:58:33 +0200

    Rename event_types variable to event_names
    This is consistent with the `name` column in `ahoy_events`.

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -1,6 +1,6 @@
 class Admin::StatsController < Admin::BaseController
   def show
-    @event_types = Ahoy::Event.distinct.order(:name).pluck(:name)
+    @event_names = Ahoy::Event.distinct.order(:name).pluck(:name)
 
     @visits    = Visit.count
     @debates   = Debate.with_hidden.count
