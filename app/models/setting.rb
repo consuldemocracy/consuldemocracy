631b48f5866e80a81f2ef271c6bd786c6ff0004e
Javi Martín <javim@elretirao.net>
2024-04-26 01:03:07 +0200

    Remove public stats
    This page isn't linked from anywhere and most Consul Democracy
    installations don't even know it exists, so it's useless for most
    people.
    
    If we ever bring it back, we should at least add a link pointing to this
    page.

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -70,7 +70,6 @@ class Setting < ApplicationRecord
         "feature.google_login": true,
         "feature.twitter_login": true,
         "feature.wordpress_login": false,
-        "feature.public_stats": true,
         "feature.signature_sheets": true,
         "feature.user.recommendations": true,
         "feature.user.recommendations_on_debates": true,
--- spec/system/notifications_spec.rb
+++ spec/system/notifications_spec.rb
@@ -164,14 +164,15 @@ describe "Notifications" do
     end
 
     scenario "With internal link" do
-      admin_notification.update!(link: "/stats")
+      admin_notification.update!(link: "/debates")
 
       visit notifications_path
       expect(page).to have_content("Notification title")
       expect(page).to have_content("Notification body")
 
       first("#notification_#{notification.id} a").click
-      expect(page).to have_current_path("/stats")
+
+      expect(page).to have_current_path "/debates"
     end
 
     scenario "Without a link" do
