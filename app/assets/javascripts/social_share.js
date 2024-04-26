d19e72b432b27a5e23ddb11f2a0aa32457d412bc
Javi Martín <javim@elretirao.net>
2024-04-26 18:09:40 +0200

    Remove Turbolinks to test accessibility on click_link
    Note we're commenting a couple of failures that have nothing to do with
    missing labels in forms:
    
    * In the proposals spec, the browser doesn't follow the redirect when
      Turbolinks is disabled, so the test fails
    * In the tests for the form to edit a legislation draft, we click o a
      link in the navigation to leave the page, which triggers an alert and
      causes the accessibility checks after `click_link` to fail

--- app/assets/javascripts/social_share.js
+++ app/assets/javascripts/social_share.js
@@ -5,9 +5,6 @@
       $(".social-share-button a").each(function() {
         $(this).append("<span class='show-for-sr'>" + ($(this).data("site").replace(/_app$/, "")) + "</span>");
       });
-    },
-    destroy: function() {
-      $(".social-share-button a .show-for-sr").remove();
     }
   };
 }).call(this);
