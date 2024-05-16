e0d965db30c3c65f57df5d04bf030cd579945c74
Javi Martín <javim@elretirao.net>
2024-05-16 02:43:55 +0200

    Use checkboxes and radio buttons on poll forms
    Our original interface to vote in a poll had a few issues:
    
    * Since there was no button to send the form, it wasn't clear that
      selecting an option would automatically store it in the database.
    * The interface was almost identical for single-choice questions and
      multiple-choice questions, which made it hard to know which type of
      question we were answering.
    * Adding other type of questions, like open answers, was hard since we
      would have to add a different submit button for each answer.
    
    So we're now using radio buttons for single-choice questions and
    checkboxes for multiple-choice questions, which are the native controls
    designed for these purposes, and a button to send the whole form.
    
    Since we don't have a database table for poll ballots like we have for
    budget ballots, we're adding a new `Poll::WebVote` model to manage poll
    ballots. We're using WebVote instead of Ballot or Vote because they
    could be mistaken with other vote classes.
    
    Note that browsers don't allow removing answers with radio buttons, so
    once somebody has voted in a single-choice question, they can't remove
    the vote unless they manually edit their HTML. This is the same behavior
    we had before commit 7df0e9a96.
    
    As mentioned in c2010f975, we're now adding the `ChangeByZero` rubocop
    rule, since we've removed the test that used `and change`.

--- app/components/polls/questions/question_component.rb
+++ app/components/polls/questions/question_component.rb
@@ -1,13 +1,65 @@
 class Polls::Questions::QuestionComponent < ApplicationComponent
-  attr_reader :question
+  attr_reader :question, :disabled
+  alias_method :disabled?, :disabled
+  use_helpers :current_user
 
-  def initialize(question)
+  def initialize(question, disabled: false)
     @question = question
+    @disabled = disabled
   end
 
-  def options_read_more_links
-    safe_join(question.options_with_read_more.map do |option|
-      link_to option.title, "#option_#{option.id}"
-    end, ", ")
-  end
+  private
+
+    def fieldset_attributes
+      tag.attributes(
+        id: dom_id(question),
+        disabled: ("disabled" if disabled?),
+        data: { max_votes: question.max_votes }
+      )
+    end
+
+    def options_read_more_links
+      safe_join(question.options_with_read_more.map do |option|
+        link_to option.title, "#option_#{option.id}"
+      end, ", ")
+    end
+
+    def multiple_choice?
+      question.multiple?
+    end
+
+    def multiple_choice_help_text
+      tag.span(
+        t("poll_questions.description.multiple", maximum: question.max_votes),
+        class: "help-text"
+      )
+    end
+
+    def multiple_choice_field(option)
+      choice_field(option) do
+        check_box_tag "web_vote[#{question.id}][option_id][]",
+                      option.id,
+                      checked?(option),
+                      id: "web_vote_option_#{option.id}"
+      end
+    end
+
+    def single_choice_field(option)
+      choice_field(option) do
+        radio_button_tag "web_vote[#{question.id}][option_id]",
+                         option.id,
+                         checked?(option),
+                         id: "web_vote_option_#{option.id}"
+      end
+    end
+
+    def choice_field(option, &block)
+      label_tag("web_vote_option_#{option.id}") do
+        block.call + option.title
+      end
+    end
+
+    def checked?(option)
+      question.answers.where(author: current_user, option: option).any?
+    end
 end
---
+++ app/models/poll/web_vote.rb
@@ -0,0 +1,43 @@
+class Poll::WebVote
+  attr_reader :poll, :user
+
+  def initialize(poll, user)
+    @poll = poll
+    @user = user
+  end
+
+  def questions
+    poll.questions.for_render.sort_for_list
+  end
+
+  def update(params)
+    all_valid = true
+
+    user.with_lock do
+      unless questions.any? { |question| params.dig(question.id.to_s, :option_id).present? }
+        Poll::Voter.find_by(user: user, poll: poll, origin: "web")&.destroy!
+      end
+
+      questions.each do |question|
+        question.answers.where(author: user).destroy_all
+        next unless params[question.id.to_s]
+
+        option_ids = params[question.id.to_s][:option_id]
+
+        answers = Array(option_ids).map do |option_id|
+          question.find_or_initialize_user_answer(user, option_id)
+        end
+
+        if answers.map(&:valid?).all?(true)
+          Poll::Voter.find_or_create_by!(user: user, poll: poll, origin: "web")
+          answers.each(&:save!)
+        else
+          all_valid = false
+          raise ActiveRecord::Rollback
+        end
+      end
+    end
+
+    all_valid
+  end
+end
