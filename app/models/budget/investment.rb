2701c3a6ad3212d2269c93c50f7af4a7b4936b1a
CoslaJohn <johnmunro@cosla.gov.uk>
2024-06-25 09:03:15 +0000

    do not display estimated price if hide money

--- app/models/budget/investment.rb
+++ app/models/budget/investment.rb
@@ -352,6 +352,10 @@ class Budget
     def should_show_price_explanation?
       should_show_price? && price_explanation.present?
     end
+    
+    def should_show_estimated_price?
+      estimated_price.present? && budget.show_money?
+    end
 
     def should_show_unfeasibility_explanation?
       unfeasible? && valuation_finished? && unfeasibility_explanation.present?
