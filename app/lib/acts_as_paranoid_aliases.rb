f690e70b9ec55637314288b3147df94ae75bcfe2
Javi Martín <javim@elretirao.net>
2024-07-02 18:22:38 +0200

    Add and apply Style/SuperArguments rubocop rule
    This rule was added in rubocop 1.64.0.
    
    For clarity, in order to make it obvious that we're modifying the object
    we received, we're excluding the Ahoy initializer, whose code was copied
    from the Ahoy documentation.
    
    We're also changing the `Types::BaseObject` class so we don't use a
    variable with the same name as the parameter and we don't get a false
    positive for this rule.

--- app/lib/acts_as_paranoid_aliases.rb
+++ app/lib/acts_as_paranoid_aliases.rb
@@ -27,7 +27,7 @@ module ActsAsParanoidAliases
       def restore(opts = {})
         return false unless hidden?
 
-        super(opts)
+        super
         update_attribute(:confirmed_hide_at, nil) if self.class.column_names.include? "confirmed_hide_at"
         after_restore
       end
