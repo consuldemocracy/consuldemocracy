69361028b6f83791759df818adeff872ec843bf8
CoslaJohn <johnmunro@cosla.gov.uk>
2024-07-05 09:11:16 +0000

    tidying up release 2.2 merge

--- app/controllers/custom/budgets/investments_controller.rb
+++ app/controllers/custom/budgets/investments_controller.rb
@@ -109,7 +109,7 @@ module Budgets
       end
 
       def allowed_params
-        attributes = [:video_url, :estimated_price,:summary,  :heading_id, :tag_list, :organization_name, :location,
+        attributes = [:video_url, :estimated_price, :summary,  :heading_id, :tag_list, :organization_name, :location,
                       :terms_of_service, :related_sdg_list,
                       image_attributes: image_attributes,
                       documents_attributes: document_attributes,
