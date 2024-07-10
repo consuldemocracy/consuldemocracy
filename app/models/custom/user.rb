c84625d13535ded892eae0246f5d67e07f3df00c
muki <filipdobranic@gmail.com>
2024-07-10 18:29:58 +0200

    fix emšo loophole

--- app/models/custom/user.rb
+++ app/models/custom/user.rb
@@ -41,29 +41,31 @@ class User < ApplicationRecord
     if (Rails.env.development? && email == 'admin@consul.dev') || Rails.env.test?
       return true
     end
-    if /[0123]\d[01]\d[09]\d\d\d{6}/.match(document_number)
-      if document_number[12].to_i == 11 - (
-      (
-      ((document_number[0].to_i + document_number[6].to_i) * 7) +
-        ((document_number[1].to_i + document_number[7].to_i) * 6) +
-        ((document_number[2].to_i + document_number[8].to_i) * 5) +
-        ((document_number[3].to_i + document_number[9].to_i) * 4) +
-        ((document_number[4].to_i + document_number[10].to_i) * 3) +
-        ((document_number[5].to_i + document_number[11].to_i) * 2)
-      ) % 11
-      )
-        return is_old_enough(date_from_emso(document_number))
-      elsif document_number[12].to_i == (
-      ((document_number[0].to_i + document_number[6].to_i) * 7) +
-        ((document_number[1].to_i + document_number[7].to_i) * 6) +
-        ((document_number[2].to_i + document_number[8].to_i) * 5) +
-        ((document_number[3].to_i + document_number[9].to_i) * 4) +
-        ((document_number[4].to_i + document_number[10].to_i) * 3) +
-        ((document_number[5].to_i + document_number[11].to_i) * 2)
-      ) % 11
-        return is_old_enough(date_from_emso(document_number))
-      else
-        return false
+    if document_number.length == 13
+      if /[0123]\d[01]\d[09]\d\d\d{6}/.match(document_number)
+        if document_number[12].to_i == 11 - (
+        (
+        ((document_number[0].to_i + document_number[6].to_i) * 7) +
+          ((document_number[1].to_i + document_number[7].to_i) * 6) +
+          ((document_number[2].to_i + document_number[8].to_i) * 5) +
+          ((document_number[3].to_i + document_number[9].to_i) * 4) +
+          ((document_number[4].to_i + document_number[10].to_i) * 3) +
+          ((document_number[5].to_i + document_number[11].to_i) * 2)
+        ) % 11
+        )
+          return is_old_enough(date_from_emso(document_number))
+        elsif document_number[12].to_i == (
+        ((document_number[0].to_i + document_number[6].to_i) * 7) +
+          ((document_number[1].to_i + document_number[7].to_i) * 6) +
+          ((document_number[2].to_i + document_number[8].to_i) * 5) +
+          ((document_number[3].to_i + document_number[9].to_i) * 4) +
+          ((document_number[4].to_i + document_number[10].to_i) * 3) +
+          ((document_number[5].to_i + document_number[11].to_i) * 2)
+        ) % 11
+          return is_old_enough(date_from_emso(document_number))
+        else
+          return false
+        end
       end
     end
     false
