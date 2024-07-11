7b4f2ccb11a216597d7e4dc4f689c870802f9e62
Max Stevens <maxstevens2708@gmail.com>
2024-07-11 13:20:56 +0200

    Don't include explicit version in footer

--- spec/system/custom/installation_details_spec.rb
+++ spec/system/custom/installation_details_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 describe "Installation details" do
   scenario "Show the current version of CONSUL" do
     visit root_path
-    within(".footer") { expect(page).to have_content "2.0.1" }
+    within(".footer") { expect(page).to have_content "2.1.0" }
   end
 end
