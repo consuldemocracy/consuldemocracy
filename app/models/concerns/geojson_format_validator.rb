8b11af244fd1a0993c2ad3da376ebea300349c31
CoslaJohn <johnmunro@cosla.gov.uk>
2024-07-22 11:14:08 +0100

    update concern to check that coordinates are in the WGS84 format required by Leaflet and provide meaningfule error

--- app/models/concerns/geojson_format_validator.rb
+++ app/models/concerns/geojson_format_validator.rb
@@ -2,8 +2,27 @@ class GeojsonFormatValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if value.present?
       geojson = parse_json(value)
+
       unless valid_geojson?(geojson)
-        record.errors.add(attribute, :invalid)
+        record.errors.add(attribute, :invalid, message: I18n.t("errors.geozone.attributes.geojson.invalid"))
+        return
+      end
+
+      # Validate coordinates if the GeoJSON is valid
+      if geojson["type"] == "FeatureCollection"
+        geojson["features"].each do |feature|
+          unless valid_coordinates?(feature["geometry"])
+            record.errors.add(attribute, :invalid_coordinates, message: I18n.t("errors.geozone.attributes.geojson.invalid_coordinates"))
+          end
+        end
+      elsif geojson["type"] == "Feature"
+        unless valid_coordinates?(geojson["geometry"])
+          record.errors.add(attribute, :invalid_coordinates, message: I18n.t("errors.geozone.attributes.geojson.invalid_coordinates"))
+        end
+      elsif geojson["geometry"]
+        unless valid_coordinates?(geojson["geometry"])
+          record.errors.add(attribute, :invalid_coordinates, message: I18n.t("errors.geozone.attributes.geojson.invalid_coordinates"))
+        end
       end
     end
   end
@@ -19,16 +38,21 @@ class GeojsonFormatValidator < ActiveModel::EachValidator
     def valid_geojson?(geojson)
       return false unless geojson.is_a?(Hash)
 
-      case geojson["type"]
-      when "FeatureCollection"
-        valid_feature_collection?(geojson)
-      when "Feature"
-        valid_feature?(geojson)
-      when "Point", "LineString", "Polygon", "MultiPoint", "MultiLineString",
-           "MultiPolygon", "GeometryCollection"
-        valid_geometry?(geojson)
+      if geojson["type"]
+        case geojson["type"]
+        when "FeatureCollection"
+          valid_feature_collection?(geojson)
+        when "Feature"
+          valid_feature?(geojson)
+        when "Point", "LineString", "Polygon", "MultiPoint", "MultiLineString",
+             "MultiPolygon", "GeometryCollection"
+          valid_geometry?(geojson)
+        else
+          false
+        end
       else
-        false
+        # Check if it is a top-level geometry object
+        geojson["geometry"] && valid_geometry?(geojson["geometry"])
       end
     end
 
@@ -44,7 +68,6 @@ class GeojsonFormatValidator < ActiveModel::EachValidator
       return false unless feature["type"] == "Feature"
       return false unless feature["geometry"].is_a?(Hash)
       return false unless valid_geometry?(feature["geometry"])
-
       true
     end
 
@@ -60,5 +83,40 @@ class GeojsonFormatValidator < ActiveModel::EachValidator
 
       # Additional checks can be added for specific geometry types if needed
       true
+    end
+
+    def valid_coordinates?(geometry)
+      # Coordinates must be an array of numbers
+      return false unless geometry["coordinates"].is_a?(Array)
+
+      case geometry["type"]
+      when "Point"
+        valid_wgs84_coordinates?(geometry["coordinates"])
+      when "LineString", "MultiPoint"
+        geometry["coordinates"].all? { |coord| valid_wgs84_coordinates?(coord) }
+      when "Polygon", "MultiLineString"
+        geometry["coordinates"].all? do |ring|
+          ring.all? { |coord| valid_wgs84_coordinates?(coord) }
+        end
+      when "MultiPolygon"
+        geometry["coordinates"].all? do |polygon|
+          polygon.all? do |ring|
+            ring.all? { |coord| valid_wgs84_coordinates?(coord) }
+          end
+        end
+      when "GeometryCollection"
+        geometry["geometries"].all? { |geom| valid_coordinates?(geom) }
+      else
+        false
+      end
+    end
+
+    def valid_wgs84_coordinates?(coords)
+      # Coordinates should be in [longitude, latitude] format
+      return false unless coords.is_a?(Array) && coords.size == 2
+      longitude, latitude = coords
+      # Check if latitude and longitude are valid numbers and within valid ranges
+      longitude.is_a?(Numeric) && latitude.is_a?(Numeric) &&
+        longitude.between?(-180.0, 180.0) && latitude.between?(-90.0, 90.0)
     end
 end
