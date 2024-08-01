9edd58533765f315ccadf58e20640848c41b741d
CoslaJohn <johnmunro@cosla.gov.uk>
2024-08-01 10:23:12 +0100

    Fix Rubocop errors

--- app/models/concerns/geojson_format_validator.rb
+++ app/models/concerns/geojson_format_validator.rb
@@ -1,29 +1,28 @@
 class GeojsonFormatValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if value.present?
-      geojson = parse_json(value)
-      
+      geojson = parse_json(value)      
       unless valid_geojson?(geojson)
-        record.errors.add(attribute, :invalid, message: I18n.t('errors.geozone.attributes.geojson.invalid'))
+        record.errors.add(attribute, :invalid, message: I18n.t("errors.geozone.attributes.geojson.invalid"))
         return
       end
 
       # Validate coordinates if the GeoJSON is valid
-      if geojson['type'] == 'FeatureCollection'
-        geojson['features'].each do |feature|
-          unless valid_coordinates?(feature['geometry'])
-            record.errors.add(attribute, :invalid_coordinates, message: I18n.t('errors.geozone.attributes.geojson.invalid_coordinates'))
+      if geojson["type"] == "FeatureCollection"
+        geojson["features"].each do |feature|
+          unless valid_coordinates?(feature["geometry"])
+            record.errors.add(attribute, :invalid_coordinates, message: I18n.t("errors.geozone.attributes.geojson.invalid_coordinates"))
             return
           end
         end
-      elsif geojson['type'] == 'Feature'
-        unless valid_coordinates?(geojson['geometry'])
-          record.errors.add(attribute, :invalid_coordinates, message: I18n.t('errors.geozone.attributes.geojson.invalid_coordinates'))
+      elsif geojson["type"] == "Feature"
+        unless valid_coordinates?(geojson["geometry"])
+          record.errors.add(attribute, :invalid_coordinates, message: I18n.t("errors.geozone.attributes.geojson.invalid_coordinates"))
           return
         end
-      elsif geojson['geometry']
-        unless valid_coordinates?(geojson['geometry'])
-          record.errors.add(attribute, :invalid_coordinates, message: I18n.t('errors.geozone.attributes.geojson.invalid_coordinates'))
+      elsif geojson["geometry"]
+        unless valid_coordinates?(geojson["geometry"])
+          record.errors.add(attribute, :invalid_coordinates, message: I18n.t("errors.geozone.attributes.geojson.invalid_coordinates"))
           return
         end
       end
@@ -41,47 +40,47 @@ class GeojsonFormatValidator < ActiveModel::EachValidator
   def valid_geojson?(geojson)
     return false unless geojson.is_a?(Hash)
 
-    if geojson['type']
-      case geojson['type']
-      when 'FeatureCollection'
+    if geojson["type"]
+      case geojson["type"]
+      when "FeatureCollection"
         valid_feature_collection?(geojson)
-      when 'Feature'
+      when "Feature"
         valid_feature?(geojson)
-      when 'Point', 'LineString', 'Polygon', 'MultiPoint', 'MultiLineString', 'MultiPolygon', 'GeometryCollection'
+      when "Point", "LineString", "Polygon", "MultiPoint", "MultiLineString", "MultiPolygon", "GeometryCollection"
         valid_geometry?(geojson)
       else
         false
       end
     else
       # Check if it is a top-level geometry object
-      geojson['geometry'] && valid_geometry?(geojson['geometry'])
+      geojson["geometry"] && valid_geometry?(geojson["geometry"])
     end
   end
 
   def valid_feature_collection?(geojson)
-    return false unless geojson['features'].is_a?(Array)
+    return false unless geojson["features"].is_a?(Array)
 
-    geojson['features'].all? do |feature|
+    geojson["features"].all? do |feature|
       valid_feature?(feature)
     end
   end
 
   def valid_feature?(feature)
-    return false unless feature['type'] == 'Feature'
-    return false unless feature['geometry'].is_a?(Hash)
-    return false unless valid_geometry?(feature['geometry'])
+    return false unless feature["type"] == "Feature"
+    return false unless feature["geometry"].is_a?(Hash)
+    return false unless valid_geometry?(feature["geometry"])
     true
   end
 
   def valid_geometry?(geometry)
     valid_geometry_types = [
-      'Point', 'LineString', 'Polygon',
-      'MultiPoint', 'MultiLineString', 'MultiPolygon',
-      'GeometryCollection'
+      "Point", "LineString", "Polygon",
+      "MultiPoint", "MultiLineString", "MultiPolygon",
+      "GeometryCollection"
     ]
 
-    return false unless valid_geometry_types.include?(geometry['type'])
-    return false unless geometry['coordinates'].is_a?(Array)
+    return false unless valid_geometry_types.include?(geometry["type"])
+    return false unless geometry["coordinates"].is_a?(Array)
 
     # Additional checks can be added for specific geometry types if needed
     true
@@ -89,25 +88,25 @@ class GeojsonFormatValidator < ActiveModel::EachValidator
 
   def valid_coordinates?(geometry)
     # Coordinates must be an array of numbers
-    return false unless geometry['coordinates'].is_a?(Array)
+    return false unless geometry["coordinates"].is_a?(Array)
 
-    case geometry['type']
-    when 'Point'
-      return valid_wgs84_coordinates?(geometry['coordinates'])
-    when 'LineString', 'MultiPoint'
-      return geometry['coordinates'].all? { |coord| valid_wgs84_coordinates?(coord) }
-    when 'Polygon', 'MultiLineString'
-      return geometry['coordinates'].all? do |ring|
+    case geometry["type"]
+    when "Point"
+      return valid_wgs84_coordinates?(geometry["coordinates"])
+    when "LineString", "MultiPoint"
+      return geometry["coordinates"].all? { |coord| valid_wgs84_coordinates?(coord) }
+    when "Polygon", "MultiLineString"
+      return geometry["coordinates"].all? do |ring|
         ring.all? { |coord| valid_wgs84_coordinates?(coord) }
       end
-    when 'MultiPolygon'
-      return geometry['coordinates'].all? do |polygon|
+    when "MultiPolygon"
+      return geometry["coordinates"].all? do |polygon|
         polygon.all? do |ring|
           ring.all? { |coord| valid_wgs84_coordinates?(coord) }
         end
       end
-    when 'GeometryCollection'
-      return geometry['geometries'].all? { |geom| valid_coordinates?(geom) }
+    when "GeometryCollection"
+      return geometry["geometries"].all? { |geom| valid_coordinates?(geom) }
     else
       return false
     end
