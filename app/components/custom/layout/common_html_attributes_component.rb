10473c4a39c2873f540ac3a5d8ccee52c96ed691
CoslaJohn <johnmunro@cosla.gov.uk>
2024-08-06 14:58:54 +0100

    update custom html attributes

--- app/components/custom/layout/common_html_attributes_component.rb
+++ app/components/custom/layout/common_html_attributes_component.rb
@@ -1,15 +1,32 @@
-require_dependency Rails.root.join("app", "components", "layout", "common_html_attributes_component").to_s
+class Layout::CommonHTMLAttributesComponent < ApplicationComponent; end
+load Rails.root.join("app", "components", "layout", "common_html_attributes_component.rb")
 
 class Layout::CommonHTMLAttributesComponent
-  private
+  use_helpers :rtl?
 
-    alias_method :consul_html_class, :html_class
+  private
+#    alias_method :consul_html_class, :html_class
+
+
+    def attributes
+      sanitize([dir, lang, html_class].compact.join(" "))
+    end
+
+    def dir
+      'dir="rtl"' if rtl?
+    end
+
+    def lang
+      "lang=\"#{I18n.locale}\""
+    end
 
     def html_class
+      
       if Rails.application.secrets.site_name.present?
         "class=\"site-#{Rails.application.secrets.site_name}\""
       else
-        consul_html_class
+        "class=\"tenant-#{Tenant.current_schema}\"" if Rails.application.config.multitenancy
+        # else consul_html_class  
       end
     end
 end
