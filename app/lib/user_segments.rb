c9e10580f8b69e6beac338c55bfe675fdf4c3cd0
CoslaJohn <johnmunro@cosla.gov.uk>
2024-09-23 15:24:01 +0100

    improved user segments for newsletters

--- app/lib/user_segments.rb
+++ app/lib/user_segments.rb
@@ -1,18 +1,17 @@
 class UserSegments
   def self.segments
-    %w[all_users
-       administrators
-       all_proposal_authors
-       proposal_authors
-       investment_authors
-       feasible_and_undecided_investment_authors
-       selected_investment_authors
-       winner_investment_authors
-       not_supported_on_current_budget] + geozones.keys
+    static_segments + budget_segments
   end
 
   def self.segment_name(segment)
-    geozones[segment.to_s]&.name || I18n.t("admin.segment_recipient.#{segment}") if valid_segment?(segment)
+    if budget_segment?(segment)
+      budget_segment_name(segment)
+    elsif geozones[segment.to_s]
+      "Geozone: geozones[segment.to_s]&.name"
+    else
+      # || I18n.t("admin.segment_recipient.#{segment}") if valid_segment?(segment)
+      I18n.t("admin.segment_recipient.#{segment}", default: segment.to_s.humanize) if valid_segment?(segment) 
+    end
   end
 
   def self.all_users
@@ -75,21 +74,56 @@ class UserSegments
 
   private
 
-    def self.current_budget_investments
-      Budget.current.investments
-    end
+  def self.static_segments
+    %w[all_users
+       administrators
+       ] + geozones.keys.map { |geozone| "geozone: #{geozone}" }
+  end
 
-    def self.author_ids(author_ids)
-      all_users.where(id: author_ids)
-    end
+  def self.budget_segments
+    open_budgets.map do |budget|
+      %W[
+        #{budget.name}_all_proposal_authors
+        #{budget.name}_proposal_authors
+        #{budget.name}_investment_authors
+        #{budget.name}_feasible_and_undecided_investment_authors
+        #{budget.name}_selected_investment_authors
+        #{budget.name}_winner_investment_authors
+      ]
+    end.flatten
+  end
 
-    def self.geozones
-      Geozone.order(:name).to_h do |geozone|
-        [geozone.name.gsub(/./) { |char| character_approximation(char) }.underscore.tr(" ", "_"), geozone]
-      end
-    end
+  def self.open_budgets
+    Budget.where.not(phase: "finished").order(created_at: :desc).limit(2)
+  end
 
-    def self.character_approximation(char)
-      I18n::Backend::Transliterator::HashTransliterator::DEFAULT_APPROXIMATIONS[char] || char
+  def self.budget_segment?(segment)
+    open_budgets.any? { |budget| segment.start_with?(budget.name) }
+  end
+
+  def self.budget_segment_name(segment)
+    budget_name = segment.split('_').first
+    segment_type = segment.sub("#{budget_name}_", '')
+
+    budget = open_budgets.find { |b| b.name == budget_name }
+    I18n.t("admin.segment_recipient.#{segment_type}", budget_name: budget.name)
+  end
+
+  def self.current_budget_investments
+    Budget.current.investments
+  end
+
+  def self.author_ids(author_ids)
+    all_users.where(id: author_ids)
+  end
+
+  def self.geozones
+    Geozone.order(:name).to_h do |geozone|
+      [geozone.name.gsub(/./) { |char| character_approximation(char) }.underscore.tr(" ", "_"), geozone]
     end
+  end
+
+  def self.character_approximation(char)
+    I18n::Backend::Transliterator::HashTransliterator::DEFAULT_APPROXIMATIONS[char] || char
+  end
 end
