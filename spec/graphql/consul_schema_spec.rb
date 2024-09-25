5f80a75161e1ae26aa468a82293eb3d3e8a1cd0f
Javi Martín <javim@elretirao.net>
2024-09-25 14:55:12 +0200

    Limit GraphQL queries complexity once again
    We accidentally removed the code for maximum complexity in commit
    c984e666f. As mentioned in the documentation:
    
    > The main risk factor is multiple collections of resources being
    > requested in the same query.
    
    We reject these requests by limiting the complexity.
    
    The `max_complexity` option depends on the page size being set. Without
    it, we get an error:
    
    ```
    Can't calculate complexity for User.public_debates, no `first:`,
    `last:`, `max_page_size` or `default_max_page_size`
    ```
    
    So we're also adding a default max page size.
    
    Note that the documentation mentioned that the default page size was 25.
    However, before commit c984e666f, we were using a page size of 50 in
    some cases. We're going with the one mentioned in the documentation
    since we don't fully understand the old code.

--- spec/graphql/consul_schema_spec.rb
+++ spec/graphql/consul_schema_spec.rb
@@ -34,4 +34,43 @@ describe ConsulSchema do
     expect(response["errors"]).not_to be nil
     expect(response["errors"].first["message"]).to match(/exceeds max depth/)
   end
+
+  it "returns an error for queries requesting all records from more than 2 collections" do
+    query = <<~GRAPHQL
+      {
+        users {
+          edges {
+            node {
+              public_debates {
+                edges {
+                  node {
+                    title
+                  }
+                }
+              }
+              public_proposals {
+                edges {
+                  node {
+                    title
+                  }
+                }
+              }
+              public_comments {
+                edges {
+                  node {
+                    body
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    GRAPHQL
+
+    response = execute(query)
+
+    expect(response["errors"]).not_to be nil
+    expect(response["errors"].first["message"]).to match(/Query has complexity/)
+  end
 end
