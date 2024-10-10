84260c0ae12fa155d5e3bce9729812ba48dc9274
Javi Martín <javim@elretirao.net>
2024-10-11 01:38:44 +0200

    Simplify creating management investments search components

--- app/components/management/budgets/investments/search_component.rb
+++ app/components/management/budgets/investments/search_component.rb
@@ -1,9 +1,19 @@
 class Management::Budgets::Investments::SearchComponent < ApplicationComponent
-  attr_reader :budget, :url
+  attr_reader :budget, :action
   use_helpers :budget_heading_select_options
 
-  def initialize(budget, url:)
+  def initialize(budget, action:)
     @budget = budget
-    @url = url
+    @action = action
   end
+
+  private
+
+    def url
+      url_for(
+        controller: "management/budgets/investments",
+        action: action,
+        budget_id: budget.id
+      )
+    end
 end
