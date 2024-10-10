5ca65a787feb5f5349eab6638abe91b51c95841b
Javi Martín <javim@elretirao.net>
2024-10-10 23:47:32 +0200

    Add an ARIA label to site customization images fields
    This way it'll be easier for people using screen readers to know which
    field they're on.

--- spec/system/admin/site_customization/images_spec.rb
+++ spec/system/admin/site_customization/images_spec.rb
@@ -10,7 +10,7 @@ describe "Admin custom images", :admin do
     end
 
     within("tr#image_logo_header") do
-      attach_file "site_customization_image_image", file_fixture("logo_header.png")
+      attach_file "logo_header", file_fixture("logo_header.png")
       click_button "Update"
     end
 
@@ -22,7 +22,7 @@ describe "Admin custom images", :admin do
     visit admin_site_customization_images_path
 
     within("tr#image_map") do
-      attach_file "site_customization_image_image", file_fixture("custom_map.jpg")
+      attach_file "map", file_fixture("custom_map.jpg")
       click_button "Update"
     end
 
@@ -38,7 +38,7 @@ describe "Admin custom images", :admin do
     visit admin_site_customization_images_path
 
     within("tr#image_map") do
-      attach_file "site_customization_image_image", file_fixture("custom_map.jpg")
+      attach_file "map", file_fixture("custom_map.jpg")
       click_button "Update"
     end
 
@@ -78,7 +78,7 @@ describe "Admin custom images", :admin do
     visit admin_site_customization_images_path
 
     within("tr#image_logo_email") do
-      attach_file "site_customization_image_image", file_fixture("logo_email_custom.png")
+      attach_file "logo_email", file_fixture("logo_email_custom.png")
       click_button "Update"
     end
 
@@ -93,7 +93,7 @@ describe "Admin custom images", :admin do
     visit admin_site_customization_images_path
 
     within("tr#image_social_media_icon") do
-      attach_file "site_customization_image_image", file_fixture("logo_header.png")
+      attach_file "social_media_icon", file_fixture("logo_header.png")
       click_button "Update"
     end
 
@@ -105,7 +105,7 @@ describe "Admin custom images", :admin do
     visit admin_site_customization_images_path
 
     within("tr#image_social_media_icon") do
-      attach_file "site_customization_image_image", file_fixture("social_media_icon.png")
+      attach_file "social_media_icon", file_fixture("social_media_icon.png")
       click_button "Update"
     end
 
