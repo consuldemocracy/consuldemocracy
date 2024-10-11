3cc0583662cf1f2b8dfd1d37176f416fa99a536a
Javi Martín <javim@elretirao.net>
2024-10-11 23:12:17 +0200

    Move investments search form partial to a component
    As a bonus, we now have a few less helper methods :).

---
+++ app/components/admin/budget_investments/search_form_component.rb
@@ -0,0 +1,45 @@
+class Admin::BudgetInvestments::SearchFormComponent < ApplicationComponent
+  attr_reader :budget
+  use_helpers :budget_heading_select_options
+
+  def initialize(budget)
+    @budget = budget
+  end
+
+  private
+
+    def init_advanced_menu
+      params[:advanced_filters] = [] unless params[:advanced_filters]
+    end
+
+    def advanced_menu_visibility
+      if params[:advanced_filters].empty? &&
+         params["min_total_supports"].blank? &&
+         params["max_total_supports"].blank?
+        "hide"
+      else
+        ""
+      end
+    end
+
+    def admin_select_options
+      budget.administrators.with_user.map { |v| [v.description_or_name, v.id] }.sort_by { |a| a[0] }
+    end
+
+    def valuator_or_group_select_options
+      valuator_group_select_options + valuator_select_options
+    end
+
+    def valuator_group_select_options
+      ValuatorGroup.order("name ASC").map { |g| [g.name, "group_#{g.id}"] }
+    end
+
+    def valuator_select_options
+      budget.valuators.order("description ASC").order("users.email ASC").includes(:user)
+            .map { |v| [v.description_or_email, "valuator_#{v.id}"] }
+    end
+
+    def investment_tags_select_options(context)
+      budget.investments.tags_on(context).order(:name).pluck(:name)
+    end
+end
