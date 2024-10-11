f8faabf7d1bedffbd32ba81cb4a0bf2eb483823f
Javi Martín <javim@elretirao.net>
2024-10-11 17:43:16 +0200

    Extract component to mark a debate as featured
    We're also moving the path argument in the `link_to` calls to a
    different line, since it's what we usually do.

---
+++ app/components/debates/mark_featured_action_component.rb
@@ -0,0 +1,12 @@
+class Debates::MarkFeaturedActionComponent < ApplicationComponent
+  attr_reader :debate
+  use_helpers :can?
+
+  def initialize(debate)
+    @debate = debate
+  end
+
+  def render
+    can? :mark_featured, debate
+  end
+end
