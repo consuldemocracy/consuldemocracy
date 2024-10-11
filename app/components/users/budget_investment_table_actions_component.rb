cbdf2f7f220d27653eaf4bf578f63f66753e01ff
Javi Martín <javim@elretirao.net>
2024-10-11 16:44:25 +0200

    Extract methods in user investment table actions

--- app/components/users/budget_investment_table_actions_component.rb
+++ app/components/users/budget_investment_table_actions_component.rb
@@ -5,4 +5,20 @@ class Users::BudgetInvestmentTableActionsComponent < ApplicationComponent
   def initialize(investment)
     @investment = investment
   end
+
+  private
+
+    def edit_link
+      link_to t("shared.edit"),
+              edit_budget_investment_path(investment.budget, investment),
+              class: "button hollow"
+    end
+
+    def destroy_button
+      button_to t("shared.delete"),
+                budget_investment_path(investment.budget, investment),
+                method: :delete,
+                class: "button hollow alert",
+                data: { confirm: t("users.show.delete_alert") }
+    end
 end
