2fb8eaf6c7e9a2b90cd643320d6ad3bbfccc75eb
Javi Martín <javim@elretirao.net>
2024-10-11 17:03:45 +0200

    Add aria-labels to user investment actions
    This way it'll be easier for people using screen readers to know which
    link/button they're about to click.
    
    Note that, at least for now, we aren't reusing the code en
    `Admin::ActionComponent`. We might do so in the future if we implement
    similar code in more parts of the public area.

--- app/components/users/budget_investment_table_actions_component.rb
+++ app/components/users/budget_investment_table_actions_component.rb
@@ -11,6 +11,7 @@ class Users::BudgetInvestmentTableActionsComponent < ApplicationComponent
     def edit_link
       link_to t("shared.edit"),
               edit_budget_investment_path(investment.budget, investment),
+              "aria-label": action_label(t("shared.edit")),
               class: "button hollow"
     end
 
@@ -18,7 +19,12 @@ class Users::BudgetInvestmentTableActionsComponent < ApplicationComponent
       button_to t("shared.delete"),
                 budget_investment_path(investment.budget, investment),
                 method: :delete,
+                "aria-label": action_label(t("shared.delete")),
                 class: "button hollow alert",
                 data: { confirm: t("users.show.delete_alert") }
+    end
+
+    def action_label(action_text)
+      t("shared.actions.label", action: action_text, name: investment.title)
     end
 end
