ba5d88e2ae3be18f2ebef2e1a5f4403c33bbe53a
Javi Martín <javim@elretirao.net>
2024-10-14 21:24:58 +0200

    Simplify passing the booth in shift form component
    Since we're using helpers instead of components, the booth is now a
    method and we don't need to pass it around.

--- app/components/admin/poll/shifts/form_component.rb
+++ app/components/admin/poll/shifts/form_component.rb
@@ -17,28 +17,28 @@ class Admin::Poll::Shifts::FormComponent < ApplicationComponent
       booth.polls.current_or_recounting
     end
 
-    def shift_vote_collection_dates(booth, polls)
+    def shift_vote_collection_dates(polls)
       return [] if polls.blank?
 
-      date_options((start_date(polls)..end_date(polls)), Poll::Shift.tasks[:vote_collection], booth)
+      date_options((start_date(polls)..end_date(polls)), Poll::Shift.tasks[:vote_collection])
     end
 
-    def shift_recount_scrutiny_dates(booth, polls)
+    def shift_recount_scrutiny_dates(polls)
       return [] if polls.blank?
 
       dates = polls.map(&:ends_at).map(&:to_date).sort.reduce([]) do |total, date|
         initial_date = [date, Date.current].max
         total << (initial_date..date + Poll::RECOUNT_DURATION).to_a
       end
-      date_options(dates.flatten.uniq, Poll::Shift.tasks[:recount_scrutiny], booth)
+      date_options(dates.flatten.uniq, Poll::Shift.tasks[:recount_scrutiny])
     end
 
-    def date_options(dates, task_id, booth)
-      valid_dates(dates, task_id, booth).map { |date| [l(date, format: :long), l(date)] }
+    def date_options(dates, task_id)
+      valid_dates(dates, task_id).map { |date| [l(date, format: :long), l(date)] }
     end
 
-    def valid_dates(dates, task_id, booth)
-      dates.reject { |date| officer_shifts(task_id, booth).include?(date) }
+    def valid_dates(dates, task_id)
+      dates.reject { |date| officer_shifts(task_id).include?(date) }
     end
 
     def start_date(polls)
@@ -50,7 +50,7 @@ class Admin::Poll::Shifts::FormComponent < ApplicationComponent
       polls.maximum(:ends_at).to_date
     end
 
-    def officer_shifts(task_id, booth)
+    def officer_shifts(task_id)
       officer.shifts.where(task: task_id, booth: booth).map(&:date)
     end
 end
