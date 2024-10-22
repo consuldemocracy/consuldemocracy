4ddb30fd82018940222a144e265eac8cd133882c
Javi Martín <javim@elretirao.net>
2024-10-22 23:23:10 +0200

    Move helper method to the component

--- app/components/polls/poll_component.rb
+++ app/components/polls/poll_component.rb
@@ -1,6 +1,6 @@
 class Polls::PollComponent < ApplicationComponent
   attr_reader :poll
-  use_helpers :link_to_poll
+  use_helpers :can?
 
   def initialize(poll)
     @poll = poll
@@ -10,5 +10,15 @@ class Polls::PollComponent < ApplicationComponent
 
     def dates
       t("polls.dates", open_at: l(poll.starts_at.to_date), closed_at: l(poll.ends_at.to_date))
+    end
+
+    def link_to_poll(text, poll, options = {})
+      if can?(:results, poll)
+        link_to text, results_poll_path(id: poll.slug || poll.id), options
+      elsif can?(:stats, poll)
+        link_to text, stats_poll_path(id: poll.slug || poll.id), options
+      else
+        link_to text, poll_path(id: poll.slug || poll.id), options
+      end
     end
 end
