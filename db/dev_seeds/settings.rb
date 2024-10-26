65e3d4b6cff416c23d43c315bcf426bb889210ca
Javi Martín <javim@elretirao.net>
2024-10-26 22:53:46 +0200

    Fix invalid HTML caused by the dev seeds
    These settings are used in links, and were generating invalid `href`
    attributes that contained spaces.

--- db/dev_seeds/settings.rb
+++ db/dev_seeds/settings.rb
@@ -2,21 +2,21 @@ section "Creating Settings" do
   Setting.reset_defaults
 
   {
-    "facebook_handle": "CONSUL DEMOCRACY",
+    "facebook_handle": "CONSULDEMOCRACY",
     "feature.featured_proposals": "true",
     "feature.map": "true",
-    "instagram_handle": "CONSUL DEMOCRACY",
+    "instagram_handle": "CONSULDEMOCRACY",
     "meta_description": "Citizen participation tool for an open, " \
                         "transparent and democratic government",
     "meta_keywords": "citizen participation, open government",
     "meta_title": "CONSUL DEMOCRACY",
     "proposal_code_prefix": "MAD",
     "proposal_notification_minimum_interval_in_days": 0,
-    "telegram_handle": "CONSUL DEMOCRACY",
+    "telegram_handle": "CONSULDEMOCRACY",
     "twitter_handle": "@consuldemocracy_dev",
     "twitter_hashtag": "#consuldemocracy_dev",
     "votes_for_proposal_success": "100",
-    "youtube_handle": "CONSUL DEMOCRACY"
+    "youtube_handle": "CONSULDEMOCRACY"
   }.each do |name, value|
     Setting[name] = value
   end
