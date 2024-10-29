10cdfadb9b9897a33b847d3b7d99ce03cd82a1eb
Javi Martín <javim@elretirao.net>
2024-10-29 01:46:58 +0100

    Move executions images system tests to the component
    We forgot to do so in commit f0ab7bcfc. We're still leaving one system
    spec to test that these images are rendered when browsing the site.
    
    We're also changing the existing component tests to check the `alt`
    attribute, just like the remaining system test does.

--- spec/components/budgets/executions/image_component_spec.rb
+++ spec/components/budgets/executions/image_component_spec.rb
@@ -1,13 +1,50 @@
 require "rails_helper"
 
 describe Budgets::Executions::ImageComponent do
+  let(:component) { Budgets::Executions::ImageComponent.new(investment) }
+
+  context "investment with image" do
+    let(:investment) { create(:budget_investment, :with_image) }
+
+    it "shows a milestone image if available" do
+      create(:milestone, :with_image, image_title: "Building in progress", milestoneable: investment)
+
+      render_inline component
+
+      expect(page).to have_css "img[alt='Building in progress']"
+      expect(page).not_to have_css "img[alt='#{investment.image.title}']"
+    end
+
+    it "shows the investment image if no milestone image is available" do
+      create(:milestone, :with_image, image_title: "Not related", milestoneable: create(:budget_investment))
+
+      render_inline component
+
+      expect(page).to have_css "img[alt='#{investment.image.title}']"
+      expect(page).not_to have_css "img[alt='Not related']"
+    end
+
+    it "shows the last milestone's image if the investment has multiple milestones with images associated" do
+      create(:milestone, milestoneable: investment)
+      create(:milestone, :with_image, image_title: "First image", milestoneable: investment)
+      create(:milestone, :with_image, image_title: "Second image", milestoneable: investment)
+      create(:milestone, milestoneable: investment)
+
+      render_inline component
+
+      expect(page).to have_css "img[alt='Second image']"
+      expect(page).not_to have_css "img[alt='First image']"
+      expect(page).not_to have_css "img[alt='#{investment.image.title}']"
+    end
+  end
+
   context "investment and milestone without image" do
-    let(:component) { Budgets::Executions::ImageComponent.new(Budget::Investment.new) }
+    let(:investment) { Budget::Investment.new(title: "New and empty") }
 
     it "shows the default image" do
       render_inline component
 
-      expect(page).to have_css "img[src*='budget_execution_no_image']"
+      expect(page).to have_css "img[src*='budget_execution_no_image'][alt='New and empty']"
     end
 
     it "shows a custom default image when available" do
@@ -18,7 +55,7 @@ describe Budgets::Executions::ImageComponent do
 
       render_inline component
 
-      expect(page).to have_css "img[src$='logo_header-260x80.png']"
+      expect(page).to have_css "img[src$='logo_header-260x80.png'][alt='New and empty']"
       expect(page).not_to have_css "img[src*='budget_execution_no_image']"
     end
   end
