0826e25fe35d6e99941ac9c8ab317cae786d502f
justin_yamberla_gadmco <justin.yamberla@epn.edu.ec>
2024-10-29 11:24:30 -0500

    Change
    Cambio en zona horaria

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,7 @@ module Consul
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = Rails.application.secrets.time_zone.presence || "Madrid"
+    config.time_zone = Rails.application.secrets.time_zone.presence || "Quito"
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
