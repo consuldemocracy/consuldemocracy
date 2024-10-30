a30384a23be895e2371221ebc99b74016d7ab02b
CoslaJohn <johnmunro@cosla.gov.uk>
2024-10-30 10:07:37 +0000

    add 2fa methods to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,23 @@
 class User < ApplicationRecord
+  devise :two_factor_authenticatable
   include Verification
   attribute :registering_from_web, default: false
 
-  devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable,
+  devise :registerable, :confirmable, :recoverable, :rememberable,
          :trackable, :validatable, :omniauthable, :password_expirable, :secure_validatable,
          authentication_keys: [:login]
   devise :lockable if Rails.application.config.devise_lockable
+  
+  devise :two_factor_authenticatable,
+         :otp_secret_encryption_key => ENV['DEVISE_OTP_ENCRYPTION_KEY']
 
+
+  # If you want to use backup codes
+  devise :two_factor_backupable, otp_number_of_backup_codes: 10
+  serialize :otp_backup_codes, JSON
+  
+#  before_create :generate_otp_secret  
+  
   acts_as_voter
   acts_as_paranoid column: :hidden_at
   include ActsAsParanoidAliases
@@ -445,6 +456,54 @@ class User < ApplicationRecord
     username.to_s.parameterize
   end
 
+  def otp_provisioning_uri(account, options = {})
+    issuer = options[:issuer]
+    secret = self.otp_secret
+   "otpauth://totp/#{issuer}:#{account}?secret=#{secret}&issuer=#{issuer}"
+  end
+
+   def otp_qr_code
+    issuer = 'consuldev.communitychoices.scot:3000'
+    label = "#{issuer}:#{email}"
+    uri = otp_provisioning_uri(label, issuer: issuer)
+    RQRCode::QRCode.new(uri).as_png(size: 200).to_data_url
+  end
+
+   def requires_2fa?
+    administrator?
+   end
+   
+   def generate_otp_secret
+    self.otp_secret = ROTP::Base32.random_base32
+  end
+  
+   # Generate an OTP secret it it does not already exist
+  def generate_two_factor_secret_if_missing!
+    return unless otp_secret.nil?
+    update!(otp_secret: User.generate_otp_secret)
+  end
+   
+  def otp_two_factor_enabled?
+    otp_required_for_login
+  end 
+   
+  # Ensure that the user is prompted for their OTP when they login
+  def enable_two_factor!
+    update!(otp_required_for_login: true)
+  end
+
+  # Disable the use of OTP-based two-factor.
+  def disable_two_factor!
+    update!(
+        otp_required_for_login: false,
+        otp_secret: nil,
+        otp_backup_codes: nil)
+  end
+  
+  def can_be_administrator?
+    self.otp_required_for_login
+  end   
+   
   private
 
     def clean_document_number
