7ee00554bd9022eb2fd46797ab52f335cfffc398
CoslaJohn <johnmunro@cosla.gov.uk>
2024-11-11 13:59:18 +0000

    Add proposals location to model

--- app/models/map_location.rb
+++ app/models/map_location.rb
@@ -1,5 +1,5 @@
 class MapLocation < ApplicationRecord
-  belongs_to :proposal, touch: true
+  belongs_to :proposal, class_name: "Proposal", touch: true
   belongs_to :investment, class_name: "Budget::Investment", touch: true
 
   validates :longitude, :latitude, :zoom, presence: true, numericality: true
@@ -24,7 +24,18 @@ class MapLocation < ApplicationRecord
       longitude: (heading.longitude.to_f if heading.longitude.present?)
     )
   end
-
+  
+  def self.from_settings
+    latitude_setting = Setting.find_by(key: "map.latitude")
+    longitude_setting = Setting.find_by(key: "map.longitude")
+    zoom_setting = Setting.find_by(key: "map.zoom")
+    new(
+      zoom: zoom_setting,
+      latitude: (latitude_setting.value.to_f if latitude_setting&.value.present?),
+      longitude: (longitude_setting.value.to_f if longitude_setting&.value.present?)
+    )
+  end
+  
   def self.investments_json_data(investments)
     return [] if investments.none?
 
@@ -33,6 +44,7 @@ class MapLocation < ApplicationRecord
     data = investments.joins(:map_location)
                       .with_fallback_translation
                       .pluck(:id, :title, :latitude, :longitude)
+    Rails.logger.info "Proposals data: #{data.inspect}"
 
     data.map do |values|
       {
@@ -43,4 +55,23 @@ class MapLocation < ApplicationRecord
       }
     end
   end
+
+  def self.proposals_json_data(proposals)
+    return [] if proposals.none?
+    Rails.logger.info "Inside proposals_json_data #{proposals.inspect}"
+
+    data = proposals.joins(:map_location)
+                    .with_fallback_translation 
+                    .pluck(:id, :title, :latitude, :longitude) 
+        
+    data.map do |values|
+      {
+        title: values[1],
+        link: "/proposals/#{values[0]}", 
+        lat: values[2],
+        long: values[3]
+      }
+    end
+  end
+
 end
