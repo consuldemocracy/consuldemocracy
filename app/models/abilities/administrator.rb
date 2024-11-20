86934752a276ebd168178736dca615d0beaabd17
coslajohn <102246590+coslajohn@users.noreply.github.com>
2024-11-20 14:09:51 +0000

    Update administrator.rb
    Allow administrators to edit proposals

--- app/models/abilities/administrator.rb
+++ app/models/abilities/administrator.rb
@@ -72,7 +72,7 @@ module Abilities
 
       can [:read, :create, :update, :destroy], Budget::Group
       can [:read, :create, :update, :destroy], Budget::Heading
-      can [:hide, :admin_update, :toggle_selection], Budget::Investment
+      can [:hide, :admin_update, :update, :toggle_selection], Budget::Investment
       can [:valuate, :comment_valuation], Budget::Investment
       cannot [:admin_update, :toggle_selection, :valuate, :comment_valuation],
              Budget::Investment, budget: { phase: "finished" }
