c6bb0db0b14772641c6b6543f7d64579acc2ac46
taitus <sebastia.roig@gmail.com>
2024-11-29 14:19:40 +0100

    Add modal setup for show essential cookies information

---
+++ app/components/layout/cookies_consent/setup_component.rb
@@ -0,0 +1,2 @@
+class Layout::CookiesConsent::SetupComponent < Layout::CookiesConsent::BaseComponent
+end
---
+++ spec/components/layout/cookies_consent/setup_component_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe Layout::CookiesConsent::SetupComponent do
+  before { Setting["feature.cookies_consent"] = true }
+
+  it "is not rendered when the cookies consent feature is disabled" do
+    Setting["feature.cookies_consent"] = false
+
+    render_inline Layout::CookiesConsent::SetupComponent.new
+
+    expect(page).not_to have_css ".cookies-consent-setup"
+  end
+
+  it "is rendered with essential cookies content when the cookies consent is enabled" do
+    render_inline Layout::CookiesConsent::SetupComponent.new
+
+    expect(page).to have_css ".cookies-consent-setup"
+    expect(page).to have_css "h2", text: "Cookies setup"
+    expect(page).to have_css "h3", text: "Essential cookies"
+  end
+end
