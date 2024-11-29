0ab17d511e22746ada0cd5d6e0b9751e8acd8087
taitus <sebastia.roig@gmail.com>
2024-11-29 15:56:20 +0100

    Render more info link in setup component

--- app/components/layout/cookies_consent/setup_component.rb
+++ app/components/layout/cookies_consent/setup_component.rb
@@ -2,4 +2,10 @@ class Layout::CookiesConsent::SetupComponent < ApplicationComponent
   def render?
     feature?(:cookies_consent)
   end
+
+  private
+
+    def more_info_link
+      Setting["cookies_consent.more_info_link"]
+    end
 end
--- spec/components/layout/cookies_consent/setup_component_spec.rb
+++ spec/components/layout/cookies_consent/setup_component_spec.rb
@@ -11,12 +11,23 @@ describe Layout::CookiesConsent::SetupComponent do
     expect(page).not_to have_css ".cookies-consent-setup"
   end
 
-  it "is rendered with essential cookies content when the cookies consent is enabled" do
+  it "is rendered setup content when the cookies consent is enabled" do
+    Setting["cookies_consent.more_info_link"] = "/cookies_policy"
+
     render_inline Layout::CookiesConsent::SetupComponent.new
 
     expect(page).to have_css ".cookies-consent-setup"
     expect(page).to have_css "h2", text: "Cookies setup"
+    expect(page).to have_link "More information", href: "/cookies_policy"
     expect(page).to have_css "h3", text: "Essential cookies"
     expect(page).to have_button "Accept essential cookies"
+  end
+
+  it "does not renders a link when the setting `cookies_consent.more_info_link` is not defined" do
+    Setting["cookies_consent.more_info_link"] = ""
+
+    render_inline Layout::CookiesConsent::SetupComponent.new
+
+    expect(page).not_to have_link "More information"
   end
 end
