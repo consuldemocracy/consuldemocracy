2b764a663c1be194cbc2483fcb78c7952fecc260
taitus <sebastia.roig@gmail.com>
2024-12-03 15:55:49 +0100

    Add cookies vendors to the cookies setup page

--- app/assets/javascripts/cookies_consent.js
+++ app/assets/javascripts/cookies_consent.js
@@ -9,9 +9,51 @@
         $("#cookies_consent_setup").foundation("close");
       }
     },
+    setCookiesConsent: function(id, value) {
+      document.getElementById(id).checked = value;
+    },
+    vendors: function() {
+      return $(".cookies-vendors input[type=checkbox]:not([disabled])");
+    },
+    dispatchCookieAllowanceEvent: function(name) {
+      var event = new CustomEvent(name + "_consented");
+      document.dispatchEvent(event);
+    },
+    dispatchCookieRejectionEvent: function(name) {
+      var event = new CustomEvent(name + "_rejected");
+      document.dispatchEvent(event);
+    },
     initialize: function() {
+      $(".accept-cookies").on("click", function() {
+        App.Cookies.saveCookie("cookies_consent", "all", 365);
+        App.CookiesConsent.vendors().each(function() {
+          App.Cookies.saveCookie(this.name, true, 365);
+          App.CookiesConsent.dispatchCookieAllowanceEvent(this.name);
+          App.CookiesConsent.setCookiesConsent(this.id, true);
+        });
+        App.CookiesConsent.hide();
+      });
+
       $(".accept-essential-cookies").on("click", function() {
         App.Cookies.saveCookie("cookies_consent", "essential", 365);
+        App.CookiesConsent.vendors().each(function() {
+          App.Cookies.saveCookie(this.name, false, 365);
+          App.CookiesConsent.dispatchCookieRejectionEvent(this.name);
+          App.CookiesConsent.setCookiesConsent(this.id, false);
+        });
+        App.CookiesConsent.hide();
+      });
+
+      $(".save-cookies-preferences").on("click", function() {
+        App.Cookies.saveCookie("cookies_consent", "custom", 365);
+        App.CookiesConsent.vendors().each(function() {
+          App.Cookies.saveCookie(this.name, this.checked, 365);
+          if (this.checked) {
+            App.CookiesConsent.dispatchCookieAllowanceEvent(this.name);
+          } else {
+            App.CookiesConsent.dispatchCookieRejectionEvent(this.name);
+          }
+        });
         App.CookiesConsent.hide();
       });
     }
--- app/components/layout/cookies_consent/setup_component.rb
+++ app/components/layout/cookies_consent/setup_component.rb
@@ -3,6 +3,10 @@ class Layout::CookiesConsent::SetupComponent < ApplicationComponent
     feature?(:cookies_consent)
   end
 
+  def vendors
+    Cookies::Vendor.all
+  end
+
   private
 
     def more_info_link
