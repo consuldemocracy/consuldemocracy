1f3976be6619afd1b936cea6bf44dbb3176e5ec9
taitus <sebastia.roig@gmail.com>
2024-12-09 16:28:02 +0100

    Render third party cookies in the setup component

--- app/assets/javascripts/cookies_consent.js
+++ app/assets/javascripts/cookies_consent.js
@@ -13,15 +13,38 @@
       var callout = $("#cookies_consent_banner").data("notice");
       App.Callout.show(new DOMParser().parseFromString(callout, "text/html").documentElement.textContent);
     },
+    setCookiesConsent: function(id, value) {
+      document.getElementById(id).checked = value;
+    },
+    vendors: function() {
+      return $(".cookies-vendors input[type=checkbox]:not([disabled])");
+    },
     initialize: function() {
       $(".accept-all-cookies").on("click", function() {
         App.Cookies.saveCookie("cookies_consent", "all", 365);
+        App.CookiesConsent.vendors().each(function() {
+          App.Cookies.saveCookie(this.name, true, 365);
+          App.CookiesConsent.setCookiesConsent(this.id, true);
+        });
         App.CookiesConsent.showCallout();
         App.CookiesConsent.hide();
       });
 
       $(".accept-essential-cookies").on("click", function() {
         App.Cookies.saveCookie("cookies_consent", "essential", 365);
+        App.CookiesConsent.vendors().each(function() {
+          App.Cookies.saveCookie(this.name, false, 365);
+          App.CookiesConsent.setCookiesConsent(this.id, false);
+        });
+        App.CookiesConsent.showCallout();
+        App.CookiesConsent.hide();
+      });
+
+      $(".save-cookies-preferences").on("click", function() {
+        App.Cookies.saveCookie("cookies_consent", "custom", 365);
+        App.CookiesConsent.vendors().each(function() {
+          App.Cookies.saveCookie(this.name, this.checked, 365);
+        });
         App.CookiesConsent.showCallout();
         App.CookiesConsent.hide();
       });
---
+++ app/components/layout/cookies_consent/vendors_component.rb
@@ -0,0 +1,15 @@
+class Layout::CookiesConsent::VendorsComponent < ApplicationComponent
+  delegate :cookies, :dom_id, to: :helpers
+
+  def render?
+    vendors.any?
+  end
+
+  def vendors
+    Cookies::Vendor.all
+  end
+
+  def enabled?(vendor)
+    cookies[vendor.cookie] == "true"
+  end
+end
