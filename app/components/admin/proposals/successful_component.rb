994d2d2dc752a94994093ed108381db14155738a
Javi Martín <javim@elretirao.net>
2025-02-23 14:51:48 +0100

    Remove obsolete questions index in the admin area
    We removed the link to this page in commit 83e8d6035 because poll
    questions don't really make sense without a poll.
    
    However, this page also contained information about successful
    proposals, which might be interesting so administrators don't have to
    navigate to the public area in order to find and create questions based
    on successful proposals.
    
    So we're keeping the part about successful proposals and linking it from
    the proposals part of the admin area.
    
    Note we're using translation keys like `successful_proposals_tab`, which
    don't make sense anymore, for the successful proposals. We're doing so
    because we've already got translations for these keys and, if we renamed
    them, we'd lose the existing translations and our translators would have
    to add them again.
    
    Also note we're changing one poll question test a little bit so we
    create the question from a successful proposal using the new page. There
    are other tests checking how to create a question from the
    admin/proposals#show action and other tests checking what happens when
    accessing a successful proposal in the admin section, so we don't lose
    any test coverage by changing an existing test instead of adding a new
    one.
    
    Finally, note that we've removing the `search` method in poll question
    because we no longer use it. This currently makes the
    `author_visible_name` database column useless; we aren't removing it
    right now because we don't want to risk a possible data loss in a patch
    release (we're about to release version 2.3.1), but we might remove it
    in the future.

---
+++ app/components/admin/proposals/successful_component.rb
@@ -0,0 +1,12 @@
+class Admin::Proposals::SuccessfulComponent < ApplicationComponent
+  include Header
+  attr_reader :proposals
+
+  def initialize(proposals)
+    @proposals = proposals
+  end
+
+  def title
+    t("admin.questions.index.successful_proposals_tab")
+  end
+end
