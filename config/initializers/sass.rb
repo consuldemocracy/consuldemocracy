abf8a6b559e953a75036a1f0a603e2a19a1e23c7
Javi Martín <javim@elretirao.net>
2025-03-01 00:06:46 +0100

    Don't quiet deps to check progress

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -5,6 +5,6 @@ SassC::Engine.class_eval do
   alias_method :original_initialize, :initialize
 
   def initialize(template, options = {})
-    original_initialize(template, options.merge(quiet_deps: true, silence_deprecations: [:import]))
+    original_initialize(template, options.merge(silence_deprecations: [:import]))
   end
 end
