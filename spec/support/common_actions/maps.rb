b230b54097b5c3dde9ae939e53b818927b5f01cf
Javi Martín <javim@elretirao.net>
2025-03-10 17:36:38 +0100

    Add missing expectations in common actions
    We weren't checking that the request caused by clicking on the "Send
    instructions" button had finished before continuing with the test.
    Perhaps that's why this test has recently failed on our CI:
    
    ```
    3) Emails Reset password
         Failure/Error: email = open_last_email
    
         RuntimeError:
           No email has been sent!
    ```
    
    We're also adding an expectation to the `login_as_manager` method and
    the methods to submit proposal and investment forms to make sure that,
    when these method finish, the request finishes as well.

--- spec/support/common_actions/maps.rb
+++ spec/support/common_actions/maps.rb
@@ -7,6 +7,7 @@ module Maps
   def submit_proposal_form
     check :proposal_terms_of_service
     click_button "Create proposal"
+    expect(page).to have_content "Proposal created successfully."
 
     if page.has_content?("Not now, go to my proposal")
       click_link "Not now, go to my proposal"
@@ -22,6 +23,7 @@ module Maps
   def submit_budget_investment_form
     check :budget_investment_terms_of_service
     click_button "Create Investment"
+    expect(page).to have_content "Budget Investment created successfully"
   end
 
   def set_arguments(arguments, mappable, mappable_path_arguments)
