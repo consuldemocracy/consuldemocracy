358965703255653df2774df3093e06d1bc49d6d9
Javi Martín <javim@elretirao.net>
2025-03-10 02:13:29 +0100

    Rails 7.2 TODO: test Docker once we remove the github repository from the Gemfile
    Note we now need to write the `secrets` method, since it's been removed
    in Rails 7.2. We're using a simplified version of the `secrets` method
    in previous versions of Rails.
    
    Also note we're removing the `secret_key_base` key from the test and
    development secrets, since it's ignored in these environments. Quoting
    from the Rails documentation [1]:
    
    > In development and test, this is randomly generated and stored in a
    > temporary file in <tt>tmp/local_secret.txt</tt>.
    
    TODO: ADD AS INFO TO RELEASE NOTES that, in **development**, existing
    cookies won't be used after upgrading. If this is important for you (for
    example, if you're constantly switching branches with Consul Democracy
    2.4 and Consul Democracy 2.5 in order to change the upgrade), **on your
    local machine**, delete your `secret_key_base` references for the
    **development** environment in your `config/secrets.yml`.
    
    Finally, we also need to overwrite the `secret_key_base` method so it
    reads the one in `config/secrets.yml`. However, doing so is tricky since
    this method contains a few conditionals related to the current
    environment and to certain environment variables. So, instead, we're
    overwriting the `credentials` method so it returns secrets instead;
    since the `secret_key_base` method calls `credentials.secret_key_base`
    when running the application on production, overwriting the
    `credentials` method will cause the application to get the secret key
    base from the `config/secrets.yml` file. Since we don't use credentials
    at all, this change doesn't affect the rest of the application.
    
    To avoid confusion, we're changing the Devise initializer so it doesn't
    reference the `secrets.secret_key_base` method.
    
    [1] https://api.rubyonrails.org/v7.2/classes/Rails/Application.html#method-i-secret_key_base

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,9 @@ Rails.application.configure do
   # Configure force_ssl in secrets.yml
   config.force_ssl = Rails.application.secrets.force_ssl
 
+  # Skip http-to-https redirect for the default health check endpoint.
+  # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
@@ -80,6 +83,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "consul_#{Rails.env}"
 
+  # Disable caching for Action Mailer templates even if Action Controller
+  # caching is enabled.
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
@@ -103,6 +108,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Only use :id for inspections in production.
+  config.active_record.attributes_for_inspect = [:id]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
