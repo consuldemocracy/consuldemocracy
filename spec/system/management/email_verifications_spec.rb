da9b81920af890a9aba841edfe9a8be152600c52
Javi Martín <javim@elretirao.net>
2025-03-12 15:23:11 +0100

    Don't reload
    One of these tests has recently failed on our CI:
    
    ```
    3) Users Create a level 3 user with email from scratch
       Failure/Error: expect(user.reload).to be_confirmed
         expected `#<User id: 2060, email: "pepe@gmail.com", created_at:
         "2025-03-12 19:51:03.688867000 +0100", updated_...d_debates: true,
         recommended_proposals: true, subscriptions_token: nil,
         registering_from_web: false>.confirmed?` to be truthy, got false
    ```
    
    Note we're adding an offset when editing the map marker by clicking on
    `map-location` with `.click(x: 30, y: 30)`. This way we make sure that
    both the latitude and longitude change from the original values; we used
    to clicking in the middle (no offset), which didn't change the longitude
    and changed the latitude just by coincidence.

--- spec/system/management/email_verifications_spec.rb
+++ spec/system/management/email_verifications_spec.rb
@@ -2,35 +2,46 @@ require "rails_helper"
 
 describe "EmailVerifications" do
   scenario "Verifying a level 1 user via email" do
-    login_as_manager
-
     user = create(:user)
 
-    visit management_document_verifications_path
-    fill_in "document_verification_document_number", with: "12345678Z"
-    click_button "Check document"
+    in_browser(:user) do
+      login_as user
+      visit root_path
 
-    expect(page).to have_content "Please introduce the email used on the account"
+      expect(page).to have_content "Debates"
+    end
 
-    fill_in "email_verification_email", with: user.email
-    click_button "Send verification email"
+    in_browser(:manager) do
+      login_as_manager
+      visit management_document_verifications_path
+      fill_in "document_verification_document_number", with: "12345678Z"
+      click_button "Check document"
 
-    expect(page).to have_content "In order to completely verify this user, " \
-                                 "it is necessary that the user clicks on a link"
+      expect(page).to have_content "Please introduce the email used on the account"
 
-    user.reload
+      fill_in "email_verification_email", with: user.email
+      click_button "Send verification email"
 
-    login_as(user)
+      expect(page).to have_content "In order to completely verify this user, " \
+                                   "it is necessary that the user clicks on a link"
+    end
 
     sent_token = /.*email_verification_token=(.*)".*/.match(ActionMailer::Base.deliveries.last.body.to_s)[1]
-    visit email_path(email_verification_token: sent_token)
 
-    expect(page).to have_content "You are a verified user"
+    in_browser(:user) do
+      visit email_path(email_verification_token: sent_token)
 
-    expect(page).not_to have_link "Verify my account"
-    expect(page).to have_content "Account verified"
+      expect(page).to have_content "You are a verified user"
+      expect(page).to have_content "Account verified"
+      expect(page).not_to have_link "Verify my account"
+    end
 
-    expect(user.reload.document_number).to eq("12345678Z")
-    expect(user).to be_level_three_verified
+    in_browser(:manager) do
+      visit management_document_verifications_path
+      fill_in "document_verification_document_number", with: "12345678Z"
+      click_button "Check document"
+
+      expect(page).to have_content "This user account is already verified"
+    end
   end
 end
