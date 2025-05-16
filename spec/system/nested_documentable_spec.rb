9e805d95de460fd7f15e905fca5e0f12be08ef96
taitus <sebastia.roig@gmail.com>
2025-05-16 15:17:14 +0200

    Unify tests from "Only for edit path" describe in order to reduce duplicated code

--- spec/system/nested_documentable_spec.rb
+++ spec/system/nested_documentable_spec.rb
@@ -218,13 +218,7 @@ describe "Nested documentable" do
 
         expect(page).not_to have_link "Add new document"
         expect(page).to have_css ".document-fields", count: 1
-      end
 
-      scenario "Should remove nested field after remove document" do
-        Setting["uploads.documents.max_amount"] = 1
-        create(:document, documentable: proposal)
-        login_as user
-        visit edit_proposal_path(proposal)
         click_link "Remove document"
 
         expect(page).to have_link "Add new document"
