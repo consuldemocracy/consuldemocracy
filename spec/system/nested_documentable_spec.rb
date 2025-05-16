88ba548343c07e1e0c6502d46f5d31c8a001c4da
taitus <sebastia.roig@gmail.com>
2025-05-16 12:34:01 +0200

    Unify fill_in field in one line

--- spec/system/nested_documentable_spec.rb
+++ spec/system/nested_documentable_spec.rb
@@ -102,8 +102,7 @@ describe "Nested documentable" do
 
         click_link "Add new document"
         within "#nested-documents" do
-          input = find("input[name$='[title]']")
-          fill_in input[:id], with: "My Title"
+          fill_in "Title", with: "My Title"
           attach_file "Choose document", file_fixture("empty.pdf")
 
           expect(page).to have_css ".loading-bar.complete"
