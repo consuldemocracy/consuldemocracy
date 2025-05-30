7d648103c4a6d5cf48e610612d0875c0e61e5065
CoslaJohn <johnmunro@cosla.gov.uk>
2025-05-30 14:48:48 +0100

    migration to add 2fa

--- db/migrate/20241028165107_add_devise_two_factor_to_users.rb
+++ db/migrate/20241028165107_add_devise_two_factor_to_users.rb
@@ -3,5 +3,6 @@ class AddDeviseTwoFactorToUsers < ActiveRecord::Migration[7.0]
     add_column :users, :otp_secret, :string
     add_column :users, :consumed_timestep, :integer
     add_column :users, :otp_required_for_login, :boolean
+    add_column :users, :otp_backup_codes, :string, array: true
   end
 end
