006f19e6c5b5a575aaeb78c684c8ecdbf22a7e18
Tamir Levy <tamirlevi123@gmail.com>
2025-06-03 12:27:59 +0300

    Add Hebrew translations and component updates

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -1,5 +1,11 @@
 class ApplicationComponent < ViewComponent::Base
   include SettingsHelper
+  include ActionView::Helpers::TranslationHelper
+
+  def t(key = nil, **options)
+    ActionView::Helpers::TranslationHelper.instance_method(:t).bind(self).call(key, **options)
+  end
+ 
   delegate :back_link_to, to: :helpers
   delegate :default_form_builder, to: :controller
 end
