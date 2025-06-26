4b96ddabd053b2fb2e5df739533b541b5349b7f9
Javi Martín <javim@elretirao.net>
2025-06-26 14:52:06 +0200

    Fix Rubocop offenses in demo seeds

--- db/demo_seeds/sdg.rb
+++ db/demo_seeds/sdg.rb
@@ -14,14 +14,19 @@ end
 section "Creating DEMO SDG homepage cards" do
   card = Widget::Card.create!(cardable: SDG::Phase.first,
                               title: "What is your proposal?",
-                              description: "Submit your citizen proposal. Organize a community around your proposal, find the support of your neighbors and when you have enough support, your proposal will be debated in the municipal plenary.",
+                              description: "Submit your citizen proposal. Organize a community around " \
+                                           "your proposal, find the support of your neighbors and when " \
+                                           "you have enough support, your proposal will be debated in " \
+                                           "the municipal plenary.",
                               link_url: "/proposals",
                               link_text: "Make your proposal",
                               label: "Ideas")
 
   card.image = Image.create!(imageable: card,
                              title: card.title,
-                             attachment: demo_seed_image_attachment("widgets/decide-which-should-be-the-new-square.jpg"),
+                             attachment: demo_seed_image_attachment(
+                               "widgets/decide-which-should-be-the-new-square.jpg"
+                             ),
                              user_id: 1)
 
   card = Widget::Card.create!(cardable: SDG::Phase.second,
@@ -38,13 +43,15 @@ section "Creating DEMO SDG homepage cards" do
 
   card = Widget::Card.create!(cardable: SDG::Phase.third,
                               title: "Comment the plan for a 100% green city",
-                              description: "Inform yourself and comment on the proposal about the plan for a 100% green city",
+                              description: "Inform yourself and comment on the proposal about " \
+                                           "the plan for a 100% green city",
                               link_url: "/proposals/1#tab-comments",
                               link_text: "See comments")
 
   card.image = Image.create!(imageable: card,
                              title: card.title,
-                             attachment: demo_seed_image_attachment("proposals/strategic-plan-for-a-100-green-city.jpg"),
+                             attachment: demo_seed_image_attachment(
+                               "proposals/strategic-plan-for-a-100-green-city.jpg"
+                             ),
                              user_id: 1)
-
 end
