93a6655de0b0b6b8469397bd7de09ff6132c515e
taitus <sebastia.roig@gmail.com>
2025-06-30 11:26:55 +0200

    Use with_collection to render dashboard poll component

--- app/components/dashboard/poll_component.rb
+++ app/components/dashboard/poll_component.rb
@@ -1,7 +1,9 @@
 class Dashboard::PollComponent < ApplicationComponent
+  with_collection_parameter :poll
+
   attr_reader :poll, :proposal
 
-  def initialize(poll, proposal)
+  def initialize(poll:, proposal:)
     @poll = poll
     @proposal = proposal
   end
