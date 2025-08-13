33ad19d7a872b143efc969dbcacd078b425b1d25
vero <veronica.garcia@alten.es>
2025-08-13 09:52:50 +0200

    Cambiado el nombre de la tabla

--- db/migrate/R9001_AñadirCheckMesaZonaEnPropuestaGasto/20251707120000_add_budget_zona_mesa_to_investments.rb
+++ db/migrate/R9001_AñadirCheckMesaZonaEnPropuestaGasto/20251707120000_add_budget_zona_mesa_to_investments.rb
@@ -1,6 +1,6 @@
 class AddBudgetZonaMesaToInvestments < ActiveRecord::Migration[4.2]
   def change
-	add_column :budget_investment, :zona_mesa, :boolean, default: false
-    add_index :budget_investment, :zona_mesa
+	add_column :budget_investments, :zona_mesa, :boolean, default: false
+    add_index :budget_investments, :zona_mesa
   end
 end
