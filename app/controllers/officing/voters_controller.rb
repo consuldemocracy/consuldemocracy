enredacoop/consuldemocracy-multitenant

Add unique index to poll voters table TODO: check whether to remove the uniqueness validation in poll voter if we don't ever try to validate one
Note that Rails 7.1 changes `find_or_create_by!` so it calls
`create_or_find_by!` when no record is found, meaning we'll rarely get
`RecordNotUnique` exception when using this method during a race
condition. We still might get a `RecordInvalid` exception in the race
condition scenario, though, due to the uniqueness validation. According
to the `create_or_find_by` documentation [1]:

> Columns with unique database constraints should not have uniqueness
> validations defined, otherwise create will fail due to validation
> errors and find_by will never be called.

[1] Currently only for 7.2... TODO

Undo

app/controllers/officing/voters_controller.rb
@@ -14,18 +14,16 @@ class Officing::VotersController < Officing::BaseController
     @poll = Poll.find(voter_params[:poll_id])
     @user = User.find(voter_params[:user_id])
 
-    @user.with_lock do
-      @voter = Poll::Voter.new(document_type: @user.document_type,
-                               document_number: @user.document_number,
-                               user: @user,
-                               poll: @poll,
-                               origin: "booth",
-                               officer: current_user.poll_officer,
-                               booth_assignment: current_booth.booth_assignments.find_by(poll: @poll),
-                               officer_assignment: officer_assignment(@poll))
-
-      @voter.save!
-    end
+    Poll::Voter.create_or_find_by!(
+      document_type: @user.document_type,
+      document_number: @user.document_number,
+      user: @user,
+      poll: @poll,
+      origin: "booth",
+      officer: current_user.poll_officer,
+      booth_assignment: current_booth.booth_assignments.find_by(poll: @poll),
+      officer_assignment: officer_assignment(@poll)
+    )
   end
 
   private

consuldemocracy/consuldemocracy

app/controllers/officing/voters_controller.rb
@@ -14,18 +14,16 @@ class Officing::VotersController < Officing::BaseController
     @poll = Poll.find(voter_params[:poll_id])
     @user = User.find(voter_params[:user_id])
 
-    @user.with_lock do
-      @voter = Poll::Voter.new(document_type: @user.document_type,
-                               document_number: @user.document_number,
-                               user: @user,
-                               poll: @poll,
-                               origin: "booth",
-                               officer: current_user.poll_officer,
-                               booth_assignment: current_booth.booth_assignments.find_by(poll: @poll),
-                               officer_assignment: officer_assignment(@poll))
-
-      @voter.save!
-    end
+    Poll::Voter.find_or_create_by!(
+      document_type: @user.document_type,
+      document_number: @user.document_number,
+      user: @user,
+      poll: @poll,
+      origin: "booth",
+      officer: current_user.poll_officer,
+      booth_assignment: current_booth.booth_assignments.find_by(poll: @poll),
+      officer_assignment: officer_assignment(@poll)
+    )
   end
 
   private