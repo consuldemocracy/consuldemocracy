0bb9725a0ff0acdfb97e55271e46ab5daeec163a
taitus <sebastia.roig@gmail.com>
2025-08-18 11:42:41 +0200

    Allow marking options as open text
    Add a boolean field to poll question options to indicate whether
    they should allow users to enter custom text. This prepares the
    backend for allowing "Other (please specify)" type answers.

--- db/schema.rb
+++ db/schema.rb
@@ -1116,6 +1116,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_09_085528) do
     t.integer "question_id"
     t.integer "given_order", default: 1
     t.boolean "most_voted", default: false
+    t.boolean "open_text", default: false
     t.index ["question_id"], name: "index_poll_question_answers_on_question_id"
   end
 
