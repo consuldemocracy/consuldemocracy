205da7010d00bc178631a922dacabd77496899af
taitus <sebastia.roig@gmail.com>
2025-08-22 07:44:39 +0200

    Manage correctly results and stats for open answers questions

--- app/components/polls/results/question_component.rb
+++ app/components/polls/results/question_component.rb
@@ -12,4 +12,32 @@ class Polls::Results::QuestionComponent < ApplicationComponent
   def most_voted_option?(option)
     question.most_voted_option_ids.include?(option.id)
   end
+
+  def essay_total_responses
+    essay_answered_count + essay_unanswered_count
+  end
+
+  def essay_answered_count
+    @essay_answered_count ||= question.answers.where.not(text_answer: [nil, ""]).count
+  end
+
+  def essay_unanswered_count
+    valid_participants_count - essay_answered_count
+  end
+
+  def essay_answered_percentage
+    return 0.0 if essay_total_responses.zero?
+
+    (essay_answered_count * 100.0) / essay_total_responses
+  end
+
+  def essay_unanswered_percentage
+    return 0.0 if essay_total_responses.zero?
+
+    (essay_unanswered_count * 100.0) / essay_total_responses
+  end
+
+  def valid_participants_count
+    @valid_participants_count ||= question.poll.answers.distinct.count(:author_id)
+  end
 end
