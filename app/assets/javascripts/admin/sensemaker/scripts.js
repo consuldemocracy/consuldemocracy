21cbc58a3ed9ecd5e712a928bc73b83ab78006d1
Rory Gianni <hello@rgianni.cc>
2025-09-04 15:49:15 +0100

    more lint

--- app/assets/javascripts/admin/sensemaker/scripts.js
+++ app/assets/javascripts/admin/sensemaker/scripts.js
@@ -3,20 +3,25 @@
   App.AdminSensemakerScripts = {
 
     initialize: function() {
-      const remoteSubmits = $(".admin .sensemaker form button[type='submit'][data-remote='true'], .admin .sensemaker form input[type='submit'][data-remote='true']");
-      remoteSubmits.on("click", function(event){
+      var buttons = ".admin .sensemaker form button[type='submit'][data-remote='true']";
+      var inputs = ".admin .sensemaker form input[type='submit'][data-remote='true']";
+      var remoteSubmits = $(buttons + ", " + inputs);
+
+      remoteSubmits.on("click", function(event) {
         event.preventDefault();
-        const form = $(this).closest("form")
+        var form = $(this).closest("form");
         App.AdminSensemakerScripts.remoteSubmit(form, $(this));
       });
 
-      const forms = $(".admin .sensemaker form");
+      var forms = $(".admin .sensemaker form");
       forms.on("submit", this.handleTempDisable);
     },
 
-    handleTempDisable: function(event) {
-      const form = $(this);
-      const submitButtons = form.find("button[type='submit'][data-temp-disable='true'], input[type='submit'][data-temp-disable='true']");
+    handleTempDisable: function() {
+      var form = $(this);
+      var submitButtons = form.find(
+        "button[type='submit'][data-temp-disable='true'], input[type='submit'][data-temp-disable='true']"
+      );
       submitButtons.attr("disabled", "disabled");
       setTimeout(function() {
         submitButtons.removeAttr("disabled");
@@ -25,22 +30,16 @@
 
     remoteSubmit: function(form, submitter) {
       // Serialize the form data
-      const formData = form.serialize();
-      const formAction = submitter.attr("formaction") || form.attr("action");
-      const formMethod = form.attr("method") || "POST";
-      
+      var formData = form.serialize();
+      var formAction = submitter.attr("formaction") || form.attr("action");
+      var formMethod = form.attr("method") || "POST";
+
       // Submit via AJAX
       $.ajax({
         url: formAction,
         type: formMethod,
         data: formData,
-        dataType: "script", // This tells Rails to expect JavaScript response
-        success: function(response) {
-          console.log("Form submitted successfully");
-        },
-        error: function(xhr, status, error) {
-          console.error("Form submission failed:", error);
-        }
+        dataType: "script" // This tells Rails to expect JavaScript response
       });
     },
   };
