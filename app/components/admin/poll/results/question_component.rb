e3e475b4df2fadb9d3c07eb35146d493d067c348
taitus <sebastia.roig@gmail.com>
2025-09-16 15:19:11 +0200

    Add votes_for(option) method and simplify results template
    Move the summing logic from the template into the component. Introduce
    a votes_for(option) method that looks up grouped partial results and
    returns the total amount or 0.

--- app/components/admin/poll/results/question_component.rb
+++ app/components/admin/poll/results/question_component.rb
@@ -6,7 +6,14 @@ class Admin::Poll::Results::QuestionComponent < ApplicationComponent
     @partial_results = partial_results
   end
 
-  def by_answer
-    @by_answer ||= partial_results.where(question: question).group_by(&:answer)
+  def votes_for(option)
+    grouped = by_answer[option.title] || []
+    grouped.sum(&:amount)
   end
+
+  private
+
+    def by_answer
+      @by_answer ||= partial_results.where(question: question).group_by(&:answer)
+    end
 end
