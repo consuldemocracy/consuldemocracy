7996933fc2a4aba0a409bfc7c6a8a036c9418f78
taitus <sebastia.roig@gmail.com>
2025-09-17 10:44:28 +0200

    Extract by_answer variable to component method

--- app/components/admin/poll/results/question_component.rb
+++ app/components/admin/poll/results/question_component.rb
@@ -6,7 +6,13 @@ class Admin::Poll::Results::QuestionComponent < ApplicationComponent
     @partial_results = partial_results
   end
 
-  def by_question
-    @by_question ||= partial_results.group_by(&:question_id)
+  def by_answer
+    @by_answer ||= by_question[question.id].present? ? by_question[question.id].group_by(&:answer) : {}
   end
+
+  private
+
+    def by_question
+      @by_question ||= partial_results.group_by(&:question_id)
+    end
 end
