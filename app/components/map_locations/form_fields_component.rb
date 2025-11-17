8a575ae83c66111166c5f55015541e67f302621d
Javi Martín <javim@elretirao.net>
2025-11-17 02:21:52 +0100

    Remove duplicate map location translations
    We were using the same texts twice. For the remove marker label text,
    however, we were using the text defined in proposals for both proposals
    and investments.
    
    Ideally the translation keys for these texts would go in another
    namespace, since they no longer refer to just proposals. However,
    renaming the translation keys would mean losing the existing
    translations in every language we manage through Crowdin. So we aren't
    doing so.

--- app/components/map_locations/form_fields_component.rb
+++ app/components/map_locations/form_fields_component.rb
@@ -1,15 +1,23 @@
 class MapLocations::FormFieldsComponent < ApplicationComponent
-  attr_reader :form, :map_location, :label, :help
+  attr_reader :form, :map_location
   use_helpers :render_map
 
-  def initialize(form, map_location:, label:, help:)
+  def initialize(form, map_location:)
     @form = form
     @map_location = map_location
-    @label = label
-    @help = help
   end
 
   def render?
     feature?(:map)
   end
+
+  private
+
+    def label
+      t("proposals.form.map_location")
+    end
+
+    def help
+      t("proposals.form.map_location_instructions")
+    end
 end
--- spec/components/map_locations/form_fields_component_spec.rb
+++ spec/components/map_locations/form_fields_component_spec.rb
@@ -4,11 +4,7 @@ describe MapLocations::FormFieldsComponent do
   let(:proposal) { Proposal.new }
   let(:map_location) { MapLocation.new }
   let(:form) { ConsulFormBuilder.new(:proposal, proposal, ApplicationController.new.view_context, {}) }
-  let(:label) { "Map location" }
-  let(:help) { "Add a marker" }
-  let(:component) do
-    MapLocations::FormFieldsComponent.new(form, map_location: map_location, label: label, help: help)
-  end
+  let(:component) { MapLocations::FormFieldsComponent.new(form, map_location: map_location) }
 
   it "is rendered when the map feature is enabled" do
     Setting["feature.map"] = true
