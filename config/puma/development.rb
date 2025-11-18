e887cd068678ecf0be0c4aabbed99cef68a1713a
Javi Martín <javim@elretirao.net>
2025-11-18 14:38:17 +0100

    Upgrade to Rails 8.0
    Compared to the code generated by `rails app:update`, we're reverting a
    change in `bin/setup` that used `bin/dev` insted of `bin/rails restart`.
    The reason is that we currently don't use `bin/dev` and it fails when a
    server is already running.
    
    We're also keeping using `null_store` in development when
    `tmp/caching-dev.txt` isn't present; the opposite was a bit confusing in
    my opinion.
    
    In the test environment, we keep disabling the cache to keep the
    existing behavior. Enabling the cache might catch bugs related to cache
    issues, but we need to check whether there are side effects.
    
    The `config.assets.quiet` option in the development environment was
    removed in Rails 8.0 but added back in Rails 8.1 [1], so we're keeping
    it.
    
    Since `config.assume_ssl` is true by default on production, we're
    setting it to the same value as `force_ssl`.
    
    We also keep serving static assets with Nginx by default and rotating
    the logger on production.
    
    Note we aren't using the `ActiveSupport::TaggedLogging.logger` method.
    We didn't manage to do so and still keep using a formatter with the PID
    and timestamp. When doing so, we got an error:
    
    ```
    <NoMethodError: undefined method 'push_tags' for an instance of
    Logger::Formatter>
    ```
    
    As mentioned in commit cb0084c41, we no longer need to keep using
    `connection_pool` 2.x, so we're upgrading it.
    
    [1] Pull request 53627 in https://github.com/rails/rails/

--- config/puma/development.rb
+++ config/puma/development.rb
@@ -1,9 +1,17 @@
+# This configuration file will be evaluated by Puma. The top-level methods that
+# are invoked here are part of Puma's configuration DSL. For more information
+# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
+#
 # Puma starts a configurable number of processes (workers) and each process
 # serves each request in a thread from an internal thread pool.
 #
+# You can control the number of workers using ENV["WEB_CONCURRENCY"]. You
+# should only set this value when you want to run 2 or more workers. The
+# default is already 1.
+#
 # The ideal number of threads per worker depends both on how much time the
 # application spends waiting for IO operations and on how much you wish to
-# to prioritize throughput over latency.
+# prioritize throughput over latency.
 #
 # As a rule of thumb, increasing the number of threads will increase how much
 # traffic a given process can handle (throughput), but due to CRuby's
@@ -19,10 +27,12 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-port        ENV.fetch("PORT", 3000)
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+port ENV.fetch("PORT", 3000)
+
 environment "development"
 
-# Allow puma to be restarted by `rails restart` command.
+# Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_09_085528) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_09_085528) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
-  enable_extension "plpgsql"
   enable_extension "unaccent"
 
   create_table "active_poll_translations", id: :serial, force: :cascade do |t|
