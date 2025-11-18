9b337487ab740d1a47b824f7ebccc514c26f7e98
Javi Martín <javim@elretirao.net>
2025-11-18 04:05:33 +0100

    Extract method in executions image component
    This makes the code more readable.

--- app/components/budgets/executions/image_component.rb
+++ app/components/budgets/executions/image_component.rb
@@ -11,4 +11,8 @@ class Budgets::Executions::ImageComponent < ApplicationComponent
     def milestone
       investment.milestones.where.associated(:image).order_by_publication_date.last
     end
+
+    def image
+      milestone&.image || investment.image
+    end
 end
