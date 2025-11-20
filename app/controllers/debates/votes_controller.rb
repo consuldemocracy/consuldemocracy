4f469a36e095f6b8fc1ee5191c5134ccb713b933
CoslaJohn <johnmunro@cosla.gov.uk>
2025-11-20 16:03:26 +0000

    debate controller test failing. Reverted to using existing register_vote method updated method to accept vote_params

--- app/controllers/debates/votes_controller.rb
+++ app/controllers/debates/votes_controller.rb
@@ -4,33 +4,20 @@ module Debates
     before_action :authenticate_user!
     load_and_authorize_resource :debate
     
-    # Updated: Load and authorize @vote for :update as well as :destroy
-    # CanCanCan will find the vote based on params[:id]
     load_and_authorize_resource through: :debate, through_association: :votes_for, only: [:destroy, :update]
 
-    # POST /debates/:debate_id/votes
-    # (Called when a user votes for the first time)
     def create
       authorize! :create, Vote.new(voter: current_user, votable: @debate)
-
-      # Use the standard `vote_by` method from the gem.
-      # It's designed to accept :vote_weight
-      @debate.vote_by(
-        voter: current_user,
-        vote_weight: vote_params[:vote_weight],
-        vote_flag: vote_params[:vote_flag]
-      )
-
+      
+      @debate.register_vote(current_user, vote_params)
+      
       respond_to do |format|
         format.html { redirect_to request.referer, notice: I18n.t("flash.actions.create.vote") }
         format.js { render :show }
       end
     end
 
-    # PATCH /debates/:debate_id/votes/:id
-    # (Called when a user *changes* their vote, e.g., from "Yes" to "No")
     def update
-      # @vote is already loaded and authorized by CanCanCan
       @vote.update(vote_params)
 
       respond_to do |format|
@@ -39,11 +26,7 @@ module Debates
       end
     end
 
-    # DELETE /debates/:debate_id/votes/:id
-    # (Called when a user clicks an *active* button to un-vote)
     def destroy
-      # @vote is already loaded and authorized.
-      # Using @vote.destroy is cleaner than @debate.unvote_by
       @vote.destroy
 
       respond_to do |format|
@@ -54,7 +37,6 @@ module Debates
 
     private
 
-    # Add strong parameters to permit the new attributes
     def vote_params
       params.require(:vote).permit(:vote_weight, :vote_flag)
     end
