9104f3298dede53d2334adf6861fca77cf9b7bb1
Javi Martín <javim@elretirao.net>
2025-11-24 03:28:35 +0100

    Simplify code in comment status bar component
    The name of the `parent_id` parameter was misleading, because we were
    passing a comment to that method.
    
    And we can extract a method that checks whether it's possible to add a
    comment.

--- app/components/comments/status_bar_component.rb
+++ app/components/comments/status_bar_component.rb
@@ -11,7 +11,17 @@ class Comments::StatusBarComponent < ApplicationComponent
 
   private
 
-    def comment_link_text(parent_id)
-      parent_id.present? ? t("comments_helper.reply_link") : t("comments_helper.comment_link")
+    def link_text
+      if comment.present?
+        t("comments_helper.reply_link")
+      else
+        t("comments_helper.comment_link")
+      end
+    end
+
+    def can_comment?
+      current_user &&
+        !comments_closed_for_commentable?(comment.commentable) &&
+        !require_verified_resident_for_commentable?(comment.commentable, current_user)
     end
 end
