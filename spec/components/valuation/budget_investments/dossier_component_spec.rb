29471a2bde850cf09b10992cc5f183c402634321
Javi Martín <javim@elretirao.net>
2025-11-26 16:10:08 +0100

    Use the budget currency in investments dossier
    We were using different currencies in different languages, when the
    currency doesn't depend on the language but on the budget.

---
+++ spec/components/valuation/budget_investments/dossier_component_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+describe Valuation::BudgetInvestments::DossierComponent do
+  it "uses the same currency as the investment's budget" do
+    dollar_investment = create(:budget_investment, budget: create(:budget, currency_symbol: "$"))
+    euro_investment = create(:budget_investment, budget: create(:budget, currency_symbol: "€"))
+
+    render_inline Valuation::BudgetInvestments::DossierComponent.new(dollar_investment)
+
+    expect(page).to have_content "Price ($)", normalize_ws: true
+    expect(page).to have_content "Cost during the first year ($)", normalize_ws: true
+
+    render_inline Valuation::BudgetInvestments::DossierComponent.new(euro_investment)
+
+    expect(page).to have_content "Price (€)", normalize_ws: true
+    expect(page).to have_content "Cost during the first year (€)", normalize_ws: true
+  end
+end
