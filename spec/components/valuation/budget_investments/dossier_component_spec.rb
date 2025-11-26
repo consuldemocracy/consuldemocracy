bf75320d7838de284c029743217cdbdfaf3b71a6
Javi Martín <javim@elretirao.net>
2025-11-26 16:28:17 +0100

    Use currency in translations in investment dossiers
    This is the same we do for the form to edit the investment. This way we
    don't use parenthesis even for languages that wouldn't use parenthesis
    in this case.
    
    Note we're changing the i18n key for these texts. The reason is that
    translations for existing languages are obsolete and, if we keep the
    same translation key, existing languages will no longer display the
    currency until translators update them.
    
    Also note that, in the `_dossier_detail` partial, we were passing the
    currency to the `show.price_first_year` translation, but we weren't
    using the currency in that translation, and so it wasn't displayed.
    We're fixing that as well.

--- spec/components/valuation/budget_investments/dossier_component_spec.rb
+++ spec/components/valuation/budget_investments/dossier_component_spec.rb
@@ -7,12 +7,12 @@ describe Valuation::BudgetInvestments::DossierComponent do
 
     render_inline Valuation::BudgetInvestments::DossierComponent.new(dollar_investment)
 
-    expect(page).to have_content "Price ($)", normalize_ws: true
-    expect(page).to have_content "Cost during the first year ($)", normalize_ws: true
+    expect(page).to have_content "Price ($)"
+    expect(page).to have_content "Cost during the first year ($)"
 
     render_inline Valuation::BudgetInvestments::DossierComponent.new(euro_investment)
 
-    expect(page).to have_content "Price (€)", normalize_ws: true
-    expect(page).to have_content "Cost during the first year (€)", normalize_ws: true
+    expect(page).to have_content "Price (€)"
+    expect(page).to have_content "Cost during the first year (€)"
   end
 end
