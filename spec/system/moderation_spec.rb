92e61dccd0f7ad5312700f1522a5b27e58f355bf
taitus <sebastia.roig@gmail.com>
2025-12-09 14:42:51 +0100

    Unify "Hide the resource" tests for debates and proposals

--- spec/system/moderation_spec.rb
+++ spec/system/moderation_spec.rb
@@ -128,5 +128,38 @@ describe "Moderation" do
         expect(page).not_to have_button "Block author"
       end
     end
+
+    describe "/moderation/ screen" do
+      let(:moderation_resource_index_path) { send("moderation_#{factory.to_s.pluralize}_path") }
+
+      before { login_as moderator.user }
+
+      describe "moderate in bulk" do
+        describe "When a resource has been selected for moderation" do
+          before do
+            visit moderation_resource_index_path
+            click_link "All"
+
+            check resource.title
+          end
+
+          scenario "Hide the resource" do
+            accept_confirm("Are you sure? Hide #{factory.to_s.pluralize}") do
+              click_button "Hide #{factory.to_s.pluralize}"
+            end
+
+            expect(page).not_to have_css "##{dom_id(resource)}"
+
+            click_link "Block users"
+            fill_in "email or name of user", with: resource.author.email
+            click_button "Search"
+
+            within "tr", text: resource.author.name do
+              expect(page).to have_button "Block"
+            end
+          end
+        end
+      end
+    end
   end
 end
