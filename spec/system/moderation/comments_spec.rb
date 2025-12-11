7e4fe1e4844d145ee3b266de06dd5fb4d64360fb
taitus <sebastia.roig@gmail.com>
2025-12-11 15:08:49 +0100

    Add comment as factory in moderation specs

--- spec/system/moderation/comments_spec.rb
+++ spec/system/moderation/comments_spec.rb
@@ -1,19 +1,6 @@
 require "rails_helper"
 
 describe "Moderate comments" do
-  scenario "Can not hide own comment" do
-    moderator = create(:moderator)
-    comment = create(:comment, user: moderator.user)
-
-    login_as(moderator.user)
-    visit debate_path(comment.commentable)
-
-    within("#comment_#{comment.id}") do
-      expect(page).not_to have_button "Hide"
-      expect(page).not_to have_button "Block author"
-    end
-  end
-
   scenario "Visit items with flagged comments" do
     moderator = create(:moderator)
     debate = create(:debate, title: "Debate with spam comment")
@@ -38,53 +25,5 @@ describe "Moderate comments" do
     click_link "Proposal with spam comment"
     expect(page).to have_content("Proposal with spam comment")
     expect(page).to have_content("This is SPAM comment on proposal")
-  end
-
-  describe "/moderation/ screen" do
-    before do
-      moderator = create(:moderator)
-      login_as(moderator.user)
-    end
-
-    describe "moderate in bulk" do
-      scenario "select all/none" do
-        create_list(:comment, 2)
-
-        visit moderation_comments_path
-        click_link "All"
-
-        expect(page).to have_field type: :checkbox, count: 2
-
-        within(".check-all-none") { click_button "Select all" }
-
-        expect(all(:checkbox)).to all(be_checked)
-
-        within(".check-all-none") { click_button "Select none" }
-
-        all(:checkbox).each { |checkbox| expect(checkbox).not_to be_checked }
-      end
-    end
-
-    scenario "Current filter is properly highlighted" do
-      visit moderation_comments_path
-      expect(page).not_to have_link("Pending")
-      expect(page).to have_link("All")
-      expect(page).to have_link("Marked as viewed")
-
-      visit moderation_comments_path(filter: "all")
-      expect(page).not_to have_link("All")
-      expect(page).to have_link("Pending")
-      expect(page).to have_link("Marked as viewed")
-
-      visit moderation_comments_path(filter: "pending_flag_review")
-      expect(page).to have_link("All")
-      expect(page).not_to have_link("Pending")
-      expect(page).to have_link("Marked as viewed")
-
-      visit moderation_comments_path(filter: "with_ignored_flag")
-      expect(page).to have_link("All")
-      expect(page).to have_link("Pending")
-      expect(page).not_to have_link("Marked as viewed")
-    end
   end
 end
