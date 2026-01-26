consuldemocracy/consuldemocracy

commit 30ea5f0d3c44836ac8698a114b963ddceddc890b

Bump ahoy_matey from 5.4.0 to 5.4.1
Bumps [ahoy_matey](https://github.com/ankane/ahoy) from 5.4.0 to 5.4.1.
- [Changelog](https://github.com/ankane/ahoy/blob/master/CHANGELOG.md)
- [Commits](https://github.com/ankane/ahoy/compare/v5.4.0...v5.4.1)

---
updated-dependencies:
- dependency-name: ahoy_matey
  dependency-version: 5.4.1
  dependency-type: direct:production
  update-type: version-update:semver-patch
...

Signed-off-by: dependabot[bot] <support@github.com>

Gemfile
@@ -6,7 +6,7 @@ gem "rails", "~> 7.2.3"
 
 gem "acts-as-taggable-on", "~> 12.0.0"
 gem "acts_as_votable", "~> 0.14.0"
-gem "ahoy_matey", "~> 5.4.0"
+gem "ahoy_matey", "~> 5.4.1"
 gem "airbrake", "~> 13.0.5"
 gem "ancestry", "~> 4.3.3"
 gem "audited", "~> 5.8.0"

commit cb0084c41b486e255d60a9e7b071270edb1e9637

Downgrade connection_pool to version 2.x
Upgrading to version 3, which we did in commit 30ea5f0d3, broke the mem
cache store on staging/production environments. This is already solved
in Rails [1] and backported to Rails 8.0 [2], but it hasn't been
backported to Rails 7.2.

[1] See pull request 56294 in https://github.com/rails/rails
[2] See commit 4854f71692 in https://github.com/rails/rails

Gemfile
@@ -17,6 +17,7 @@ gem "caxlsx", "~> 4.2.0"
 gem "caxlsx_rails", "~> 0.6.4"
 gem "ckeditor", "~> 4.3.0"
 gem "cocoon", "~> 1.2.15"
+gem "connection_pool", "~> 2.5" # TODO: remove after upgrading to Rails 8.0
 gem "csv", "~> 3.3.5"
 gem "daemons", "~> 1.4.1"
 gem "dalli", "~> 3.2.8"