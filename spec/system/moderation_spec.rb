BlackRoad-Gov/blackroad-consul-gov

Unify moderation access tests
Unify duplicated unauthorized access tests into a single
parameterized scenario and reorganizes tests into separate contexts
to improve readability and reduce duplication.

spec/system/moderation_spec.rb
@@ -5,87 +5,52 @@ include ActionView::RecordIdentifier
 describe "Moderation" do
   let(:user) { create(:user) }
 
-  scenario "Access as regular user is not authorized" do
-    login_as(user)
+  describe "Access" do
+    context "is not authorized" do
+      let(:factory) { [:user, :valuator, :manager, :sdg_manager, :poll_officer].sample }
 
-    visit moderation_root_path
+      before { create(factory, user: user) if factory != :user }
 
-    expect(page).not_to have_current_path(moderation_root_path)
-    expect(page).to have_current_path(root_path)
-    expect(page).to have_content "You do not have permission to access this page"
-  end
+      scenario "for regular users, valuators, managers, SDG managers and poll officers" do
+        login_as(user)
+        visit moderation_root_path
 
-  scenario "Access as valuator is not authorized" do
-    create(:valuator, user: user)
-    login_as(user)
+        expect(page).not_to have_current_path moderation_root_path
+        expect(page).to have_current_path root_path
+        expect(page).to have_content "You do not have permission to access this page"
+      end
+    end
 
-    visit moderation_root_path
+    context "is authorized" do
+      scenario "for moderators" do
+        Setting["org_name"] = "OrgName"
+        create(:moderator, user: user)
 
-    expect(page).not_to have_current_path(moderation_root_path)
-    expect(page).to have_current_path(root_path)
-    expect(page).to have_content "You do not have permission to access this page"
-  end
+        login_as(user)
+        visit root_path
+        click_link "Menu"
+        click_link "Moderation"
 
-  scenario "Access as manager is not authorized" do
-    create(:manager, user: user)
-    login_as(user)
+        expect(page).to have_current_path(moderation_root_path)
+        expect(page).to have_link "Go back to OrgName"
+        expect(page).to have_css "#moderation_menu"
+        expect(page).not_to have_css "#admin_menu"
+        expect(page).not_to have_css "#valuation_menu"
+        expect(page).not_to have_content "You do not have permission to access this page"
+      end
 
-    visit moderation_root_path
+      scenario "Access as an administrator is authorized" do
+        create(:administrator, user: user)
 
-    expect(page).not_to have_current_path(moderation_root_path)
-    expect(page).to have_current_path(root_path)
-    expect(page).to have_content "You do not have permission to access this page"
-  end
+        login_as(user)
+        visit root_path
+        click_link "Menu"
+        click_link "Moderation"
 
-  scenario "Access as SDG manager is not authorized" do
-    create(:sdg_manager, user: user)
-    login_as(user)
-
-    visit moderation_root_path
-
-    expect(page).not_to have_current_path(moderation_root_path)
-    expect(page).to have_current_path(root_path)
-    expect(page).to have_content "You do not have permission to access this page"
-  end
-
-  scenario "Access as poll officer is not authorized" do
-    create(:poll_officer, user: user)
-    login_as(user)
-
-    visit moderation_root_path
-
-    expect(page).not_to have_current_path(moderation_root_path)
-    expect(page).to have_current_path(root_path)
-    expect(page).to have_content "You do not have permission to access this page"
-  end
-
-  scenario "Access as a moderator is authorized" do
-    Setting["org_name"] = "OrgName"
-    create(:moderator, user: user)
-
-    login_as(user)
-    visit root_path
-    click_link "Menu"
-    click_link "Moderation"
-
-    expect(page).to have_current_path(moderation_root_path)
-    expect(page).to have_link "Go back to OrgName"
-    expect(page).to have_css "#moderation_menu"
-    expect(page).not_to have_css "#admin_menu"
-    expect(page).not_to have_css "#valuation_menu"
-    expect(page).not_to have_content "You do not have permission to access this page"
-  end
-
-  scenario "Access as an administrator is authorized" do
-    create(:administrator, user: user)
-
-    login_as(user)
-    visit root_path
-    click_link "Menu"
-    click_link "Moderation"
-
-    expect(page).to have_current_path(moderation_root_path)
-    expect(page).not_to have_content "You do not have permission to access this page"
+        expect(page).to have_current_path(moderation_root_path)
+        expect(page).not_to have_content "You do not have permission to access this page"
+      end
+    end
   end
 
   describe "Moderate resources" do

consuldemocracy/consuldemocracy

spec/system/moderation_spec.rb
@@ -5,87 +5,42 @@ include ActionView::RecordIdentifier
 describe "Moderation" do
   let(:user) { create(:user) }
 
-  scenario "Access as regular user is not authorized" do
-    login_as(user)
+  describe "Access" do
+    context "for regular users, valuators, managers, SDG managers and poll officers" do
+      let(:factory) { [:user, :valuator, :manager, :sdg_manager, :poll_officer].sample }
 
-    visit moderation_root_path
+      before { create(factory, user: user) if factory != :user }
 
-    expect(page).not_to have_current_path(moderation_root_path)
-    expect(page).to have_current_path(root_path)
-    expect(page).to have_content "You do not have permission to access this page"
-  end
+      scenario "is not authorized" do
+        login_as(user)
+        visit moderation_root_path
 
-  scenario "Access as valuator is not authorized" do
-    create(:valuator, user: user)
-    login_as(user)
+        expect(page).not_to have_current_path moderation_root_path
+        expect(page).to have_current_path root_path
+        expect(page).to have_content "You do not have permission to access this page"
+      end
+    end
 
-    visit moderation_root_path
+    context "for moderators and administrators" do
+      let(:factory) { [:moderator, :administrator].sample }
 
-    expect(page).not_to have_current_path(moderation_root_path)
-    expect(page).to have_current_path(root_path)
-    expect(page).to have_content "You do not have permission to access this page"
-  end
+      scenario "is authorized" do
+        Setting["org_name"] = "OrgName"
+        create(factory, user: user)
 
-  scenario "Access as manager is not authorized" do
-    create(:manager, user: user)
-    login_as(user)
+        login_as(user)
+        visit root_path
+        click_link "Menu"
+        click_link "Moderation"
 
-    visit moderation_root_path
-
-    expect(page).not_to have_current_path(moderation_root_path)
-    expect(page).to have_current_path(root_path)
-    expect(page).to have_content "You do not have permission to access this page"
-  end
-
-  scenario "Access as SDG manager is not authorized" do
-    create(:sdg_manager, user: user)
-    login_as(user)
-
-    visit moderation_root_path
-
-    expect(page).not_to have_current_path(moderation_root_path)
-    expect(page).to have_current_path(root_path)
-    expect(page).to have_content "You do not have permission to access this page"
-  end
-
-  scenario "Access as poll officer is not authorized" do
-    create(:poll_officer, user: user)
-    login_as(user)
-
-    visit moderation_root_path
-
-    expect(page).not_to have_current_path(moderation_root_path)
-    expect(page).to have_current_path(root_path)
-    expect(page).to have_content "You do not have permission to access this page"
-  end
-
-  scenario "Access as a moderator is authorized" do
-    Setting["org_name"] = "OrgName"
-    create(:moderator, user: user)
-
-    login_as(user)
-    visit root_path
-    click_link "Menu"
-    click_link "Moderation"
-
-    expect(page).to have_current_path(moderation_root_path)
-    expect(page).to have_link "Go back to OrgName"
-    expect(page).to have_css "#moderation_menu"
-    expect(page).not_to have_css "#admin_menu"
-    expect(page).not_to have_css "#valuation_menu"
-    expect(page).not_to have_content "You do not have permission to access this page"
-  end
-
-  scenario "Access as an administrator is authorized" do
-    create(:administrator, user: user)
-
-    login_as(user)
-    visit root_path
-    click_link "Menu"
-    click_link "Moderation"
-
-    expect(page).to have_current_path(moderation_root_path)
-    expect(page).not_to have_content "You do not have permission to access this page"
+        expect(page).to have_current_path moderation_root_path
+        expect(page).to have_link "Go back to OrgName"
+        expect(page).to have_css "#moderation_menu"
+        expect(page).not_to have_css "#admin_menu"
+        expect(page).not_to have_css "#valuation_menu"
+        expect(page).not_to have_content "You do not have permission to access this page"
+      end
+    end
   end
 
   describe "Moderate resources" do