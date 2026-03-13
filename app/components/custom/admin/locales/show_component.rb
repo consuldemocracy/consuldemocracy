ee03f0a139f726265f988d8a66c2b5c25ca494a4
Javi Martín <javim@elretirao.net>
2026-03-13 15:47:46 +0100

    Update custom locales component to Consul 2.5.0
    The `use_helpers` method is no longer available in ViewComponent 4, and
    we already include the `can?` method in the ApplicationComponent class.

--- app/components/custom/admin/locales/show_component.rb
+++ app/components/custom/admin/locales/show_component.rb
@@ -3,5 +3,4 @@ class Admin::Locales::ShowComponent < ApplicationComponent; end
 load Rails.root.join("app", "components", "admin", "locales", "show_component.rb")
 
 class Admin::Locales::ShowComponent
-  use_helpers :can?
 end
