fe73a6bc4c4e0494d9230d6813217c18e3a2bab9
taitus <sebastia.roig@gmail.com>
2026-04-20 15:09:07 +0200

    Extract investments shared remotely translatable specs to system specs

--- spec/system/remotely_translatable_spec.rb
+++ spec/system/remotely_translatable_spec.rb
@@ -3,6 +3,7 @@ require "sessions_helper"
 
 describe "Remotely translatable" do
   factories = [
+    :budget_investment,
     :debate,
     :proposal
   ]
@@ -13,7 +14,7 @@ describe "Remotely translatable" do
   let(:client) { provider::Client }
   let(:available_locales) { provider::AvailableLocales }
   let(:collection_symbol) { factory.to_s.pluralize.to_sym }
-  let(:path) { [resource, collection_symbol].sample }
+  let(:path) { [:show_path, :index_path].sample }
 
   before do
     allow(available_locales).to receive(:locales).and_return(%w[de en es fr pt zh-Hans])
@@ -33,20 +34,20 @@ describe "Remotely translatable" do
 
   context "Button to request remote translation" do
     scenario "should not be present when current locale translation exists" do
-      visit polymorphic_path(path)
+      visit send(path)
 
       expect(page).not_to have_button "Translate page"
     end
 
     scenario "should be present when current locale translation does not exists" do
-      visit polymorphic_path(path, locale: :es)
+      visit send(path, locale: :es)
 
       expect(page).to have_button "Traducir página"
     end
 
     scenario "should not be present when new current locale translation exists" do
       add_translations(resource, :es)
-      visit polymorphic_path(resource)
+      visit send(path)
 
       expect(page).not_to have_button "Traducir página"
 
@@ -57,11 +58,11 @@ describe "Remotely translatable" do
     end
 
     context "on index path" do
-      let(:path) { collection_symbol }
+      let(:path) { :index_path }
 
       scenario "should not be present when there are no resources to translate" do
         resource.destroy!
-        visit polymorphic_path(path, locale: :es)
+        visit send(path, locale: :es)
 
         expect(page).not_to have_button "Traducir página"
       end
@@ -71,7 +72,7 @@ describe "Remotely translatable" do
           add_translations(resource, :es)
           create(:comment, commentable: resource)
 
-          visit polymorphic_path(path)
+          visit send(path)
 
           expect(page).not_to have_button "Translate page"
 
@@ -89,7 +90,7 @@ describe "Remotely translatable" do
           add_translations(resource, :es)
           create_featured_debates
 
-          visit polymorphic_path(path)
+          visit send(path)
 
           expect(page).not_to have_button "Translate page"
 
@@ -106,7 +107,7 @@ describe "Remotely translatable" do
           add_translations(resource, :es)
           create_featured_proposals
 
-          visit polymorphic_path(path)
+          visit send(path)
 
           expect(page).not_to have_button "Translate page"
 
@@ -120,7 +121,7 @@ describe "Remotely translatable" do
     describe "with delayed job active", :delay_jobs do
       scenario "should not be present when an equal RemoteTranslation is enqueued" do
         create(:remote_translation, remote_translatable: resource, locale: :es)
-        visit polymorphic_path(path, locale: :es)
+        visit send(path, locale: :es)
 
         expect(page).to have_content "Por favor, espera 5 segundos y actualiza la página " \
                                      "para que se muestre el contenido traducido."
@@ -129,13 +130,13 @@ describe "Remotely translatable" do
     end
 
     context "on show path" do
-      let(:path) { resource }
+      let(:path) { :show_path }
 
       describe "should evaluate missing translations on resource comments" do
         scenario "display when exists resource translations but the comment does not have a translation" do
           add_translations(resource, :es)
           create(:comment, commentable: resource)
-          visit polymorphic_path(path)
+          visit send(path)
 
           expect(page).not_to have_button "Translate page"
 
@@ -147,7 +148,7 @@ describe "Remotely translatable" do
         scenario "not display when exists resource translations but his comment has translations" do
           add_translations(resource, :es)
           create_comment_with_translations(resource, :es)
-          visit polymorphic_path(path)
+          visit send(path)
 
           expect(page).not_to have_button "Translate page"
 
@@ -163,7 +164,7 @@ describe "Remotely translatable" do
   context "After click remote translations button" do
     describe "with delayed jobs", :delay_jobs do
       scenario "shows informative text when content is enqueued" do
-        visit polymorphic_path(path, locale: :es)
+        visit send(path, locale: :es)
 
         click_button "Traducir página"
 
@@ -185,7 +186,7 @@ describe "Remotely translatable" do
       scenario "content is immediately translated" do
         response = generate_response(resource)
         expect_any_instance_of(client).to receive(:call).and_return(response)
-        visit polymorphic_path(path, locale: :es)
+        visit send(path, locale: :es)
 
         expect(page).not_to have_content response.first
 
@@ -200,13 +201,13 @@ describe "Remotely translatable" do
         expect_any_instance_of(client).to receive(:call).and_return(response)
 
         in_browser(:one) do
-          visit polymorphic_path(path, locale: :es)
+          visit send(path, locale: :es)
 
           expect(page).to have_button "Traducir página"
         end
 
         in_browser(:two) do
-          visit polymorphic_path(path, locale: :es)
+          visit send(path, locale: :es)
           click_button "Traducir página"
 
           expect(page).to have_content "Se han solicitado correctamente las traducciones"
@@ -235,5 +236,19 @@ describe "Remotely translatable" do
   def generate_response(resource)
     field_text = Faker::Lorem.characters(number: 10)
     resource.translated_attribute_names.map { field_text }
+  end
+
+  def show_path(**opts)
+    case factory
+    when :budget_investment then budget_investment_path(resource.budget, resource, **opts)
+    else polymorphic_path(resource, **opts)
+    end
+  end
+
+  def index_path(**opts)
+    case factory
+    when :budget_investment then budget_investments_path(resource.budget, **opts)
+    else polymorphic_path(collection_symbol, **opts)
+    end
   end
 end
