a0e5e4a594c752c6d5e3c1124e8e480ed48123fb
Javi Martín <javim@elretirao.net>
2026-05-21 17:06:22 +0200

    Extract component to render legal links

--- app/components/layout/footer_component.rb
+++ app/components/layout/footer_component.rb
@@ -1,12 +1,6 @@
 class Layout::FooterComponent < ApplicationComponent
-  delegate :content_block, to: :helpers
-
   def render?
     !Rails.application.multitenancy_management_mode?
-  end
-
-  def footer_legal_content_block
-    content_block("footer_legal")
   end
 
   private
---
+++ app/components/layout/legal_links_component.rb
@@ -0,0 +1,7 @@
+class Layout::LegalLinksComponent < ApplicationComponent
+  delegate :content_block, to: :helpers
+
+  def footer_legal_content_block
+    content_block("footer_legal")
+  end
+end
