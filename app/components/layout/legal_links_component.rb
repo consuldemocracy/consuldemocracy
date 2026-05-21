78eb6bcaf6fe0c16fb2015da80fb5e331fde2754
Javi Martín <javim@elretirao.net>
2026-05-21 17:12:00 +0200

    Extract methods in legal links component
    This way customizing it will be easier.

--- app/components/layout/legal_links_component.rb
+++ app/components/layout/legal_links_component.rb
@@ -4,4 +4,18 @@ class Layout::LegalLinksComponent < ApplicationComponent
   def footer_legal_content_block
     content_block("footer_legal")
   end
+
+  private
+
+    def privacy_link
+      link_to t("layouts.footer.privacy"), page_path("privacy")
+    end
+
+    def conditions_link
+      link_to t("layouts.footer.conditions"), page_path("conditions")
+    end
+
+    def accessibility_link
+      link_to t("layouts.footer.accessibility"), page_path("accessibility")
+    end
 end
