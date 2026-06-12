c048ee7d76b4dd8b95c53186f5c04458c548ffee
Javi Martín <javim@elretirao.net>
2026-06-12 15:44:49 +0200

    Remove duplicate label in suggested image buttons
    We were showing an aria-label for screen readers but were also showing
    text exclusively for screen readers. The aria-label overwrites the
    contents of the button, so people using screen readers couldn't hear
    eiher the alt text of the image inside the button or the text inside the
    `show-for-sr` element.

--- spec/components/image_suggestions/suggestions_component_spec.rb
+++ spec/components/image_suggestions/suggestions_component_spec.rb
@@ -52,11 +52,9 @@ describe ImageSuggestions::SuggestionsComponent do
     it "includes accessibility attributes for container, buttons and images" do
       render_inline component
 
-      expect(page).to have_css ".suggested-images-container[role='region'][aria-label='Suggested images']"
-      expect(page).to have_css ".js-attach-suggested-image[aria-label='Attach suggested image 1 of 1']"
+      expect(page).to have_button "Attach suggested image 1 of 1"
       expect(page).to have_css "img.suggested-image[alt='Photographer 1']"
-      expect(page).to have_css "[aria-describedby='suggested-image-desc-1']"
-      expect(page).to have_css "#suggested-image-desc-1"
+      expect(page).to have_css ".suggested-images-container[role='region'][aria-label='Suggested images']"
     end
   end
 end
