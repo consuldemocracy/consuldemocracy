a2e0b5e5fa8114e7216ec5ee7aedaac28cb95cc5
Javi Martín <javim@elretirao.net>
2026-06-16 14:33:58 +0200

    Only test external link

--- spec/airbrake_spec.rb
+++
@@ -1,7 +0,0 @@
-require "rails_helper"
-
-describe "Airbrake" do
-  it "uses the same filtering rules that Consul Democracy" do
-    expect(Airbrake::Config.instance.blocklist_keys).to eq Rails.application.config.filter_parameters
-  end
-end
--- spec/components/dashboard/polls_component_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-describe Dashboard::PollsComponent do
-  it "is not rendered when there aren't any polls" do
-    render_inline Dashboard::PollsComponent.new(Poll.none)
-
-    expect(page).not_to be_rendered
-  end
-end
