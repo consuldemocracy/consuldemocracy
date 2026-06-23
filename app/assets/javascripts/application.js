46fbad43dbcba5663806e0ec3cff68c576259fbe
taitus <sebastia.roig@gmail.com>
2026-06-23 15:10:13 +0200

    Remove blueimp file upload dependency
    Remove blueimp-file-upload after migrating document and image uploads to
    Dropzone.
    The upload flows now use hidden Dropzone instances wired to the existing
    file inputs, preserving the current markup and behavior.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -50,8 +50,6 @@
 //= require jquery-ui/ui/i18n/datepicker-en-GB
 //= require jquery-ui/ui/widgets/autocomplete
 //= require jquery-ui/ui/widgets/sortable
-//= require blueimp-file-upload/js/jquery.iframe-transport
-//= require blueimp-file-upload/js/jquery.fileupload
 //= require @deltablot/dropzone/dist/dropzone-min
 //= require dropzone_autodiscover_off
 //= require foundation-sites
