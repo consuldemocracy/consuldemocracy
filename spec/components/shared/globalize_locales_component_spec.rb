b12a12c211f284d9573ad14456c5708e60d78b06
Javi Martín <javim@elretirao.net>
2026-07-07 14:42:45 +0200

    Add aria-invalid and aria-errormessage to language selector
    This way people using screen readers will be notified about the presence
    of the error.

--- spec/components/shared/globalize_locales_component_spec.rb
+++ spec/components/shared/globalize_locales_component_spec.rb
@@ -1,6 +1,23 @@
 require "rails_helper"
 
 describe Shared::GlobalizeLocalesComponent do
+  before do
+    dummy_banner = Class.new(ApplicationRecord) do
+      def self.name
+        "DummyBanner"
+      end
+      self.table_name = "banners"
+
+      translates :title, touch: true
+      validates :title, presence: true
+      include Globalizable
+
+      has_many :translations, class_name: "DummyBanner::Translation", foreign_key: "banner_id"
+    end
+
+    stub_const("DummyBanner", dummy_banner)
+  end
+
   describe "Language selector" do
     it "only includes enabled locales" do
       Setting["locales.enabled"] = "en nl"
@@ -44,6 +61,41 @@ describe Shared::GlobalizeLocalesComponent do
       render_inline Shared::GlobalizeLocalesComponent.new
 
       expect(page).to have_select "Add language", options: ["", "English", "Nederlands"]
+    end
+
+    describe "errors" do
+      it "adds ARIA attributes when there are errors" do
+        dummy_record = DummyBanner.new
+        allow(dummy_record).to receive(:new_record?).and_return(false)
+        dummy_record.valid?
+
+        render_inline Shared::GlobalizeLocalesComponent.new(dummy_record)
+
+        expect(page).to have_css "select[aria-invalid][aria-errormessage='select_language_error']"
+        expect(page).to have_css "#select_language_error",
+                                 exact_text: "It's mandatory to provide at least one translation"
+      end
+
+      it "does not add an error field when there aren't any errors" do
+        dummy_record = DummyBanner.new
+        dummy_record.valid?
+
+        render_inline Shared::GlobalizeLocalesComponent.new(dummy_record)
+
+        expect(page).not_to have_css "select[aria-invalid]"
+        expect(page).not_to have_css "select[aria-errormessage]"
+        expect(page).not_to have_css "#select_language_error"
+        expect(page).not_to have_content "provide at least one translation"
+      end
+
+      it "does not add an error field when there isn't a resource" do
+        render_inline Shared::GlobalizeLocalesComponent.new
+
+        expect(page).not_to have_css "select[aria-invalid]"
+        expect(page).not_to have_css "select[aria-errormessage]"
+        expect(page).not_to have_css "#select_language_error"
+        expect(page).not_to have_content "provide at least one translation"
+      end
     end
   end
 end
