96d668df5d2544cc01e23938482c6f6369ac33a4
Javi Martín <javim@elretirao.net>
2026-07-17 17:14:19 +0200

    Move image partial to a component
    This way it'll be easier to render it inside a JSON response, which is
    something we'll do in the next commit.

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -5,14 +5,8 @@ module ImagesHelper
     polymorphic_url(image.variant(version))
   end
 
-  def image_class(image)
-    image.persisted? ? "persisted-image" : "cached-image"
-  end
-
   def render_image(image, version, show_caption = true)
-    render "images/image", image: image,
-                           version: (version if image.persisted?),
-                           show_caption: show_caption
+    render Images::ImageComponent.new(image, (version if image.persisted?), show_caption: show_caption)
   end
 
   def attached_background_css(path)
