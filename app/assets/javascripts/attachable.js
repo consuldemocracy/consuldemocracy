consuldemocracy/consuldemocracy

commit 34ab45ab6d0776749e7b6c8b9ec83fb30d00a740

Migrate document uploads from blueimp to Dropzone
Use Dropzone for document uploads instead of blueimp.
The user still picks a file with the same input, and Dropzone uploads it
in the background.

Progress bar, errors, document limits and Cocoon still work the same.
Image uploads still use blueimp in this commit.

app/assets/javascripts/documentable.js
@@ -22,51 +22,91 @@
       App.Documentable.initializeRemoveCachedDocumentLinks();
     },
     initializeDirectUploadInput: function(input) {
-      var inputData;
-      inputData = this.buildData([], input);
-      $(input).fileupload({
+      var $input, uploadData, dropzone, $zone;
+
+      $input = $(input);
+
+      if ($input.data("documentableDropzone")) {
+        return;
+      }
+
+
+      $zone = $input.closest(".document-attachment").find(".js-document-dropzone");
+      if ($zone.length === 0) {
+        $zone = $("<div>", { class: "js-document-dropzone hidden-dropzone-upload" });
+        $input.closest(".document-attachment").append($zone);
+      }
+
+      uploadData = {};
+
+      dropzone = new Dropzone($zone[0], {
+        url: $input.data("url"),
         paramName: "attachment",
-        formData: null,
-        add: function(e, data) {
-          var upload_data;
-          upload_data = App.Documentable.buildData(data, e.target);
-          App.Documentable.clearProgressBar(upload_data);
-          App.Documentable.setProgressBar(upload_data, "uploading");
-          upload_data.submit();
-        },
-        change: function(e, data) {
-          data.files.forEach(function(file) {
-            App.Documentable.setFilename(inputData, file.name);
-          });
-        },
-        fail: function(e, data) {
-          $(data.cachedAttachmentField).val("");
-          App.Documentable.clearFilename(data);
-          App.Documentable.setProgressBar(data, "errors");
-          App.Documentable.clearInputErrors(data);
-          App.Documentable.setInputErrors(data);
-          $(data.destroyAttachmentLinkContainer).find("a.delete:not(.remove-nested)").remove();
-          $(data.addAttachmentLabel).addClass("error");
-        },
-        done: function(e, data) {
-          var destroyAttachmentLink;
-          $(data.cachedAttachmentField).val(data.result.cached_attachment);
-          App.Documentable.setTitleFromFile(data, data.result.filename);
-          App.Documentable.setProgressBar(data, "complete");
-          App.Documentable.setFilename(data, data.result.filename);
-          App.Documentable.clearInputErrors(data);
-          destroyAttachmentLink = $(data.result.destroy_link);
-          $(data.destroyAttachmentLinkContainer).html(destroyAttachmentLink);
-          if (input.lockUpload) {
-            App.Documentable.showNotice();
-          }
-        },
-        progress: function(e, data) {
-          var progress;
-          progress = parseInt(data.loaded / data.total * 100, 10);
-          $(data.progressBar).find(".loading-bar").css("width", progress + "%");
+        maxFiles: 1,
+        clickable: false,
+        autoProcessQueue: true,
+        headers: { "X-CSRF-Token": $("meta[name=csrf-token]").attr("content") },
+        previewTemplate: "<div></div>"
+      });
+
+      $input.on("change.documentable", function() {
+        if (this.files.length === 0) {
+          return;
+        }
+
+        uploadData = App.Documentable.buildData([], input);
+        App.Documentable.setFilename(uploadData, this.files[0].name);
+        dropzone.removeAllFiles(true);
+        dropzone.addFile(this.files[0]);
+      });
+
+      dropzone.on("addedfile", function() {
+        uploadData = App.Documentable.buildData([], input);
+        App.Documentable.clearProgressBar(uploadData);
+        App.Documentable.setProgressBar(uploadData, "uploading");
+      });
+
+      dropzone.on("uploadprogress", function(_file, progress) {
+        $(uploadData.progressBar).find(".loading-bar").css("width", progress + "%");
+      });
+
+      dropzone.on("success", function(_file, response) {
+        var destroyAttachmentLink;
+
+        $(uploadData.cachedAttachmentField).val(response.cached_attachment);
+        App.Documentable.setTitleFromFile(uploadData, response.filename);
+        App.Documentable.setProgressBar(uploadData, "complete");
+        App.Documentable.setFilename(uploadData, response.filename);
+        App.Documentable.clearInputErrors(uploadData);
+        destroyAttachmentLink = $(response.destroy_link);
+        $(uploadData.destroyAttachmentLinkContainer).html(destroyAttachmentLink);
+
+        if (input.lockUpload) {
+          App.Documentable.showNotice();
         }
       });
+
+      dropzone.on("error", function(file, message, xhr) {
+        var errors;
+
+        if (xhr && xhr.responseJSON && xhr.responseJSON.errors) {
+          errors = xhr.responseJSON.errors;
+        } else {
+          errors = message;
+        }
+
+        uploadData.jqXHR = xhr || { responseJSON: { errors: errors } };
+        $(uploadData.cachedAttachmentField).val("");
+        App.Documentable.clearFilename(uploadData);
+        App.Documentable.setProgressBar(uploadData, "errors");
+        App.Documentable.clearInputErrors(uploadData);
+        App.Documentable.setInputErrors(uploadData);
+        $(uploadData.destroyAttachmentLinkContainer).find("a.delete:not(.remove-nested)").remove();
+        $(uploadData.addAttachmentLabel).addClass("error");
+        dropzone.removeFile(file);
+      });
+
+      $input.data("documentableDropzone", dropzone);
     },
     buildData: function(data, input) {
       var wrapper;

commit a7fffc42cbe56b51bfe75b9dda8e0347fb9346f7

Simplify JavaScript to handle attachments
Most of the time, when we make an AJAX call, the JavaScript is pretty
simple: it takes the contents of the response, and replaces an existing
element with them.

Attachments were an exception. Here the server didn't generate HTML, so
the code in the client was complex. In particular, there was duplication
in things like previewing an image or managing errors; here the client
was mimicking what the server does.

In commit 17ff8194b, we added the `aria-invalid` and `aria-errormessage`
attributes to invalid fields. However, this didn't work when attaching
images or documents, because in this case the JavaScript code was
handling the errors.

We could duplicate this logic in JavaScript, but instead, we think it's
better to remove the duplication and simplify the JavaScript code so it
renders whatever the server sends.

This also fixes a bug we introduced in commits 34ab45ab6 and 00ffe208e
when submitting the form after attaching an invalid document or image.
The page crashed when we did so. We're expanding the invalid file upload
system tests in order to check this case.

In order to make sure we don't return several document fields with
identical HTML ID and name attributes, we're passing an index to
`fields_for`. This is similar to what Cocoon does, only Cocoon uses the
current time and we're using `SecureRandom`. Cocoon generates integers
with 13 numbers; we're using 12 because a billion/trillion is a more
familiar number.

To keep the title that has already been typed by the user, we're making
that title available during the upload request, so the file name is only
used when the title is still empty.

We're also changing the CSS to add a fixed height to the uploaded image
preview. If we didn't do so, the page would show the "remove image" link
before loading the image and, after loading the image, the position of
the "remove image" link (as well as the position of other elements)

.erb_lint.yml
@@ -32,6 +32,7 @@ linters:
     exclude:
       - app/views/mailer/proposal_notification_digest.html.erb
       - app/views/**/*.js.erb
+      - app/views/**/*.json.erb
   NoJavascriptTagHelper:
     enabled: true
   ParserErrors:

app/assets/javascripts/attachable.js
@@ -3,14 +3,14 @@
 
   App.Attachable = {
     setupInput: function(config) {
-      var $input, $container, uploadData, dropzone, $zone, dropzoneOptions;
+      var $input, $container, $fieldsContainer, dropzone, $progressBar, $zone, dropzoneOptions;
 
       $input = $(config.input);
+      $fieldsContainer = $input.closest(".nested-fields");
+      $progressBar = $fieldsContainer.find("progress");
       $container = $input.closest(config.attachmentContainer);
       $zone = $("<div>", { class: "hidden-dropzone-upload" });
       $container.append($zone);
-
-      uploadData = {};
 
       dropzoneOptions = {
         url: $input.data("url"),
@@ -24,91 +24,44 @@
       dropzone = new Dropzone($zone[0], dropzoneOptions);
 
       $input.on("change", function() {
-        uploadData = App.Attachable.buildData(config.input);
-        App.Attachable.setFilename(uploadData, this.files[0].name);
+        var uploadUrl, title;
+
+        uploadUrl = new URL($input.data("url"), window.location.href);
+        title = $fieldsContainer.find("input[name$='[title]']").val() || "";
+        uploadUrl.searchParams.set("direct_upload[title]", title);
+
+        dropzone.options.url = uploadUrl;
         dropzone.addFile(this.files[0]);
       });
 
       dropzone.on("addedfile", function() {
-        uploadData = App.Attachable.buildData(config.input);
-        App.Attachable.setProgressBar(uploadData, "uploading");
+        $progressBar.addClass("uploading");
       });
 
       dropzone.on("uploadprogress", function(_file, progress) {
-        uploadData.progressBar.val(progress);
+        $progressBar.val(progress);
       });
 
       dropzone.on("success", function(_file, response) {
-        var destroyAttachmentLink;
-
-        $(uploadData.cachedAttachmentField).val(response.cached_attachment);
-        App.Attachable.setTitleFromFile(uploadData, response.filename);
-        App.Attachable.setProgressBar(uploadData, "complete");
-        App.Attachable.setFilename(uploadData, response.filename);
-        App.Attachable.clearInputErrors(uploadData);
-        destroyAttachmentLink = $(response.destroy_link);
-        $(uploadData.destroyAttachmentLinkContainer).html(destroyAttachmentLink);
+        App.Attachable.setNewContent($fieldsContainer, response);
 
         if (config.onSuccess) {
-          config.onSuccess(uploadData, response);
+          config.onSuccess($fieldsContainer.find("[type=file]"));
         }
       });
 
-      dropzone.on("error", function(file, message) {
-        $(uploadData.cachedAttachmentField).val("");
-        App.Attachable.clearFilename(uploadData);
-        App.Attachable.setProgressBar(uploadData, "errors");
-        App.Attachable.clearInputErrors(uploadData);
-        App.Attachable.setInputErrors(uploadData, message.errors);
-        $(uploadData.destroyAttachmentLinkContainer).find("a.delete:not(.remove-nested)").remove();
-        $(uploadData.addAttachmentLabel).addClass("error");
+      dropzone.on("error", function(file, response) {
+        App.Attachable.setNewContent($fieldsContainer, response);
 
         if (config.onError) {
-          config.onError(uploadData);
+          config.onError($fieldsContainer.find("[type=file]"));
         }
 
         dropzone.removeFile(file);
       });
     },
-    buildData: function(input) {
-      var data, wrapper;
-
-      data = [];
-      wrapper = $(input).closest(".direct-upload");
-
-      data.wrapper = wrapper;
-      data.progressBar = $(wrapper).find("progress");
-      data.errorContainer = $(wrapper).find(".action-add");
-      data.fileNameContainer = $(wrapper).find("p.file-name");
-      data.destroyAttachmentLinkContainer = $(wrapper).find(".action-remove");
-      data.addAttachmentLabel = $(wrapper).find(".action-add label");
-      data.cachedAttachmentField = $(wrapper).find("input[name$='[cached_attachment]']");
-      data.titleField = $(wrapper).find("input[name$='[title]']");
-
-      return data;
-    },
-    clearFilename: function(data) {
-      $(data.fileNameContainer).text("");
-    },
-    clearInputErrors: function(data) {
-      $(data.errorContainer).find("small.error").remove();
-    },
-    setFilename: function(data, file_name) {
-      $(data.fileNameContainer).text(file_name);
-    },
-    setProgressBar: function(data, klass) {
-      data.progressBar.addClass(klass);
-    },
-    setTitleFromFile: function(data, title) {
-      if ($(data.titleField).val() === "") {
-        $(data.titleField).val(title);
-      }
-    },
-    setInputErrors: function(data, errors) {
-      if (!errors) {
-        return;
-      }
-      $(data.errorContainer).append("<small class='error'>" + errors + "</small>");
+    setNewContent: function(fields_container, response) {
+      fields_container.html($(response.content).html()).find("progress").val(100);
     }
   };
 }).call(this);

app/assets/javascripts/documentable.js
@@ -29,7 +29,8 @@
           if (input.lockUpload) {
             App.Documentable.showNotice();
           }
-        }
+        },
+        onError: App.Documentable.initializeDirectUploadInput
       });
     },
     lockUploads: function() {

app/assets/javascripts/imageable.js
@@ -25,31 +25,8 @@
       App.Attachable.setupInput({
         input: input,
         attachmentContainer: ".image-attachment",
-        onSuccess: function(uploadData, response) {
-          uploadData.result = response;
-          App.Imageable.setPreview(uploadData);
-        },
-        onError: function(uploadData) {
-          App.Imageable.clearPreview(uploadData);
-        }
+        onError: App.Imageable.initializeDirectUploadInput
       });
-    },
-    clearPreview: function(data) {
-      $(data.wrapper).find(".image-preview").remove();
-    },
-
-    setPreview: function(data) {
-      var preview, image_preview;
-
-      image_preview = "<div class='small-12 column text-center image-preview'>" +
-        "<figure><img src='" + data.result.attachment_url + "' class='cached-image'></figure></div>";
-      preview = data.wrapper.find(".image-preview");
-
-      if ($(preview).length > 0) {
-        $(preview).replaceWith(image_preview);
-      } else {
-        $(image_preview).insertBefore($(data.wrapper).find(".attachment-actions"));
-      }
     },
     initializeRemoveCachedImageLinks: function() {
       $("#nested-image").on("click", "a.remove-cached-attachment", function(event) {
@@ -84,8 +61,6 @@
         }
 
         dataString = App.Imageable.imageSuggestionsParams(form, resourceType);
-        var uploadData = App.Attachable.buildData(button.closest(".image-fields.direct-upload"));
-        App.Attachable.clearInputErrors(uploadData);
         $.ajax({
           url: "/image_suggestions",
           type: "POST",
@@ -101,36 +76,25 @@
         });
       });
     },
-    attachSuggestedImageSuccess: function(responseData) {
-      var data = App.Attachable.buildData(this);
-      data.result = {
-        cached_attachment: responseData.cached_attachment,
-        filename: responseData.filename,
-        attachment_url: responseData.attachment_url,
-        destroy_link: responseData.destroy_link
-      };
-      $(data.cachedAttachmentField).val(data.result.cached_attachment);
-      App.Attachable.setTitleFromFile(data, data.result.filename);
-      App.Attachable.setFilename(data, data.result.filename);
-      App.Imageable.setPreview(data);
-      $(data.destroyAttachmentLinkContainer).html(data.result.destroy_link);
+    attachSuggestedImage: function($fieldsContainer, response) {
+      App.Attachable.setNewContent($fieldsContainer, response);
       $("#new_image_link").addClass("hide");
-      App.Attachable.clearInputErrors(data);
-    },
-    attachSuggestedImageError: function(xhr) {
-      var data = App.Attachable.buildData(this);
-      App.Attachable.clearInputErrors(data);
-      App.Attachable.setInputErrors(data, xhr.responseJSON && xhr.responseJSON.errors);
+      App.Imageable.initializeDirectUploadInput($fieldsContainer.find("[type=file]"));
     },
     initializeAttachSuggestedImage: function() {
       $("body").on("click", ".suggested-image-button", function() {
-        var imageId, resourceType, resourceId, dataString, wrapper;
+        var imageId, resourceType, resourceId, dataString, $fieldsContainer, wrapper;
         imageId = $(this).data("image-id");
         wrapper = $(this).closest(".suggested-images-wrapper");
         resourceType = wrapper.data("resource-type");
         resourceId = wrapper.data("resource-id");
+        $fieldsContainer = $(this).closest(".nested-fields");
 
-        dataString = { resource_type: resourceType };
+        dataString = {
+          resource_type: resourceType,
+          title: $fieldsContainer.find("input[name$='[title]']").val()
+        };
+
         if (resourceId) {
           dataString.resource_id = resourceId;
         }
@@ -139,9 +103,16 @@
           type: "POST",
           data: dataString,
           dataType: "json",
-          context: $(this).closest(".image-fields.direct-upload"),
-          success: App.Imageable.attachSuggestedImageSuccess,
-          error: App.Imageable.attachSuggestedImageError
+          success: function(response) {
+            App.Imageable.attachSuggestedImage($fieldsContainer, response);
+          },
+          error: function(xhr) {
+            var response = xhr.responseJSON;
+
+            if (response && response.content) {
+              App.Imageable.attachSuggestedImage($fieldsContainer, response);
+            }
+          }
         });
       });
     },

app/assets/stylesheets/layout.scss
@@ -148,7 +148,8 @@ button,
   }
 }
 
-.button.hollow.error {
+.button.hollow.error,
+.button.hollow.is-invalid-label {
   border-color: $alert-border;
   color: $color-alert;
 }

app/assets/stylesheets/mixins/uploads.scss
@@ -12,6 +12,9 @@
 }
 
 @mixin direct-uploads {
+  .image-preview figure:has(.cached-image) {
+    height: rem-calc(150);
+  }
 
   .cached-image {
     max-height: rem-calc(150);
@@ -111,10 +114,6 @@
       display: none;
     }
 
-    &.uploading {
-      @include progress-bar-color($dark-gray);
-    }
-
     &.complete {
       @include progress-bar-color($success-color);
     }
@@ -122,6 +121,10 @@
     &.errors {
       @include progress-bar-color($alert-color);
       margin-top: calc($line-height / 2);
+    }
+
+    &.uploading {
+      @include progress-bar-color($dark-gray);
     }
   }
 }

app/components/attachable/fields_component.rb
@@ -24,11 +24,13 @@ class Attachable::FieldsComponent < ApplicationComponent
     end
 
     def valid_image?
-      attachable.attachment.attached? && attachable.attachment.image?
+      attachable.attachment.attached? &&
+        attachable.attachment.image? &&
+        attachable.errors[:attachment].empty?
     end
 
     def file_name
-      attachable.attachment_file_name
+      attachable.attachment_file_name if attachable.errors.empty?
     end
 
     def destroy_link
@@ -72,6 +74,16 @@ class Attachable::FieldsComponent < ApplicationComponent
     end
 
     def progress_bar
-      tag.progress max: "100", "aria-label": t("documents.form.progress")
+      tag.progress max: "100",
+                   class: progress_bar_status_class,
+                   "aria-label": t("documents.form.progress")
+    end
+
+    def progress_bar_status_class
+      if attachable.errors.any?
+        "errors"
+      else
+        "complete"
+      end
     end
 end

app/components/direct_uploads/fields_component.html.erb
@@ -1,0 +1,1 @@
+<%= fields_content %>

app/components/direct_uploads/fields_component.rb
@@ -1,0 +1,52 @@
+class DirectUploads::FieldsComponent < ApplicationComponent
+  attr_reader :direct_upload
+
+  def initialize(direct_upload)
+    @direct_upload = direct_upload
+  end
+
+  private
+
+    def fields_content
+      form_for resource, url: false do |form|
+        case attachable.class.name
+        when "Document"
+          return document_fields(form)
+        when "Image"
+          return image_fields(form)
+        end
+      end
+    end
+
+    def document_fields(form)
+      form.fields_for :documents, attachable, child_index: nested_index do |documents_builder|
+        render Documents::FieldsComponent.new(documents_builder)
+      end
+    end
+
+    def image_fields(form)
+      form.fields_for image_fields_name, attachable, child_index: nested_index do |image_builder|
+        render Images::FieldsComponent.new(image_builder, imageable: resource)
+      end
+    end
+
+    def nested_index
+      SecureRandom.random_number(10**12)
+    end
+
+    def image_fields_name
+      if resource.class.reflect_on_association(:image)
+        :image
+      else
+        :images
+      end
+    end
+
+    def resource
+      direct_upload.resource
+    end
+
+    def attachable
+      direct_upload.relation
+    end
+end

app/components/images/image_component.html.erb
@@ -2,11 +2,11 @@
   <figure>
     <%= image_tag image.variant(version),
                   class: image_class,
-                  alt: image.title.unicode_normalize,
-                  title: image.title.unicode_normalize %>
+                  alt: image.title&.unicode_normalize,
+                  title: image.title&.unicode_normalize %>
     <% if show_caption %>
       <figcaption class="text-right">
-        <em><%= image.title.unicode_normalize %></em>
+        <em><%= image.title&.unicode_normalize %></em>
       </figcaption>
     <% end %>
   </figure>

app/controllers/direct_uploads_controller.rb
@@ -1,12 +1,7 @@
 class DirectUploadsController < ApplicationController
-  include DirectUploadsHelper
-  include ActionView::Helpers::UrlHelper
-
   before_action :authenticate_user!
 
   skip_authorization_check only: :create
-
-  helper_method :render_destroy_upload_link
 
   def create
     @direct_upload = DirectUpload.new(
@@ -14,13 +9,9 @@ class DirectUploadsController < ApplicationController
     )
 
     if @direct_upload.save
-      render json: { cached_attachment: @direct_upload.relation.cached_attachment,
-                     filename: @direct_upload.relation.attachment_file_name,
-                     destroy_link: render_destroy_upload_link(@direct_upload),
-                     attachment_url: polymorphic_path(@direct_upload.relation.attachment) }
+      render
     else
-      render json: { errors: @direct_upload.errors[:attachment].join(", ") },
-             status: :unprocessable_content
+      render status: :unprocessable_content
     end
   end
 
@@ -34,7 +25,7 @@ class DirectUploadsController < ApplicationController
     def allowed_params
       [
         :resource, :resource_type, :resource_id, :resource_relation,
-        :attachment, :cached_attachment, attachment_attributes: []
+        :attachment, :cached_attachment, :title, attachment_attributes: []
       ]
     end
 end

app/controllers/image_suggestions_controller.rb
@@ -1,7 +1,4 @@
 class ImageSuggestionsController < ApplicationController
-  include DirectUploadsHelper
-  include ActionView::Helpers::UrlHelper
-
   before_action :authenticate_user!
   skip_authorization_check
 
@@ -34,17 +31,14 @@ class ImageSuggestionsController < ApplicationController
       resource_id: params[:resource_id],
       resource_relation: "image",
       attachment: attachment,
+      title: params[:title],
       user: current_user
     )
 
     if @direct_upload.save
-      render json: { cached_attachment: @direct_upload.relation.cached_attachment,
-                     filename: @direct_upload.relation.attachment_file_name,
-                     destroy_link: render_destroy_upload_link(@direct_upload),
-                     attachment_url: polymorphic_path(@direct_upload.relation.attachment) }
+      render "direct_uploads/create"
     else
-      render json: { errors: @direct_upload.errors[:attachment].join(", ") },
-             status: :unprocessable_content
+      render "direct_uploads/create", status: :unprocessable_content
     end
   end
 

app/helpers/direct_uploads_helper.rb
@@ -1,6 +1,0 @@
-module DirectUploadsHelper
-  def render_destroy_upload_link(direct_upload)
-    label = direct_upload.resource_relation == "image" ? "images" : "documents"
-    link_to t("#{label}.form.delete_button"), "#", class: "delete remove-cached-attachment"
-  end
-end

app/models/direct_upload.rb
@@ -5,7 +5,7 @@ class DirectUpload
 
   attr_accessor :resource, :resource_type, :resource_id,
                 :relation, :resource_relation,
-                :attachment, :cached_attachment, :user
+                :attachment, :cached_attachment, :user, :title
 
   validates :attachment, :resource_type, :resource_relation, :user, presence: true
   validate :parent_resource_attachment_validations,
@@ -42,6 +42,8 @@ class DirectUpload
   end
 
   def save
+    @relation.title = @relation.title.presence || @relation.attachment_file_name
+
     if valid?
       save_attachment
       relation.set_cached_attachment_from_attachment
@@ -67,7 +69,8 @@ class DirectUpload
       {
         attachment: @attachment,
         cached_attachment: @cached_attachment,
-        user: @user
+        user: @user,
+        title: @title
       }
     end
 end

app/views/direct_uploads/create.json.erb
@@ -1,0 +1,3 @@
+{
+  "content": "<%= j render DirectUploads::FieldsComponent.new(@direct_upload) %>"
+}

spec/controllers/direct_uploads_controller_spec.rb
@@ -1,0 +1,36 @@
+require "rails_helper"
+
+describe DirectUploadsController do
+  render_views
+  before { sign_in create(:user) }
+
+  describe "POST create" do
+    let(:upload_params) do
+      {
+        resource_type: "Proposal",
+        resource_relation: "image",
+        title: "My Title"
+      }
+    end
+
+    it "preserves a title provided in the request" do
+      post :create, params: {
+        direct_upload: upload_params,
+        attachment: fixture_file_upload("clippy.jpg")
+      }, format: :json
+
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body["content"]).to include('value="My Title"')
+    end
+
+    it "uses the filename as title when none is provided" do
+      post :create, params: {
+        direct_upload: upload_params.except(:title),
+        attachment: fixture_file_upload("clippy.jpg")
+      }, format: :json
+
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body["content"]).to include('value="clippy.jpg"')
+    end
+  end
+end

spec/controllers/image_suggestions_controller_spec.rb
@@ -90,6 +90,8 @@ describe ImageSuggestionsController do
   end
 
   describe "POST attach" do
+    render_views
+
     let(:resource_type) { "Budget::Investment" }
     let(:photo_id) { "12345" }
     let(:uploaded_file) { fixture_file_upload("clippy.jpg") }
@@ -99,12 +101,13 @@ describe ImageSuggestionsController do
     end
 
     context "when download succeeds and DirectUpload is valid" do
-      it "returns success and JSON with attachment data" do
-        post :attach, params: { id: photo_id, resource_type: resource_type }
+      it "returns success and JSON with rendered fields" do
+        post :attach, params: { id: photo_id, resource_type: resource_type }, format: :json
 
         expect(response).to have_http_status(:ok)
         json = response.parsed_body
-        expect(json).to include("cached_attachment", "filename", "destroy_link", "attachment_url")
+        expect(json["content"]).to be_present
+        expect(json["content"]).to include("cached_attachment")
       end
     end
 
@@ -142,11 +145,11 @@ describe ImageSuggestionsController do
                                                              .and_return(fixture_file_upload("empty.pdf"))
       end
 
-      it "returns 422 with validation errors in JSON" do
-        post :attach, params: { id: photo_id, resource_type: resource_type }
+      it "returns 422 with rendered fields" do
+        post :attach, params: { id: photo_id, resource_type: resource_type }, format: :json
 
         expect(response).to have_http_status(:unprocessable_content)
-        expect(response.parsed_body["errors"]).to be_present
+        expect(response.parsed_body["content"]).to be_present
       end
     end
   end

spec/system/nested_documentable_spec.rb
@@ -114,6 +114,12 @@ describe "Nested documentable" do
 
       cached_attachment_field = find("input[name$='[cached_attachment]']", visible: :hidden)
       expect(cached_attachment_field.value).to be_empty
+
+      click_button submit_button_text
+
+      within "#nested-documents .document-attachment" do
+        expect(page).to have_content "can't be blank"
+      end
     end
 
     scenario "Should show document errors after documentable submit with empty document fields" do

spec/system/nested_imageable_spec.rb
@@ -65,6 +65,12 @@ describe "Nested imageable" do
       cached_attachment_field = find("input[name$='[cached_attachment]']", visible: :hidden)
 
       expect(cached_attachment_field.value).to be_empty
+
+      click_button submit_button_text
+
+      within "#nested-image .image-attachment" do
+        expect(page).to have_content "can't be blank"
+      end
     end
 
     scenario "Shows image errors after invalid submit and restores add link on cancel" do