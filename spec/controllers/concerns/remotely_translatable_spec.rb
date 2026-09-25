e4c082d712ca758d9b6122591bb0908d836b28b3
Javi Martín <javim@elretirao.net>
2026-09-25 21:14:13 +0200

    Make remote translatable test descriptions more readable
    These were legacy tests where we included the context in the `it`
    statement and the expected result in the `describe` or `context` block.
    The usual approach is the opposite: context in the `context` statement
    and expected result in the `it` statement.
    
    Here we're simplifying the tests by adding both expected result and
    context to the `it` statements.

--- spec/controllers/concerns/remotely_translatable_spec.rb
+++ spec/controllers/concerns/remotely_translatable_spec.rb
@@ -7,48 +7,46 @@ describe RemotelyTranslatable do
   end
 
   describe "#detect_remote_translations" do
-    describe "Should detect remote_translations" do
-      it "When collections and featured_proposals are not defined in current locale" do
-        proposals = create_list(:proposal, 3)
-        featured_proposals = create_featured_proposals
+    it "detects remote translations when collections and featured_proposals are not translated" do
+      proposals = create_list(:proposal, 3)
+      featured_proposals = create_featured_proposals
 
-        I18n.with_locale(:es) do
-          expect(detect_remote_translations(proposals, featured_proposals).count).to eq 6
-        end
-      end
-
-      it "When we have nil as argument and collections are not defined in current locale" do
-        proposals = create_list(:proposal, 3)
-
-        I18n.with_locale(:es) do
-          expect(detect_remote_translations(proposals, nil).count).to eq 3
-        end
-      end
-
-      it "When we have [] as argument and collections are not defined in current locale" do
-        proposals = create_list(:proposal, 3)
-
-        I18n.with_locale(:es) do
-          expect(detect_remote_translations(proposals, []).count).to eq 3
-        end
-      end
-
-      it "When widget feeds are not defined in current locale" do
-        create_list(:proposal, 3)
-        create_list(:debate, 3)
-        create_list(:legislation_process, 3)
-        create(:widget_feed, kind: "proposals")
-        create(:widget_feed, kind: "debates")
-        create(:widget_feed, kind: "processes")
-        widget_feeds = Widget::Feed.active
-
-        I18n.with_locale(:es) do
-          expect(detect_remote_translations(widget_feeds).count).to eq 9
-        end
+      I18n.with_locale(:es) do
+        expect(detect_remote_translations(proposals, featured_proposals).count).to eq 6
       end
     end
 
-    it "When remote translations are not configured should not detect remote_translations" do
+    it "detects remote translations with nil as argument when collections are not translated" do
+      proposals = create_list(:proposal, 3)
+
+      I18n.with_locale(:es) do
+        expect(detect_remote_translations(proposals, nil).count).to eq 3
+      end
+    end
+
+    it "detects remote translations with [] as argument when collections are not translated" do
+      proposals = create_list(:proposal, 3)
+
+      I18n.with_locale(:es) do
+        expect(detect_remote_translations(proposals, []).count).to eq 3
+      end
+    end
+
+    it "detects remote translations when widget feeds are not translated" do
+      create_list(:proposal, 3)
+      create_list(:debate, 3)
+      create_list(:legislation_process, 3)
+      create(:widget_feed, kind: "proposals")
+      create(:widget_feed, kind: "debates")
+      create(:widget_feed, kind: "processes")
+      widget_feeds = Widget::Feed.active
+
+      I18n.with_locale(:es) do
+        expect(detect_remote_translations(widget_feeds).count).to eq 9
+      end
+    end
+
+    it "does not detect remote translations when they aren't configured" do
       allow(RemoteTranslations::Caller).to receive(:configured?).and_return(false)
       proposal = create(:proposal)
       comment = create(:comment, commentable: proposal)
@@ -58,14 +56,14 @@ describe RemotelyTranslatable do
       end
     end
 
-    it "When defined in current locale should not detect remote_translations" do
+    it "does not detect remote translations when collections are already translated" do
       proposal = create(:proposal)
       comment = create(:comment, commentable: proposal)
 
       expect(detect_remote_translations([proposal, comment])).to eq []
     end
 
-    it "When resource class is not translatable should not detect remote_translations" do
+    it "does not detect remote translations when the resource class is not translatable" do
       legislation_proposal = create(:legislation_proposal)
 
       I18n.with_locale(:es) do
