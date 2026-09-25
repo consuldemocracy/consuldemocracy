8ebf16de0961add3c57de3d21d46b918ba3f4f58
Javi Martín <javim@elretirao.net>
2026-09-25 23:35:08 +0200

    Simplify create action in management proposals controller
    We were referencing polymorphic methods and variables. This made the
    code harder to read.

--- app/controllers/management/proposals_controller.rb
+++ app/controllers/management/proposals_controller.rb
@@ -12,14 +12,12 @@ class Management::ProposalsController < Management::BaseController
   has_orders %w[most_voted newest], only: :show
 
   def create
-    @resource = resource_model.new(proposal_params.merge(author: current_user,
-                                                         published_at: Time.current))
+    @proposal = Proposal.new(proposal_params.merge(author: current_user, published_at: Time.current))
 
-    if @resource.save
-      redirect_path = url_for(controller: controller_name, action: :show, id: @resource.id)
+    if @proposal.save
+      redirect_path = url_for(controller: controller_name, action: :show, id: @proposal.id)
       redirect_to redirect_path, notice: t("flash.actions.create.#{resource_name.underscore}")
     else
-      set_resource_instance
       render :new
     end
   end
